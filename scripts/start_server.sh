30bd6cd79c9f3db4fc018adbb4f58fbc75ddfb6f
SumanthPuram <sumanth.iitm@gmail.com>
2019-07-22 13:23:28 +0530

    Codepipeline

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-cd /home/ubuntu/transformer
-npm install
-pm2 stop all
-pm2 start npm -- start 
+cd /home/ubuntu
+chown -R ubuntu:ubuntu /home/ubuntu/transformer
+sudo -u ubuntu npm install
+sudo -u ubuntu pm2 stop all
+sudo -u ubuntu pm2 start npm -- start 

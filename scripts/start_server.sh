2d1f21c7fc7da2b9d11d25dc4a1fb837f25c84b7
Sumanth Puram <sumanth@rudderlabs.com>
2019-07-23 17:50:33 +0530

    Changing pm2 start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -5,4 +5,4 @@ chown -R ubuntu:ubuntu /home/ubuntu/transformer
 cd /home/ubuntu/transformer
 sudo -u ubuntu npm install
 sudo -u ubuntu pm2 stop all
-sudo -u ubuntu pm2 start npm -- start 
+sudo -u ubuntu pm2 start GATransform.js

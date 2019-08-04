bb39818094c79a24abc8fd1fedad7dac761ad479
Soumyadeb Mitra <soumyadeb@rudderlabs.com>
2019-08-04 06:17:27 +0000

    Increased payload threshold in echo

--- echoServer.js
+++ echoServer.js
@@ -38,7 +38,7 @@ function start(port){
 
                     // Too much POST data, kill the connection!
                     // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-                    if (body.length > 1e6)
+                    if (body.length > 1e8)
                         request.connection.destroy();
                 });
 

4676a1921214a96553a91e2a2d9f91f151665089
Soumyadeb Mitra <soumyadeb@rudderlabs.com>
2019-08-04 06:18:03 +0000

    Increased payload in server

--- transformerServer.js
+++ transformerServer.js
@@ -34,7 +34,7 @@ function start(port, route) {
 
                     // Too much POST data, kill the connection!
                     // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-                    if (body.length > 1e6)
+                    if (body.length > 1e8)
                         request.connection.destroy();
                 });
 

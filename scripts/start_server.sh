344e88e9611c4a3fd7def561b41d2b48424f2ea5
Sumanth Puram <sumanth.iitm@gmail.com>
2019-08-06 15:22:26 +0530

    Fixing CodeDeploy

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Stop all servers and start the server as a daemon
-cd /home/ubuntu
-chown -R ubuntu:ubuntu /home/ubuntu/transformer
+chown -R ubuntu:ubuntu /home/ubuntu/rudder-transformer
+
+cd /home/ubuntu/rudder-transformer
 sudo -u ubuntu npm install
 sudo -u ubuntu pm2 stop all
 sudo -u ubuntu pm2 start echoServer.js

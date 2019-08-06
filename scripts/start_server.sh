928b5a2ba21906b9efc5e1de075f8b683749ca9f
Sumanth Puram <sumanth.iitm@gmail.com>
2019-08-06 15:35:50 +0530

    Changing from pm2 to systemd script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Stop all servers and start the server as a daemon
 chown -R ubuntu:ubuntu /home/ubuntu/rudder-transformer
-
-cd /home/ubuntu/rudder-transformer
-sudo -u ubuntu npm install
-sudo -u ubuntu pm2 stop all
-sudo -u ubuntu pm2 start echoServer.js
-sudo -u ubuntu pm2 start transformerIndex.js
+systemctl enable user-transformer.service
+systemctl enable dest-transformer.service
+systemctl restart user-transformer.service
+systemctl restart dest-transformer.service

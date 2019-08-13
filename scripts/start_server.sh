54d0b754ce4503d2863615a8076c2d637157804d
Sumanth Puram <sumanth.iitm@gmail.com>
2019-08-13 15:15:12 +0530

    Changes to enable AWS cloudwatch logs

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,6 +3,7 @@
 chown -R ubuntu:ubuntu /home/ubuntu/rudder-transformer
 cd /home/ubuntu/rudder-transformer
 npm install
+systemctl daemon-reload
 systemctl enable user-transformer.service
 systemctl enable dest-transformer.service
 systemctl restart user-transformer.service

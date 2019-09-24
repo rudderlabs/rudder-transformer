0d07422d41638a3270b3dcd85c12e64e25ae2011
Srikanth Paladugu <srikanth.iit@gmail.com>
2019-09-24 17:10:12 +0530

    Add Dockerfile-et

--- userTransformer.js
+++ userTransformer.js
@@ -44,8 +44,12 @@ function start(port){
 
                     // Too much POST data, kill the connection!
                     // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-                    if (body.length > 1e6)
-                        request.connection.destroy();
+                    // if (body.length > 1e6)
+                    //     request.connection.destroy();
+                    if (body.length > 1e8) {
+                      response.statusCode = 413;
+                      response.end();
+                    }
                 });
 
                 request.on('end', async function () {

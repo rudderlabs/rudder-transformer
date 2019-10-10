45762e269c73c1cbcdf22382ca940df26b717f96
Sumanth Puram <sumanth.iitm@gmail.com>
2019-10-10 17:14:17 +0530

    Removing user transforms from the service files

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,7 +4,5 @@ chown -R ubuntu:ubuntu /home/ubuntu/rudder-transformer
 cd /home/ubuntu/rudder-transformer
 npm install
 systemctl daemon-reload
-systemctl enable user-transformer.service
 systemctl enable dest-transformer.service
-systemctl restart user-transformer.service
 systemctl restart dest-transformer.service

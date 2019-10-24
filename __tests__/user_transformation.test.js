5260ad61859a2e0d599a22e12f21263e4b00c05f
lokey <lokeshiitm08@gmail.com>
2019-10-24 14:54:48 +0530

    Add tests for user transformations.

---
+++ __tests__/user_transformation.test.js
@@ -0,0 +1,76 @@
+jest.mock("node-fetch");
+const fetch = require("node-fetch", () => jest.fn());
+const { Response } = jest.requireActual("node-fetch");
+
+const integration = "user_transformation";
+const name = "User Transformations";
+
+const fs = require("fs");
+const path = require("path");
+describe("User transformation", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
+  it(`Simple ${name} Test`, async () => {
+    const inputDataFile = fs.readFileSync(
+      path.resolve(__dirname, `./data/${integration}_input.json`)
+    );
+    const outputDataFile = fs.readFileSync(
+      path.resolve(__dirname, `./data/${integration}_output.json`)
+    );
+    const { userTransformHandler } = require("../util/customTransformer");
+    const respBody = {
+      code: `function transform(events) {
+            return JSON.stringify(events);
+          }
+          `
+    };
+    fetch.mockReturnValueOnce(
+      Promise.resolve(new Response(JSON.stringify(respBody)))
+    );
+
+    const inputData = JSON.parse(inputDataFile);
+    const expectedData = JSON.parse(outputDataFile);
+    const output = await userTransformHandler(inputData);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      "https://api.rudderlabs.com/transformation/getByVersionId?versionId=23"
+    );
+
+    expect(output).toEqual(expectedData);
+  });
+
+  it(`Filtering ${name} Test`, async () => {
+    const inputDataFile = fs.readFileSync(
+      path.resolve(__dirname, `./data/${integration}_filter_input.json`)
+    );
+    const outputDataFile = fs.readFileSync(
+      path.resolve(__dirname, `./data/${integration}_filter_output.json`)
+    );
+    const { userTransformHandler } = require("../util/customTransformer");
+    const respBody = {
+      code: `function transform(events) {
+                        const filteredEvents = events.filter(event => {
+                          const eventType = event.message.type;
+                          return eventType && eventType.match(/track/g);
+                        });
+                        return JSON.stringify(filteredEvents);
+                      }
+                      `
+    };
+    fetch.mockReturnValue(
+      Promise.resolve(new Response(JSON.stringify(respBody)))
+    );
+    const inputData = JSON.parse(inputDataFile);
+    const expectedData = JSON.parse(outputDataFile);
+    const output = await userTransformHandler(inputData);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      "https://api.rudderlabs.com/transformation/getByVersionId?versionId=24"
+    );
+
+    expect(output).toEqual(expectedData);
+  });
+});

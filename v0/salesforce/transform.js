7edfa3e1f5bc6990bdd6250edc5bd21cf52c9df6
Dipanjan Biswas <dipanjan@rudderlabs.com>
2019-11-18 16:33:26 +0530

    addressed PR comments

--- v0/salesforce/transform.js
+++ v0/salesforce/transform.js
@@ -5,7 +5,7 @@ const {
   ConfigCategory,
   SF_API_VERSION,
   SF_TOKEN_REQUEST_URL,
-  mappingConfig,
+  mappingConfig
 } = require("./config");
 const {
   removeUndefinedValues,
@@ -15,20 +15,45 @@ const {
 
 var authorizationHeader;
 
-//Utility method to construct the header to be used for SFDC API calls
-//The "Authorization: Bearer <token>" header element needs to be passed for 
-//authentication for all SFDC REST API calls
-async function getSFDCHeader(destination){
-   
-  const response = await axios.post(SF_TOKEN_REQUEST_URL
-    +"?username="+destination.Config.userName
-    +"&password="+destination.Config.password+destination.Config.initialAccessToken
-    +"&client_id="+destination.Config.consumerKey
-    +"&client_secret="+destination.Config.consumerSecret
-    +"&grant_type=password",{});
-    
-    return "Bearer " + response.data.access_token;
+// Utility method to construct the header to be used for SFDC API calls
+// The "Authorization: Bearer <token>" header element needs to be passed for
+// authentication for all SFDC REST API calls
+async function getSFDCHeader(destination) {
+  const response = await axios.post(
+    SF_TOKEN_REQUEST_URL +
+      "?username=" +
+      destination.Config.userName +
+      "&password=" +
+      destination.Config.password +
+      destination.Config.initialAccessToken +
+      "&client_id=" +
+      destination.Config.consumerKey +
+      "&client_secret=" +
+      destination.Config.consumerSecret +
+      "&grant_type=password",
+    {}
+  );
+
+  return "Bearer " + response.data.access_token;
 }
+
+function getParamsFromConfig(message, destination) {
+  const params = {};
+
+  var obj = {};
+  // customMapping: [{from:<>, to: <>}] , structure of custom mapping
+  if (destination.Config.customMappings) {
+    destination.Config.customMappings.forEach(mapping => {
+      obj[mapping.from] = mapping.to;
+    });
+  }
+  const keys = Object.keys(obj);
+  keys.forEach(key => {
+    params[obj[key]] = get(message.properties, key);
+  });
+  return params;
+}
+
 
 // Basic response builder
 // We pass the parameterMap with any processing-specific key-value prepopulated
@@ -40,32 +65,30 @@ async function responseBuilderSimple(
   destination,
   targetEndpoint
 ) {
-
-  //Need to get the authorization header element
-  //Need this to be async 
-   if (!authorizationHeader) {
-     authorizationHeader = await getSFDCHeader(destination);
-   }
- 
-  const rawPayload =  {};
-
-  //First name and last name need to be extracted from the name field
-  //and inserted into the message
-  
-  var firstName = "";
-  var lastName = "";
-  if(message.context.traits.name){
-    //Split by space and then take first and last elements
-    var nameComponents = message.context.traits.name.split(" ");
-    firstName = nameComponents[0]; //first element
-    lastName = nameComponents[nameComponents.length - 1]; //last element
-    
+  // Need to get the authorization header element
+  // Need this to be async
+  if (!authorizationHeader) {
+    authorizationHeader = await getSFDCHeader(destination);
   }
 
-  //Insert first and last names separately into message
+  const rawPayload = {};
+
+  // First name and last name need to be extracted from the name field
+  // and inserted into the message
+
+  var firstName = "";
+  var lastName = "";
+  if (message.context.traits.name) {
+    // Split by space and then take first and last elements
+    var nameComponents = message.context.traits.name.split(" ");
+    firstName = nameComponents[0]; // first element
+    lastName = nameComponents[nameComponents.length - 1]; // last element
+  }
+
+  // Insert first and last names separately into message
   message.context.traits.firstName = firstName;
   message.context.traits.lastName = lastName;
-  
+
   const sourceKeys = Object.keys(mappingJson);
   sourceKeys.forEach(sourceKey => {
     rawPayload[mappingJson[sourceKey]] = get(message, sourceKey);
@@ -73,87 +96,69 @@ async function responseBuilderSimple(
   // Remove keys with undefined values
   const payload = removeUndefinedValues(rawPayload);
 
-  //Get custom params from destination config
+  // Get custom params from destination config
   let customParams = getParamsFromConfig(message, destination);
   customParams = removeUndefinedValues(customParams);
-  //request configuration will be conditional
-  //POST for create, PATCH for update
+  // request configuration will be conditional
+  // POST for create, PATCH for update
 
   const response = {
     endpoint: targetEndpoint,
     requestConfig: defaultPostRequestConfig,
     header: {
-      "Content-Type" : "application/json",
-      "Authorization" : authorizationHeader
+      "Content-Type": "application/json",
+      Authorization: authorizationHeader
     },
     userId: message.anonymousId,
     payload: { ...customParams, ...payload }
   };
   return response;
-};
-
-function getParamsFromConfig(message, destination) {
-  let params = {};
-
-  var obj = {};
-  // customMapping: [{from:<>, to: <>}] , structure of custom mapping
-  if (destination.Config.customMappings) {
-    destination.Config.customMappings.forEach(mapping => {
-      obj[mapping.from] = mapping.to;
-    });
-  }
-  let keys = Object.keys(obj);
-  keys.forEach(key => {
-    params[obj[key]] = get(message.properties, key);
-  });
-  return params;
 }
 
 
+// Function for handling identify events
+async function processIdentify(message, destination) {
+  // start with creation endpoint, update only if Lead does not exist
+  var targetEndpoint =
+    "https://" +
+    destination.Config.instanceName +
+    ".salesforce.com" +
+    "/services/data/v" +
+    SF_API_VERSION +
+    "/sobjects/Lead";
 
-
-//Function for handling identify events
-async function processIdentify(message,destination) {
-
-  //start with creation endpoint, update only if Lead does not exist
-  var targetEndpoint = "https://" 
-                      + destination.Config.instanceName 
-                      + ".salesforce.com"
-                      + "/services/data/v"
-                      + SF_API_VERSION
-                      + "/sobjects/Lead";
-
-  //Get the authorization header if not available
+  // Get the authorization header if not available
   if (!authorizationHeader) {
     authorizationHeader = await getSFDCHeader(destination);
   }
-  
-  //check if the lead exists
-  //need to perform a parameterized search for this using email
+
+  // check if the lead exists
+  // need to perform a parameterized search for this using email
   var email = message.context.traits.email;
-  
-  var leadQueryUrl = "https://" 
-                      + destination.Config.instanceName 
-                      + ".salesforce.com"
-                      + "/services/data/v"
-                      + SF_API_VERSION
-                      + "/parameterizedSearch/?q="
-                      + email
-                      + "&sobject=Lead&Lead.fields=id"
 
+  var leadQueryUrl =
+    "https://" +
+    destination.Config.instanceName +
+    ".salesforce.com" +
+    "/services/data/v" +
+    SF_API_VERSION +
+    "/parameterizedSearch/?q=" +
+    email +
+    "&sobject=Lead&Lead.fields=id";
 
-  var leadQueryResponse = await axios.get(leadQueryUrl, 
-                                        {headers: {
-                                          "Authorization" : authorizationHeader  
-                                          }
-                                        });
+  var leadQueryResponse = await axios.get(leadQueryUrl, {
+    headers: {
+      Authorization: authorizationHeader
+    }
+  });
 
   var retrievedLeadCount = leadQueryResponse.data.searchRecords.length;
-  //if count is greater than zero, it means that lead exists, then only update it
-  //else the original endpoint, which is the one for creation - can be used 
-  if(retrievedLeadCount > 0) {
-    targetEndpoint += "/"+leadQueryResponse.data.searchRecords[0].Id+"?_HttpMethod=PATCH";
-  }                                      
+  // if count is greater than zero, it means that lead exists, then only update it
+  // else the original endpoint, which is the one for creation - can be used
+  if (retrievedLeadCount > 0) {
+    targetEndpoint +=
+      "/" + leadQueryResponse.data.searchRecords[0].Id + "?_HttpMethod=PATCH";
+  }
 
   return responseBuilderSimple(
     message,
@@ -184,7 +189,7 @@ async function process(events) {
   let respList = [];
   respList = await Promise.all(
     events.map(event => processSingleMessage(event.message, event.destination))
-  );  
+  );
   return respList;
 }
 

6057a490122d48fba7caa2adb6e6da5394a9ea6d
Srikanth <srikanth.iit@gmail.com>
2019-12-12 17:56:44 +0530

    Bigquery transformer

--- v0/bq/transform.js
+++ v0/bq/transform.js
@@ -1,4 +1,5 @@
 const get = require("get-value");
+const moment = require("moment");
 const {
   toSnakeCase,
   toSafeDBString,
@@ -10,31 +11,14 @@ const { ConfigCategory, mappingConfig } = require("./config");
 const whDefaultConfigJson = mappingConfig[ConfigCategory.DEFAULT.name];
 const whTrackConfigJson = mappingConfig[ConfigCategory.TRACK.name];
 
-function setFromConfig(output, input, configJson) {
-  Object.keys(configJson).forEach(key => {
-    const val = get(input, key);
-    if (val !== undefined) {
-      output[configJson[key]] = val;
-    }
-  });
-}
-
-function setFromProperties(output, input, prefix = "") {
-  if (!input) return;
-  Object.keys(input).forEach(key => {
-    if (isObject(input[key])) {
-      setFromProperties(output, input[key], `${key}_`);
-    } else {
-      output[toSafeDBString(prefix + key)] = input[key];
-    }
-  });
-}
-
 function dataType(val) {
   // TODO: find a better way to check for valid datetime
   // const datetimeRegex = /[0-9]{4}\-(?:0[1-9]|1[0-2])\-(?:0[1-9]|[1-2][0-9]|3[0-1])\s+(?:2[0-3]|[0-1][0-9]):[0-5][0-9]:[0-5][0-9]/;
 
-  if (validTimestamp(val)) {
+  if (
+    validTimestamp(val) ||
+    moment(val, "YYYY-MM-DD hh:mm:ss z", true).isValid()
+  ) {
     return "datetime";
   }
 
@@ -51,6 +35,37 @@ function dataType(val) {
     default:
       return "string";
   }
+}
+
+function setFromConfig(output, input, configJson) {
+  Object.keys(configJson).forEach(key => {
+    let val = get(input, key);
+    if (val !== undefined) {
+      if (dataType(val) === "datetime") {
+        val = moment(val)
+          .utc()
+          .format("YYYY-MM-DD hh:mm:ss z");
+      }
+      output[configJson[key]] = val;
+    }
+  });
+}
+
+function setFromProperties(output, input, prefix = "") {
+  if (!input) return;
+  Object.keys(input).forEach(key => {
+    if (isObject(input[key])) {
+      setFromProperties(output, input[key], `${key}_`);
+    } else {
+      let val = input[key];
+      if (dataType(val) === "datetime") {
+        val = moment(val)
+          .utc()
+          .format("YYYY-MM-DD hh:mm:ss z");
+      }
+      output[toSafeDBString(prefix + key)] = input[key];
+    }
+  });
 }
 
 function getColumns(obj) {
--- v0/util.js
+++ v0/util.js
@@ -1,3 +1,4 @@
+/* eslint-disable radix */
 const fs = require("fs");
 const path = require("path");
 const _ = require("lodash");
@@ -44,7 +45,7 @@ const removeUndefinedValues = obj => _.pickBy(obj, isDefined);
 const isObject = value => {
   var type = typeof value;
   return value != null && (type == "object" || type == "function");
-}
+};
 
 const toSnakeCase = str => {
   if (!str) return "";
@@ -53,6 +54,18 @@ const toSnakeCase = str => {
     .replace(/([a-z])([A-Z])/g, (m, a, b) => a + "_" + b.toLowerCase())
     .replace(/[^A-Za-z0-9]+|_+/g, "_")
     .toLowerCase();
+};
+
+const toSafeDBString = str => {
+  if (parseInt(str[0]) > 0) str = `_${str}`;
+  str = str.replace(/[^a-zA-Z0-9_]+/g, "");
+  return str.substr(0, 127);
+};
+
+function validTimestamp(input) {
+  // eslint-disable-next-line no-restricted-globals
+  if (!isNaN(input)) return false;
+  return new Date(input).getTime() > 0;
 }
 
 const defaultGetRequestConfig = {
@@ -72,6 +85,8 @@ module.exports = {
   removeUndefinedValues,
   isObject,
   toSnakeCase,
+  toSafeDBString,
+  validTimestamp,
   defaultGetRequestConfig,
   defaultPostRequestConfig
 };

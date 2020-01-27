b4db4b4a1f2af838da91af3fd4a323eaea5aa15d
Rajashekar Varkala <raj@Rajashekars-MacBook-Pro.local>
2020-01-27 18:54:50 +0530

    salesforce merge from develop

---
+++ __tests__/salesforce.test.js
@@ -0,0 +1,26 @@
+const integration = "salesforce";
+const name = "Salesforce";
+
+const fs = require("fs");
+const path = require("path");
+const transformer = require(`../v0/${integration}/transform`);
+// const { compareJSON } = require("./util");
+jest.setTimeout(30000);
+test(`${name} Tests`, async () => {
+  const inputDataFile = fs.readFileSync(
+    path.resolve(__dirname, `./data/${integration}_input.json`)
+  );
+  const outputDataFile = fs.readFileSync(
+    path.resolve(__dirname, `./data/${integration}_output.json`)
+  );
+  const inputData = JSON.parse(inputDataFile);
+  const expectedData = JSON.parse(outputDataFile);
+
+  inputData.forEach(async (input, index) => {
+    var output = transformer.process(input); 
+    output.header.Authorization = ''
+    expectedData.header.Authorization='' 
+    expect(output).toEqual(expectedData[index]);
+  });
+
+});

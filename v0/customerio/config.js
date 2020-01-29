354e1eef68222497af4ba4e8b624ab52c8df7e0b
Sayan Mitra <sayan@rudderlabs.com>
2020-01-29 17:18:12 +0530

    update transformation

--- v0/customerio/config.js
+++ v0/customerio/config.js
@@ -5,8 +5,16 @@ const USER_EVENT_ENDPOINT =
 
 const ANON_EVENT_ENDPOINT = "https://track.customer.io/api/v1/events";
 
+const DEVICE_REGISTER_ENDPOINT =
+  "https://track.customer.io/api/v1/customers/:id/devices";
+
+const DEVICE_DELETE_ENDPOINT =
+  "https://track.customer.io/api/v1/customers/:id/devices/:device_id";
+
 module.exports = {
   IDENTITY_ENDPOINT,
   USER_EVENT_ENDPOINT,
-  ANON_EVENT_ENDPOINT
+  ANON_EVENT_ENDPOINT,
+  DEVICE_REGISTER_ENDPOINT,
+  DEVICE_DELETE_ENDPOINT
 };

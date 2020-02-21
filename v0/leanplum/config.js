b09b4c3d24cd603e44271465910813cd32143e5c
arnab <arnab@rudderlabs.com>
2020-02-21 10:43:04 +0530

    Prettier formatting changed context.traits is passed to setUserAttributes

--- v0/leanplum/config.js
+++ v0/leanplum/config.js
@@ -1,18 +1,18 @@
 const { getMappingConfig } = require("../util");
 
 const ConfigCategory = {
-    PAGE: {
-        name: "LPPageConfig",
-        action: "advance"
-    },
-    IDENTIFY: {
-        name: "LPIdentifyConfig",
-        action: "setUserAttributes"
-    },
-    TRACK: {
-        name: "LPTrackConfig",
-        action: "track"
-    }
+  PAGE: {
+    name: "LPPageConfig",
+    action: "advance"
+  },
+  IDENTIFY: {
+    name: "LPIdentifyConfig",
+    action: "setUserAttributes"
+  },
+  TRACK: {
+    name: "LPTrackConfig",
+    action: "track"
+  }
 };
 
 const ENDPOINT = "https://api.leanplum.com/api";
@@ -20,7 +20,7 @@ const ENDPOINT = "https://api.leanplum.com/api";
 const mappingConfig = getMappingConfig(ConfigCategory, __dirname);
 
 module.exports = {
-    ConfigCategory,
-    mappingConfig,
-    ENDPOINT
+  ConfigCategory,
+  mappingConfig,
+  ENDPOINT
 };
--- v0/leanplum/transform.js
+++ v0/leanplum/transform.js
@@ -2,74 +2,71 @@ const get = require("get-value");
 const set = require("set-value");
 
 const { EventType } = require("../../constants");
-const {ConfigCategory, mappingConfig, ENDPOINT} = require("./config");
+const { ConfigCategory, mappingConfig, ENDPOINT } = require("./config");
 const {
-    removeUndefinedValues,
-    defaultPostRequestConfig,
-    defaultRequestConfig
-  } = require("../util");
+  removeUndefinedValues,
+  defaultPostRequestConfig,
+  defaultRequestConfig
+} = require("../util");
 
 function responseBuilderSimple(message, category, destination) {
-    mappingJson = mappingConfig[category.name];
-    const rawPayload = {
-        appId: destination.Config.appId,
-        clientKey: destination.Config.clientKey,
-        apiVersion: destination.Config.apiVersion
-    };
-    
-    const sourceKeys = Object.keys(mappingJson);
-    sourceKeys.forEach(sourceKey => {
-        set(rawPayload, mappingJson[sourceKey], get(message, sourceKey));
-    });
-    // sending anonymousId if userId is not present
-    rawPayload.userId = rawPayload.userId ? rawPayload.userId : message.anonymousId;
-    const payload = removeUndefinedValues(rawPayload);
-    
-    const response = defaultRequestConfig();
-    response.endpoint = ENDPOINT;
-    response.method = defaultPostRequestConfig.requestMethod;
-    response.headers = {
-        "Content-Type": "application/json"
-    };
-    //check userId
-    response.userId = message.userId ? message.userId : message.anonymousId;
-    response.body.JSON = payload;
-    response.params = {
-        action: category.action
-    };
+  mappingJson = mappingConfig[category.name];
+  const rawPayload = {
+    appId: destination.Config.appId,
+    clientKey: destination.Config.clientKey,
+    apiVersion: destination.Config.apiVersion
+  };
 
-    return response;
+  const sourceKeys = Object.keys(mappingJson);
+  sourceKeys.forEach(sourceKey => {
+    set(rawPayload, mappingJson[sourceKey], get(message, sourceKey));
+  });
+  // sending anonymousId if userId is not present
+  rawPayload.userId = rawPayload.userId
+    ? rawPayload.userId
+    : message.anonymousId;
+  const payload = removeUndefinedValues(rawPayload);
+
+  const response = defaultRequestConfig();
+  response.endpoint = ENDPOINT;
+  response.method = defaultPostRequestConfig.requestMethod;
+  response.headers = {
+    "Content-Type": "application/json"
+  };
+  // check userId
+  response.userId = message.userId ? message.userId : message.anonymousId;
+  response.body.JSON = payload;
+  response.params = {
+    action: category.action
+  };
+
+  return response;
 }
 
 function processSingleMessage(message, destination) {
-    let messageType = message.type.toLowerCase();
-    let category;
+  const messageType = message.type.toLowerCase();
+  let category;
 
-    switch(messageType)
-    {
-        case EventType.PAGE:
-            category = ConfigCategory.PAGE;
-            break;
-        case EventType.IDENTIFY:
-            category = ConfigCategory.IDENTIFY;
-            break;
-        case EventType.TRACK:
-            category = ConfigCategory.TRACK;
-            break;
-        default:
-            throw new Error("Message type not supported");
-    }
-    
-    return responseBuilderSimple(
-        message,
-        category,
-        destination
-    );
+  switch (messageType) {
+    case EventType.PAGE:
+      category = ConfigCategory.PAGE;
+      break;
+    case EventType.IDENTIFY:
+      category = ConfigCategory.IDENTIFY;
+      break;
+    case EventType.TRACK:
+      category = ConfigCategory.TRACK;
+      break;
+    default:
+      throw new Error("Message type not supported");
+  }
+
+  return responseBuilderSimple(message, category, destination);
 }
 
 function process(event) {
-    const resp = processSingleMessage(event.message, event.destination);
-    return resp;
+  const resp = processSingleMessage(event.message, event.destination);
+  return resp;
 }
 
 exports.process = process;

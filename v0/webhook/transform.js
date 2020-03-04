1fcc89a86a7671964822b913d21b16bea2a132df
Sayan Mitra <sayan@rudderlabs.com>
2020-03-04 19:53:47 +0530

    changes for webhook dest and testcases

---
+++ v0/webhook/transform.js
@@ -0,0 +1,26 @@
+const { defaultPostRequestConfig, defaultRequestConfig } = require("../util");
+
+function process(event) {
+  const { message, destination } = event;
+  let response = defaultRequestConfig();
+  response.endpoint = destination.Config.webhookUrl;
+  response.method = defaultPostRequestConfig.requestMethod;
+  // add content-type by default as our payload is json
+  response.headers = {
+    "Content-Type": "application/json"
+  };
+
+  const configHeaders = destination.Config.header;
+  configHeaders.forEach(header => {
+    response.headers[header.from] = header.to;
+  });
+
+  response.userId = message.userId ? message.userId : message.anonymousId;
+  response.body.JSON = { ...message };
+  if (!response.statusCode) {
+    response.statusCode = 200;
+  }
+  return [response];
+}
+
+exports.process = process;

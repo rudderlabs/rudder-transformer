d4a3a3a36a157dd06e5b16b3488a43f23d6275b6
prabrisha-rudder <prabrisha@rudderlabs.com>
2020-03-17 18:56:44 +0530

    remove user from group

--- v0/zendesk/config.js
+++ v0/zendesk/config.js
@@ -1,6 +1,13 @@
 const { getMappingConfig } = require("../util");
 
-const defaultFields = ['email', 'name', 'organizationId', 'timezone', 'phone', 'userId'];
+const defaultFields = [
+  "email",
+  "name",
+  "organizationId",
+  "timezone",
+  "phone",
+  "userId"
+];
 
 const ConfigCategory = {
   IDENTIFY: {
@@ -10,13 +17,12 @@ const ConfigCategory = {
     createOrUpdateUserEndpoint: "users/create_or_update.json"
   },
   GROUP: {
-      name: "ZDGroupConfig",
-      json_name: "organization",
-      userMembershipEndpoint: "organization_memberships.json",
-      createEndpoint: "organizations/create_or_update.json"
+    name: "ZDGroupConfig",
+    json_name: "organization",
+    userMembershipEndpoint: "organization_memberships.json",
+    createEndpoint: "organizations/create_or_update.json"
   }
 };
-
 
 const mappingConfig = getMappingConfig(ConfigCategory, __dirname);
 

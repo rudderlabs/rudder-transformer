d5eee0d3d44018d0396283f2b5966c894e758071
Ruchira <ruchira@rudderlabs.com>
2020-03-19 15:55:04 +0530

    made changes in  transformer for personalize according to config in backend

--- v0/personalize/transform.js
+++ v0/personalize/transform.js
@@ -1,55 +1,71 @@
-// config = {
-//             destinationID : destinationID,
-//             accessKeyId : accessKeyId,
-//             secretAccessKey : secretAccessKey,
-//             region : region,
-//             datasetName  : name,
-//             eventName : eventName,
-//             keyFields : keyField,
-//             mappedFields : mappedField,
-//             fieldTypes : fieldType,
-//             isCampaign : isCampaign,
-//             isSolution : isSolution,
-//             noOfFields : noOfFields,
-//             extras:   result
-//     }
+// Config =
+//   {"accessKeyId":"AKIAWTVBJHCTAG52M2ZA",
+//   "secretAccessKey":"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",
+//   "region":"us-east-1",
+//   "trackingId":"12345",
+//   "eventName":"track",
+//   "customMappings":
+//   [
+//     {"from":"user_id","to":"anon_id"},
+//     {"from":"item_id","to":"game_id"},
+//     {"from":"timestamp","to":"date"},
+//     {"from":"item_desc","to":"desc"}
+//   ]
+// }
 
 async function process(event) {
   var payload = {};
+  var noOfFields = event.destination.Config.customMappings.length;
+  console.log(noOfFields);
+  var keyField = [];
+  var mappedField = [];
+  for (var i = 0; i < noOfFields; i++) {
+    keyField.push(event.destination.Config.customMappings[i].from);
+  }
 
-  if (event.message.type == event.destination.config.eventName) {
+  for (var j = 0; j < noOfFields; j++) {
+    mappedField.push(event.destination.Config.customMappings[j].to);
+  }
+  console.log(mappedField);
+  console.log(keyField);
+  if (event.message.event == event.destination.Config.eventName) {
     var property = {};
-    var mappedFields = JSON.parse(
-      event.destination.config.mappedFields.replace(/(?<!\\)'/g, '"')
-    );
-    var keyFields = JSON.parse(
-      event.destination.config.keyFields.replace(/(?<!\\)'/g, '"')
-    );
+    for (var k = 0; k < noOfFields; k++) {
+      var mappedFields = mappedField[k];
+      console.log(event.message[mappedFields]);
 
-    for (var i = 0; i < event.destination.config.noOfFields; i++) {
-      var mappedField = mappedFields[i];
-      console.log(mappedField);
-      property[keyFields[i]] = event.message[mappedField];
+      if (typeof event.message[mappedFields] !== "undefined") {
+        console.log("inside if");
+        property[keyField[k]] = event.message[mappedFields];
+      } else {
+        console.log("inside else");
+        payload = {
+          statusCode: 400,
+          error: "Mapped Field " + mappedFields + " not found"
+        };
+        return payload;
+      }
     }
     console.log(property);
 
     payload = {
-      accessKeyId: event.destination.config.accessKeyId,
-      secretAccessKey: event.destination.config.secretAccessKey,
-      region: event.destination.config.region,
-      datasetName: event.destination.config.datasetName,
+      accessKeyId: event.destination.Config.accessKeyId,
+      secretAccessKey: event.destination.Config.secretAccessKey,
+      region: event.destination.Config.region,
+      datasetName: event.destination.Config.datasetName,
       eventList: [
         /* required */
         {
           eventId: event.message.messageId,
-          eventType: event.destination.config.eventName,
+          eventType: event.destination.Config.eventName,
           properties: property,
           sentAt: event.message.sentAt
         }
       ],
       sessionId: event.message.originalTimestamp /* required */,
-      trackingId: event.destination.extras.trackingId /* required */,
-      userId: event.message.userId
+      trackingId: event.destination.Config.trackingId /* required */,
+      userId: event.message.userId,
+      statusCode: 200
     };
   }
 

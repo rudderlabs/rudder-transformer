0f34bd6a4e0b31c9778ae1b3b366fd0f9091972c
Srikanth <srikanth.iit@gmail.com>
2020-04-21 10:37:59 +0530

    support for alias call

--- v0/util.js
+++ v0/util.js
@@ -433,6 +433,30 @@ function processWarehouseMessage(
       responses.push({ metadata, data: event });
       break;
     }
+    case "alias": {
+      const event = {};
+      setFromProperties(provider, event, message.traits, columnTypes);
+      setFromProperties(
+        provider,
+        event,
+        message.context,
+        columnTypes,
+        "context_"
+      );
+      setFromConfig(provider, event, message, whDefaultConfigJson, columnTypes);
+
+      // add groupId from top level properties
+      const previousId = safeColumnName(provider, "previous_id");
+      event[previousId] = message.previousId;
+      columnTypes[previousId] = "string";
+
+      const metadata = {
+        table: safeTableName(provider, "aliases"),
+        columns: getColumns(event, columnTypes)
+      };
+      responses.push({ metadata, data: event });
+      break;
+    }
     default:
       throw new Error("Unknown event type", eventType);
   }

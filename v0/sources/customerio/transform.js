14f92712c6956c367e2ee2c5d22ee20a8b76d4c7
Srikanth <srikanth.iit@gmail.com>
2020-05-21 21:22:02 +0530

    Add anonymousId when userId is missing

--- v0/sources/customerio/transform.js
+++ v0/sources/customerio/transform.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const fs = require("fs");
-
 // import mapping json using JSON.parse to preserve object key order
 const mapping = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "./mapping.json"), "utf-8")
@@ -12,7 +11,7 @@ const eventNameMap = {
   opened: "Email Opened",
   bounced: "Email Bounced",
   delivered: "Email Delivered",
-  spammed: "Email Spammed",
+  spammed: "Email Marked as Spam",
   unsubscribed: "Email Unsubscribed"
 };
 
@@ -40,6 +39,17 @@ function process(event) {
     const ts = new Date(event.timestamp * 1000).toISOString();
     message.setProperty("originalTimestamp", ts);
     message.setProperty("sentAt", ts);
+  }
+
+  // when customer.io does not pass an associated userId, set the email address as anonymousId
+  if (message.userId === null || message.userId === undefined) {
+    if (
+      message.context &&
+      message.context.traits &&
+      message.context.traits.email
+    ) {
+      message.anonymousId = message.context.traits.email;
+    }
   }
 
   return message;

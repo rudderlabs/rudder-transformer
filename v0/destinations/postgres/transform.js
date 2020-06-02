0a39e366d7b82ac1bf88e82fd90cc545fa7a2c58
Srikanth <srikanth.iit@gmail.com>
2020-06-02 17:29:48 +0530

    Remove unused func args

--- v0/destinations/postgres/transform.js
+++ v0/destinations/postgres/transform.js
@@ -1,12 +1,12 @@
 const { processWarehouseMessage } = require("../util");
 
-function processSingleMessage(message, destination, schemaVersion) {
+function processSingleMessage(message, schemaVersion) {
   return processWarehouseMessage("postgres", message, schemaVersion);
 }
 
 function process(event) {
   const schemaVersion = event.request.query.whSchemaVersion;
-  return processSingleMessage(event.message, event.destination, schemaVersion);
+  return processSingleMessage(event.message, schemaVersion);
 }
 
 exports.process = process;

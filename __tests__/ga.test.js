f5effa1d27acfacaa6faf18e2df33ba0ace772b5
arnab <arnab@rudderlabs.com>
2020-06-19 12:32:30 +0530

    Minor code formatting fixes

--- __tests__/ga.test.js
+++ __tests__/ga.test.js
@@ -11,9 +11,11 @@ test(`${name} Tests`, async () => {
   const inputDataFile = fs.readFileSync(
     path.resolve(__dirname, `./data/${integration}_input.json`)
   );
+
   const outputDataFile = fs.readFileSync(
     path.resolve(__dirname, `./data/${integration}_output.json`)
   );
+
   const inputData = JSON.parse(inputDataFile);
   const expectedData = JSON.parse(outputDataFile);
   inputData.forEach(async (input, index) => {

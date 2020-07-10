d69576e7c098868cd831bb9cb0ec3ae3a4a1edb7
Ruchira <ruchira@rudderlabs.com>
2020-07-10 19:49:46 +0530

    added logic for taking config from transformer for firehose

--- v0/destinations/firehose/transform.js
+++ v0/destinations/firehose/transform.js
@@ -1,7 +1,44 @@
+function getDeliveryStreamMapTo(event) {
+  const config = event.destination.Config;
+  const { message } = event;
+
+  const { mapEvents } = config;
+  let check = true;
+  let deliveryStreamMapFrom;
+  let deliveryStreamMapTo;
+  mapEvents.forEach(mapEvent => {
+    deliveryStreamMapFrom = mapEvent.from;
+    if (deliveryStreamMapFrom === "*") {
+      check = false;
+      deliveryStreamMapTo = mapEvent.to;
+    }
+  });
+
+  if (check) {
+    mapEvents.forEach(mapEvent => {
+      deliveryStreamMapFrom = mapEvent.from;
+      if (deliveryStreamMapFrom === message.type) {
+        check = false;
+        deliveryStreamMapTo = mapEvent.to;
+      }
+    });
+  }
+  if (check) {
+    mapEvents.forEach(mapEvent => {
+      deliveryStreamMapFrom = mapEvent.from;
+      if (deliveryStreamMapFrom === message.event) {
+        deliveryStreamMapTo = mapEvent.to;
+      }
+    });
+  }
+  return deliveryStreamMapTo;
+}
+
 function process(event) {
   const result = {
     message: event.message,
-    userId: event.message.userId || event.message.anonymousId
+    userId: event.message.userId || event.message.anonymousId,
+    deliveryStreamMapTo: getDeliveryStreamMapTo(event)
   };
   return result;
 }

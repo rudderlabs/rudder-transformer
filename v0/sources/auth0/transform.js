95ecedd063ed5cc57808e03276b655ce8251db5e
Arnab <arnab.compsc@gmail.com>
2020-07-13 23:44:08 +0530

    Removed null and undefined values from transformed event

--- v0/sources/auth0/transform.js
+++ v0/sources/auth0/transform.js
@@ -1,14 +1,25 @@
 const path = require("path");
 const fs = require("fs");
+const _ = require("lodash");
 // import mapping json using JSON.parse to preserve object key order
 const mapping = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "./mapping.json"), "utf-8")
 );
 const Message = require("../message");
 
+// Ref: https://auth0.com/docs/logs/references/log-event-type-codes
 const eventNameMap = JSON.parse(
   fs.readFileSync(path.resolve(__dirname, "./eventMapping.json"), "utf-8")
 );
+
+// ideally this should go under utils
+// we should move the utils class out of the destinations folder
+// and put under a common place so that both sources and destinations can use that
+// will take that in a separate PR
+const isDefined = x => !_.isUndefined(x);
+const isNotNull = x => x != null;
+const isDefinedAndNotNull = x => isDefined(x) && isNotNull(x);
+const removeUndefinedAndNullValues = obj => _.pickBy(obj, isDefinedAndNotNull);
 
 function processEvent(event) {
   const messageType = "track";
@@ -39,7 +50,7 @@ function process(events) {
   const responses = [];
   events.forEach(event => {
     try {
-      responses.push(processEvent(event));
+      responses.push(removeUndefinedAndNullValues(processEvent(event)));
     } catch (error) {
       // TODO: figure out a way to handle partial failures within batch
       // responses.push({

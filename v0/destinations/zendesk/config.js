60b5243c8be1e4cf93a3b423f7f38707e39465c9
Arnab <arnab.compsc@gmail.com>
2020-07-24 01:40:25 +0530

    Added marketplace credentials for Braze and Zendesk

--- v0/destinations/zendesk/config.js
+++ v0/destinations/zendesk/config.js
@@ -39,8 +39,15 @@ const defaultFields = {
 
 const mappingConfig = getMappingConfig(ConfigCategory, __dirname);
 
+const ZENDEDK_MARKET_PLACE_NAME = "RudderStack";
+const ZENDEDK_MARKET_PLACE_ORG_ID = "3339";
+const ZENDEDK_MARKET_PLACE_APP_ID = "263241";
+
 module.exports = {
   ConfigCategory,
   mappingConfig,
-  defaultFields
+  defaultFields,
+  ZENDEDK_MARKET_PLACE_NAME,
+  ZENDEDK_MARKET_PLACE_ORG_ID,
+  ZENDEDK_MARKET_PLACE_APP_ID
 };

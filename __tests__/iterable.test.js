98c139dff041c2c7e614005a564bc31abc2d9998
Arnab <arnab.compsc@gmail.com>
2020-08-05 03:12:36 +0530

    Updated test file

--- __tests__/iterable.test.js
+++ __tests__/iterable.test.js
@@ -6,16 +6,18 @@ const path = require("path");
 
 const transformer = require(`../v1/destinations/${integration}/transform`);
 
-test(`${name} Tests`, () => {
-  const inputDataFile = fs.readFileSync(
-    path.resolve(__dirname, `./data/${integration}_input.json`)
-  );
-  const outputDataFile = fs.readFileSync(
-    path.resolve(__dirname, `./data/${integration}_output.json`)
-  );
-  const inputData = JSON.parse(inputDataFile);
-  const expectedData = JSON.parse(outputDataFile);
-  inputData.forEach(async (input, index) => {
+const inputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_input.json`)
+);
+const outputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_output.json`)
+);
+
+const inputData = JSON.parse(inputDataFile);
+const expectedData = JSON.parse(outputDataFile);
+
+inputData.forEach((input, index) => {
+  it(`${name} Tests: payload: ${index}`, async () => {
     try {
       const output = await transformer.process(input);
       expect(output).toEqual(expectedData[index]);

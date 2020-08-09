a2d8005087b504863b3b9967403e062be1b109fb
Arnab <arnab.compsc@gmail.com>
2020-08-10 02:09:36 +0530

    Fixes for Braze test cases

--- __tests__/mailchimp.test.js
+++ __tests__/mailchimp.test.js
@@ -3,23 +3,31 @@ const name = "Mailchimp";
 
 const fs = require("fs");
 const path = require("path");
+const version = "v1";
 
-const transformer = require(`../v0/destinations/${integration}/transform`);
+const transformer = require(`../${version}/destinations/${integration}/transform`);
 // const { compareJSON } = require("./util");
 
-test(`${name} Tests`, async () => {
-  const inputDataFile = fs.readFileSync(
-    path.resolve(__dirname, `./data/${integration}_input.json`)
-  );
-  const outputDataFile = fs.readFileSync(
-    path.resolve(__dirname, `./data/${integration}_output.json`)
-  );
-  const inputData = JSON.parse(inputDataFile);
-  const expectedData = JSON.parse(outputDataFile);
-  await Promise.all(
-    inputData.map(async (input, index) => {
-      const output = await transformer.process(input);
-      expect(output).toEqual(expectedData[index]);
-    })
-  );
+const inputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_input.json`)
+);
+const outputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_output.json`)
+);
+
+const inputData = JSON.parse(inputDataFile);
+const expectedData = JSON.parse(outputDataFile);
+
+inputData.forEach((input, index) => {
+  test(`${name} Tests: payload - ${index}`, () => {
+    let output, expected;
+    try {
+      output = transformer.process(input);
+      expected = expectedData[index]
+    } catch (error) {
+      output = error.message;
+      expected = expectedData[index].message;
+    }
+    expect(output).toEqual(expected);
+  });
 });

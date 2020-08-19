afe76b22bdfcfebafb243c63d3e1bb914509fbcf
dhawal1248 <sanghvi.dhawal101433@gmail.com>
2020-08-19 19:04:55 +0530

    webhook - added get support

--- v0/destinations/util.js
+++ v0/destinations/util.js
@@ -125,13 +125,94 @@ function formatValue(value) {
   if (!value || value < 0) return 0;
   return Math.round(value);
 }
+
 function getHashFromArray(arrays, fromKey = "from", toKey = "to") {
   const hashMap = {};
-  arrays.forEach(array => {
-    hashMap[array[fromKey]] = array[toKey];
-  });
+  if (Array.isArray(arrays)) {
+    arrays.forEach(array => {
+      hashMap[array[fromKey]] = array[toKey];
+    });
+  }
   return hashMap;
 }
+
+const MESSAGE_MAPPING = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, `./GenericFieldMapping.json`))
+);
+
+// Important !@!
+// - get value from a list of sourceKeys in precedence order
+// - get value from a string key
+const getValueFromMessage = (message, sourceKey) => {
+  if (Array.isArray(sourceKey) && sourceKey.length > 0) {
+    if (sourceKey.length === 1) {
+      logger.warn(
+        "List with single element is not ideal. Use it as string instead"
+      );
+    }
+    // got the possible sourceKeys
+    for (let index = 0; index < sourceKey.length; index += 1) {
+      const val = get(message, sourceKey[index]);
+      if (val) {
+        // return only if the value is valid.
+        // else look for next possible source in precedence
+        return val;
+      }
+    }
+  } else if (typeof sourceKey === "string") {
+    // got a single key
+    // - we don't need to iterate over a loop for a single possible value
+    return get(message, sourceKey);
+  } else {
+    // wrong sourceKey type. abort
+    // DEVELOPER ERROR
+    throw new Error("Wrong sourceKey type or blank sourceKey array");
+  }
+  return null;
+};
+
+// get a field value from message.
+// check `data/message.json` for actual field precedence
+// Example usage: getFieldValueFromMessage(message, "userId")
+//                This will return the first nonnull value from
+//                ["userId", "context.traits.userId", "context.traits.id", "anonymousId"]
+const getFieldValueFromMessage = (message, field) => {
+  const sourceKey = MESSAGE_MAPPING[field];
+  if (sourceKey) {
+    return getValueFromMessage(message, sourceKey);
+  }
+  return null;
+};
+
+// function to flatten a json
+
+function flattenJson(data) {
+  const result = {};
+  let l;
+  // a recursive function to loop through the array of the data
+  function recurse(cur, prop) {
+    let i;
+    if (Object(cur) !== cur) {
+      result[prop] = cur;
+    } else if (Array.isArray(cur)) {
+      for (i = 0, l = cur.length; i < l; i += 1)
+        recurse(cur[i], `${prop}[${i}]`);
+      if (l === 0) result[prop] = [];
+    } else {
+      let isEmpty = true;
+      Object.keys(cur).forEach(key => {
+        isEmpty = false;
+        recurse(cur[key], prop ? `${prop}.${key}` : key);
+      });
+      if (isEmpty && prop) result[prop] = {};
+    }
+  }
+  if (data) {
+    recurse(data, "");
+  }
+  return result;
+}
+
 module.exports = {
   getMappingConfig,
   getDateInFormat,
@@ -148,5 +229,7 @@ module.exports = {
   getParsedIP,
   setValues,
   formatValue,
-  getHashFromArray
+  flattenJson,
+  getHashFromArray,
+  getFieldValueFromMessage
 };
--- v0/destinations/webhook/transform.js
+++ v0/destinations/webhook/transform.js
@@ -1,39 +1,51 @@
-const { defaultPostRequestConfig, defaultRequestConfig } = require("../util");
+const {
+  defaultPostRequestConfig,
+  defaultGetRequestConfig,
+  defaultRequestConfig,
+  getHashFromArray,
+  getFieldValueFromMessage,
+  flattenJson
+} = require("../util");
+const { EventType } = require("../../../constants");
 
-function handleEvent(event) {
-  const { message, destination } = event;
-  const response = defaultRequestConfig();
-  if (destination.Config.webhookUrl) {
-    response.endpoint = destination.Config.webhookUrl;
-    response.method = defaultPostRequestConfig.requestMethod;
-    // add content-type by default as our payload is json
-    response.headers = {
-      "Content-Type": "application/json"
-    };
-
-    const configHeaders = destination.Config.headers;
-    if (configHeaders) {
-      configHeaders.forEach(header => {
-        response.headers[header.from] = header.to;
-      });
-    }
-
-    response.userId = message.userId || message.anonymousId;
-    response.body.JSON = { ...message };
-    return response;
+const getPropertyParams = message => {
+  if (message.type === EventType.IDENTIFY) {
+    return flattenJson(getFieldValueFromMessage(message, "traits"));
   }
-  throw new Error("Invalid Url in destination");
-}
-
-const process = event => {
-  try {
-    return handleEvent(event);
-  } catch (error) {
-    return {
-      statusCode: 400,
-      error: `${error.message}`
-    };
-  }
+  return flattenJson(message.properties);
 };
+
+function process(event) {
+  try {
+    const { message, destination } = event;
+    const response = defaultRequestConfig();
+    const url = destination.Config.webhookUrl;
+    const method = destination.Config.webhookMethod;
+    const headers = destination.Config.headers;
+
+    if (url) {
+      if (method === defaultGetRequestConfig.requestMethod) {
+        response.method = defaultGetRequestConfig.requestMethod;
+        // send properties as query params for GET
+        response.params = getPropertyParams(message);
+      } else {
+        response.method = defaultPostRequestConfig.requestMethod;
+        response.body.JSON = message;
+        response.headers = {
+          "Content-Type": "application/json"
+        };
+      }
+
+      Object.assign(response.headers, getHashFromArray(headers));
+      response.userId = getFieldValueFromMessage(message, "userId");
+      response.endpoint = url;
+
+      return response;
+    }
+    throw new Error("Invalid URL in destination config");
+  } catch (err) {
+    throw new Error(err.message || "[Webhook] Failed to process request");
+  }
+}
 
 exports.process = process;

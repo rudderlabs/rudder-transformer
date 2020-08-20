fa38b07016bb733a8d391679c0db33577a5e3770
dhawal1248 <sanghvi.dhawal101433@gmail.com>
2020-08-20 10:23:45 +0530

    Added try catch around process

--- v1/destinations/webhook/transform.js
+++ v1/destinations/webhook/transform.js
@@ -1,41 +1,51 @@
-const { defaultPostRequestConfig, defaultGetRequestConfig, defaultRequestConfig, getHashFromArray, getFieldValueFromMessage, flattenJson } = require("../../util");
+const {
+  defaultPostRequestConfig,
+  defaultGetRequestConfig,
+  defaultRequestConfig,
+  getHashFromArray,
+  getFieldValueFromMessage,
+  flattenJson
+} = require("../../util");
 const { EventType } = require("../../../constants");
 
-const getPropertyParams = (message) => {
+const getPropertyParams = message => {
   if (message.type === EventType.IDENTIFY) {
     return flattenJson(getFieldValueFromMessage(message, "traits"));
   }
   return flattenJson(message.properties);
-}
+};
 
-function process(event){
-  const { message, destination } = event;
-  const response = defaultRequestConfig();
-  const url = destination.Config.webhookUrl;
-  const method = destination.Config.webhookMethod;
-  const headers = destination.Config.headers;
+function process(event) {
+  try {
+    const { message, destination } = event;
+    const response = defaultRequestConfig();
+    const url = destination.Config.webhookUrl;
+    const method = destination.Config.webhookMethod;
+    const headers = destination.Config.headers;
 
-  if(url){
-  
-    if (method === defaultGetRequestConfig.requestMethod) {
-      response.method = defaultGetRequestConfig.requestMethod;
-      // send properties as query params for GET
-      response.params = getPropertyParams(message);
-    } else {
-      response.method = defaultPostRequestConfig.requestMethod;
-      response.body.JSON = message;
-      response.headers = {
-        "Content-Type" : "application/json"
-      };
+    if (url) {
+      if (method === defaultGetRequestConfig.requestMethod) {
+        response.method = defaultGetRequestConfig.requestMethod;
+        // send properties as query params for GET
+        response.params = getPropertyParams(message);
+      } else {
+        response.method = defaultPostRequestConfig.requestMethod;
+        response.body.JSON = message;
+        response.headers = {
+          "Content-Type": "application/json"
+        };
+      }
+
+      Object.assign(response.headers, getHashFromArray(headers));
+      response.userId = getFieldValueFromMessage(message, "userId");
+      response.endpoint = url;
+
+      return response;
     }
-
-    Object.assign(response.headers, getHashFromArray(headers));
-    response.userId = getFieldValueFromMessage(message, "userId");
-    response.endpoint = url;
-    
-    return response;
+    throw new Error("Invalid URL in destination config");
+  } catch (err) {
+    throw new Error(err.message || "[webhook] Failed to process request");
   }
-  throw new Error("Invalid URL in destination config");
 }
 
 exports.process = process;

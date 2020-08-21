866c35cddadb99419fbda628184694d7093b97d3
Arnab <arnab.compsc@gmail.com>
2020-08-21 23:18:48 +0530

    Braze test file updated

--- __tests__/braze_test.js
+++ __tests__/braze_test.js
@@ -3,24 +3,31 @@ const name = "Braze";
 
 const fs = require("fs");
 const path = require("path");
+const version = "v0";
 
-const transformer = require(`../v0/destinations/${integration}/transform`);
+const transformer = require(`../${version}/destinations/${integration}/transform`);
 // const { compareJSON } = require("./util");
-jest.setTimeout(30000);
-test(`${name} Tests`, async () => {
-  const inputDataFile = fs.readFileSync(
-    path.resolve(__dirname, `./data/${integration}_input.json`)
-  );
-  const outputDataFile = fs.readFileSync(
-    path.resolve(__dirname, `./data/${integration}_output.json`)
-  );
-  const inputData = JSON.parse(inputDataFile);
-  const expectedData = JSON.parse(outputDataFile);
 
-  inputData.forEach(async (input, index) => {
-    const outputArr = transformer.process(input);
-    outputArr.forEach((output, innerIndex) => {
-      expect(output).toEqual(expectedData[index][innerIndex]);
-    });
+const inputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_input.json`)
+);
+const outputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_output.json`)
+);
+
+const inputData = JSON.parse(inputDataFile);
+const expectedData = JSON.parse(outputDataFile);
+
+inputData.forEach((input, index) => {
+  it(`${name} Tests: payload - ${index}`, () => {
+    let output, expected;
+    try {
+      output = transformer.process(input);
+      expected = expectedData[index]
+    } catch (error) {
+      output = error.message;
+      expected = expectedData[index].message;
+    }
+    expect(output).toEqual(expected);
   });
 });

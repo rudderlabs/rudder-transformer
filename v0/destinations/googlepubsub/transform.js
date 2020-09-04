639a9ad876be3371634a9a7538cd0cf955cd9dde
Ruchira <ruchira@rudderlabs.com>
2020-09-04 15:38:41 +0530

    changed from topic path to topic id for google pub/sub

--- v0/destinations/googlepubsub/transform.js
+++ v0/destinations/googlepubsub/transform.js
@@ -8,21 +8,13 @@ function getTopic(event) {
 }
 
 function process(event) {
-  const topicPath = getTopic(event);
-  if (topicPath) {
-    const splitPath = topicPath.split("/");
-    if (splitPath && splitPath.length === 4) {
-      const topicId = splitPath[3];
-      if (topicId) {
-        return {
-          message: event.message,
-          userId: event.message.anonymousId,
-          topicId
-        };
-      }
-      throw new Error("Topic id cannot be null.");
-    }
-    throw new Error("Topic path without topic id.");
+  const topicId = getTopic(event);
+  if (topicId) {
+    return {
+      message: event.message,
+      userId: event.message.anonymousId,
+      topicId
+    };
   }
   throw new Error("No topic set for this event");
 }

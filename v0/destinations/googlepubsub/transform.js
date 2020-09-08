bfa0029a65a6cc3c4b8ba30c1b5dc3d9d25c9686
Ruchira <ruchira@rudderlabs.com>
2020-09-08 12:01:17 +0530

    updated test files

--- v0/destinations/googlepubsub/transform.js
+++ v0/destinations/googlepubsub/transform.js
@@ -4,10 +4,11 @@ function getTopic(event) {
   const { message } = event;
   const { eventToTopicMap } = event.destination.Config;
   const hashMap = getHashFromArray(eventToTopicMap, "from", "to");
+
   return (
     hashMap["*"] ||
     hashMap[message.type.toLowerCase()] ||
-    hashMap[message.event.toLowerCase()]
+    (message.event ? hashMap[message.event.toLowerCase()] : null)
   );
 }
 

ea6326f8a12aa0c57fae95fc008827c5d98b604e
Ruchira <ruchira@rudderlabs.com>
2020-09-21 16:39:58 +0530

    minor formatting

--- v0/destinations/lytics/transform.js
+++ v0/destinations/lytics/transform.js
@@ -1,29 +1,28 @@
 const { EventType } = require("../../../constants");
-const { CONFIG_CATEGORIES, MAPPING_CONFIG, ENDPOINT} = require("./config");
+const { CONFIG_CATEGORIES, MAPPING_CONFIG, ENDPOINT } = require("./config");
 const {
-    constructPayload,
-    defaultPostRequestConfig,
-    removeUndefinedAndNullValues,
-    defaultRequestConfig,
-    flattenJson
-  } = require("../../util");
+  constructPayload,
+  defaultPostRequestConfig,
+  removeUndefinedAndNullValues,
+  defaultRequestConfig,
+  flattenJson
+} = require("../../util");
 function responseBuilderSimple(message, category, destination) {
-    const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
-    const response = defaultRequestConfig();
-    const stream = destination.Config.stream;
-    const apiKey = destination.Config.apiKey;
-    const timestamp = message.originalTimestamp;
-    response.endpoint = `${ENDPOINT}/${stream}?timestamp_field=${timestamp}`;
-    response.method = defaultPostRequestConfig.requestMethod;
-    
-    response.userId = message.anonymousId;
-    response.body.JSON = removeUndefinedAndNullValues(flattenJson(payload));
-    response.headers = {
-        "Content-Type": "application/json",
-        Authorization: apiKey
-      };
-    return response;
+  const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
+  const response = defaultRequestConfig();
+  const { stream, apiKey } = destination.Config;
 
+  const timestamp = message.originalTimestamp;
+  response.endpoint = `${ENDPOINT}/${stream}?timestamp_field=${timestamp}`;
+  response.method = defaultPostRequestConfig.requestMethod;
+
+  response.userId = message.anonymousId;
+  response.body.JSON = removeUndefinedAndNullValues(flattenJson(payload));
+  response.headers = {
+    "Content-Type": "application/json",
+    Authorization: apiKey
+  };
+  return response;
 }
 
 const processEvent = (message, destination) => {
@@ -34,21 +33,21 @@ const processEvent = (message, destination) => {
   let category;
   switch (messageType) {
     case EventType.IDENTIFY:
-        category = CONFIG_CATEGORIES.IDENTIFY;
-        break;
+      category = CONFIG_CATEGORIES.IDENTIFY;
+      break;
     case EventType.PAGE:
     case EventType.SCREEN:
-        category = CONFIG_CATEGORIES.PAGESCREEN;
-        break;
+      category = CONFIG_CATEGORIES.PAGESCREEN;
+      break;
     case EventType.TRACK:
-        category = CONFIG_CATEGORIES.TRACK;
-        break;
+      category = CONFIG_CATEGORIES.TRACK;
+      break;
     default:
-        throw new Error("Message type not supported");
-    }
-      // build the response
-  return responseBuilderSimple(message, category, destination);
+      throw new Error("Message type not supported");
   }
+  // build the response
+  return responseBuilderSimple(message, category, destination);
+};
 
 const process = event => {
   return processEvent(event.message, event.destination);

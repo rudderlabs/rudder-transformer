4c08dc9142874b990ece5a18dfd966959a3d6e47
gane5hvarma <ganesh@rudderlabs>
2020-10-01 14:42:24 +0530

    pool optimization

--- util/ivmPool.js
+++ util/ivmPool.js
@@ -2,29 +2,32 @@ const genericPool = require("generic-pool");
 const { getFactory } = require("./ivmFactory");
 const logger = require("../logger");
 
-const poolCache = {};
-
+const transformationPoolCache = {};
+const transformationLibraryCache = {};
 const opts = {
   min: 2, // minimum size of the pool
   max: 10 // maximum size of the pool
 };
 
 async function getPool(versionId, libraryVersionIds) {
-  const poolId = versionId + libraryVersionIds.sort().toString();
-  if (!poolCache[poolId]) {
+  const sortedLibrariesIdString = libraryVersionIds.sort().toString();
+  if (
+    !transformationPoolCache[versionId] ||
+    (transformationPoolCache[versionId] &&
+      transformationLibraryCache[versionId] !== sortedLibrariesIdString)
+  ) {
     const factory = await getFactory(versionId, libraryVersionIds);
-    poolCache[poolId] = genericPool.createPool(factory, opts);
-
+    transformationPoolCache[versionId] = genericPool.createPool(factory, opts);
+    transformationLibraryCache[versionId] = sortedLibrariesIdString;
     // Added to stop retrying and infinite loop on error
     // TODO: Figure out if we should we do this
-    poolCache[poolId].on("factoryCreateError", error => {
+    transformationPoolCache[versionId].on("factoryCreateError", error => {
       // eslint-disable-next-line no-underscore-dangle
-      poolCache[poolId]._waitingClientsQueue.dequeue().reject(error);
+      transformationPoolCache[versionId]._waitingClientsQueue.dequeue().reject(error);
     });
-
     logger.debug("pool created");
   }
-  return poolCache[poolId];
+  return transformationPoolCache[versionId];
 }
 
 exports.getPool = getPool;

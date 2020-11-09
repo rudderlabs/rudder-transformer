8f5e7a7a4af3e265acecd113d635887a467e6e48
Shivam@RS <shivam441@gmail.com>
2020-11-09 22:07:44 +0530

    posthog-transformer

---
+++ v0/util/enum/message.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line no-unused-vars
+const Message = {
+    TypeNotFound: "Invalid payload. Property Type is not present. ",
+    TypeNotSupported: "Message type not supported.",
+    FailedToConstructPayload: "Payload could not be constructed"
+  };
+  

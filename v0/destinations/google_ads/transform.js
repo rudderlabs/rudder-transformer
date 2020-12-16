a01fab2291aad9a445758b6325073831418499fc
Ruchira <ruchira@rudderlabs.com>
2020-12-16 11:34:17 +0530

    added comments for track calls

--- v0/destinations/google_ads/transform.js
+++ v0/destinations/google_ads/transform.js
@@ -7,6 +7,16 @@ const {
 } = require("../../util");
 const { EventType } = require("../../../constants");
 
+/**
+ *
+ * @param {Rudder Payload} message
+ * To set app_event_name
+ * The name of any custom app event which is not accepted in the app_event_type field.
+ * This field should contain 1 to 64 Unicode characters (using UTF-8 encoding). This field is required if app_event_type is custom.
+ * https://developers.google.com/app-conversion-tracking/api/request-response-specs#conversion_tracking_request
+ *
+ */
+
 function setEventName(message) {
   const defaultEventTypes = [
     "first_open",
@@ -20,6 +30,10 @@ function setEventName(message) {
     "custom"
   ];
   const eventName = get(message, "event");
+  /**  check if the event name is one of the default event types.
+   * This field must not contain any of the values reserved for app_event_type.
+   * If a reserved event name is used, the API will return an APP_EVENT_NAME_RESERVED_VALUE error.
+   */
   const isDefaultName = defaultEventTypes.indexOf(eventName) >= 0;
   if (isDefaultName) {
     throw new Error(
@@ -28,12 +42,21 @@ function setEventName(message) {
   }
   return eventName;
 }
+/**
+ *
+ * @param {Rudder Payload} message
+ * To set app_event_type
+ * The name of the app event that occurred. This field is an enumeration, and will only accept valued predefined by google ads.
+ * https://developers.google.com/app-conversion-tracking/api/request-response-specs#conversion_tracking_request
+ *
+ */
 function setEventType(message) {
   const eventName = get(message, "event");
   switch (eventName.toLowerCase()) {
     case "application installed":
       return "first_open";
     case "order completed":
+      // Use in_app_purchase for purchases made through the native app store; use ecommerce_purchase for all other purchases.
       if (
         get(message, "properties.affiliation") === "App Store" ||
         get(message, "properties.affiliation") === "Google Store"
@@ -61,24 +84,38 @@ function responseBuilderSimple(message, category, destination) {
     payload.dev_token = devToken;
     payload.link_id = linkId;
     payload.app_event_type = setEventType(message);
+    // This field is required if app_event_type is custom.
     if (payload.app_event_type === "custom") {
       payload.app_event_name = setEventName(message);
     }
     payload.lat = lat ? 1 : 0;
+    /**
+     * The type of identifier stored in the rdid field
+     * For Android: advertisingid
+     * For iOS: idfa
+     */
     if (get(message, "context.device.type") === "Android") {
       payload.id_type = "advertisingid";
     } else {
       payload.id_type = "idfa";
     }
+    // if fall back to Idfa zero setting is true and no rdid is not present it will be set to default value
     if (fallbackIDFAZero && !payload.rdid) {
       payload.rdid = "00000000-0000-0000-0000-000000000000";
-    } else if (!fallbackIDFAZero && !payload.rdid) {
+    }
+    // if fall back to value is false and rdid is not present the message will not be accepted by google ads so it will be aborted by rudder.
+    else if (!fallbackIDFAZero && !payload.rdid) {
       throw new Error("No advertisingId set. Aborting message");
     }
     const response = defaultRequestConfig(message);
     response.endpoint = `${baseEndpoint}/${version}`;
     response.method = defaultPostRequestConfig.requestMethod;
     response.body.JSON = get(message, "properties");
+    response.params = payload;
+    /**
+     * if the request body is empty (in cases where no rich event data is passed in the app_event_data payload),
+     * Google ads server requires that we explicitly set the Content-Length: 0 header on your request.
+     */
     if (response.body.JSON) {
       response.headers = {
         "User-Agent": get(message, "context.userAgent"),

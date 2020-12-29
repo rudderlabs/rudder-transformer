d566f29de32c00f343fc2a2617bb133c3badac16
Benedikt Deicke <benedikt@benediktdeicke.com>
2020-12-29 13:54:22 +0100

    Improves Userlist integration implementation

--- __tests__/userlist.test.js
+++ __tests__/userlist.test.js
@@ -18,9 +18,9 @@ const inputData = JSON.parse(inputDataFile);
 const expectedData = JSON.parse(outputDataFile);
 
 inputData.forEach((input, index) => {
-  it(`${name} Tests: payload: ${index}`, async () => {
+  it(`${name} Tests: payload: ${index}`, () => {
     try {
-      const output = await transformer.process(input);
+      const output = transformer.process(input);
       expect(output).toEqual(expectedData[index]);
     } catch (error) {
       expect(error.message).toEqual(expectedData[index].error);

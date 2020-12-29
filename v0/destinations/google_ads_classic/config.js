b5bac7acf8c7fa4ab92a23fe45ab6e5a601f14fb
Ruchira <ruchira@rudderlabs.com>
2020-12-29 15:51:34 +0530

    did refactoring

--- v0/destinations/google_ads_classic/config.js
+++ v0/destinations/google_ads_classic/config.js
@@ -5,10 +5,33 @@ const baseEndpoint = "https://www.googleadservices.com/pagead/conversion/app/";
 const CONFIG_CATEGORIES = {
   TRACK: { name: "GoogleAdsTrackConfig" }
 };
+
 const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
-const trackConfig = MAPPING_CONFIG[CONFIG_CATEGORIES.TRACK.name];
+
+const reservedEventNames = [
+  "first_open",
+  "session_start",
+  "in_app_purchase",
+  "view_item_list",
+  "view_item",
+  "view_search_results",
+  "add_to_cart",
+  "ecommerce_purchase",
+  "custom"
+];
+
+const mappedEventTypes = {
+  "application installed": "first_open",
+  "application opened": "session_start",
+  "product list viewed": "view_item_list",
+  "product added": "add_to_cart",
+  "products searched": "view_search_results",
+  "product viewed": "view_item"
+};
 
 module.exports = {
-  trackConfig,
-  baseEndpoint
+  trackConfig: MAPPING_CONFIG[CONFIG_CATEGORIES.TRACK.name],
+  baseEndpoint,
+  reservedEventNames,
+  mappedEventTypes
 };
--- v0/destinations/google_ads_classic/transform.js
+++ v0/destinations/google_ads_classic/transform.js
@@ -1,5 +1,10 @@
 const get = require("get-value");
-const { trackConfig, baseEndpoint } = require("./config");
+const {
+  trackConfig,
+  baseEndpoint,
+  reservedEventNames,
+  mappedEventTypes
+} = require("./config");
 const {
   constructPayload,
   defaultPostRequestConfig,
@@ -11,53 +16,22 @@ const { EventType } = require("../../../constants");
 /**
  *
  * @param {Rudder Payload} message
- * To set app_event_name
- * The name of any custom app event which is not accepted in the app_event_type field.
- * This field should contain 1 to 64 Unicode characters (using UTF-8 encoding). This field is required if app_event_type is custom.
- * https://developers.google.com/app-conversion-tracking/api/request-response-specs#conversion_tracking_request
- *
- */
-
-function setEventName(message) {
-  const reservedEventNames = [
-    "first_open",
-    "session_start",
-    "in_app_purchase",
-    "view_item_list",
-    "view_item",
-    "view_search_results",
-    "add_to_cart",
-    "ecommerce_purchase",
-    "custom"
-  ];
-  const eventName = get(message, "event");
-  /**  check if the event name is one of the default event types.
-   * This field must not contain any of the values reserved for app_event_type.
-   * If a reserved event name is used, the API will return an APP_EVENT_NAME_RESERVED_VALUE error.
-   */
-
-  if (reservedEventNames.indexOf(eventName) >= 0) {
-    throw new Error(
-      "Event name is a reserved event name in Google Ads. Aborting Message"
-    );
-  }
-  return eventName;
-}
-/**
- *
- * @param {Rudder Payload} message
  * To set app_event_type
  * The name of the app event that occurred. This field is an enumeration, and will only accept valued predefined by google ads.
  * https://developers.google.com/app-conversion-tracking/api/request-response-specs#conversion_tracking_request
  *
  */
-function setEventType(message) {
+const setEventType = message => {
   const eventName = get(message, "event");
+  let eventType;
+  let eventValue;
+  Object.keys(mappedEventTypes).forEach(mappedEventType => {
+    if (mappedEventType === eventName) {
+      eventType = mappedEventType;
+      eventValue = mappedEventTypes[mappedEventType];
+    }
+  });
   switch (eventName.toLowerCase()) {
-    case "application installed":
-      return "first_open";
-    case "application opened":
-      return "session_start";
     case "order completed":
       // Use in_app_purchase for purchases made through the native app store; use ecommerce_purchase for all other purchases.
       if (
@@ -67,45 +41,38 @@ function setEventType(message) {
         return "in_app_purchase";
       }
       return "ecommerce_purchase";
-    case "product list viewed":
-      return "view_item_list";
-    case "product added":
-      return "add_to_cart";
-    case "poducts searched":
-      return "view_search_results";
-    case "product viewed":
-      return "view_item";
+    case eventType:
+      return eventValue;
     default:
       return "custom";
   }
-}
-function responseBuilderSimple(message, category, destination) {
+};
+
+const responseBuilderSimple = (message, destination) => {
   const {
     devToken,
     linkId,
     version,
-    lat,
+    limitAdTracking,
     fallbackIDFAZero
   } = destination.Config;
   // if fall back to value is false and rdid is not present the message will not be accepted by google ads so it will be aborted by rudder.
   if (!fallbackIDFAZero && !get(message, "context.device.advertisingId")) {
     throw new Error("No advertisingId set. Aborting message");
   }
-  const payload = constructPayload(message, category);
+  const payload = constructPayload(message, trackConfig);
   if (payload) {
     payload.dev_token = devToken;
     payload.link_id = linkId;
     payload.app_event_type = setEventType(message);
-    // This field is required if app_event_type is custom.
-    if (payload.app_event_type === "custom") {
-      payload.app_event_name = setEventName(message);
-    }
+
     /**
      * Limit-ad-tracking status for the device.
      * 0: User has not chosen to limit ad tracking.
      * 1: User has chosen to limit ad tracking.
      */
-    payload.lat = lat ? 1 : 0;
+    payload.lat = limitAdTracking ? 1 : 0;
+
     /**
      * The type of identifier stored in the rdid field
      * For Android: advertisingid
@@ -133,7 +100,7 @@ function responseBuilderSimple(message, category, destination) {
      * if the request body is empty (in cases where no rich event data is passed in the app_event_data payload),
      * Google ads server requires that we explicitly set the Content-Length: 0 header on your request.
      */
-    const commonHeader = {
+    response.headers = {
       "User-Agent": get(message, "context.userAgent"),
       "X-Forwarded-For":
         get(message, "context.ip") || get(message, "request_ip"),
@@ -143,41 +110,43 @@ function responseBuilderSimple(message, category, destination) {
       Object.keys(response.body.JSON).length === 0 &&
       response.body.JSON.constructor === Object
     ) {
-      response.headers = {
-        ...commonHeader,
-        "Content-Length": 0
-      };
-    } else {
-      response.headers = {
-        ...commonHeader
-      };
+      response.headers["Content-Length"] = 0;
     }
     return response;
   }
   // fail-safety for developer error
   throw new Error("Payload could not be constructed");
-}
+};
 
 const processEvent = (message, destination) => {
   if (!message.type) {
     throw Error("Message Type is not present. Aborting message.");
   }
-
+  let eventName;
   const messageType = message.type.toLowerCase();
-  let category;
   switch (messageType) {
     case EventType.TRACK:
-      if (!get(message, "event")) {
+      eventName = get(message, "event");
+      if (!eventName) {
         throw new Error("Track event without event name. Aborting message.");
       }
-      category = trackConfig;
+
+      /**  check if the event name is one of the default event types.
+       * This field must not contain any of the values reserved for app_event_type.
+       * If a reserved event name is used, the API will return an APP_EVENT_NAME_RESERVED_VALUE error.
+       */
+      if (reservedEventNames.indexOf(eventName) >= 0) {
+        throw new Error(
+          "Event name is a reserved event name in Google Ads. Aborting Message"
+        );
+      }
       break;
     default:
       throw new Error("Message type not supported");
   }
 
   // build the response
-  return responseBuilderSimple(message, category, destination);
+  return responseBuilderSimple(message, destination);
 };
 
 const process = event => {

6859daddcd6bcfbcfdaa4f24c8a16182cbbb412a
Utsab Chowdhury <utsab.c97@gmail.com>
2021-02-01 23:21:03 +0530

    klaviyo cloud mode changes

--- v0/destinations/klaviyo/config.js
+++ v0/destinations/klaviyo/config.js
@@ -1,12 +1,12 @@
 const { getMappingConfig } = require("../../util");
 
 const BASE_ENDPOINT = "https://a.klaviyo.com";
+const LIST_SUBSCRIBE_URL = "/api/v2/list/###/members";
 
 const CONFIG_CATEGORIES = {
   IDENTIFY: { name: "KlaviyoIdentify", apiUrl: "/api/identify" },
-  PAGE: { name: "KlaviyoPage" },
-  SCREEN: { name: "KlaviyoScreen" },
-  TRACK: { name: "KlaviyoTrack" }
+  SCREEN: { name: "KlaviyoScreen", apiUrl: "/api/track" },
+  TRACK: { name: "KlaviyoTrack", apiUrl: "/api/track" }
 };
 
 const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
@@ -14,5 +14,6 @@ const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
 module.exports = {
   BASE_ENDPOINT,
   CONFIG_CATEGORIES,
-  MAPPING_CONFIG
+  MAPPING_CONFIG,
+  LIST_SUBSCRIBE_URL
 };
--- v0/destinations/klaviyo/transform.js
+++ v0/destinations/klaviyo/transform.js
@@ -1,45 +1,102 @@
+/* eslint-disable  array-callback-return */
+const get = require("get-value");
+const axios = require("axios");
+const logger = require("../../../logger");
 const { EventType } = require("../../../constants");
 const {
   CONFIG_CATEGORIES,
   BASE_ENDPOINT,
-  MAPPING_CONFIG
+  MAPPING_CONFIG,
+  LIST_SUBSCRIBE_URL
 } = require("./config");
 const {
   defaultRequestConfig,
   constructPayload,
-  defaultPostRequestConfig,
-  defaultGetRequestConfig
+  getFieldValueFromMessage,
+  defaultGetRequestConfig,
+  extractCustomFields,
+  removeUndefinedValues
 } = require("../../util");
 
-// const responseBuilderSimple = (payload, category, destination) => {
-//   if (payload) {
-//     const responseBody = { ...payload, apiKey: destination.Config.apiKey };
-//     const response = defaultRequestConfig();
-//     response.endpoint = category.endPoint;
-//     response.method = defaultPostRequestConfig.requestMethod;
-//     response.headers = {
-//       "Content-Type": "application/json"
-//     };
-//     response.body.JSON = responseBody;
-//     return response;
-//   }
-//   // fail-safety for developer error
-//   throw new Error("Payload could not be constructed");
-// };
+const identifyRequestHandler = async (message, category, destination) => {
+  const subscribeUrl = `${BASE_ENDPOINT}${LIST_SUBSCRIBE_URL}`.replace(
+    "###",
+    get(message.properties, "listId")
+  );
+  let profile = {
+    email: getFieldValueFromMessage(message, "email"),
+    phone_number: getFieldValueFromMessage(message, "phone")
+  };
+  profile = removeUndefinedValues(profile);
+  try {
+    const res = await axios.post(
+      subscribeUrl,
+      {
+        api_key: destination.Config.privateApiKey,
+        profiles: [profile]
+      },
+      {
+        headers: {
+          "Content-Type": "application/json"
+        }
+      }
+    );
+    if (res.status !== 200) logger.error("Unable to Subscribe User to List");
+  } catch (err) {
+    logger.error(err);
+  }
 
-const identifyRequestHandler = (message, category, destination) => {
+  let propertyPayload = constructPayload(
+    message,
+    MAPPING_CONFIG[category.name]
+  );
+  propertyPayload = extractCustomFields(
+    message,
+    propertyPayload,
+    ["traits", "context.traits"],
+    [
+      "email",
+      "firstName",
+      "lastName",
+      "phone",
+      "title",
+      "organization",
+      "city",
+      "region",
+      "country",
+      "zip",
+      "image",
+      "timezone",
+      "anonymousId",
+      "userId"
+    ]
+  );
   const payload = {
-    token: destination.Config.pubApiKey,
-    properties: constructPayload(message, MAPPING_CONFIG[category.name])
+    token: destination.Config.publicApiKey,
+    properties: propertyPayload
   };
   const encodedData = Buffer.from(JSON.stringify(payload)).toString("base64");
   const response = defaultRequestConfig();
-  response.endpoint = `${BASE_ENDPOINT}${category.endPoint}?data=${encodedData}`;
+  response.endpoint = `${BASE_ENDPOINT}${category.apiUrl}?data=${encodedData}`;
   response.method = defaultGetRequestConfig.requestMethod;
   return response;
 };
 
-const processEvent = (message, destination) => {
+const trackRequestHandler = (message, category, destination) => {
+  const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
+  payload.token = destination.Config.publicApiKey;
+  payload.customer_properties = {
+    $email: getFieldValueFromMessage(message, "email"),
+    $phone_number: getFieldValueFromMessage(message, "phone")
+  };
+  const encodedData = Buffer.from(JSON.stringify(payload)).toString("base64");
+  const response = defaultRequestConfig();
+  response.endpoint = `${BASE_ENDPOINT}${category.apiUrl}?data=${encodedData}`;
+  response.method = defaultGetRequestConfig.requestMethod;
+  return response;
+};
+
+const processEvent = async (message, destination) => {
   if (!message.type) {
     throw Error("Message Type is not present. Aborting message.");
   }
@@ -50,16 +107,12 @@ const processEvent = (message, destination) => {
   switch (messageType) {
     case EventType.IDENTIFY:
       category = CONFIG_CATEGORIES.IDENTIFY;
-      response = identifyRequestHandler(message, category, destination);
-      break;
-    case EventType.PAGE:
-      category = CONFIG_CATEGORIES.PAGE;
+      response = await identifyRequestHandler(message, category, destination);
       break;
     case EventType.SCREEN:
-      category = CONFIG_CATEGORIES.SCREEN;
-      break;
     case EventType.TRACK:
       category = CONFIG_CATEGORIES.TRACK;
+      response = trackRequestHandler(message, category, destination);
       break;
     default:
       throw new Error("Message type not supported");
@@ -67,8 +120,9 @@ const processEvent = (message, destination) => {
   return response;
 };
 
-const process = event => {
-  return processEvent(event.message, event.destination);
+const process = async event => {
+  const result = await processEvent(event.message, event.destination);
+  return result;
 };
 
 exports.process = process;

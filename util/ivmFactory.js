31213c0bea52023fa923ed099b6677f134f7f9cd
srikanth <srikanth.iit@gmail.com>
2021-03-01 12:22:38 +0530

    Add missing await

--- util/ivmFactory.js
+++ util/ivmFactory.js
@@ -3,7 +3,7 @@ const fetch = require("node-fetch");
 const _ = require("lodash");
 
 const stats = require("./stats");
-const {  getLibraryCodeV1 } = require("./customTransforrmationsStore-v1");
+const { getLibraryCodeV1 } = require("./customTransforrmationsStore-v1");
 
 const isolateVmMem = 8;
 async function loadModule(isolateInternal, contextInternal, moduleCode) {
@@ -55,8 +55,8 @@ async function createIvm(code, libraryVersionIds) {
           outputEvents = await Promise.all(eventMessages.map(async ev => {
             try{
               const currMsgId = ev.messageId;
-              let transformedEvent = transformEvent(ev, metadata);
-              return {transformedEvent, metadata: eventsMetadata[currMsgId]};
+              let transformedEvent = await transformEvent(ev, metadata);
+              return {transformedEvent, metadata: eventsMetadata[currMsgId] || {}};
             } catch (error) {
               // Handling the errors in versionedRouter.js
               return {error: error.toString(), metadata: metadata(ev)};

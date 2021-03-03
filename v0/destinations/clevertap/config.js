664b7ed7492b0af828eeb7311693f810118566b1
Utsab Chowdhury <utsab.c97@gmail.com>
2021-03-03 20:16:16 +0530

    clever-tap cloudmode fixes and code cleanups added TCs

--- v0/destinations/clevertap/config.js
+++ v0/destinations/clevertap/config.js
@@ -3,15 +3,46 @@ const { getMappingConfig } = require("../../util");
 const BASE_ENDPOINT = "https://api.clevertap.com/1/upload";
 
 const CONFIG_CATEGORIES = {
-  IDENTIFY: { name: "CleverTapIdentify" },
-  PAGESCREEN: { name: "CleverTapPageScreen" },
-  TRACK: { name: "CleverTapTrack" }
+  IDENTIFY: { name: "CleverTapIdentify", type: "identify" },
+  PAGE: { name: "CleverTapPage", type: "page" },
+  SCREEN: { name: "CleverTapScreen", type: "screen" },
+  TRACK: { name: "CleverTapTrack", type: "track" }
 };
+
+const CLEVERTAP_DEFAULT_EXCLUSION = [
+  "email",
+  "name",
+  "phone",
+  "employed",
+  "gender",
+  "education",
+  "graduate",
+  "birthday",
+  "married",
+  "customerType",
+  "anonymousId",
+  "userId",
+  "id",
+  "msg_sms",
+  "msgSMS",
+  "msgSms",
+  "msgsms",
+  "msg_email",
+  "msgEmail",
+  "msgemail",
+  "msg_push",
+  "msgPush",
+  "msgpush",
+  "msg_whatsapp",
+  "msgwhatsapp",
+  "msgWhatsapp"
+];
 
 const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
 
 module.exports = {
   BASE_ENDPOINT,
+  CLEVERTAP_DEFAULT_EXCLUSION,
   CONFIG_CATEGORIES,
   MAPPING_CONFIG
 };
--- v0/destinations/clevertap/transform.js
+++ v0/destinations/clevertap/transform.js
@@ -1,6 +1,11 @@
 const get = require("get-value");
 const { EventType } = require("../../../constants");
-const { CONFIG_CATEGORIES, MAPPING_CONFIG } = require("./config");
+const {
+  BASE_ENDPOINT,
+  CONFIG_CATEGORIES,
+  MAPPING_CONFIG,
+  CLEVERTAP_DEFAULT_EXCLUSION
+} = require("./config");
 
 const {
   defaultRequestConfig,
@@ -8,101 +13,55 @@ const {
   extractCustomFields,
   constructPayload,
   defaultPostRequestConfig,
-  removeUndefinedValues
+  removeUndefinedAndNullValues
 } = require("../../util");
 
 const responseBuilderSimple = (message, category, destination) => {
-  const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
+  let payload;
+  if (category.type === "identify") {
+    let profile = constructPayload(message, MAPPING_CONFIG[category.name]);
+    // Extract other K-V property from traits about user custom properties
+    profile = extractCustomFields(
+      message,
+      profile,
+      ["traits", "context.traits"],
+      CLEVERTAP_DEFAULT_EXCLUSION
+    );
+    removeUndefinedAndNullValues(profile);
+    payload = {
+      d: [
+        {
+          identity: getFieldValueFromMessage(message, "userId"),
+          type: "profile",
+          profileData: profile
+        }
+      ]
+    };
+  } else {
+    const eventData = constructPayload(message, MAPPING_CONFIG[category.name]);
+    removeUndefinedAndNullValues(eventData);
+    eventData.type = "event";
+    payload = {
+      d: [eventData]
+    };
+  }
+
   if (payload) {
-    const responseBody = { ...payload, apiKey: destination.Config.apiKey };
     const response = defaultRequestConfig();
-    response.endpoint = category.endPoint;
+    response.endpoint = BASE_ENDPOINT;
     response.method = defaultPostRequestConfig.requestMethod;
     response.headers = {
-      "Content-Type": "application/json",
-      "Api-Token": destination.Config.apiKey
+      "X-CleverTap-Account-Id": destination.Config.accountId,
+      "X-CleverTap-Passcode": destination.Config.passcode,
+      "Content-Type": "application/json"
     };
-    response.userId = getFieldValueFromMessage(message, "userId");
-    response.body.JSON = responseBody;
+    response.body.JSON = payload;
     return response;
   }
   // fail-safety for developer error
   throw new Error("Payload could not be constructed");
 };
 
-const identifyEventHandler = (message, category) => {
-  let profile = constructPayload(message, MAPPING_CONFIG[category.name]);
-
-  // Extract other K-V property from traits about user custom properties
-  profile = extractCustomFields(
-    message,
-    profile,
-    ["traits", "context.traits"],
-    [
-      "email",
-      "name",
-      "phone",
-      "employed",
-      "gender",
-      "education",
-      "graduate",
-      "married",
-      "msg_sms",
-      "customer_type",
-      "anonymousId",
-      "userId",
-      "properties"
-    ]
-  );
-
-  let data = {
-    objectId: get(message.traits, "objectId")
-      ? get(message.traits, "objectId")
-      : get(message.context.traits, "objectId"),
-    FBID: get(message.traits, "fbId")
-      ? get(message.traits, "fbId")
-      : get(message.context.traits, "fbId"),
-    identity: get(message.traits, "identity")
-      ? get(message.traits, "identity")
-      : get(message.context.traits, "identity"),
-    GPID: get(message.traits, "gpId")
-      ? get(message.traits, "gpId")
-      : get(message.context.traits, "gpId"),
-    type: "profile",
-    profileData: profile
-  };
-
-  data = removeUndefinedValues(data);
-
-  const response = {
-    d: [data]
-  };
-  return response;
-};
-
-const trackEventHandler = (message, category) => {
-  let evData = {};
-  evData = extractCustomFields(
-    message,
-    evData,
-    ["traits", "context.traits", "properties"],
-    [
-      "identity",
-      "fbId",
-      "gpId",
-      "objectId",
-      "anonymousId",
-      "userId",
-      "properties"
-    ]
-  );
-  const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
-  payload.evtData = evData;
-  const response = {
-    d: [removeUndefinedValues(payload)]
-  };
-  return response;
-};
 const processEvent = (message, destination) => {
   if (!message.type) {
     throw Error("Message Type is not present. Aborting message.");
@@ -110,22 +69,23 @@ const processEvent = (message, destination) => {
   const messageType = message.type.toLowerCase();
 
   let category;
-  let payload;
   switch (messageType) {
     case EventType.IDENTIFY:
-      payload = identifyEventHandler(message, CONFIG_CATEGORIES.IDENTIFY);
+      category = CONFIG_CATEGORIES.IDENTIFY;
       break;
     case EventType.PAGE:
+      category = CONFIG_CATEGORIES.PAGE;
+      break;
     case EventType.SCREEN:
-      payload = trackEventHandler(message, CONFIG_CATEGORIES.PAGESCREEN);
+      category = CONFIG_CATEGORIES.SCREEN;
       break;
     case EventType.TRACK:
-      payload = trackEventHandler(message, CONFIG_CATEGORIES.TRACK);
+      category = CONFIG_CATEGORIES.TRACK;
       break;
     default:
       throw new Error("Message type not supported");
   }
-  return responseBuilderSimple(payload, category, destination);
+  return responseBuilderSimple(message, category, destination);
 };
 
 const process = event => {

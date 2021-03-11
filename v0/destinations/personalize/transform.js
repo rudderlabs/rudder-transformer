8f02da176a5bc7ad879bf34520a19fd87bed917f
shrouti <shrouti.gangopadhyay@gmail.com>
2021-03-11 16:28:22 +0530

    PutEvents_values_sent_both_insideAndOutside_property

--- v0/destinations/personalize/transform.js
+++ v0/destinations/personalize/transform.js
@@ -1,16 +1,10 @@
-const { EventType } = require("../../../constants");
-const { getFieldValueFromMessage } = require("../../util");
-
 async function process(event) {
-  let payload = {};
+  const payload = {};
   const noOfFields = event.destination.Config.customMappings.length;
-  const keyField = []; // schema field
-  const mappedField = []; // payload field
-  let traits = {};
-  let context = {};
+  const keyField = [];
+  const mappedField = [];
   const property = {};
   const { message } = event;
-  const messageType = message.type.toLowerCase();
 
   for (let i = 0; i < noOfFields; i += 1) {
     keyField.push(event.destination.Config.customMappings[i].from);
@@ -22,79 +16,64 @@ async function process(event) {
   if (message.event === event.destination.Config.eventName) {
     for (let k = 0; k < noOfFields; k += 1) {
       if (
-        keyField[k].toUpperCase() === "USER_ID" ||
-        keyField[k].toUpperCase() === "EVENT_TYPE" ||
-        keyField[k].toUpperCase() === "TIMESTAMP"
+        !(
+          keyField[k].toUpperCase() === "USER_ID" ||
+          keyField[k].toUpperCase() === "EVENT_TYPE" ||
+          keyField[k].toUpperCase() === "TIMESTAMP"
+        )
       ) {
-        keyField[k] = keyField[k].replace(/_([a-z])/g, function (g) {
-          return g[1].toUpperCase();
-        });
-      } else {
         const mappedFields = mappedField[k];
-        switch (messageType) {
-          case EventType.TRACK:
-          case EventType.PAGE:
-          case EventType.SCREEN:
-            keyField[k] = keyField[k]
-              .toLowerCase()
-              .replace(/_([a-z])/g, function (g) {
-                return g[1].toUpperCase();
-              });
-            if (message.properties && message.properties[mappedFields]) {
-              property[keyField[k]] = message.properties[mappedFields];
-            } else if (typeof message[mappedFields] !== "undefined") {
-              property[keyField[k]] = message[mappedFields];
-            } else {
-              throw new Error(`Mapped Field ${mappedFields} not found`);
-            }
-            break;
-          default:
-            traits = getFieldValueFromMessage(message, "traits");
-            keyField[k] = keyField[k]
-              .toLowerCase()
-              .replace(/_([a-z])/g, function (g) {
-                return g[1].toUpperCase();
-              });
-            if (traits[mappedFields]) {
-              property[keyField[k]] = traits[mappedFields];
-            } else if (
-              // if traits present in both roots and context
-              message.traits &&
-              message.context &&
-              message.context.traits
-            ) {
-              // retrieve traits inside context
-              context = message.context;
-              if (context.traits[mappedFields]) {
-                property[keyField[k]] = context.traits[mappedFields];
-              } else if (typeof event.message[mappedFields] !== "undefined") {
-                // if field is not present both in messsage.traits and message.context.traits we have search in the root
-                property[keyField[k]] = event.message[mappedFields];
-              }
-            } else if (typeof event.message[mappedFields] !== "undefined") {
-              // if no trait is present and we have search in the root
-              property[keyField[k]] = event.message[mappedFields];
-            } else {
-              throw new Error(`Mapped Field ${mappedFields} not found`);
-            }
-            break;
+
+        keyField[k] = keyField[k]
+          .toLowerCase()
+          .replace(/_([a-z])/g, function (g) {
+            return g[1].toUpperCase();
+          });
+        if (message.properties && message.properties[mappedFields]) {
+          property[keyField[k]] = message.properties[mappedFields];
+        } else if (typeof message[mappedFields] !== "undefined") {
+          property[keyField[k]] = message[mappedFields];
+        } else {
+          throw new Error(`Mapped Field ${mappedFields} not found`);
         }
       }
     }
+    const eventList = [];
+    const eventObj = {};
 
-    payload = {
-      eventList: [
-        {
-          eventId: event.message.messageId,
-          eventType: event.destination.Config.eventName,
-          properties: property,
-          sentAt: event.message.sentAt
-        }
-      ],
-      sessionId: event.message.originalTimestamp,
-      trackingId: event.destination.Config.trackingId,
-      userId: event.message.userId
-    };
+    if (property.eventValue) {
+      eventObj.eventValue = property.eventValue;
+      delete property.eventValue;
+    }
+    if (property.impression) {
+      eventObj.impression = property.impression;
+      delete property.impression;
+    }
+    if (property.itemId) {
+      eventObj.itemId = property.itemId;
+      delete property.itemId;
+    }
+    if (property.recommendationId) {
+      eventObj.recommendationId = property.recommendationId;
+      delete property.recommendationId;
+    }
+    let eventId;
+    if (property.eventId) {
+      eventId = property.eventId;
+      delete property.eventId;
+    } else {
+      eventId = event.message.messageId;
+    }
+
+    eventObj.eventId = eventId;
+    eventObj.eventType = event.destination.Config.eventName;
+    eventObj.sentAt = event.message.sentAt;
+    eventObj.properties = property;
+    payload.sessionId = event.message.originalTimestamp;
+    payload.trackingId = event.destination.Config.trackingId;
+    payload.userId = event.message.userId;
+    eventList.push(eventObj);
+    payload.eventList = eventList;
   }
   return payload;
 }

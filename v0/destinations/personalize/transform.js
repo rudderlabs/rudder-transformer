8f2cd34880598e51ddfd66291481939cc5baa3a0
shrouti <shrouti.gangopadhyay@gmail.com>
2021-03-12 18:09:32 +0530

    code_reformatted

--- v0/destinations/personalize/transform.js
+++ v0/destinations/personalize/transform.js
@@ -1,26 +1,31 @@
 const { getHashFromArray } = require("../../util");
 
 function getDeliveryStreamMapTo(event) {
-  const { message } = event;
-  const { eventName } = event.destination.Config;
+  const { message, destination } = event;
+  const { eventName } = destination.Config;
   const hashMap = getHashFromArray(eventName, "from", "to");
   return hashMap[message.event.toLowerCase()];
 }
 
 async function process(event) {
+  const { message, destination } = event;
+  const { Config } = destination;
+  const { properties, sentAt, anonymousId, userId } = message;
   const payload = {};
-  const noOfFields = event.destination.Config.customMappings.length;
-  const keyField = [];
-  const mappedField = [];
+  const noOfFields = Config.customMappings.length;
+  const keyField = []; // schema field
+  const mappedField = []; // payload fields
   const property = {};
-  const { message } = event;
+  const eventObj = {};
+  const eventList = [];
+  let itemId;
 
   for (let i = 0; i < noOfFields; i += 1) {
-    keyField.push(event.destination.Config.customMappings[i].from);
+    keyField.push(Config.customMappings[i].from);
   }
 
   for (let j = 0; j < noOfFields; j += 1) {
-    mappedField.push(event.destination.Config.customMappings[j].to);
+    mappedField.push(Config.customMappings[j].to);
   }
   const deliveryStreamMapTo = getDeliveryStreamMapTo(event);
   if (deliveryStreamMapTo) {
@@ -29,62 +34,57 @@ async function process(event) {
         !(
           keyField[k].toUpperCase() === "USER_ID" ||
           keyField[k].toUpperCase() === "EVENT_TYPE" ||
-          keyField[k].toUpperCase() === "TIMESTAMP"
+          keyField[k].toUpperCase() === "TIMESTAMP" ||
+          keyField[k].toUpperCase() === "ITEM_ID" ||
+          keyField[k].toUpperCase() === "EVENT_VALUE" ||
+          keyField[k].toUpperCase() === "IMPRESSION" ||
+          keyField[k].toUpperCase() === "RECOMMENDATION_ID" ||
+          keyField[k].toUpperCase() === "EVENT_ID"
         )
       ) {
         const mappedFields = mappedField[k];
-
         keyField[k] = keyField[k]
           .toLowerCase()
-          .replace(/_([a-z])/g, function (g) {
+          .replace(/_([a-z])/g, function camelCase(g) {
             return g[1].toUpperCase();
           });
-        if (message.properties && message.properties[mappedFields]) {
-          property[keyField[k]] = message.properties[mappedFields];
+        if (properties && properties[mappedFields]) {
+          property[keyField[k]] = properties[mappedFields];
         } else if (typeof message[mappedFields] !== "undefined") {
           property[keyField[k]] = message[mappedFields];
         } else {
           throw new Error(`Mapped Field ${mappedFields} not found`);
         }
+      } else if (
+        !(
+          keyField[k].toUpperCase() === "USER_ID" ||
+          keyField[k].toUpperCase() === "EVENT_TYPE"
+        )
+      ) {
+        keyField[k] = keyField[k]
+          .toLowerCase()
+          .replace(/_([a-z])/g, function camelCase(g) {
+            return g[1].toUpperCase();
+          });
+        const mappedFields = mappedField[k];
+        eventObj[keyField[k]] = properties[mappedFields];
       }
     }
-    const eventList = [];
-    const eventObj = {};
-
-    if (property.eventValue) {
-      eventObj.eventValue = property.eventValue;
-      delete property.eventValue;
-    }
-    if (property.impression) {
-      eventObj.impression = property.impression;
-      delete property.impression;
-    }
-    if (property.itemId) {
-      eventObj.itemId = property.itemId;
-      delete property.itemId;
-    }
-    if (property.recommendationId) {
-      eventObj.recommendationId = property.recommendationId;
-      delete property.recommendationId;
-    }
-    let eventId;
-    if (property.eventId) {
-      eventId = property.eventId;
-      delete property.eventId;
-    } else {
-      eventId = event.message.messageId;
+    if (!property.itemId) {
+      itemId = message.messageId;
     }
 
-    eventObj.eventId = eventId;
+    eventObj.itemId = itemId;
     eventObj.eventType = deliveryStreamMapTo;
-    eventObj.sentAt = event.message.sentAt;
+    eventObj.sentAt = sentAt;
     eventObj.properties = property;
-    payload.sessionId = event.message.originalTimestamp;
-    payload.trackingId = event.destination.Config.trackingId;
-    payload.userId = event.message.userId;
+    payload.sessionId = anonymousId;
+    payload.trackingId = Config.trackingId;
+    payload.userId = userId;
     eventList.push(eventObj);
     payload.eventList = eventList;
   } else throw new Error("Event type not set for this event");
+
   return payload;
 }
 

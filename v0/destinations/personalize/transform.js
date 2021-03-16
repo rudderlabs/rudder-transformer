5d619e2b61a205233075ceab942c9f82034bda12
shrouti <shrouti.gangopadhyay@gmail.com>
2021-03-16 18:53:06 +0530

    transformer code optimized and test case edited

--- v0/destinations/personalize/transform.js
+++ v0/destinations/personalize/transform.js
@@ -1,58 +1,49 @@
 const { getHashFromArray, getFieldValueFromMessage } = require("../../util");
 
-function mapEventName(event) {
-  const { message, destination } = event;
-  const { eventName } = destination.Config;
-  const hashMap = getHashFromArray(eventName, "from", "to");
-  return hashMap[message.event.toLowerCase()];
-}
-
 async function process(event) {
   const { message, destination } = event;
   const { Config } = destination;
-  const { properties, sentAt, anonymousId, userId } = message;
+  const { eventName } = Config;
+  const { properties, anonymousId, userId } = message;
   const payload = {};
   const noOfFields = Config.customMappings.length;
   const keyField = []; // schema field
-  const mappedField = []; // payload fields
+  const mappedFields = []; // payload fields
   const property = {};
   const eventObj = {};
   const eventList = [];
+  const keyCheckList = [
+    "USER_ID",
+    "EVENT_TYPE",
+    "TIMESTAMP",
+    "ITEM_ID",
+    "EVENT_VALUE",
+    "IMPRESSION",
+    "RECOMMENDATION_ID",
+    "EVENT_ID"
+  ];
 
   if (message.event) {
     for (let i = 0; i < noOfFields; i += 1) {
       keyField.push(Config.customMappings[i].from);
+      mappedFields.push(Config.customMappings[i].to);
     }
-
-    for (let j = 0; j < noOfFields; j += 1) {
-      mappedField.push(Config.customMappings[j].to);
-    }
-    const mappedEvent = mapEventName(event);
-    if (mappedEvent) {
+    if (eventName === message.event) {
       for (let k = 0; k < noOfFields; k += 1) {
         if (
           // except these every thing will go inside properties
-          !(
-            keyField[k].toUpperCase() === "USER_ID" ||
-            keyField[k].toUpperCase() === "EVENT_TYPE" ||
-            keyField[k].toUpperCase() === "TIMESTAMP" ||
-            keyField[k].toUpperCase() === "ITEM_ID" ||
-            keyField[k].toUpperCase() === "EVENT_VALUE" ||
-            keyField[k].toUpperCase() === "IMPRESSION" ||
-            keyField[k].toUpperCase() === "RECOMMENDATION_ID" ||
-            keyField[k].toUpperCase() === "EVENT_ID"
-          )
+          !keyCheckList.includes(keyField[k].toUpperCase())
         ) {
-          const mappedFields = mappedField[k];
+          const mappedField = mappedFields[k];
           keyField[k] = keyField[k]
             .toLowerCase()
             .replace(/_([a-z])/g, function camelCase(g) {
               return g[1].toUpperCase();
             });
-          if (properties && properties[mappedFields]) {
-            property[keyField[k]] = properties[mappedFields];
+          if (properties && properties[mappedField]) {
+            property[keyField[k]] = properties[mappedField];
           } else {
-            throw new Error(`Mapped Field ${mappedFields} not found`);
+            throw new Error(`Mapped Field ${mappedField} not found`);
           }
         } else if (
           // userId and eventType is handled later as shown below and others are saved outside properties
@@ -66,8 +57,8 @@ async function process(event) {
             .replace(/_([a-z])/g, function camelCase(g) {
               return g[1].toUpperCase();
             });
-          const mappedFields = mappedField[k];
-          eventObj[keyField[k]] = properties[mappedFields];
+          const mappedField = mappedFields[k];
+          eventObj[keyField[k]] = properties[mappedField];
         }
       }
       // itemId is a mandatory field, so even if user doesn't mention, it is needed to be provided
@@ -87,7 +78,7 @@ async function process(event) {
         payload.userId = userId;
       }
 
-      eventObj.eventType = mappedEvent;
+      eventObj.eventType = message.event;
       eventObj.sentAt = getFieldValueFromMessage(
         message,
         "historicalTimestamp"
@@ -99,7 +90,7 @@ async function process(event) {
       eventObj.itemId = eventObj.itemId ? eventObj.itemId : message.messageId;
       eventList.push(eventObj);
       payload.eventList = eventList;
-    } else throw new Error("Event type not set for this event");
+    } else throw new Error("Event type does not match with your config");
   } else {
     throw new Error(" Cannot process if no event name specified");
   }

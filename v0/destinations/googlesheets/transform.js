baf6ad43e3278dca9c24ac0d6234ee8962d66a84
Utsab Chowdhury <utsab.c97@gmail.com>
2021-04-08 14:14:49 +0530

    google sheets transformer onboarding

--- v0/destinations/googlesheets/transform.js
+++ v0/destinations/googlesheets/transform.js
@@ -1,9 +1,18 @@
-const { getHashFromArray } = require("../../util");
+const {
+  getHashFromArray,
+  getMappingConfig,
+  constructPayload
+} = require("../../util");
 
-const getSpreadSheetId = event => {
+const CONFIG_CATEGORIES = {
+  SHEETS: { name: "GoogleSheetsMapping" }
+};
+const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
+
+const getTabName = event => {
   const { message } = event;
-  const { eventToSpreadSheetIdMap } = event.destination.Config;
-  const hashMap = getHashFromArray(eventToSpreadSheetIdMap, "from", "to");
+  const { eventToTabMap } = event.destination.Config;
+  const hashMap = getHashFromArray(eventToTabMap, "from", "to");
 
   return (
     (message.event ? hashMap[message.event.toLowerCase()] : null) ||
@@ -13,13 +22,17 @@ const getSpreadSheetId = event => {
 };
 
 const process = event => {
-  const spreadSheetId = getSpreadSheetId(event);
-  if (spreadSheetId) {
-    return {
-      message: event.message,
-      spreadSheetId
-    };
+  const { message, destination } = event;
+  const tabName = getTabName(event);
+  if (tabName) {
+    const payload = constructPayload(
+      message,
+      MAPPING_CONFIG.GoogleSheetsMapping
+    );
+    payload.spreadSheetId = destination.Config.sheetId;
+    payload.spreadSheetTab = tabName;
+    return payload;
   }
-  throw new Error("No Spread Sheet Id set for this event");
+  throw new Error("No Spread Sheet Tab set for this event");
 };
 exports.process = process;

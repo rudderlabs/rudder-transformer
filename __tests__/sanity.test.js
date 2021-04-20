06ed25f9f9346d9168f651eb927281cabeb12657
Utsab Chowdhury <utsab.c97@gmail.com>
2021-04-20 17:27:42 +0530

    updates

---
+++ __tests__/sanity.test.js
@@ -0,0 +1,40 @@
+const name = "Sanity";
+const path = require("path");
+const version = "v0";
+const { getDirectories } = require('./util');
+
+const integrations = getDirectories(
+  path.resolve(__dirname, `../${version}/destinations/`)
+);
+
+const inputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/sanity/sanity_input.json`)
+);
+
+const sanityInput = JSON.parse(inputDataFile);
+const { message, config } = sanityInput;
+
+integrations.forEach(intg => {
+    const transformer = require(`../${version}/destinations/${intg}/transform`);
+    const outputDataFile = fs.readFileSync(
+        path.resolve(__dirname, `./data/sanity/${intg}_output.json`)
+    );
+    const expectedData = JSON.parse(outputDataFile);
+    
+    const event = {
+      message : message,
+      destination : config[`${intg}`]
+    }
+
+    it(`${name} - integration: ${intg}`, async () => {
+      try {
+        const output = await transformer.process(event);
+        expect(output).toEqual(expectedData);
+      } catch (error) {
+        expect(error.message).toEqual(expectedData.error);
+      }
+    })
+  
+  });
+
+console.log(output);

554eff6e8eaffac09add11b53d0276fc176bb6ef
Utsab Chowdhury <utsab.c97@gmail.com>
2021-05-06 22:01:15 +0530

    revamp of transformer to support properties

--- v0/destinations/googlesheets/transform.js
+++ v0/destinations/googlesheets/transform.js
@@ -1,36 +1,59 @@
-const { getHashFromArray, getValueFromMessage } = require("../../util");
+const get = require("get-value");
+const { getValueFromMessage } = require("../../util");
 
-const processWithCustomMapping = (message, attributeMap) => {
+const SOURCE_KEYS = ["properties", "traits", "context.traits"];
+
+const getMappingFieldValueFormMessage = (message, sourceKey, mappingKey) => {
+  let value;
+  const tempStore = getValueFromMessage(message, sourceKey);
+  if (tempStore) {
+    value = tempStore[mappingKey] || get(tempStore, mappingKey);
+  }
+  return value;
+};
+
+const processWithCustomMapping = (message, attributeKeyMapping) => {
   const responseMessage = {};
+  const fromKey = "from";
+  const toKey = "to";
   let count = 0;
-  Object.keys(attributeMap).forEach(key => {
-    const keyArr = [
-      `${key}`,
-      `properties.${key}`,
-      `traits.${key}`,
-      `context.traits.${key}`
-    ];
-    const value = getValueFromMessage(message, keyArr);
-    responseMessage[count] = {
-      attributeKey: attributeMap[key],
-      attributeValue: value || ""
-    };
-    count += 1;
-  });
+
+  if (Array.isArray(attributeKeyMapping)) {
+    attributeKeyMapping.forEach(mapping => {
+      let value;
+      value = getValueFromMessage(message, mapping[fromKey]);
+      if (!value) {
+        SOURCE_KEYS.some(sourceKey => {
+          value = getMappingFieldValueFormMessage(
+            message,
+            sourceKey,
+            mapping[fromKey]
+          );
+          if (value) {
+            return true;
+          }
+          return false;
+        });
+      }
+
+      responseMessage[count] = {
+        attributeKey: mapping[toKey],
+        attributeValue: value || ""
+      };
+      count += 1;
+    });
+  }
   return responseMessage;
 };
 // Main process Function to handle transformation
 const process = event => {
   const { message, destination } = event;
-  const eventAttributeMap = getHashFromArray(
-    destination.Config.eventKeyMap,
-    "from",
-    "to",
-    false
-  );
   if (destination.Config.sheetName) {
     const payload = {
-      message: processWithCustomMapping(message, eventAttributeMap),
+      message: processWithCustomMapping(
+        message,
+        destination.Config.eventKeyMap
+      ),
       spreadSheetId: destination.Config.sheetId,
       spreadSheet: destination.Config.sheetName
     };

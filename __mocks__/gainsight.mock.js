b6b5737a05bb1822241a41b7423b702e7ff2fe29
anuraj <anurajguha@gmail.com>
2021-05-15 14:15:54 +0530

    separate axios mock data

--- __mocks__/gainsight.mock.js
+++ __mocks__/gainsight.mock.js
@@ -1,47 +1,7 @@
-const responseData = {
-  "https://demo-domain.gainsightcloud.com/v1/data/objects/query/Company": {
-    "result": true,
-    "errorCode": null,
-    "errorDesc": null,
-    "requestId": "47d9c8be-4912-4610-806c-0eec22b73236",
-    "data": {
-      "records": []
-    },
-    "message": null
-  },
-  "https://demo-domain.gainsightcloud.com/v1/data/objects/Company": {
-    "result": true,
-    "errorCode": null,
-    "errorDesc": null,
-    "requestId": "3ce46d4a-6a83-4a92-97b3-d9788a296af8",
-    "data": {
-      "count": 1,
-      "errors": null,
-      "records": [
-        {
-          "Gsid": "1P0203VCESP7AUQMV9E953G"
-        }
-      ]
-    },
-    "message": null
-  },
-  "https://demo-domain.gainsightcloud.com/v1/data/objects/Company?keys=Name": {
-    "result": true,
-    "errorCode": null,
-    "errorDesc": null,
-    "requestId": "30630809-40a7-45d2-9673-ac2e80d06f33",
-    "data": {
-      "count": 1,
-      "errors": null,
-      "records": [
-        {
-          "Gsid": "1P0203VCESP7AUQMV9E953G"
-        }
-      ]
-    },
-    "message": null
-  }
-}
+const fs = require("fs");
+const path = require("path");
+const responseJson = fs.readFileSync(path.resolve(__dirname, "./data/gainsight/response.json"));
+const responseData = JSON.parse(responseJson);
 
 const gainsightRequestHandler = (url, payload) => {
   if(Object.keys(responseData).includes(url)) {

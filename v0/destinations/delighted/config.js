5efdec7dbaad4ee02ac8f9eeb4ec5bd27b484759
Ankit Kumar <ankit8540988674@gmail.com>
2021-07-14 17:15:39 +0530

    minor

--- v0/destinations/delighted/config.js
+++ v0/destinations/delighted/config.js
@@ -1,26 +1,28 @@
 const { getMappingConfig } = require("../../util");
-const ENDPOINT = "https://api.delighted.com/v1/people.json"; 
+
+const ENDPOINT = "https://api.delighted.com/v1/people.json";
 
 const CONFIG_CATEGORIES = {
-    IDENTIFY: { type: "identify", name: "DelightedIdentify" }
+  IDENTIFY: { type: "identify", name: "DelightedIdentify" }
 };
 const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
 
-const DELIGHTED_EXCLUSION_FIELDS=[
-    "email",
-    "name",
-    "phone",
-    "delay",
-    "send",
-    "channel",
-    "firstName",
-    "firstname",
-    "first_name",
-    "lastName",
-    "lastname",
-    "last_name"
+const DELIGHTED_EXCLUSION_FIELDS = [
+  "email",
+  "name",
+  "phone",
+  "delay",
+  "send",
+  "channel",
+  "firstName",
+  "firstname",
+  "first_name",
+  "lastName",
+  "lastname",
+  "last_name"
 ];
 module.exports = {
-    ENDPOINT,
-    identifyMapping: MAPPING_CONFIG[CONFIG_CATEGORIES.IDENTIFY.name]
+  ENDPOINT,
+  identifyMapping: MAPPING_CONFIG[CONFIG_CATEGORIES.IDENTIFY.name],
+  DELIGHTED_EXCLUSION_FIELDS
 };
--- v0/destinations/delighted/transform.js
+++ v0/destinations/delighted/transform.js
@@ -1,118 +1,107 @@
 const {
-    getFieldValueFromMessage,
-    CustomError,
-    defaultRequestConfig,
-    defaultPostRequestConfig,
-    extractCustomFields,
-    removeUndefinedAndNullValues,
-    getValueFromMessage,
-    getDestinationExternalID,
-    constructPayload,
-    getFieldValueFromMessage
+  getFieldValueFromMessage,
+  CustomError,
+  defaultRequestConfig,
+  defaultPostRequestConfig,
+  extractCustomFields,
+  removeUndefinedAndNullValues,
+  getDestinationExternalID,
+  constructPayload
 } = require("../../util");
 const {
-    ENDPOINT,
-    DELIGHTED_EXCLUSION_FIELDS,
-    identifyMapping
+  ENDPOINT,
+  DELIGHTED_EXCLUSION_FIELDS,
+  identifyMapping
 } = require("./config");
 
-function ValidateEmail(email){
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+function ValidateEmail(email) {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
 }
-function ValidatePhone(phone){
-    var phoneformat= /^\+[1-9]\d{10,14}$/;
-    return phoneformat.test(String(phone));
+function ValidatePhone(phone) {
+  const phoneformat = /^\+[1-9]\d{10,14}$/;
+  return phoneformat.test(String(phone));
 }
 
 const identifyResponseBuilder = async (message, { destination }) => {
-    const userId = getFieldValueFromMessage(message, "userIdOnly");
-    if (!userId) {
-      throw new CustomError(
-        "userId is required for identify",
-        400
-      );
-    }
-    let channel = getDestinationExternalID(message, "delightedChannelType") || destination.Config.channel;
-    channel = channel.toLowerCase();
+  const userId = getFieldValueFromMessage(message, "userIdOnly");
+  if (!userId) {
+    throw new CustomError("userId is required for identify", 400);
+  }
+  let channel =
+    getDestinationExternalID(message, "delightedChannelType") ||
+    destination.Config.channel;
+  channel = channel.toLowerCase();
 
-    //validate userId
-    if(channel == "email"){
-        if(!ValidateEmail(userId)){
-            throw new CustomError(
-                "Email format is not correct.",
-                400
-            );
-        }
-    }else if(channel === "phone"){
-        if(!ValidatePhone(userId)){
-            throw new CustomError(
-                "Phone number format must be E.164.",
-                400
-            );
-        }
+  // validate userId
+  if (channel === "email") {
+    if (!ValidateEmail(userId)) {
+      throw new CustomError("Email format is not correct.", 400);
     }
-
-    let payload = constructPayload(message, identifyMapping);
-
-    payload.send = false;
-    payload.channel = channel;
-    payload.delay = destination.Config.delay || message.context.traits.delay || 0 ;
-    //payload.last_sent_at = message.timestamp;
-
-    if(!payload.name){
-        const fName = getFieldValueFromMessage(message, "firstName" );
-        const lName = getFieldValueFromMessage(message, "lastName");
-        let name= fName.concat(lName);
-        if(name)
-            payload.name = name;
-        else
-            payload.name = userId;
+  } else if (channel === "phone") {
+    if (!ValidatePhone(userId)) {
+      throw new CustomError("Phone number format must be E.164.", 400);
     }
-    //not sure if this is how we will extract the custom properties
-    let properties = {};
-    properties = extractCustomFields(
-        message,
-        properties,
-        ["context.traits"],
-        DELIGHTED_EXCLUSION_FIELDS
-    );
-    payload = {
-        ...payload,
-        properties
-    };
-    //update/create the user
-    const response = defaultRequestConfig();
-    response.headers = {
-        "Authorization": destination.Config.apiKey,
-        "Content-Type":  "application/json"
-    }
-    response.method = defaultPostRequestConfig.requestMethd;
-    response.endpoint=`${ENDPOINT}`; // since ENDPOINT remains same
-    response.body.JSON = removeUndefinedAndNullValues(payload);
-    return response;
+  }
+
+  let payload = constructPayload(message, identifyMapping);
+
+  payload.send = false;
+  payload.channel = channel;
+  payload.delay = destination.Config.delay || message.context.traits.delay || 0;
+  // payload.last_sent_at = message.timestamp;
+
+  if (!payload.name) {
+    const fName = getFieldValueFromMessage(message, "firstName");
+    const lName = getFieldValueFromMessage(message, "lastName");
+    const name = fName.concat(lName);
+    if (name) payload.name = name;
+    else payload.name = userId;
+  }
+  // not sure if this is how we will extract the custom properties
+  let properties = {};
+  properties = extractCustomFields(
+    message,
+    properties,
+    ["context.traits"],
+    DELIGHTED_EXCLUSION_FIELDS
+  );
+  payload = {
+    ...payload,
+    properties
+  };
+  // update/create the user
+  const response = defaultRequestConfig();
+  response.headers = {
+    Authorization: destination.Config.apiKey,
+    "Content-Type": "application/json"
+  };
+  response.method = defaultPostRequestConfig.requestMethd;
+  response.endpoint = `${ENDPOINT}`; // since ENDPOINT remains same
+  response.body.JSON = removeUndefinedAndNullValues(payload);
+  return response;
 };
 
-const trackResponseBuilder = (message, {destination}) => {
-    let payload = constructPayload(message, trackMapping);
-
-}
+const trackResponseBuilder = (message, { destination }) => {
+  let payload = constructPayload(message, trackMapping);
+};
 const process = async event => {
-    const { message,destination } = event;
-    if(!messgae.type){
-        throw new CustomError(
-            "Message Type is not present. Aborting message.",
-            400
-        );
-    }
+  const { message, destination } = event;
+  if (!message.type) {
+    throw new CustomError(
+      "Message Type is not present. Aborting message.",
+      400
+    );
+  }
 
-    const { apiKey } = destination.Config;
-    if(!apiKey){
-        throw new CustomError("Inavalid API Key. Aborting message.", 400);
-    }
+  const { apiKey } = destination.Config;
+  if (!apiKey) {
+    throw new CustomError("Inavalid API Key. Aborting message.", 400);
+  }
 
-    const messageType =  message.type.toLowerCase();
+  const messageType = message.type.toLowerCase();
 
-    let reponse;
-    switch(messageType){};
+  let reponse;
+  switch (messageType) {
+  }
 };

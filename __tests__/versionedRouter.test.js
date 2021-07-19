d3e2f705b8164c1c7c34e03d93dc7b31b16c72e2
Ruchira <ruchira@rudderlabs.com>
2021-07-19 10:17:16 +0530

    added for all

--- __tests__/versionedRouter.test.js
+++ __tests__/versionedRouter.test.js
@@ -9,11 +9,13 @@ const typeArg = process.argv.filter(x => x.startsWith("--type="))[0]; // send ar
 // eg: jest versionedRouter --destName=am --type=batch
 // eg: jest versionedRouter --destName=heap --type=processor
 // eg: jest versionedRouter --destName=heap --type=router
+// eg: jest versionedRouter --type=all
 
 let destination;
-if (destArg && typeArg) {
-  destination = destArg.split("=")[1];
+if (typeArg) {
+  destination = destArg ? destArg.split("=")[1] : "heap"; // default
   type = typeArg.split("=")[1];
+  if(type !== "all"){
   const reqBody = JSON.parse(
     fs.readFileSync(
       path.resolve(
@@ -30,7 +32,7 @@ if (destArg && typeArg) {
       )
     )
   );
-
+      }
   if (type === "router") {
     it(`Testing: routerHandleDest`, async () => {
       const output = await vRouter.routerHandleDest(reqBody);
@@ -46,11 +48,73 @@ if (destArg && typeArg) {
       const output = await vRouter.batchHandler(reqBody);
       expect(output).toEqual(respBody);
     });
-  }
+  } else if (type === "all") {
+  it(`Testing: routerHandleDest`, async () => {
+    const reqBody = JSON.parse(
+        fs.readFileSync(
+          path.resolve(
+            __dirname,
+            `./data/versioned_router_${destination}_input.json`
+          )
+        )
+      );
+      const respBody = JSON.parse(
+        fs.readFileSync(
+          path.resolve(
+            __dirname,
+            `./data/versioned_router_${destination}_output.json`
+          )
+        )
+      );
+    const output = await vRouter.routerHandleDest(reqBody);
+    expect(output).toEqual(respBody);
+  });
+  it(`Testing: handleDest`, async () => {
+    const reqBody = JSON.parse(
+        fs.readFileSync(
+          path.resolve(
+            __dirname,
+            `./data/versioned_processor_${destination}_input.json`
+          )
+        )
+      );
+      const respBody = JSON.parse(
+        fs.readFileSync(
+          path.resolve(
+            __dirname,
+            `./data/versioned_processor_${destination}_output.json`
+          )
+        )
+      );
+    destination = destination || "heap"; // default
+    const output = await vRouter.handleDest(reqBody, version, destination);
+    expect(output).toEqual(respBody);
+  });
+  it(`Testing: batchHandler`, async () => {
+    const reqBody = JSON.parse(
+        fs.readFileSync(
+          path.resolve(
+            __dirname,
+            `./data/versioned_batch_braze_input.json`
+          )
+        )
+      );
+      const respBody = JSON.parse(
+        fs.readFileSync(
+          path.resolve(
+            __dirname,
+            `./data/versioned_batch_braze_output.json`
+          )
+        )
+      );
+    const output = await vRouter.batchHandler(reqBody);
+    expect(output).toEqual(respBody);
+  });
 } else {
   it(`No type and destination mentioned for testing versionedRouter`, () => {
     expect("no command line argument provided").toEqual(
       "no command line argument provided"
     );
   });
+}
 }

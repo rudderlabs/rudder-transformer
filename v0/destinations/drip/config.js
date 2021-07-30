3eef4c694e8ba9fbd4244d5b37768d592fd1e280
Ankit Kumar <ankit8540988674@gmail.com>
2021-07-30 16:14:26 +0530

    Added track and identify call,their mapping jsons, util file and config files for drip destination

---
+++ v0/destinations/drip/config.js
@@ -0,0 +1,49 @@
+const { getMappingConfig } = require("../../util");
+
+const ENDPOINT = "https://https://api.getdrip.com/v2";
+
+const CONFIG_CATEGORIES = {
+  IDENTIFY: { type: "identify", name: "DripIdentify" },
+  TRACK: { type: "track", name: "DripTrack" },
+  CAMPAIGN: { type: "campaign", name: "DripCampaign" }
+};
+
+const IDENTIFY_EXCLUSION_FIELDS = [
+  "email",
+  "firstName",
+  "firstname",
+  "first_name",
+  "lastName",
+  "lastname",
+  "last_name",
+  "name",
+  "phone",
+  "userId",
+  "anonymousId",
+  "id",
+  "status",
+  "initial_status",
+  "time_zone",
+  "country",
+  "city",
+  "zip",
+  "ip_address",
+  "euConsentMessage",
+  "euConsent",
+  "address",
+  "tags",
+  "removeTags"
+];
+
+const TRACKING_EXLCUSION_FIELDS = ["action", "email", "prospect"];
+
+const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
+
+module.exports = {
+  ENDPOINT,
+  identifyMapping: MAPPING_CONFIG[CONFIG_CATEGORIES.IDENTIFY.name],
+  trackMapping: MAPPING_CONFIG[CONFIG_CATEGORIES.TRACK.name],
+  campaignMapping: MAPPING_CONFIG[CONFIG_CATEGORIES.CAMPAIGN.name],
+  IDENTIFY_EXCLUSION_FIELDS,
+  TRACKING_EXLCUSION_FIELDS
+};

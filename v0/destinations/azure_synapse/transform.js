176c51efd95eb3162de022231ee816f0cc8ec35d
srikanth <srikanth.iit@gmail.com>
2021-08-10 21:35:29 +0530

    WH: Add missing receivedAt

--- v0/destinations/azure_synapse/transform.js
+++ v0/destinations/azure_synapse/transform.js
@@ -13,6 +13,7 @@ function process(event) {
   const whStoreEvent = event.destination.Config.storeFullEvent === true;
   const provider = azureSynapse;
   return processSingleMessage(event.message, {
+    metadata: event.metadata,
     whSchemaVersion,
     whStoreEvent,
     getDataTypeOverride,

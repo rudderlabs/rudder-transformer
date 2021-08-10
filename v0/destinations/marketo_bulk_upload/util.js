2360b095c576fdce6741abac43c4a29cde6f9399
Ruchira <ruchira@rudderlabs.com>
2021-08-10 22:11:38 +0530

    inital error handling

--- v0/destinations/marketo_bulk_upload/util.js
+++ v0/destinations/marketo_bulk_upload/util.js
@@ -1,13 +1,20 @@
 const axios = require("axios");
+const { CustomError } = require("../../util");
 
 const getAccessToken = async config => {
   const { clientId, clientSecret, munchkinId } = config;
 
-  const resp = await axios.get(
-    `https://${munchkinId}.mktorest.com/identity/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`
-  );
-
-  return resp.data.access_token;
+  try {
+    const resp = await axios.get(
+      `https://${munchkinId}.mktorest.com/identity/oauth/token?client_id=${clientId}&client_secret=${clientSecret}&grant_type=client_credentials`
+    );
+    if (resp && resp.data) {
+      return resp.data.access_token;
+    }
+    throw new CustomError("Could not retrieve authorisation token", 400);
+  } catch (error) {
+    throw new CustomError("Could not retrieve authorisation token", 400);
+  }
 };
 
 module.exports = { getAccessToken };

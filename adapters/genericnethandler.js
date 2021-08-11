b6aeff2688d516a93a1209984fa79d44b057e988
utsab <utsab.c97@gmail.com>
2021-08-11 21:15:55 +0530

    refactor of net-handler

--- adapters/genericnethandler.js
+++ adapters/genericnethandler.js
@@ -1,3 +1,4 @@
+const get = require("get-value");
 const { sendRequest } = require("./network");
 /**
  * network handler as a fall back for all destination nethandlers, this file provides abstraction for all the network comms btw
@@ -8,6 +9,38 @@ const { sendRequest } = require("./network");
  * to formatted response for server.
  *
  */
+
+const nodeSysErrorToStatus = code => {
+  const sysErrorToStatusMap = {
+    EACCES: { status: 400, message: "[EACCES] :: Permission denied" },
+    EADDRINUSE: {
+      status: 400,
+      message: "[EADDRINUSE] :: Address already in use"
+    },
+    ECONNREFUSED: {
+      status: 500,
+      message: "[ECONNREFUSED] :: Connection refused"
+    },
+    ECONNRESET: {
+      status: 500,
+      message: "[ECONNRESET] :: Connection reset by peer"
+    },
+    EEXIST: { status: 400, message: "[EEXIST] :: File exists" },
+    EISDIR: { status: 400, message: "[EEXIST] :: Is a directory" },
+    EMFILE: {
+      status: 400,
+      message: "[EMFILE] :: Too many open files in system"
+    },
+    ENOENT: { status: 400, message: "[ENOENT] :: No such file or directory" },
+    ENOTDIR: { status: 400, message: "[ENOTDIR] :: Not a directory" },
+    ENOTEMPTY: { status: 400, message: "[ENOTEMPTY] :: Directory not empty)" },
+    ENOTFOUND: { status: 400, message: "[ENOTFOUND] :: DNS lookup failed" },
+    EPERM: { status: 400, message: "[EPERM] :: Operation not permitted" },
+    EPIPE: { status: 400, message: "[EPIPE] :: Broken pipe" },
+    ETIMEDOUT: { status: 500, message: "[ETIMEDOUT] :: Operation timed out" }
+  };
+  return sysErrorToStatusMap[code];
+};
 
 /*
 Response format:
@@ -26,22 +59,46 @@ Response format:
 }
 */
 const handleDestinationResponse = (dresponse, metadata) => {
-  const { responseData, responseStatusText, responseStatus } = dresponse;
   let status;
-  if (responseStatus >= 200 && responseStatus <= 300) {
+  let message;
+
+  const trimmedRes = {
+    code: get(dresponse, "response.code"),
+    status: get(dresponse, "response.status"),
+    statusText: get(dresponse, "response.statusText"),
+    headers: get(dresponse, "response.headers"),
+    data: get(dresponse, "response.headers"),
+    success: get(dresponse, "suceess")
+  };
+
+  if (
+    trimmedRes.status &&
+    trimmedRes.status >= 200 &&
+    trimmedRes.status <= 300
+  ) {
     status = 200;
   } else {
-    status = responseStatus;
+    status = trimmedRes.status;
   }
+
+  message = trimmedRes.statusText;
+
+  if (!trimmedRes.success && trimmedRes.code) {
+    const nodeSysErr = nodeSysErrorToStatus(trimmedRes.code);
+    status = nodeSysErr.status;
+    message = nodeSysErr.message;
+  }
+
   const destination = {
-    response: JSON.stringify(responseData),
-    status: responseStatus
+    response: JSON.stringify(trimmedRes),
+    status: trimmedRes.status
   };
+
   const apiLimit = {
     available: "",
     resetAt: ""
   };
-  const message = responseStatusText || "Request processed Successfully";
+
   // TODO: What other info do we need to pass here
   const response = {
     status,
@@ -50,6 +107,7 @@ const handleDestinationResponse = (dresponse, metadata) => {
     metadata,
     message
   };
+
   return response;
 };
 

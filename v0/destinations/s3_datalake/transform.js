1cd2f7c4ccbbb420b855db91f7d2b8e41012ac07
dhawal1248 <sanghvi.dhawal101433@gmail.com>
2021-08-21 05:52:20 +0530

    s3 datalake

---
+++ v0/destinations/s3_datalake/transform.js
@@ -0,0 +1,36 @@
+const { processWarehouseMessage } = require("../../../warehouse");
+
+// redshift destination string limit, if the string length crosses 512 we will change data type to text which is varchar(max) in redshift
+const RSStringLimit = 512;
+const redshift = "rs";
+
+function processSingleMessage(message, options) {
+  return processWarehouseMessage(message, options);
+}
+
+function getDataTypeOverride(val, options) {
+  if (options.rsAlterStringToText === "true") {
+    if (val && val.length > RSStringLimit) {
+      return "text";
+    }
+  }
+  return "string";
+}
+
+function process(event) {
+  const whSchemaVersion = event.request.query.whSchemaVersion || "v1";
+  const whStoreEvent = event.destination.Config.storeFullEvent === true;
+  const rsAlterStringToText =
+    event.request.query.rsAlterStringToText || "false";
+  const provider = redshift;
+  return processSingleMessage(event.message, {
+    whSchemaVersion,
+    whStoreEvent,
+    getDataTypeOverride,
+    provider,
+    rsAlterStringToText,
+    sourceCategory: event.metadata ? event.metadata.sourceCategory : null
+  });
+}
+
+exports.process = process;

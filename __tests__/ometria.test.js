5d4d312ca4fc06214b71eb13d51fef815f963ca1
anuraj <anurajguha@gmail.com>
2021-08-26 14:53:29 +0530

    update contact mapping

--- __tests__/ometria.test.js
+++ __tests__/ometria.test.js
@@ -29,25 +29,6 @@ inputData.forEach((input, index) => {
     expect(output).toEqual(expected);
   });
 });
-// Router Test Data
-// const inputRouterDataFile = fs.readFileSync(
-//   path.resolve(__dirname, `./data/${integration}_router_input.json`)
-// );
-// const outputRouterDataFile = fs.readFileSync(
-//   path.resolve(__dirname, `./data/${integration}_router_output.json`)
-// );
-// const inputRouterData = JSON.parse(inputRouterDataFile);
-// const expectedRouterData = JSON.parse(outputRouterDataFile);
-
-
-//   describe(`${name} Tests`, () => {
-//   describe("Router Tests", () => {
-//     it("Payload", async () => {
-//       const routerOutput = await transformer.processRouterDest(inputRouterData);
-//       expect(routerOutput).toEqual(expectedRouterData);
-//     });
-//   });
-// });
 
 const batchInputDataFile = fs.readFileSync(
   path.resolve(__dirname, `./data/${integration}_batch_input.json`)

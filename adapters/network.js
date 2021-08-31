f974c55df5c216841d1ab2203052903fe17704a1
utsab <utsab.c97@gmail.com>
2021-08-31 17:35:55 +0530

    adding configs for socket pooling

--- adapters/network.js
+++ adapters/network.js
@@ -1,48 +1,16 @@
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable no-undef */
-const http = require("http");
-const https = require("https");
-const axios = require("axios");
+
 const _ = require("lodash");
 const log = require("../logger");
+const customAxios = require("./networkClient");
 
 const send = async options => {
   // here the options argument K-Vs will take priority over requestOptions
-  const requestOptions = {
-    ...{
-      // `method` is the request method to be used when making the request
-      method: "get",
-
-      // `timeout` specifies the number of milliseconds before the request times out. If the request takes longer than `timeout`, the request will be aborted.
-      timeout: 1000 * 60,
-
-      // `withCredentials` indicates whether or not cross-site Access-Control requests should be made using credentials
-      withCredentials: false,
-
-      // `responseType` indicates the type of data that the server will respond with options are: 'arraybuffer', 'document', 'json', 'text', 'stream'
-      responseType: "json",
-
-      // `responseEncoding` indicates encoding to use for decoding responses (Node.js only),
-      responseEncoding: "utf8",
-
-      // `maxBodyLength` (Node only option) defines the max size of the http request content in bytes allowed,
-      maxBodyLength: 1000 * 1000 * 10,
-
-      // `maxRedirects` defines the maximum number of redirects to follow in node.js,
-      maxRedirects: 5,
-
-      // `httpAgent` and `httpsAgent` define a custom agent to be used when performing http
-      httpAgent: new http.Agent({ keepAlive: true }),
-
-      // and https requests, respectively, in node.js. This allows options to be added like `keepAlive` that are not enabled by default.
-      httpsAgent: new https.Agent({ keepAlive: true })
-    },
-    ...options
-  };
   // Send a request
   let res;
   try {
-    const response = await axios(requestOptions);
+    const response = await customAxios(options);
     res = { success: true, response };
   } catch (err) {
     res = { success: false, response: err };

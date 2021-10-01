5d8ea6d31b4484a0f2d5de175dc0656d98d77001
anuraj <anurajguha@gmail.com>
2021-10-01 10:14:12 +0530

    revert config backend url to prod

--- util/customTransforrmationsStore-v1.js
+++ util/customTransforrmationsStore-v1.js
@@ -5,9 +5,9 @@ const stats = require("./stats");
 const transformationCache = {};
 const libraryCache = {};
 
-// const CONFIG_BACKEND_URL =
-//   process.env.CONFIG_BACKEND_URL || "https://api.rudderlabs.com";
-const CONFIG_BACKEND_URL = "http://localhost:5000";
+// const CONFIG_BACKEND_URL = "http://localhost:5000";
+const CONFIG_BACKEND_URL =
+  process.env.CONFIG_BACKEND_URL || "https://api.rudderlabs.com";
 const getTransformationURL = `${CONFIG_BACKEND_URL}/transformation/getByVersionId`;
 const getLibrariesUrl = `${CONFIG_BACKEND_URL}/transformationLibrary/getByVersionId`;
 

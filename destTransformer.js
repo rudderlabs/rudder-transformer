4df2877806948dcacdb4dc9144f79dc7366e51b1
anuraj <anurajguha@gmail.com>
2021-10-04 09:43:43 +0530

    tmp

--- destTransformer.js
+++ destTransformer.js
@@ -1,3 +1,5 @@
+const fs = require("fs");
+const http2 = require("http2");
 const Koa = require("koa");
 const bodyParser = require("koa-bodyparser");
 const logger = require("./logger");
@@ -19,9 +21,20 @@ app.use(
 
 app.use(router.routes()).use(router.allowedMethods());
 
+const options = {
+  key: fs.readFileSync("./transformer.key"),
+  cert: fs.readFileSync("./transformer.crt")
+};
+
 if (clusterEnabled) {
   cluster.start(PORT, app);
 } else {
-  app.listen(PORT);
-  logger.info(`Listening on Port: ${PORT}`);
+  // app.listen(PORT);
+  http2.createServer(options, app.callback()).listen(PORT, err => {
+    if (err) {
+      throw new Error(err);
+    }
+    logger.info(`Listening on port ${PORT}`);
+  });
+  // logger.info(`Listening on Port: ${PORT}`);
 }

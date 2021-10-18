a69c27f7f41117d16cdb300b450641c46ab2f199
Akash Chetty <achetty.iitr@gmail.com>
2021-10-18 23:34:43 +0530

    code cleanup.

--- v0/destinations/deltalake/transform.js
+++ v0/destinations/deltalake/transform.js
@@ -9,17 +9,17 @@ function processSingleMessage(message, options) {
 function getDataTypeOverride(val, options) {}
 
 function process(event) {
-    const whSchemaVersion = event.request.query.whSchemaVersion || "v1";
-    const whStoreEvent = event.destination.Config.storeFullEvent === true;
-    const provider = snowflake;
-    return processSingleMessage(event.message, {
-        metadata: event.metadata,
-        whSchemaVersion,
-        whStoreEvent,
-        getDataTypeOverride,
-        provider,
-        sourceCategory: event.metadata ? event.metadata.sourceCategory : null
-    });
+  const whSchemaVersion = event.request.query.whSchemaVersion || "v1";
+  const whStoreEvent = event.destination.Config.storeFullEvent === true;
+  const provider = snowflake;
+  return processSingleMessage(event.message, {
+    metadata: event.metadata,
+    whSchemaVersion,
+    whStoreEvent,
+    getDataTypeOverride,
+    provider,
+    sourceCategory: event.metadata ? event.metadata.sourceCategory : null
+  });
 }
 
 exports.process = process;

f7ea3b636de133140afa230df47a269c09dc8639
Arnab <arnab.compsc@gmail.com>
2021-10-20 01:42:14 +0530

    Updated AppsFlyer tests

--- __tests__/appsFlyer.test.js
+++ __tests__/appsFlyer.test.js
@@ -15,35 +15,31 @@ const inputData = JSON.parse(inputDataFile);
 const expectedData = JSON.parse(outputDataFile);
 
 // Router Test Data
-const inputRouterDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_router_input.json`)
-);
-const outputRouterDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_router_output.json`)
-);
-const inputRouterData = JSON.parse(inputRouterDataFile);
-const expectedRouterData = JSON.parse(outputRouterDataFile);
+// const inputRouterDataFile = fs.readFileSync(
+//   path.resolve(__dirname, `./data/${integration}_router_input.json`)
+// );
+// const outputRouterDataFile = fs.readFileSync(
+//   path.resolve(__dirname, `./data/${integration}_router_output.json`)
+// );
+// const inputRouterData = JSON.parse(inputRouterDataFile);
+// const expectedRouterData = JSON.parse(outputRouterDataFile);
 
 inputData.forEach((input, index) => {
   test(`${name} Tests - Payload ${index}`, () => {
-    // try {
-      // ignored
-    //   const output = transformer.process(input);
-    //   expect(output).toEqual(expectedData[index]);
-    // } catch (error) {
-    //   expect(error.message).toEqual(expectedData[index].error);
-    // }
-  });
-  
-});
-
-describe(`${name} Tests`, () => {
-
-describe("Router Tests", () => {
-  it("Payload", async () => {
-    const routerOutput = await transformer.processRouterDest(inputRouterData);
-    // expect(routerOutput).toEqual(expectedRouterData);
+    try {
+      const output = transformer.process(input);
+      expect(output).toEqual(expectedData[index]);
+    } catch (error) {
+      expect(error.message).toEqual(expectedData[index].error);
+    }
   });
 });
 
-});
+// describe(`${name} Tests`, () => {
+//   describe("Router Tests", () => {
+//     it("Payload", async () => {
+//       const routerOutput = await transformer.processRouterDest(inputRouterData);
+//       expect(routerOutput).toEqual(expectedRouterData);
+//     });
+//   });
+// });

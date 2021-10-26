54df312f44090d61ea1d0d840b8a088a313a40b8
Ruchira <ruchira@rudderlabs.com>
2021-10-26 11:23:23 +0530

    dynamic-config-develop: added for batch events

--- util/dynamicConfig.js
+++ util/dynamicConfig.js
@@ -1,26 +1,27 @@
 const get = require("get-value");
-const unset = require('unset-value');
+const unset = require("unset-value");
 
 function getDynamicConfig(event) {
   const { Config } = event.destination;
-
-  Object.keys(Config).forEach(field => {
-    let value = Config[field].toString().trim();
-    if (value.startsWith("{{") && value.endsWith("}}")) {
-      value = value.replace("{{", "");
-      value = value.replace("}}", "");
-      if (value.includes("||")) {
-        const path = value.split("||")[0].trim();
-        const getFieldVal = get(event, path);
-        if (getFieldVal) {
-          Config[field] = getFieldVal;
-          unset(event, path);
-        } else {
-          Config[field] = JSON.parse(value.split("||")[1].trim());
+  if (Config) {
+    Object.keys(Config).forEach(field => {
+      let value = Config[field].toString().trim();
+      if (value.startsWith("{{") && value.endsWith("}}")) {
+        value = value.replace("{{", "");
+        value = value.replace("}}", "");
+        if (value.includes("||")) {
+          const path = value.split("||")[0].trim();
+          const getFieldVal = get(event, path);
+          if (getFieldVal) {
+            Config[field] = getFieldVal;
+            unset(event, path);
+          } else {
+            Config[field] = JSON.parse(value.split("||")[1].trim());
+          }
         }
       }
-    }
-  });
+    });
+  }
   return event;
 }
 function processDynamicConfig(event, type) {

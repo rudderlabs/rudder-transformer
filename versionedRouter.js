e0fdab1c44eefb9279b9a211ca3dab7c62beb59e
utsab <utsab.c97@gmail.com>
2021-10-29 19:33:39 +0530

    hubspot batching support: bumping api_version in transformer

--- versionedRouter.js
+++ versionedRouter.js
@@ -14,7 +14,7 @@ const { DestHandlerMap } = require("./constants/destinationCanonicalNames");
 require("dotenv").config();
 
 const versions = ["v0"];
-const API_VERSION = "1";
+const API_VERSION = "2";
 
 const transformerMode = process.env.TRANSFORMER_MODE;
 

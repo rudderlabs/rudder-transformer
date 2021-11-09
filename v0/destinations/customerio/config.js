956a96ac9e43812e863f30d896310d53da137ebb
shrouti <shrouti.gangopadhyay@gmail.com>
2021-11-09 13:29:29 +0530

    feature : alias support added

--- v0/destinations/customerio/config.js
+++ v0/destinations/customerio/config.js
@@ -5,6 +5,8 @@ const USER_EVENT_ENDPOINT =
 
 const ANON_EVENT_ENDPOINT = "https://track.customer.io/api/v1/events";
 
+const MERGE_USER_ENDPOINT = "https://track.customer.io/api/v1/merge_customers";
+
 const DEVICE_REGISTER_ENDPOINT =
   "https://track.customer.io/api/v1/customers/:id/devices";
 
@@ -16,5 +18,6 @@ module.exports = {
   USER_EVENT_ENDPOINT,
   ANON_EVENT_ENDPOINT,
   DEVICE_REGISTER_ENDPOINT,
-  DEVICE_DELETE_ENDPOINT
+  DEVICE_DELETE_ENDPOINT,
+  MERGE_USER_ENDPOINT
 };

53d3562d6bbb729e790931ccf23480c66cb6ea36
Ruchira <ruchira@rudderlabs.com>
2021-11-10 11:09:06 +0530

    deletion-api: added deletion api for braze (not tested)

---
+++ v0/destinations/braze/deleteUsers.js
@@ -0,0 +1,50 @@
+const { httpSend } = require("../../../adapters/network");
+const { CustomError } = require("../../util");
+
+const responseHandler = async (userAttributes, config) => {
+  const { dataCenter, restApiKey } = config;
+  for (let i = 0; i < userAttributes.length; i++) {
+    const uId = userAttributes[i].userId;
+    if (!uId) {
+      throw new CustomError("User id for deletion not present", 400);
+    }
+    const data = JSON.stringify({ external_ids: [uId] });
+
+    const requestOptions = {
+      method: "post",
+      url: `https://${dataCenter}.braze.com/users/delete`,
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${restApiKey}`
+      },
+      data
+    };
+    const resp = await httpSend(requestOptions);
+    if (resp && !resp.success && !resp.response.response) {
+      throw new CustomError(resp.response.code || "Could not delete user", 400);
+    }
+    if (!resp || !resp.response) {
+      throw new CustomError("Could not get response", 500);
+    }
+    if (
+      resp &&
+      resp.response &&
+      resp.response.response &&
+      resp.response.response.status !== 200 &&
+      resp.response.response.status !== 404
+    ) {
+      throw new CustomError(
+        resp.response.response.statusText || "Error while deleting user",
+        resp.response.response.status
+      );
+    }
+  }
+  return { status: "successful" };
+};
+const processDeleteUsers = async event => {
+  const { userAttributes, config } = event;
+  const resp = await responseHandler(userAttributes, config);
+  return resp;
+};
+
+module.exports = { processDeleteUsers };

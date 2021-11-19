db5162a8234754768d7b4854ddf4cd1f11edbdb9
Sai Sankeerth <sanpj2292@github.com>
2021-11-19 11:27:58 +0530

    Revert to master branch code commit
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- util/cluster.js
+++ util/cluster.js
@@ -1,10 +1,9 @@
 const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
 const util = require("util");
-const gracefulShutdown = require("http-graceful-shutdown");
 const logger = require("../logger");
-
-const numWorkers = process.env.NUM_PROCS || numCPUs;
+const gracefulShutdown = require('http-graceful-shutdown');
+const numWorkers = process.env.NUM_PROCS || numCPUs
 
 function processInfo() {
   return {
@@ -17,15 +16,15 @@ function processInfo() {
 }
 
 function finalFunction() {
-  logger.info(`worker ${process.pid} gracefully shutted down.....`);
+  logger.info(`worker ${process.pid} gracefully shutted down.....`)
 }
 
 // This function works only in master.
 // It sends SIGTERM to all the workers
 function shutdownWorkers() {
-  for (const id in cluster.workers) {
+  for (var id in cluster.workers) {
     process.kill(cluster.workers[id].process.pid);
-    logger.info(`Sent kill signal to ${cluster.workers[id].process.pid}`);
+    logger.info(`Sent kill signal to ${cluster.workers[id].process.pid}`)
   }
 }
 
@@ -40,41 +39,42 @@ function start(port, app) {
 
     cluster.on("online", worker => {
       logger.info(`Worker ${worker.process.pid} is online`);
-      // To provide caching at pod-level
     });
-    let isShuttingDown = false;
+
+    let isShuttingDown = false
     cluster.on("exit", worker => {
-      if (!isShuttingDown) {
+      if (!isShuttingDown){
         logger.error(`worker ${worker.process.pid} died`);
         logger.error(
           `Killing Process to avoid any side effects of dead worker.\nProcess Info: `,
           util.inspect(processInfo(), false, null, true)
         );
-        isShuttingDown = true;
-        shutdownWorkers();
+        isShuttingDown = true
+        shutdownWorkers()
       }
     });
-
+    
     process.on("SIGTERM", () => {
-      logger.info("SIGTERM signal received. Closing workers");
-      isShuttingDown = true;
-      shutdownWorkers();
+      logger.info(
+        "SIGTERM signal received. Closing workers"
+      );
+      isShuttingDown = true
+      shutdownWorkers()
     });
+    
   } else {
-    const server = app.listen(port);
-
-    gracefulShutdown(server, {
-      signals: "SIGINT SIGTERM",
-      timeout: 30000, // timeout: 30 secs
-      forceExit: true, // triggers process.exit() at the end of shutdown process
-      finally: finalFunction
-    });
+    server = app.listen(port);
+    gracefulShutdown(server,
+      {
+        signals: 'SIGINT SIGTERM',
+        timeout: 30000,                  // timeout: 30 secs
+        forceExit: true,                 // triggers process.exit() at the end of shutdown process
+        finally: finalFunction
+      });
     logger.info(`Worker ${process.pid} started`);
+
   }
   logger.debug("transformerServer: started");
 }
 
-module.exports = {
-  start,
-  processInfo
-};
+exports.start = start;

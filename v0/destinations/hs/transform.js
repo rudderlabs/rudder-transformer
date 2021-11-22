c5da3c1bf9971aff781f93b5287c47828980980f
Akash Gupta <akashguptarpo@gmail.com>
2021-11-22 17:39:26 +0530

    feat: optimize "properties" endpoint
    optimizing to avoid reaching its rate-limit

--- v0/destinations/hs/transform.js
+++ v0/destinations/hs/transform.js
@@ -66,7 +66,7 @@ async function getProperties(destination) {
   return hubSpotPropertyMap;
 }
 
-async function getTransformedJSON(message, mappingJson, destination) {
+async function getTransformedJSON(message, mappingJson, propertyMap) {
   const rawPayload = {};
   const sourceKeys = Object.keys(mappingJson);
   let traits = getFieldValueFromMessage(message, "traits");
@@ -76,7 +76,6 @@ async function getTransformedJSON(message, mappingJson, destination) {
 
   if (traits) {
     const traitsKeys = Object.keys(traits);
-    const propertyMap = await getProperties(destination);
     sourceKeys.forEach(sourceKey => {
       if (get(traits, sourceKey)) {
         set(rawPayload, mappingJson[sourceKey], get(traits, sourceKey));
@@ -138,7 +137,7 @@ function responseBuilderSimple(payload, message, eventType, destination) {
   return response;
 }
 
-async function processTrack(message, destination) {
+async function processTrack(message, destination, propertyMap) {
   const parameters = {
     _a: destination.Config.hubID,
     _n: message.event
@@ -154,7 +153,7 @@ async function processTrack(message, destination) {
   const userProperties = await getTransformedJSON(
     message,
     hSIdentifyConfigJson,
-    destination
+    propertyMap
   );
 
   return responseBuilderSimple(
@@ -169,7 +168,7 @@ async function processTrack(message, destination) {
 //   throw new Error(message);
 // }
 
-async function processIdentify(message, destination) {
+async function processIdentify(message, destination, propertyMap) {
   const traits = getFieldValueFromMessage(message, "traits");
   const mappedToDestination = get(message, MappedToDestinationKey);
   // If mapped to destination, Add externalId to traits
@@ -183,7 +182,7 @@ async function processIdentify(message, destination) {
   const userProperties = await getTransformedJSON(
     message,
     hSIdentifyConfigJson,
-    destination
+    propertyMap
   );
   const properties = getPropertyValueForIdentify(userProperties);
   return responseBuilderSimple(
@@ -194,7 +193,7 @@ async function processIdentify(message, destination) {
   );
 }
 
-async function processSingleMessage(message, destination) {
+async function processSingleMessage(message, destination, propertyMap) {
   let response;
 
   if (!message.type) {
@@ -203,10 +202,10 @@ async function processSingleMessage(message, destination) {
 
   switch (message.type) {
     case EventType.TRACK:
-      response = await processTrack(message, destination);
+      response = await processTrack(message, destination, propertyMap);
       break;
     case EventType.IDENTIFY:
-      response = await processIdentify(message, destination);
+      response = await processIdentify(message, destination, propertyMap);
       break;
     default:
       throw new CustomError(
@@ -218,8 +217,11 @@ async function processSingleMessage(message, destination) {
   return response;
 }
 
-function process(event) {
-  return processSingleMessage(event.message, event.destination);
+// has been deprecated - using routerTransform for both the versions
+async function process(event) {
+  // no. of calls for properties endpoint remains the same
+  const propertyMap = await getProperties(event.destination);
+  return processSingleMessage(event.message, event.destination, propertyMap);
 }
 
 function batchEvents(destEvents) {
@@ -324,6 +326,8 @@ const processRouterDest = async inputs => {
   const errorRespList = [];
   // using the first destination config for transforming the batch
   const { destination } = inputs[0];
+  // reduce the no. of calls for properties endpoint
+  const propertyMap = await getProperties(destination);
   await Promise.all(
     inputs.map(async input => {
       try {
@@ -337,7 +341,11 @@ const processRouterDest = async inputs => {
         } else {
           // event is not transformed
           successRespList.push({
-            message: await processSingleMessage(input.message, destination),
+            message: await processSingleMessage(
+              input.message,
+              destination,
+              propertyMap
+            ),
             metadata: input.metadata,
             destination
           });

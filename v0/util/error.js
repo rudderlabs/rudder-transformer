e22fcbb4e3c9ac3fadb44cb8e462aed1e0774b97
anuraj <anurajguha@gmail.com>
2021-12-21 23:42:35 +0530

    refactor/network-handler: introducing class for  Error Builder

--- v0/util/error.js
+++ v0/util/error.js
@@ -1,46 +1,97 @@
 // Todo add documentation for internal functions
-function ErrorBuilder() {
-  this.err = new Error();
+// function ErrorBuilder() {
+//   this.err = new Error();
 
-  this.setMessage = message => {
+//   this.setMessage = message => {
+//     this.err.message = message;
+//     return this;
+//   };
+//   this.setStatus = status => {
+//     this.err.status = status;
+//     return this;
+//   };
+
+//   this.setDestinationResponse = destinationResponse => {
+//     this.err.destinationResponse = destinationResponse;
+//     return this;
+//   };
+
+//   this.isTransformResponseFailure = arg => {
+//     this.err.responseTransformFailure = arg;
+//     return this;
+//   };
+
+//   this.setErrorResponse = () => {
+//     this.err.response = {
+//       status: this.err.status,
+//       message: this.err.message
+//     };
+//     return this;
+//   };
+
+//   this.setStatTags = arg => {
+//     this.err.statTags = arg;
+//     return this;
+//   };
+
+//   // Used for only OAuth related destinations
+//   this.setAuthErrorCategory = errorCat => {
+//     this.err.authErrorCategory = errorCat;
+//     return this;
+//   };
+
+//   this.build = () => this.err;
+// }
+class ErrorBuilder {
+  constructor() {
+    // should probably use a custom error class
+    // instead of directly setting fields to
+    // the default error object
+    this.err = new Error();
+  }
+
+  setMessage(message) {
     this.err.message = message;
     return this;
-  };
-  this.setStatus = status => {
+  }
+
+  setStatus(status) {
     this.err.status = status;
     return this;
-  };
+  }
 
-  this.setDestinationResponse = destinationResponse => {
+  setDestinationResponse(destinationResponse) {
     this.err.destinationResponse = destinationResponse;
     return this;
-  };
+  }
 
-  this.isTransformResponseFailure = arg => {
+  isTransformResponseFailure(arg) {
     this.err.responseTransformFailure = arg;
     return this;
-  };
+  }
 
-  this.setErrorResponse = () => {
+  setErrorResponse() {
     this.err.response = {
       status: this.err.status,
       message: this.err.message
     };
     return this;
-  };
+  }
 
-  this.setStatTags = arg => {
+  setStatTags(arg) {
     this.err.statTags = arg;
     return this;
-  };
+  }
 
   // Used for only OAuth related destinations
-  this.setAuthErrorCategory = errorCat => {
-    this.err.authErrorCategory = errorCat;
+  setAuthErrorCategory(errorCategory) {
+    this.err.authErrorCategory = errorCategory;
     return this;
-  };
+  }
 
-  this.build = () => this.err;
+  build() {
+    return this.err;
+  }
 }
 
 module.exports = ErrorBuilder;

3ddcdaac0386e6fe6e6ec16f52faf88111dcac8c
Akash Chetty <achetty.iitr@gmail.com>
2021-12-24 01:03:39 +0530

    Added test cases.

--- __tests__/eventValidation.test.js
+++ __tests__/eventValidation.test.js
@@ -1,6 +1,93 @@
-const {checkIfEventTypeIsSupportedOrNot} = require("../util/eventValidation");
+jest.mock("node-fetch");
+const fetch = require("node-fetch", () => jest.fn());
 
-var testCases = [
+const {isEventTypeSupported, validate, handleValidation} = require("../util/eventValidation");
+
+const trackingPlan = {
+    rules: {
+        events: [
+            {
+                id: "ev_22HzyIUtuhfoI80iDfAgf47GHpw",
+                name: "Product clicked",
+                description: "Fired when an product is clicked.",
+                version: "1-0-0",
+                rules: {
+                    $schema: "http://json-schema.org/draft-07/schema#",
+                    additionalProperties: false,
+                    properties: {
+                        email: {
+                            type: [
+                                "string"
+                            ]
+                        },
+                        name: {
+                            type: [
+                                "string"
+                            ]
+                        },
+                        prop_float: {
+                            type: [
+                                "string"
+                            ]
+                        },
+                        prop_integer: {
+                            type: [
+                                "string"
+                            ]
+                        },
+                        revenue: {
+                            type: [
+                                "number"
+                            ]
+                        }
+                    },
+                    type: "object"
+                }
+            }
+        ]
+    },
+    name: "workspaces/dummy_workspace_id/tracking-plans/dummy_tracking_plan_id",
+    display_name: "Demo Tracking Plan",
+    version: 1,
+    create_time: "2021-12-14T19:19:13.666Z",
+    update_time: "2021-12-15T13:29:59.272Z"
+}
+const sourceTpConfig = {
+    group: {},
+    track: {
+        allowUnplannedEvents: "true",
+        unplannedProperties: "forward",
+        anyOtherViolation: "forward",
+        propagateValidationErrors: "true",
+        ajvOptions: {}
+    },
+    global: {
+        allowUnplannedEvents: "false",
+        unplannedProperties: "drop",
+        anyOtherViolation: "drop",
+        sendViolatedEventsTo: "procErrors",
+        ajvOptions: {}
+    },
+    identify: {}
+}
+const mergedTpConfig = {
+    group: {},
+    track: {
+        allowUnplannedEvents: "true",
+        unplannedProperties: "forward",
+        anyOtherViolation: "forward",
+        propagateValidationErrors: "true"
+    },
+    global: {
+        allowUnplannedEvents: "false",
+        unplannedProperties: "drop",
+        anyOtherViolation: "drop",
+        sendViolatedEventsTo: "procErrors"
+    },
+    identify: {}
+}
+
+const eventTypesTestCases = [
     {
         "output": false
     },
@@ -36,13 +123,288 @@ var testCases = [
         "eventType": "alias",
         "output": false
     }
-]
+];
+const eventValidationTestCases = [
+    // {
+    //     "testCase": "Empty Source TP Config",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             mergedTpConfig: mergedTpConfig,
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Empty Merged TP Config",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             sourceTpConfig: sourceTpConfig,
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Empty Source and Merged TP Config",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Page Event is not Supported",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             mergedTpConfig: mergedTpConfig,
+    //             sourceTpConfig: sourceTpConfig,
+    //         },
+    //         message: {
+    //             type: "page",
+    //             userId: "user12345",
+    //             anonymousId: "anon-id-new",
+    //             name: "Page View",
+    //             properties: {
+    //                 title: "Home",
+    //                 path: "/"
+    //             },
+    //             context: {
+    //                 ip: "14.5.67.21",
+    //                 library: {
+    //                     name: "http"
+    //                 }
+    //             },
+    //             timestamp: "2020-02-02T00:23:09.544Z"
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Screen Event is not Supported",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             mergedTpConfig: mergedTpConfig,
+    //             sourceTpConfig: sourceTpConfig,
+    //         },
+    //         message: {
+    //             type: "screen",
+    //             userId: "user12345",
+    //             anonymousId: "anon-id-new",
+    //             name: "Screen View",
+    //             properties: {
+    //                 prop1: "5"
+    //             },
+    //             context: {
+    //                 ip: "14.5.67.21",
+    //                 library: {
+    //                     name: "http"
+    //                 }
+    //             },
+    //             timestamp: "2020-02-02T00:23:09.544Z"
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Alias Event is not Supported",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             mergedTpConfig: mergedTpConfig,
+    //             sourceTpConfig: sourceTpConfig,
+    //         },
+    //         message: {
+    //             type: "alias",
+    //             userId: "user123",
+    //             previousId: "user12345",
+    //             context: {
+    //                 traits: {
+    //                     trait1: "new-val"
+    //                 },
+    //                 ip: "14.5.67.21",
+    //                 library: {
+    //                     name: "http"
+    //                 }
+    //             },
+    //             timestamp: "2020-01-21T00:21:34.208Z"
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Group is not present in Tracking Plan ",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             mergedTpConfig: mergedTpConfig,
+    //             sourceTpConfig: sourceTpConfig,
+    //         },
+    //         message: {
+    //             type: "group",
+    //             userId: "user12345",
+    //             groupId: "group1",
+    //             traits: {
+    //                 name: "Company",
+    //                 industry: "Industry",
+    //                 employees: 123
+    //             },
+    //             context: {
+    //                 traits: {
+    //                     trait1: "new-val"
+    //                 },
+    //                 ip: "14.5.67.21",
+    //                 library: {
+    //                     name: "http"
+    //                 }
+    //             },
+    //             timestamp: "2020-01-21T00:21:34.208Z"
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    // {
+    //     "testCase": "Identify is not present in Tracking Plan ",
+    //     "event": {
+    //         metadata: {
+    //             trackingPlanId: "dummy_tracking_plan_id",
+    //             trackingPlanVersion: "dummy_version",
+    //             workspaceId: "dummy_workspace_id",
+    //             mergedTpConfig: mergedTpConfig,
+    //             sourceTpConfig: sourceTpConfig,
+    //         },
+    //         message: {
+    //             type: "identify",
+    //             userId: "user12345",
+    //             anonymousId: "anon-id-new",
+    //             context: {
+    //                 traits: {
+    //                     trait1: "new-val"
+    //                 },
+    //                 ip: "14.5.67.21",
+    //                 library: {
+    //                     name: "http"
+    //                 }
+    //             },
+    //             timestamp: "2020-02-02T00:23:09.544Z"
+    //         }
+    //     },
+    //     "trackingPlan": trackingPlan,
+    //     "output": {
+    //         dropEvent: false,
+    //         violationType: "None",
+    //         validationErrors: []
+    //     }
+    // },
+    {
+        "testCase": "Track event is present in Tracking Plan ",
+        "event": {
+            metadata: {
+                trackingPlanId: "dummy_tracking_plan_id",
+                trackingPlanVersion: "dummy_version",
+                workspaceId: "dummy_workspace_id",
+                mergedTpConfig: mergedTpConfig,
+                sourceTpConfig: sourceTpConfig,
+            },
+            message: {
+                type: "track",
+                userId: "user-charitha",
+                event: "Product clicked",
+                properties: {
+                    name: "Rubik's Cube",
+                    revenue: 4.99,
+                    prop_integer: "2",
+                    prop_float: "2.3",
+                    email: "sandhya12345@rudderstack.com"
+                },
+                context: {
+                    ip: "14.5.67.21"
+                },
+                timestamp: "2020-02-02T00:23:09.544Z"
+            }
+        },
+        "trackingPlan": trackingPlan,
+        "output": {
+            dropEvent: false,
+            violationType: "None",
+            validationErrors: []
+        }
+    }
+];
+
 
 describe("Supported Event types testing", () => {
-    testCases.forEach((testCase) => {
+    eventTypesTestCases.forEach((testCase) => {
         it(`should return isSupportedOrNot ${testCase.output} for this input eventType ${testCase.eventType} everytime`, () => {
-            var isSupportedOrNot = checkIfEventTypeIsSupportedOrNot(testCase.eventType)
+            var isSupportedOrNot = isEventTypeSupported(testCase.eventType)
             expect(isSupportedOrNot).toEqual(testCase.output)
+        })
+    })
+})
+
+describe("Handle validation", () => {
+    eventValidationTestCases.forEach((testCase) => {
+        it(`should return dropEvent: ${testCase.output.dropEvent}, violationType: ${testCase.output.violationType}, length of validationErrors: ${testCase.output.validationErrors},`, async () => {
+            fetch.mockResolvedValue({
+                json: jest.fn().mockResolvedValue(testCase.trackingPlan),
+                status: 200
+            });
+            const {dropEvent, violationType, validationErrors} = await handleValidation(testCase.event);
+            expect(dropEvent).toEqual(testCase.output.dropEvent)
+            expect(violationType).toEqual(testCase.output.violationType)
+            expect(validationErrors.length).toEqual(testCase.output.validationErrors.length)
         })
     })
 })

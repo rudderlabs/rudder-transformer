e6908faeed729ed494e66b9f8067249782a0e1dd
daniel-statsig <daniel@statsig.com>
2021-12-27 15:48:30 -0800

    adding statsig

---
+++ v0/destinations/statsig/transform.js
@@ -0,0 +1,24 @@
+const {
+  defaultPostRequestConfig,
+  defaultRequestConfig,
+} = require("../../util");
+const { ENDPOINT } = require("./config");
+
+function process(event) {
+  const { message, destination } = event;
+  const response = defaultRequestConfig();
+  const { secretKey } = destination.Config;
+
+  response.method = defaultPostRequestConfig.requestMethod;
+  response.body.JSON = message;
+  response.headers = {
+    "content-type": "application/json",
+    "STATSIG-API-KEY": secretKey
+  };
+
+  response.endpoint = ENDPOINT;
+
+  return response;
+}
+
+exports.process = process;

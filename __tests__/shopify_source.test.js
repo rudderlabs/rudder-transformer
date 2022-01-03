d89bfb62ac7ba84c9082639829a9084ed5294d47
anuraj <anurajguha@gmail.com>
2022-01-03 12:52:46 +0530

    shopify-source-v2: add initial test cases

---
+++ __tests__/shopify_source.test.js
@@ -0,0 +1,27 @@
+const integration = "shopify";
+const name = "Shopify";
+
+const fs = require("fs");
+const path = require("path");
+
+const transformer = require(`../v0/sources/${integration}/transform`);
+
+test(`${name} Tests`, () => {
+  const inputDataFile = fs.readFileSync(
+    path.resolve(__dirname, `./data/${integration}_source_input.json`)
+  );
+  const outputDataFile = fs.readFileSync(
+    path.resolve(__dirname, `./data/${integration}_source_output.json`)
+  );
+  const inputData = JSON.parse(inputDataFile);
+  const expectedData = JSON.parse(outputDataFile);
+  inputData.forEach(async (input, index) => {
+    try {
+      const output = transformer.process(input);
+      delete output.anonymousId; // anonId is being set dynamically
+      expect(output).toEqual(expectedData[index]);
+    } catch (err) {
+      expect(err.message).toEqual(expectedData[index].error);
+    }
+  });
+});

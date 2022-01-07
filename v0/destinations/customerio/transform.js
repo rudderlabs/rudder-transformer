1a8a708dbad37fe609f41e81bf78d2afb41150e0
Desu Sai Venkat <saivenkatdesu@gmail.com>
2022-01-07 19:11:26 +0530

    Fixed Device Registration issues around life cycle events

--- v0/destinations/customerio/transform.js
+++ v0/destinations/customerio/transform.js
@@ -63,11 +63,9 @@ function responseBuilder(message, evType, evName, destination, messageType) {
     adduserIdFromExternalId(message);
   }
 
-  const userId =
-    message.userId && message.userId !== "" ? message.userId : undefined;
-
+  const userId = getFieldValueFromMessage(message, "userIdOnly");
   const response = defaultRequestConfig();
-  response.userId = message.userId || message.anonymousId;
+  response.userId = userId || message.anonymousId;
   response.headers = {
     Authorization: `Basic ${btoa(
       `${destination.Config.siteID}:${destination.Config.apiKey}`
@@ -163,46 +161,41 @@ function responseBuilder(message, evType, evName, destination, messageType) {
     // any other event type except identify
     const token = get(message, "context.device.token");
 
-    if (message.properties) {
-      // use this if only top level keys are to be sent
-      // DEVICE DELETE from CustomerIO
-      if (deviceDeleteRelatedEventName === evName) {
-        if (userId && token) {
-          endpoint = DEVICE_DELETE_ENDPOINT.replace(":id", userId).replace(
-            ":device_id",
-            token
-          );
-
-          response.endpoint = endpoint;
-          response.method = "DELETE";
-
-          return response;
-        }
-        throw new CustomError("userId or device_token not present", 400);
-      }
-
-      // DEVICE registration
-      if (userId && deviceRelatedEventNames.includes(evName) && token) {
-        const devProps = message.properties;
-        set(devProps, "id", get(message, "context.device.token"));
-        const deviceType = get(message, "context.device.type");
-        if (deviceType) {
-          set(devProps, "platform", deviceType.toLowerCase());
-        }
-        set(
-          devProps,
-          "last_used",
-          Math.floor(new Date(message.originalTimestamp).getTime() / 1000)
+    // use this if only top level keys are to be sent
+    // DEVICE DELETE from CustomerIO
+    if (deviceDeleteRelatedEventName === evName) {
+      if (userId && token) {
+        endpoint = DEVICE_DELETE_ENDPOINT.replace(":id", userId).replace(
+          ":device_id",
+          token
         );
-        set(rawPayload, "device", devProps);
-        requestConfig = defaultPutRequestConfig;
-      } else {
-        rawPayload.data = {};
-        set(rawPayload, "data", message.properties);
+
+        response.endpoint = endpoint;
+        response.method = "DELETE";
+
+        return response;
       }
+      throw new CustomError("userId or device_token not present", 400);
     }
 
-    if (!(deviceRelatedEventNames.includes(evName) && userId && token)) {
+    // DEVICE registration
+    if (deviceRelatedEventNames.includes(evName) && userId && token) {
+      const devProps = message.properties || {};
+      set(devProps, "id", get(message, "context.device.token"));
+      const deviceType = get(message, "context.device.type");
+      if (deviceType) {
+        set(devProps, "platform", deviceType.toLowerCase());
+      }
+      set(
+        devProps,
+        "last_used",
+        Math.floor(new Date(message.originalTimestamp).getTime() / 1000)
+      );
+      set(rawPayload, "device", devProps);
+      requestConfig = defaultPutRequestConfig;
+    } else {
+      rawPayload.data = {};
+      set(rawPayload, "data", message.properties);
       set(rawPayload, "name", evName);
       set(rawPayload, "type", evType);
       if (getFieldValueFromMessage(message, "historicalTimestamp")) {
@@ -339,6 +332,7 @@ const processRouterDest = async inputs => {
       } catch (error) {
         return getErrorRespEvents(
           [input.metadata],
+          // eslint-disable-next-line no-nested-ternary
           error.response
             ? error.response.status
             : error.code

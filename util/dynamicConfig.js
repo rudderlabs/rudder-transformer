bc46e81660a8e7afee0f77e4c10acee4610ec507
Ankit Kumar <ankit8540988674@gmail.com>
2022-01-07 17:53:34 +0530

    update to support  mapping in value field, changed name of function and added test cases to demonstrate

--- util/dynamicConfig.js
+++ util/dynamicConfig.js
@@ -2,17 +2,17 @@
 const get = require("get-value");
 const unset = require("unset-value");
 
-function recurse(event, value, Config, field) {
+function setDynamicConfigValue(event, value, config, field) {
   value = value.replace("{{", "");
   value = value.replace("}}", "");
   if (value.includes("||")) {
     const path = value.split("||")[0].trim();
     const getFieldVal = get(event, path);
     if (getFieldVal) {
-      Config[field] = getFieldVal;
+      config[field] = getFieldVal;
       unset(event, path);
     } else {
-      Config[field] = JSON.parse(value.split("||")[1].trim()).toString();
+      config[field] = JSON.parse(value.split("||")[1].trim()).toString();
     }
   }
 }
@@ -21,22 +21,28 @@ function getDynamicConfig(event) {
   const { Config } = event.destination;
   if (Config) {
     Object.keys(Config).forEach(field => {
-      const value = Config[field].toString().trim();
-      if (Array.isArray(value)) {
+      // let value = Config[field].toString().trim();
+      let value = Config[field];
+      if (typeof value !== "string") {
         value.forEach(obj => {
           Object.keys(obj).forEach(key => {
-            if (key.startsWith("{{") && key.endsWith("}}")) {
-              recurse(event, key, Config, field);
+            const val = obj[key];
+            if (val.startsWith("{{") && val.endsWith("}}")) {
+              setDynamicConfigValue(event, val, Config, field);
             }
           });
         });
-      } else if (value.startsWith("{{") && value.endsWith("}}")) {
-        recurse(event, value, Config, field);
+      } else {
+        value = value.toString().trim();
+        if (value.startsWith("{{") && value.endsWith("}}")) {
+          setDynamicConfigValue(event, value, Config, field);
+        }
       }
     });
   }
   return event;
 }
+
 function processDynamicConfig(event, type) {
   if (type === "router" || type === "batch") {
     const eventRetArr = [];

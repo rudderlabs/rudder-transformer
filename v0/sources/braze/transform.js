eb61f955f06741c092f687971a822199d6b68969
Arnab <arnab.compsc@gmail.com>
2022-01-11 18:50:38 +0530

    Handle both Object and Array for Braze Currents

--- v0/sources/braze/transform.js
+++ v0/sources/braze/transform.js
@@ -73,7 +73,7 @@ const process = events => {
   const responses = [];
 
   // Ref: Custom Currents Connector Partner Dev Documentation.pdf
-  const eventList = events[0].events;
+  const eventList = Array.isArray(events) ? events[0].events : events.events;
   eventList.forEach(event => {
     try {
       const resp = processEvent(event);

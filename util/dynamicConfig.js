3b93a6862d89456024cb565266649aed09ad41b6
Ankit Kumar <ankit8540988674@gmail.com>
2022-01-11 16:16:01 +0530

    included recursive way to update the fields

--- util/dynamicConfig.js
+++ util/dynamicConfig.js
@@ -16,20 +16,54 @@ function setDynamicConfigValue(event, value, config, field) {
   }
 }
 
+function getVal(value, event) {
+  if (Array.isArray(value)) {
+    value.forEach(key => {
+      getVal(key, event);
+    });
+  } else if (typeof value === "object") {
+    Object.keys(value).forEach(obj => {
+      if (typeof value[obj] === "object") {
+        getVal(value[obj], event);
+      } else if (typeof obj === "string") {
+        let val = value[obj];
+        // we need to configure the value here itself else we will not have path
+        if (val && val.startsWith("{{") && val.endsWith("}}")) {
+          val = val.replace("{{", "").replace("}}", "");
+          if (val.includes("||")) {
+            const path = val.split("||")[0].trim();
+            const getFieldVal = get(event, path);
+            if (getFieldVal) {
+              value[obj] = getFieldVal;
+              unset(event, path);
+            } else {
+              value[obj] = JSON.parse(val.split("||")[1].trim()).toString();
+            }
+          }
+        }
+      }
+    });
+  }
+}
+
 function getDynamicConfig(event) {
   const { Config } = event.destination;
   if (Config) {
     Object.keys(Config).forEach(field => {
       // let value = Config[field].toString().trim();
       let value = Config[field];
-      if (typeof value === "object") {
+      if (Array.isArray(value)) {
+        // console.log(value);
         value.forEach(obj => {
-          Object.keys(obj).forEach(key => {
-            const val = obj[key];
-            if (val.startsWith("{{") && val.endsWith("}}")) {
-              setDynamicConfigValue(event, val, Config, field);
-            }
-          });
+          getVal(obj, event);
+          // console.log(valArr);
+          // if (valArr.length > 0) {
+          //   valArr.forEach(val => {
+          //     if (val && val.startsWith("{{") && val.endsWith("}}")) {
+          //       setDynamicConfigValue(event, val, Config, field);
+          //     }
+          //   });
+          // }
         });
       } else {
         value = value.toString().trim();
@@ -41,6 +75,31 @@ function getDynamicConfig(event) {
   }
   return event;
 }
+// function getDynamicConfig(event) {
+//   const { Config } = event.destination;
+//   if (Config) {
+//     Object.keys(Config).forEach(field => {
+//       // let value = Config[field].toString().trim();
+//       let value = Config[field];
+//       if (typeof value === "object") {
+//         value.forEach(obj => {
+//           Object.keys(obj).forEach(key => {
+//             const val = obj[key];
+//             if (val.startsWith("{{") && val.endsWith("}}")) {
+//               setDynamicConfigValue(event, val, Config, field);
+//             }
+//           });
+//         });
+//       } else {
+//         value = value.toString().trim();
+//         if (value.startsWith("{{") && value.endsWith("}}")) {
+//           setDynamicConfigValue(event, value, Config, field);
+//         }
+//       }
+//     });
+//   }
+//   return event;
+// }
 
 function processDynamicConfig(event, type) {
   if (type === "router" || type === "batch") {

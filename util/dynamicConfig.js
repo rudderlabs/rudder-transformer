b45ea821e7d550f3333d11c25117c29b4f2e0f2d
Ankit Kumar <ankit8540988674@gmail.com>
2022-01-13 18:10:56 +0530

    code cleaning and removing test cases

--- util/dynamicConfig.js
+++ util/dynamicConfig.js
@@ -28,20 +28,9 @@ function getVal(value, event) {
         getVal(value[obj], event);
       } // if we encounter string (actual values), then we start configuring it
       else if (typeof value[obj] === "string") {
-        let val = value[obj];
-        // we need to configure the value here itself else we will not have path
+        const val = value[obj];
         if (val && val.startsWith("{{") && val.endsWith("}}")) {
-          val = val.replace("{{", "").replace("}}", "");
-          if (val.includes("||")) {
-            const path = val.split("||")[0].trim();
-            const getFieldVal = get(event, path);
-            if (getFieldVal) {
-              value[obj] = getFieldVal;
-              unset(event, path);
-            } else {
-              value[obj] = JSON.parse(val.split("||")[1].trim()).toString();
-            }
-          }
+          setDynamicConfigValue(event, val, value, obj);
         }
       }
     });
@@ -52,20 +41,10 @@ function getDynamicConfig(event) {
   const { Config } = event.destination;
   if (Config) {
     Object.keys(Config).forEach(field => {
-      // let value = Config[field].toString().trim();
       let value = Config[field];
       if (Array.isArray(value)) {
-        // console.log(value);
         value.forEach(obj => {
           getVal(obj, event);
-          // console.log(valArr);
-          // if (valArr.length > 0) {
-          //   valArr.forEach(val => {
-          //     if (val && val.startsWith("{{") && val.endsWith("}}")) {
-          //       setDynamicConfigValue(event, val, Config, field);
-          //     }
-          //   });
-          // }
         });
       } else {
         value = value.toString().trim();
@@ -77,31 +56,6 @@ function getDynamicConfig(event) {
   }
   return event;
 }
-// function getDynamicConfig(event) {
-//   const { Config } = event.destination;
-//   if (Config) {
-//     Object.keys(Config).forEach(field => {
-//       // let value = Config[field].toString().trim();
-//       let value = Config[field];
-//       if (typeof value === "object") {
-//         value.forEach(obj => {
-//           Object.keys(obj).forEach(key => {
-//             const val = obj[key];
-//             if (val.startsWith("{{") && val.endsWith("}}")) {
-//               setDynamicConfigValue(event, val, Config, field);
-//             }
-//           });
-//         });
-//       } else {
-//         value = value.toString().trim();
-//         if (value.startsWith("{{") && value.endsWith("}}")) {
-//           setDynamicConfigValue(event, value, Config, field);
-//         }
-//       }
-//     });
-//   }
-//   return event;
-// }
 
 function processDynamicConfig(event, type) {
   if (type === "router" || type === "batch") {

7801ba3da72900c196d677abea3c58ea196295b7
Ankit Kumar <ankit8540988674@gmail.com>
2022-01-16 17:09:35 +0530

    removed default value 'all' from sharingFilter

--- v0/destinations/af/transform.js
+++ v0/destinations/af/transform.js
@@ -89,7 +89,7 @@ function responseBuilderSimple(payload, message, destination) {
     updatedPayload.bundleIdentifier = bundleIdentifier;
   }
 
-  const sharingFilter = destination.Config.sharingFilter || "all";
+  const { sharingFilter } = destination.Config.sharingFilter;
   if (isDefinedAndNotNull(sharingFilter)) {
     updatedPayload.sharing_filter = sharingFilter;
   }

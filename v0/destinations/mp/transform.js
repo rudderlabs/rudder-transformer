8307d24257dde8363de2dbacfbc377b603804525
Akash Gupta <64877812+akashrpo@users.noreply.github.com>
2022-02-21 18:39:26 +0530

    rudder-saas <- master (#1003)
    * VDM-Amplitude: added visual mapper for am (#990)
    
    * VDM-Amplitude: added visual mapper for am(test cases remaining)
    
    * VDM-Amplitude: added test cases
    
    * VDM-Amplitude: removed unnecessary if
    
    * VDM-Amplitude: addressed review comments
    
    * Mixpanel: eliminate removeUndefinedAndNullValues function (#1001)
    
    * remove removeUndefinedAndNullValues
    
    * Dropping undefined properties from the payload
    
    * Removing redundant check
    
    * Active Campaign - fix invalid json issue and add case for empty err.response (#1000)
    
    * fix invalid json issue and add case for empty err.response
    
    * add errorHandler in utility func for ac
    
    Co-authored-by: Utsab Chowdhury <utsab.c97@gmail.com>
    
    Co-authored-by: Ruchira Moitra <31311577+ruchiramoitra@users.noreply.github.com>
    Co-authored-by: Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
    Co-authored-by: Utsab Chowdhury <utsab.c97@gmail.com>
    Co-authored-by: Sai Kumar B <88789928+saikumarrs@users.noreply.github.com>

--- v0/destinations/mp/transform.js
+++ v0/destinations/mp/transform.js
@@ -169,8 +169,6 @@ function getTransformedJSON(message, mappingJson, useNewMapping) {
     ["traits", "context.traits"],
     MP_IDENTIFY_EXCLUSION_LIST
   );
-  rawPayload = removeUndefinedAndNullValues(rawPayload);
-
   /*
   we are adding backward compatibility using useNewMapping key.
   TODO :: This portion need to be removed after we deciding to stop 

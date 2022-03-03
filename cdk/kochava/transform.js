b2b5352a83fdb70a8863e92b6d967b5422e734e5
anuraj <anurajguha@gmail.com>
2022-03-03 19:51:15 +0530

    update kochava screen postMapper and default yaml

--- cdk/kochava/transform.js
+++ cdk/kochava/transform.js
@@ -2,8 +2,14 @@ const { Utils } = require("rudder-transformer-cdk");
 
 function screenPostMapper(event, mappedPayload, rudderContext) {
   // TODO: throw error here if destination.Config.apiKey is missing?
-  const { message } = event;
+  const { message, destination } = event;
   const eventData = message.properties || {};
+  const eventJson = {
+    action: "event",
+    kochava_app_id: destination.Config.apiKey,
+    kochava_device_id: Utils.getValueFromMessage(message, ["context.device.id", "anonymousId"])
+  };
+
   let eventName = "screen view";
   if (message.properties && message.properties.name) {
     eventName += ` ${message.properties.name}`;
@@ -62,7 +68,7 @@ function screenPostMapper(event, mappedPayload, rudderContext) {
     event_name: eventName
   };
 
-  return { ...mappedPayload, ...additionalMapping };
+  return { ...eventJson, data: { ...mappedPayload, ...additionalMapping } };
 }
 
 module.exports = { screenPostMapper };

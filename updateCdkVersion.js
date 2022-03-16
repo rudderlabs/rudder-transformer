113287b350e0c83a9fe6cceb3f4866bd992012d4
anuraj <anurajguha@gmail.com>
2022-03-16 23:57:20 +0530

    initial commit for cdk version update script

--- updateCdkVersion.js
+++ updateCdkVersion.js
@@ -1,3 +1,7 @@
-console.log('node command line args', process.argv)
-const p = require('./package.json')
-console.log('from transformer', p)
+const fs = require("fs");
+
+const cdkVersion = process.argv[2];
+const pData = require("./package.json");
+
+pData.dependencies["rudder-transformer-cdk"] = cdkVersion;
+fs.writeFileSync("./package.json", JSON.stringify(pData));

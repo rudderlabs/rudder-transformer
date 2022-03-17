bab7e4c1cc6d347b3c94894335028346602ecc92
Sai Sankeerth <sanpj2292@github.com>
2022-03-17 11:20:55 +0530

    - Prettification of JSON with spacing 2 - Making sure version has '^'

--- updateCdkVersion.js
+++ updateCdkVersion.js
@@ -3,5 +3,5 @@ const fs = require("fs");
 const cdkVersion = process.argv[2];
 const pData = require("./package.json");
 
-pData.dependencies["rudder-transformer-cdk"] = cdkVersion;
-fs.writeFileSync("./package.json", JSON.stringify(pData));
+pData.dependencies["rudder-transformer-cdk"] = `^${cdkVersion}`;
+fs.writeFileSync("./package.json", JSON.stringify(pData, null, 2));

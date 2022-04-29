fcd1eb34e6457759058e07de4d4dddb40c0303f1
Akash Gupta <64877812+akashrpo@users.noreply.github.com>
2022-04-29 11:21:56 +0530

    feature(new integration): GA4 cloud mode Integration onboarding (#1130)
    * feature(ga4): initial commit
    
    * feature(ga4): add a basic test case
    
    * feature(ga4): add user_properties and parameters constraints
    
    * feature(ga4): add custom parameters for custom events
    
    * feature(ga4): modify inline comments
    
    * feature(ga4): refactor code
    
    * feature(ga4): add items parameters and ga4 events
    
    * feature(ga4): add event mapping
    
    * feature(ga4): modify item properties for generic events
    
    * feature(ga4): add firebase support
    
    * feature(ga4): use network layer for firebase
    
    * feature(ga4): add test case and refactor ordering events
    
    * feature(ga4): add test case for wishlist events
    
    * test(ga4): add test case for sharing events
    
    * test(ga4): add test case for group events
    
    * test(ga4): add test case for GA4 events (1)
    
    * test(ga4): add test case for GA4 events (2)
    
    * test(ga4): add negative test cases
    
    * feature(ga4): take groupId from root and properties
    
    * feature(ga4): remove async from process()
    
    * feature(ga4): refactor code
    
    * feature(ga4): take user_properties from message.properties
    
    * feature(ga4): refactor code
    
    * feature(ga4): refactor code
    
    * feature(ga4): Onboarding Proxy for GA4
    
    * feature(ga4): add firebase test case
    
    Co-authored-by: Utsab <utsab.c97@gmail.com>

---
+++ __tests__/ga4.test.js
@@ -0,0 +1,33 @@
+const integration = "ga4";
+const name = "Google Analytics 4";
+
+const fs = require("fs");
+const path = require("path");
+const version = "v0";
+
+const transformer = require(`../${version}/destinations/${integration}/transform`);
+
+// Processor Test files
+const inputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_input.json`)
+);
+const outputDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_output.json`)
+);
+const inputData = JSON.parse(inputDataFile);
+const expectedData = JSON.parse(outputDataFile);
+
+describe(`${name} Tests`, () => {
+  describe("Processor", () => {
+    inputData.forEach(async (input, index) => {
+      it(`Payload - ${index}`, async () => {
+        try {
+          const output = await transformer.process(input);
+          expect(output).toEqual(expectedData[index]);
+        } catch (error) {
+          expect(error.message).toEqual(expectedData[index].error);
+        }
+      });
+    });
+  });
+});
---
+++ v0/destinations/ga4/utils.js
@@ -0,0 +1,310 @@
+const get = require("get-value");
+const { proxyRequest } = require("../../../adapters/network");
+const {
+  getDynamicMeta,
+  processAxiosResponse
+} = require("../../../adapters/utils/networkUtils");
+const {
+  constructPayload,
+  CustomError,
+  flattenJson,
+  isEmptyObject,
+  extractCustomFields,
+  isDefinedAndNotNull,
+  isHttpStatusSuccess
+} = require("../../util");
+const { TRANSFORMER_METRIC } = require("../../util/constant");
+const ErrorBuilder = require("../../util/error");
+const { mappingConfig, ConfigCategory } = require("./config");
+
+/**
+ * converts to Unix timestamp (in microseconds)
+ * @param {*} timestamp
+ * @returns
+ */
+function msUnixTimestamp(timestamp) {
+  const time = new Date(timestamp);
+  return time.getTime() * 1000 + time.getMilliseconds();
+}
+
+/**
+ * Reserved event names cannot be used
+ * Ref - https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_names
+ * @param {*} event
+ * @returns
+ */
+function isReservedEventName(event) {
+  const reservedEventNames = [
+    "ad_activeview",
+    "ad_click",
+    "ad_exposure",
+    "ad_impression",
+    "ad_query",
+    "adunit_exposure",
+    "app_clear_data",
+    "app_install",
+    "app_update",
+    "app_remove",
+    "error",
+    "first_open",
+    "first_visit",
+    "in_app_purchase",
+    "notification_dismiss",
+    "notification_foreground",
+    "notification_open",
+    "notification_receive",
+    "os_update",
+    "screen_view",
+    "session_start",
+    "user_engagement"
+  ];
+
+  return reservedEventNames.includes(event.toLowerCase());
+}
+
+/* Event parameters */
+/**
+ * Reserved parameter names cannot be used
+ * Here user_properties is a duplicate key hence excluding it.
+ * Ref - https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_parameter_names
+ */
+const GA4_RESERVED_PARAMETER_EXCLUSION = [
+  "firebase_conversion",
+  "user_properties"
+];
+
+/**
+ * event parameter names cannot start with reserved prefixes
+ * Ref - https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_parameter_names
+ * @param {*} parameter
+ */
+function removeReservedParameterPrefixNames(parameter) {
+  const reservedPrefixesNames = ["google_", "ga_", "firebase_"];
+
+  if (!parameter) {
+    return;
+  }
+
+  Object.keys(parameter).forEach(key => {
+    const valFound = reservedPrefixesNames.some(prefix => {
+      if (key.toLowerCase().startsWith(prefix)) {
+        return true;
+      }
+      return false;
+    });
+
+    // reject if found
+    if (valFound) {
+      // eslint-disable-next-line no-param-reassign
+      delete parameter[key];
+    }
+  });
+}
+
+/* user property */
+/**
+ * Reserved user property cannot be used
+ * Ref - https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_user_property_names
+ */
+const GA4_RESERVED_USER_PROPERTY_EXCLUSION = [
+  "first_open_time",
+  "first_visit_time",
+  "last_deep_link_referrer",
+  "user_id",
+  "first_open_after_install"
+];
+
+/**
+ * user property names cannot start with reserved prefixes
+ * Ref - https://developers.google.com/analytics/devguides/collection/protocol/ga4/reference?client_type=gtag#reserved_user_property_names
+ * @param {*} userProperties
+ */
+function removeReservedUserPropertyPrefixNames(userProperties) {
+  const reservedPrefixesNames = ["google_", "ga_", "firebase_"];
+
+  if (!userProperties) {
+    return;
+  }
+
+  Object.keys(userProperties).forEach(key => {
+    const valFound = reservedPrefixesNames.some(prefix => {
+      if (key.toLowerCase().startsWith(prefix)) {
+        return true;
+      }
+      return false;
+    });
+
+    // reject if found
+    if (valFound) {
+      // eslint-disable-next-line no-param-reassign
+      delete userProperties[key];
+    }
+  });
+}
+
+/* For custom events */
+/**
+ * Reserved custom event names cannot be used (Web)
+ * Ref - https://support.google.com/analytics/answer/10085872#zippy=%2Creserved-prefixes-and-event-names%2Cweb
+ * @param {*} event
+ * @returns
+ */
+function isReservedWebCustomEventName(event) {
+  const reservedEventNames = [
+    "app_remove",
+    "app_store_refund",
+    "app_store_subscription_cancel",
+    "app_store_subscription_convert",
+    "app_store_subscription_renew",
+    "first_open",
+    "first_visit",
+    "in_app_purchase",
+    "session_start",
+    "user_engagement"
+  ];
+
+  return reservedEventNames.includes(event.toLowerCase());
+}
+
+/**
+ * Reserved custom event name cannot start with reserved prefixes (Web)
+ * Ref - https://support.google.com/analytics/answer/10085872#zippy=%2Creserved-prefixes-and-event-names%2Cweb
+ * @param {*} event
+ * @returns
+ */
+function isReservedWebCustomPrefixName(event) {
+  const reservedPrefixesNames = ["_", "firebase_", "ga_", "google_", "gtag."];
+
+  // As soon as a single true is returned, .some() will itself return true and stop
+  return reservedPrefixesNames.some(prefix => {
+    if (event.toLowerCase().startsWith(prefix)) {
+      return true;
+    }
+    return false;
+  });
+}
+
+const GA4_ITEM_EXCLUSION = [
+  "item_id",
+  "itemId",
+  "product_id",
+
+  "item_name",
+  "itemName",
+  "name",
+
+  "coupon",
+
+  "item_category",
+  "itemCategory",
+  "category",
+
+  "item_brand",
+  "itemBrand",
+  "brand",
+
+  "item_variant",
+  "itemVariant",
+  "variant",
+
+  "price",
+  "quantity",
+
+  "index",
+  "position"
+];
+
+/**
+ * get items properties for a event.
+ * @param {*} message
+ * @returns
+ */
+function getDestinationItemProperties(message, isItemsRequired) {
+  let items;
+  const products = get(message, "properties.products");
+  if ((!products && isItemsRequired) || (products && products.length === 0)) {
+    throw new CustomError(
+      `Products is an required field for '${message.event}' event`,
+      400
+    );
+  }
+  if (products && Array.isArray(products)) {
+    items = [];
+    products.forEach((item, index) => {
+      let element = constructPayload(
+        item,
+        mappingConfig[ConfigCategory.ITEMS.name]
+      );
+      if (
+        !isDefinedAndNotNull(element.item_id) &&
+        !isDefinedAndNotNull(element.item_name)
+      ) {
+        throw new CustomError("One of product_id or name is required", 400);
+      }
+
+      let itemProperties = {};
+      itemProperties = extractCustomFields(
+        message,
+        itemProperties,
+        [`properties.products.${index}`],
+        GA4_ITEM_EXCLUSION
+      );
+      if (!isEmptyObject(itemProperties)) {
+        itemProperties = flattenJson(itemProperties);
+        element = { ...element, ...itemProperties };
+      }
+
+      items.push(element);
+    });
+  } else if (products && !Array.isArray(products)) {
+    throw new CustomError("Invalid type. Expected Array of products", 400);
+  }
+  return items;
+}
+const responseHandler = (destinationResponse, dest) => {
+  const message = `[GA4 Response Handler] - Request Processed Successfully`;
+  let { status } = destinationResponse;
+  if (status === 204) {
+    status = 200;
+  }
+  // if the responsee from destination is not a success case build an explicit error
+  if (!isHttpStatusSuccess(status)) {
+    throw new ErrorBuilder()
+      .setStatus(status)
+      .setMessage(
+        `[GA4 Response Handler] Request failed for destination ${dest} with status: ${status}`
+      )
+      .isTransformResponseFailure(true)
+      .setDestinationResponse(destinationResponse)
+      .setStatTags({
+        destination: dest,
+        stage: TRANSFORMER_METRIC.TRANSFORMER_STAGE.RESPONSE_TRANSFORM,
+        scope: TRANSFORMER_METRIC.MEASUREMENT_TYPE.API.SCOPE,
+        meta: getDynamicMeta(status)
+      })
+      .build();
+  }
+  return {
+    status,
+    message,
+    destinationResponse
+  };
+};
+const networkHandler = function() {
+  this.responseHandler = responseHandler;
+  this.proxy = proxyRequest;
+  this.processAxiosResponse = processAxiosResponse;
+};
+
+module.exports = {
+  networkHandler,
+  msUnixTimestamp,
+  isReservedEventName,
+  GA4_RESERVED_PARAMETER_EXCLUSION,
+  removeReservedParameterPrefixNames,
+  GA4_RESERVED_USER_PROPERTY_EXCLUSION,
+  removeReservedUserPropertyPrefixNames,
+  isReservedWebCustomEventName,
+  isReservedWebCustomPrefixName,
+  getDestinationItemProperties
+};

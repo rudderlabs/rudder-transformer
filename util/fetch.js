18bcffcaf8a76cce7c9fe19cbea7518c3ad4948c
Utsab <utsab.c97@gmail.com>
2022-05-12 00:16:45 +0530

    adding-logs

--- util/fetch.js
+++ util/fetch.js
@@ -1,14 +1,20 @@
 const fetch = require("node-fetch");
 const HttpsProxyAgent = require("https-proxy-agent");
 const _ = require("lodash");
+const logger = require("../logger");
 
 const fetchWithProxy = (url, options = {}) => {
   const instanceOptions = {
     ...options
   };
-
+  logger.debug("logging existing agent", JSON.stringify(options));
+  logger.debug("fetch with proxy detecting proxy: ", process.env.HTTPS_PROXY);
   if (!options.agent && process.env.HTTPS_PROXY) {
     instanceOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
+    logger.debug(
+      "added proxy to instance options: ",
+      JSON.stringify(instanceOptions)
+    );
   }
 
   if (_.isEmpty(instanceOptions)) {

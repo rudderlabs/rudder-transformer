696dfe067de6f71e418161ec2369e3588a24f8a6
srikanth <srikanth.iit@gmail.com>
2022-05-13 13:29:48 -0500

    Skip proxy for NO_PROXY domains

--- util/fetch.js
+++ util/fetch.js
@@ -3,13 +3,28 @@ const HttpsProxyAgent = require("https-proxy-agent");
 const _ = require("lodash");
 const logger = require("../logger");
 
+const skipProxy = url => {
+  const domain = new URL(url).hostname;
+  if (process.env.NO_PROXY) {
+    const noProxyList = process.env.NO_PROXY.split(",");
+    // eslint-disable-next-line no-restricted-syntax
+    for (const noProxyItem of noProxyList) {
+      if (domain.trim().endsWith(noProxyItem.trim())) {
+        logger.info(`Skipping proxy for ${domain}`);
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
 const fetchWithProxy = (url, options = {}) => {
   const instanceOptions = {
     ...options
   };
   logger.info("logging existing agent", JSON.stringify(options));
   logger.info("fetch with proxy detecting proxy: ", process.env.HTTPS_PROXY);
-  if (!options.agent && process.env.HTTPS_PROXY) {
+  if (!options.agent && !skipProxy(url) && process.env.HTTPS_PROXY) {
     instanceOptions.agent = new HttpsProxyAgent(process.env.HTTPS_PROXY);
     logger.info(
       "added proxy to instance options: ",

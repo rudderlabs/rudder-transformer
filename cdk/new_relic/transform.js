ade8f2c550b19d18e75d1ada3540ef3caa4a05ac
RudderStack <rohithkaza@gmail.com>
2022-05-13 18:47:33 +0530

    feature/new-relic: adding changes to keep parity with segment

--- cdk/new_relic/transform.js
+++ cdk/new_relic/transform.js
@@ -1,4 +1,6 @@
 // const { flattenJson } = require("../../v0/util");
+const { boolean } = require("is");
+const { isBoolean, isDate } = require("lodash");
 const { Utils } = require("rudder-transformer-cdk");
 
 function commonPostMapper(event, mappedPayload, rudderContext) {
@@ -22,6 +24,73 @@ function commonPostMapper(event, mappedPayload, rudderContext) {
     if (message.anonymousId) {
       payload.anonymousId = message.anonymousId;
     }
+  }
+
+  // These are NRQL reserved words in New Relic
+  const reservedNrqlWords = [
+    "ago",
+    "and",
+    "as",
+    "auto",
+    "begin",
+    "begintime",
+    "compare",
+    "day",
+    "days",
+    "end",
+    "endtime",
+    "explain",
+    "facet",
+    "from",
+    "hour",
+    "hours",
+    "in",
+    "is",
+    "like",
+    "limit",
+    "minute",
+    "minutes",
+    "month",
+    "months",
+    "not",
+    "null",
+    "offset",
+    "or",
+    "raw",
+    "second",
+    "seconds",
+    "select",
+    "since",
+    "timeseries",
+    "until",
+    "week",
+    "weeks",
+    "where",
+    "with"
+  ];
+  const reservedWords = ["accountId", "appId", "eventType", "timestamp"];
+
+  const delKeys = [];
+
+  Object.keys(payload).forEach(item => {
+    if (reservedNrqlWords.includes(item)) {
+      const str = `'${item}'`;
+      payload[str] = payload[item];
+      delKeys.push(item);
+    }
+
+    if (reservedWords.includes(item)) {
+      delKeys.push(item);
+    }
+
+    if (isBoolean(payload[item])) {
+      payload[item] = payload[item].toString();
+    }
+  });
+
+  for (let i = 0; i < delKeys.length; i++) {
+    const v = delKeys[i];
+    delete payload.v;
   }
 
   // Upon users choice for data center, we are updating the endpoint accordingly

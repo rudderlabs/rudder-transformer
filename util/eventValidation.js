70daa5ebe8a25dd9802d531903c359e362904fec
Akash Chetty <achetty.iitr@gmail.com>
2022-05-27 02:50:44 +0530

    Review comments

--- util/eventValidation.js
+++ util/eventValidation.js
@@ -277,7 +277,7 @@ async function handleValidation(event) {
 
         const violationsByType = new Set(validationErrors.map(err => err.type));
         for (const [key, val] of Object.entries(mergedTpConfig)) {
-            const value = val.toString().toLowerCase();
+            const value = val?.toString()?.toLowerCase();
             switch (key) {
                 case "allowUnplannedEvents": {
                     const exists = violationsByType.has(violationTypes.UnplannedEvent);

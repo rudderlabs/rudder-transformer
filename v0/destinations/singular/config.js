195be9d4e15cdbea58499130ca681d7bf2756b6a
sauravlal-rudderstack <sauravlal@rudderstack.com>
2022-06-06 16:45:13 +0530

    feature[Singular]: update mapping and address review comments

--- v0/destinations/singular/config.js
+++ v0/destinations/singular/config.js
@@ -2,6 +2,9 @@ const { getMappingConfig } = require("../../util");
 
 const BASE_URL = "https://s2s.singular.net/api/v1";
 
+/* Supported events in Singular: SessionNotification, EventNotification
+   ref: https://support.singular.net/hc/en-us/articles/360048588672-Server-to-Server-S2S-API-Endpoint-Reference
+ */
 const CONFIG_CATEGORIES = {
   SESSION: {
     name: "SINGULARSessionNotificationConfig",
@@ -12,6 +15,33 @@ const CONFIG_CATEGORIES = {
     type: "track"
   }
 };
+
+const SINGULAR_SESSION_EXCLUSION = [
+  "install_source",
+  "install_receipt",
+  "asid",
+  "openuri",
+  "install_ref",
+  "custom_user_id",
+  "n",
+  "utime",
+  "attribution_token",
+  "skan_conversion_value",
+  "skan_first_call_timestamp",
+  "skan_last_call_timestamp"
+];
+
+const SINGULAR_EVENT_EXCLUSION = [
+  "skan_conversion_value",
+  "skan_first_call_timestamp",
+  "skan_last_call_timestamp",
+  "is_revenue_event",
+  "amt",
+  "cur",
+  "purchase_receipt",
+  "purchase_product_id",
+  "purchase_transaction_id"
+];
 
 const sessionEvents = [
   "Application Installed",
@@ -24,5 +54,7 @@ module.exports = {
   CONFIG_CATEGORIES,
   MAPPING_CONFIG,
   sessionEvents,
+  SINGULAR_SESSION_EXCLUSION,
+  SINGULAR_EVENT_EXCLUSION,
   BASE_URL
 };
--- v0/destinations/singular/transform.js
+++ v0/destinations/singular/transform.js
@@ -1,64 +1,101 @@
+const get = require("get-value");
 const {
   CONFIG_CATEGORIES,
   MAPPING_CONFIG,
   BASE_URL,
-  sessionEvents
+  sessionEvents,
+  SINGULAR_SESSION_EXCLUSION,
+  SINGULAR_EVENT_EXCLUSION
 } = require("./config");
 const {
   defaultRequestConfig,
   constructPayload,
   CustomError,
-  ErrorMessage,
   defaultGetRequestConfig,
   removeUndefinedAndNullAndEmptyValues,
-  isAppleFamily
+  isAppleFamily,
+  getSuccessRespEvents,
+  getErrorRespEvents,
+  extractCustomFields
 } = require("../../util");
-const get = require("get-value");
 
 const responseBuilderSimple = (message, { Config }) => {
   let eventType = message.event;
+  // trim and replace spaces with "_"
   eventType = eventType.trim().replace(/\s+/g, " ");
   const response = defaultRequestConfig();
+
+  /* eslint no-param-reassign: "error" */
+  const setPayload = payload => {
+    if (isAppleFamily(payload.p)) {
+      payload.idfa = get(message, "context.device.advertisingId");
+      payload.idfv = get(message, "context.device.id");
+      /*
+        Android - Captured from SDK. No need to pass it explicitly
+        iOS - No need to capture from SDK. Need to pass it explicitly under properties.ua.
+       */
+      payload.ua = get(message, "properties.ua");
+    } else if (payload.p.toLowerCase() === "android") {
+      payload.aifa = get(message, "context.device.advertisingId");
+      payload.andi = get(message, "context.device.id");
+    }
+    return payload;
+  };
+
   if (sessionEvents.includes(eventType)) {
-    const sessionPayload = constructPayload(
+    let sessionPayload = constructPayload(
       message,
       MAPPING_CONFIG[CONFIG_CATEGORIES.SESSION.name]
     );
     if (!sessionPayload) {
       // fail-safety for developer error
-      throw new CustomError(ErrorMessage.FailedToConstructPayload, 400);
+      throw new CustomError("Failed to Create Session Payload", 400);
     }
-    if (isAppleFamily(sessionPayload.p)) {
-      sessionPayload.idfa = get(message, "context.device.advertisingId");
-      sessionPayload.idfv = get(message, "context.device.id");
-    } else if (sessionPayload.p.toLowerCase() === "android") {
-      sessionPayload.aifa = get(message, "context.device.advertisingId");
-      sessionPayload.andi = get(message, "context.device.id");
-    }
+    const eventAttributes = {};
+    extractCustomFields(
+      message,
+      eventAttributes,
+      ["properties"],
+      SINGULAR_SESSION_EXCLUSION
+    );
+    sessionPayload = {
+      ...sessionPayload,
+      ...setPayload(sessionPayload, message),
+      e: eventAttributes
+    };
+    // sessionPayload = extractCustomFields(message, sessionPayload, )
+    // Singular maps Connection Type to either wifi or carrier
+    sessionPayload.c = message.context?.network?.wifi ? "wifi" : "carrier";
     /*
         if att_authorization_status is true then dnt will be false,
         else by default dnt value is true
     */
     sessionPayload.dnt = !sessionPayload.att_authorization_status;
-    response.endpoint = `${BASE_URL}/launch?a=${Config.apiKey}&`;
+    response.endpoint = `${BASE_URL}/launch`;
+    sessionPayload = { ...sessionPayload, a: Config.apiKey };
     response.params = removeUndefinedAndNullAndEmptyValues(sessionPayload);
   } else {
-    const eventPayload = constructPayload(
+    let eventPayload = constructPayload(
       message,
       MAPPING_CONFIG[CONFIG_CATEGORIES.EVENT.name]
     );
     if (!eventPayload) {
       // fail-safety for developer error
-      throw new CustomError(ErrorMessage.FailedToConstructPayload, 400);
+      throw new CustomError("Failed to Create Event Payload", 400);
     }
-    if (isAppleFamily(eventPayload.p)) {
-      eventPayload.idfa = get(message, "context.device.advertisingId");
-      eventPayload.idfv = get(message, "context.device.id");
-    } else if (eventPayload.p.toLowerCase() === "android") {
-      eventPayload.aifa = get(message, "context.device.advertisingId");
-      eventPayload.andi = get(message, "context.device.id");
-    }
-    response.endpoint = `${BASE_URL}/evt?a=${Config.apiKey}&`;
+    response.endpoint = `${BASE_URL}/evt`;
+    const eventAttributes = {};
+    extractCustomFields(
+      message,
+      eventAttributes,
+      ["properties"],
+      SINGULAR_EVENT_EXCLUSION
+    );
+    eventPayload = {
+      ...eventPayload,
+      ...setPayload(eventPayload, message),
+      e: eventAttributes
+    };
     response.params = removeUndefinedAndNullAndEmptyValues(eventPayload);
   }
   response.method = defaultGetRequestConfig.requestMethod;
@@ -80,4 +117,43 @@ const processEvent = (message, destination) => {
 const process = event => {
   return processEvent(event.message, event.destination);
 };
-exports.process = process;
+
+const processRouterDest = async inputs => {
+  if (!Array.isArray(inputs) || inputs.length <= 0) {
+    const respEvents = getErrorRespEvents(null, 400, "Invalid event array");
+    return [respEvents];
+  }
+
+  const respList = await Promise.all(
+    inputs.map(async input => {
+      try {
+        if (input.message.statusCode) {
+          // already transformed event
+          return getSuccessRespEvents(
+            input.message,
+            [input.metadata],
+            input.destination
+          );
+        }
+        // if not transformed
+        return getSuccessRespEvents(
+          await process(input),
+          [input.metadata],
+          input.destination
+        );
+      } catch (error) {
+        return getErrorRespEvents(
+          [input.metadata],
+          error.response
+            ? error.response.status
+            : error.code
+            ? error.code
+            : 400,
+          error.message || "Error occurred while processing payload."
+        );
+      }
+    })
+  );
+  return respList;
+};
+module.exports = { process, processRouterDest };

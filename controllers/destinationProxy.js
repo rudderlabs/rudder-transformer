5db6bba5b9399aad7af79858fa1d0a578212e9b1
saikumarrs <saibattinoju@rudderstack.com>
2022-06-13 16:40:23 +0530

    Added a TODO

--- controllers/destinationProxy.js
+++ controllers/destinationProxy.js
@@ -2,6 +2,7 @@ class DestProxyController {
   static handleProxyTestRequest = async (ctx) => {
     const destination = ctx.params.destination;
     const { transformerResponse, destinationRequest } = ctx.request.body;
+    // TODO: Add logic here
     ctx.status = 200;
     return ctx.body;
   };

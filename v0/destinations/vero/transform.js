ec379ff475258a1e78288ebbad1214672532557f
sandeepdigumarty <sandeepdsvs@gmail.com>
2022-07-04 00:47:06 +0530

    feature (New Integration): addressed review comments

--- v0/destinations/vero/transform.js
+++ v0/destinations/vero/transform.js
@@ -6,67 +6,59 @@ const {
   constructPayload,
   getValueFromMessage,
   isDefinedAndNotNull,
-  isDefinedAndNotNullAndNotEmpty
+  removeUndefinedAndNullAndEmptyValues
 } = require("../../util");
 const { EventType } = require("../../../constants");
 const { CONFIG_CATEGORIES, MAPPING_CONFIG } = require("./config");
 
 // This function handles the common response payload for the supported calls
-const commonResponseBuilder = async (message, category, destination) => {
-  const response = defaultRequestConfig();
-
+const commonResponseBuilder = (message, category, destination) => {
   const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
   const authToken = get(destination, "Config.authToken");
   set(payload, "auth_token", authToken);
 
-  // channels is defined only for Identify call. For other calls both address and platform are undefined
-  const address = get(payload, "channels.address");
-  const platform = get(payload, "channels.platform");
-  /*
-    here we are adding a K-V pair in the channels object, i.e
-    channels : {
-      "type" : "push"
+  // channels is defined only for Identify call
+  if (category.type === EventType.IDENTIFY) {
+    const channels = get(payload, "channels");
+    const address = get(channels, "address");
+    const platform = get(channels, "platform");
+
+    // Vero only accepts a valid channels object - address, platform and type should be defined and not null
+    if (isDefinedAndNotNull(address) && isDefinedAndNotNull(platform)) {
+      set(payload, "channels.type", category.channelType);
     }
-    This is a mandatory but also unchanging field for channels object.
-  */
-  if (isDefinedAndNotNull(address) && isDefinedAndNotNull(platform)) {
-    set(payload, "channels.type", category.channelType);
+    // Added this block because channels & channels.device can be defined and not null, which should be removed
+    else if (isDefinedAndNotNull(channels)) {
+      delete payload.channels;
+    }
   }
 
+  const response = defaultRequestConfig();
   response.endpoint = category.endpoint;
-  response.body.JSON = payload;
+  response.body.JSON = removeUndefinedAndNullAndEmptyValues(payload);
   response.method = category.method;
 
   return response;
 };
 
-// This function handles identify and track requests as well as
-// addition-removal of tags.
-// Identify Ref - https://developers.getvero.com/?bash#users-identify
-// Track Ref - https://developers.getvero.com/?bash#events-track
-const identifyAndTrackResponseBuilder = async (
-  message,
-  category,
-  destination
-) => {
+// This function handles identify and track requests as well as addition-removal of tags.
+const identifyAndTrackResponseBuilder = (message, category, destination) => {
   const respList = [];
-  const response = await commonResponseBuilder(message, category, destination);
+  const response = commonResponseBuilder(message, category, destination);
   respList.push(response);
 
-  // Tags are being passed around from Integrations object.
-  // This block handles any tag add or removal requests.
+  // This block handles any tag addition or removal requests.
   // Ref - https://developers.getvero.com/?bash#tags
   const tags = getValueFromMessage(message, "integrations.Vero.tags");
   const addTags = get(tags, "add");
   const removeTags = get(tags, "remove");
   // Both add and remove should be an array and at least one should have a length > 0
   if (
-    isDefinedAndNotNullAndNotEmpty(tags) &&
-    ((Array.isArray(removeTags) && removeTags.length > 0) ||
-      (Array.isArray(addTags) && addTags.length > 0))
+    (Array.isArray(removeTags) && removeTags.length > 0) ||
+    (Array.isArray(addTags) && addTags.length > 0)
   ) {
     const tagCategory = CONFIG_CATEGORIES.TAGS;
-    const tagsResponse = await commonResponseBuilder(
+    const tagsResponse = commonResponseBuilder(
       message,
       tagCategory,
       destination
@@ -77,27 +69,18 @@ const identifyAndTrackResponseBuilder = async (
   return respList;
 };
 
-// This function handles alias calls.
-// Ref - https://developers.getvero.com/?bash#users-alias
-const aliasResponseBuilder = async (message, category, destination) => {
-  return commonResponseBuilder(message, category, destination);
-};
-
 const process = async event => {
   const { message, destination } = event;
-  const authToken = get(destination, "Config.authToken");
-
   if (!message.type) {
     throw new CustomError(
       "Message Type is not present. Aborting message.",
       400
     );
   }
+
+  const authToken = get(destination, "Config.authToken");
   if (!authToken) {
-    throw new CustomError(
-      "[VERO] Auth Token required for Authentication.",
-      400
-    );
+    throw new CustomError("Auth Token required for Authentication.", 400);
   }
 
   let response;
@@ -105,21 +88,18 @@ const process = async event => {
   const category = CONFIG_CATEGORIES[message.type.toUpperCase()];
   switch (messageType) {
     case EventType.TRACK:
-      response = await identifyAndTrackResponseBuilder(
-        message,
-        category,
-        destination
-      );
-      break;
     case EventType.IDENTIFY:
-      response = await identifyAndTrackResponseBuilder(
+      // Identify Ref - https://developers.getvero.com/?bash#users-identify
+      // Track Ref - https://developers.getvero.com/?bash#events-track
+      response = identifyAndTrackResponseBuilder(
         message,
         category,
         destination
       );
       break;
     case EventType.ALIAS:
-      response = await aliasResponseBuilder(message, category, destination);
+      // Alias Ref - https://developers.getvero.com/?bash#users-alias
+      response = commonResponseBuilder(message, category, destination);
       break;
     default:
       throw new CustomError(`Message type ${messageType} not supported`, 400);

7d68601d3b3b8c1f634b81a861f02c9eabd157e1
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2022-07-05 16:48:26 +0530

    Enhancement(integration): Mixpanel , addressed review comments

--- __tests__/mixpanel.test.js
+++ __tests__/mixpanel.test.js
@@ -1,3 +1,4 @@
+const { base64decode } = require('nodejs-base64');
 const integration = "mp";
 const name = "Mixpanel";
 
@@ -18,9 +19,18 @@ const expectedData = JSON.parse(outputDataFile);
 Date.now = jest.fn(() => new Date(Date.UTC(2020, 0, 25)).valueOf());
 inputData.forEach((input, index) => {
   test(`${name} Tests: payload - ${index}`, () => {
-    let output, expected;
+    let output, expected, arrayOutput = [];
     try {
       output = transformer.process(input);
+      if(Array.isArray(output) && output.length >= 1) {
+        output.forEach((eachPayload) => {
+          const decodedPayload = JSON.parse(Buffer.from(JSON.stringify(eachPayload.params.data), 'base64').toString());
+          arrayOutput.push(decodedPayload);
+        });
+        output = arrayOutput;
+      } else {
+        output = JSON.parse(Buffer.from(JSON.stringify(output.params.data), 'base64').toString());
+      }
       expected = expectedData[index];
     } catch (error) {
       output = error.message;
@@ -42,9 +52,30 @@ const expectedRouterData = JSON.parse(outputRouterDataFile);
 
   describe(`${name} Tests`, () => {
   describe("Router Tests", () => {
+    let arrayOutput = [];
     it("Payload", async () => {
       const routerOutput = await transformer.processRouterDest(inputRouterData);
-      expect(routerOutput).toEqual(expectedRouterData);
+      let singleBatchedPayload = [];
+      routerOutput.forEach((eachPayload,index) => {
+        // when batched payload is an array itself
+        if(Array.isArray(eachPayload.batchedRequest)) {
+
+          const {batchedRequest} = eachPayload;
+          batchedRequest.forEach(request => {
+            const decodedPayload = JSON.parse(Buffer.from(JSON.stringify(request.params.data), 'base64').toString());
+           // creating for one single batch array payload
+            singleBatchedPayload.push(decodedPayload);
+          });
+          arrayOutput.push(singleBatchedPayload);
+        } else {
+         // where batched request is a simple object
+          const decodedPayload = JSON.parse(Buffer.from(JSON.stringify(eachPayload.batchedRequest.params.data), 'base64').toString());
+          arrayOutput.push(decodedPayload);
+        }
+        });
+        output = arrayOutput;
+      
+      expect(output).toEqual(expectedRouterData);
     });
   });
   

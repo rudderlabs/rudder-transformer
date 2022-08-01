a33ebd36effad0f1e5075e49e8b1480579ae7beb
RudderStack <rohithkaza@gmail.com>
2022-08-02 02:18:42 +0530

    feature[canny-destination]: createVote

--- v0/destinations/canny/transform.js
+++ v0/destinations/canny/transform.js
@@ -1,29 +1,41 @@
+const { logger } = require("handlebars");
 const { EventType } = require("../../../constants");
+const { httpPOST } = require("../../../adapters/network");
 const { ConfigCategory, mappingConfig, BASE_URL } = require("./config");
 const {
   defaultRequestConfig,
   constructPayload,
   defaultPostRequestConfig,
   removeUndefinedAndNullValues,
-  CustomError
+  CustomError,
+  getHashFromArrayWithDuplicate
 } = require("../../util");
+const {retrieveUserId}=require("./util");
 
-const responseBuilder = (payload, apiKey, endpoint) => {
+const responseBuilder = (
+  payload,
+  apiKey,
+  endpoint,
+  contentType,
+  responseBody
+) => {
   if (payload) {
     const response = defaultRequestConfig();
     response.endpoint = `${BASE_URL}${endpoint}`;
     response.headers = {
       Authorization: `Basic ${apiKey}`,
-      "Content-Type": "application/json"
+      "Content-Type": contentType
     };
     response.method = defaultPostRequestConfig.requestMethod;
-    response.body.JSON = removeUndefinedAndNullValues(payload);
+    response.body[`${responseBody}`] = removeUndefinedAndNullValues(payload);
     return response;
   }
 };
 
 const identifyResponseBuilder = (message, { Config }) => {
   const { apiKey } = Config;
+  const contentType = "application/json";
+  const responseBody = "JSON";
 
   if (!apiKey) {
     throw new CustomError("API Key is not present. Aborting message.", 400);
@@ -34,7 +46,7 @@ const identifyResponseBuilder = (message, { Config }) => {
     mappingConfig[ConfigCategory.IDENTIFY.name]
   );
 
-  if (!payload.userId) {
+  if (!payload.userID) {
     throw new CustomError("UserId is not present. Aborting message.", 400);
   }
   if (!payload.name) {
@@ -43,10 +55,72 @@ const identifyResponseBuilder = (message, { Config }) => {
 
   const { endpoint } = ConfigCategory.IDENTIFY;
 
-  return responseBuilder(payload, apiKey, endpoint);
+  return responseBuilder(payload, apiKey, endpoint, contentType, responseBody);
 };
 
-const processEvent = (message, destination) => {
+const trackResponseBuilder = async (message, { Config }) => {
+  const { apiKey } = Config;
+  const eventsToEvents = Config.eventsToEvents;
+  const eventsMap = getHashFromArrayWithDuplicate(eventsToEvents);
+
+  if (!apiKey) {
+    throw new CustomError("API Key is not present. Aborting message.", 400);
+  }
+
+  let event = message.event?.toLowerCase().trim();
+  if (!event) {
+    throw new CustomError("Event name is required", 400);
+  }
+
+  if (!eventsMap[event]) {
+    throw new CustomError(
+      `Event name (${event}) is not present in the mapping`,
+      400
+    );
+  }
+
+  let endpoint;
+  let payload = {};
+  let responseBody;
+  let contentType;
+
+  Object.keys(eventsMap).forEach(async key => {
+    if (key === event) {
+      eventsMap[event].forEach(async val => {
+        if (val === "createVote") {
+          const postID =
+            message?.postID ||
+            message?.properties?.postID ||
+            message?.postId ||
+            message?.properties?.postId ||
+            message?.properties?.post?.id;
+
+          if (!postID) {
+            throw new CustomError(
+              "postID is not present. Aborting message.",
+              400
+            );
+          }
+
+          payload.apiKey = apiKey;
+          payload.postID = postID;
+          payload.voterID = "voterid";
+          //   payload.voterID = await retrieveUserId(apiKey, message);
+          endpoint = ConfigCategory.CREATE_VOTE.endpoint;
+
+          responseBody = "FORM";
+          contentType = "application/x-www-form-urlencoded";
+        } else if (val === "createPost") {
+          endpoint = ConfigCategory.CREATE_POST.endpoint;
+        }
+      });
+    }
+  });
+
+  return responseBuilder(payload, apiKey, endpoint, contentType, responseBody);
+};
+
+const processEvent = async (message, destination) => {
   if (!message.type) {
     throw new CustomError(
       "Message Type is not present. Aborting message.",
@@ -60,17 +134,17 @@ const processEvent = (message, destination) => {
     case EventType.IDENTIFY:
       response = identifyResponseBuilder(message, destination);
       break;
-    // case EventType.TRACK:
-    //   response = trackResponseBuilder(message, destination);
-    //   break;
+    case EventType.TRACK:
+      response = await trackResponseBuilder(message, destination);
+      break;
     default:
       throw new CustomError("Message type not supported", 400);
   }
   return response;
 };
 
-const process = event => {
-  return processEvent(event.message, event.destination);
+const process = async event => {
+  return await processEvent(event.message, event.destination);
 };
 
 module.exports = { process };
---
+++ v0/destinations/canny/util.js
@@ -0,0 +1,70 @@
+const axios = require("axios");
+const qs = require("qs");
+
+const retrieveUserId = async (apiKey, message) => {
+  try {
+    let cannyId;
+    if (message?.context?.externalId?.type === "cannyUserId") {
+      cannyId = message.context.externalId.value;
+      return cannyId;
+    }
+
+    const url = "https://canny.io/api/v1/users/retrieve";
+    let response;
+
+    const email =
+      message.traits?.email ||
+      message.context?.traits?.email ||
+      message.properties?.email;
+    const userId = message.userId;
+
+    const header = {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Accept: "application/json"
+    };
+
+    if (email) {
+      response = await httpPOST(
+        url,
+        qs.stringify({
+          apiKey: `${apiKey}`,
+          email: `${email}`
+        }),
+        header
+      );
+
+      // If the request fails, throwing error.
+      if (response.success === false) {
+        throw new CustomError(
+          `[Canny]:: CannyUserID can't be gnerated due to ${response.data.error}`,
+          400
+        );
+      }
+
+      return response?.data?.id || null;
+    } else {
+      response = await httpPOST(
+        url,
+        qs.stringify({
+          apiKey: `${apiKey}`,
+          userID: `${userId}`
+        }),
+        header
+      );
+
+      // If the request fails, throwing error.
+      if (response.success === false) {
+        throw new CustomError(
+          `[Canny]:: CannyUserID can't be gnerated due to ${response.data.error}`,
+          400
+        );
+      }
+
+      return response?.data?.id || null;
+    }
+  } catch (error) {
+    throw new CustomError("Axios error", 400);
+  }
+};
+
+module.exports = { retrieveUserId };

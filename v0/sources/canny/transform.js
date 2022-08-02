d58cdfe436969a4ae13312b5f4e17ce2f47f448c
RudderStack <rohithkaza@gmail.com>
2022-08-03 00:27:02 +0530

    feature[canny-source]: refactoring code

--- v0/sources/canny/transform.js
+++ v0/sources/canny/transform.js
@@ -4,9 +4,26 @@ const Message = require("../message");
 const sha256 = require("sha256");
 
 // import mapping json using JSON.parse to preserve object key order
-const mapping = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, "./mapping.json"), "utf-8")
+const voterMapping = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "./voterMapping.json"), "utf-8")
 );
+const authorMapping = JSON.parse(
+  fs.readFileSync(path.resolve(__dirname, "./authorMapping.json"), "utf-8")
+);
+
+function settingIds(message, event, typeOfUser) {
+  try {
+    // setting up userId
+    if (event.object[`${typeOfUser}`]?.userID) {
+      message.userId = event.object[`${typeOfUser}`].userID;
+    } else {
+      // setting up anonymousId if userId is not present
+      message.anonymousId = sha256(event.object[`${typeOfUser}`]?.email);
+    }
+  } catch (e) {
+    throw new Error(`Missing essential fields from Canny. Error: (${e})`);
+  }
+}
 
 // creates message for given type of user(i.e., voter or author)
 function createMessage(event, typeOfUser) {
@@ -14,35 +31,28 @@ function createMessage(event, typeOfUser) {
 
   message.setEventType("track");
 
-  message.setPropertiesV2(event, mapping);
+  if (typeOfUser === "voter") message.setPropertiesV2(event, voterMapping);
+  else message.setPropertiesV2(event, authorMapping);
 
   message.context.integration.version = "1.0.0";
-  try {
-    if (event.object[`${typeOfUser}`]?.userID) {
-      message.userId = event.object[`${typeOfUser}`].userID;
-    } else {
-      // setting up anonymousId if userId is not present
-      message.anonymousId = sha256(event.object[`${typeOfUser}`]?.email);
-    }
 
-    if (event.object[`${typeOfUser}`]?.id) {
-      message.context.externalId = [
-        {
-          type: "cannyUserId",
-          value: event.object[`${typeOfUser}`].id
-        }
-      ];
-    }
+  settingIds(message, event, typeOfUser);
 
-    message.context.traits = event.object[`${typeOfUser}`];
-
-    // deleting already mapped fields
-    delete message.properties[`${typeOfUser}`];
-    delete message.context.traits?.userID;
-    delete message.context.traits?.id;
-  } catch (e) {
-    throw new Error(`Missing essential fields from Canny. Error: (${e})`);
+  if (event.object[`${typeOfUser}`]?.id) {
+    message.context.externalId = [
+      {
+        type: "cannyUserId",
+        value: event.object[`${typeOfUser}`].id
+      }
+    ];
   }
+
+  message.context.traits = event.object[`${typeOfUser}`];
+
+  // deleting already mapped fields
+  delete message.properties[`${typeOfUser}`];
+  delete message.context.traits?.userID;
+  delete message.context.traits?.id;
 
   return message;
 }

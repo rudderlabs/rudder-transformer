1fbf6cbb7abfdfb8563d3b548b2416d637d43cc1
ItsSudip <sudip.paul1997@gmail.com>
2022-08-04 17:52:38 +0530

    feature[Canny]:: Resolving bug.

--- constants/destinationCanonicalNames.js
+++ constants/destinationCanonicalNames.js
@@ -40,7 +40,7 @@ const DestCanonicalNames = {
   mailchimp: ["mailchimp", "MailChimp", "MAILCHIMP"],
   kafka: ["KAFKA", "kafka", "Kafka"],
   vero: ["vero", "Vero", "VERO"],
-  canny: ["canny", "Canny", "CANNY"]
+  canny: ["canny", "Canny", "CANNY"],
   one_signal: [
     "one signal",
     "one_signal",

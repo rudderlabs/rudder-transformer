4bb647faea9010e218db96ea1f8a71ae6a96b324
Sai Sankeerth <sanpj2292@github.com>
2022-08-18 13:58:01 +0530

    log only exceptions with message and metadata information

--- middlewares/log.js
+++ middlewares/log.js
@@ -2,24 +2,40 @@ const util = require("util");
 const moment = require("moment");
 
 // process's pid, date, method, path, http version, status and length
-const LOG_FORMAT = '[pid-%s] - - [%s] "%s %s HTTP/%s" %d %s\n';
+const LOG_FORMAT = '[pid-%s] - - [%s] "%s %s HTTP/%s" %d %s %s %s\n';
 const DATE_FORMAT = "DD/MM/YYYY:HH:mm:sssss";
 
 module.exports = stream => {
   return async function accesslog(ctx, next) {
-    await next();
-
-    (stream || process.stdout).write(
-      util.format(
-        LOG_FORMAT,
-        process.pid,
-        moment().format(DATE_FORMAT),
-        ctx.method,
-        ctx.path,
-        ctx.req.httpVersion,
-        ctx.status,
-        ctx.length ? ctx.length.toString() : "-"
-      )
-    );
+    try {
+      await next();
+    } catch (error) {
+      let msgMetadatas = [];
+      if (ctx.request.body) {
+        const events = ctx.request.body;
+        if (Array.isArray(events)) {
+          msgMetadatas = events.map(({message, metadata}) => ({ message, metadata }));
+        } else if (typeof events === 'object') {
+          msgMetadatas = [ {message: events.message, metadata: events.metadata} ];
+        }
+      }
+      (stream || process.stdout).write(
+        util.format(
+          LOG_FORMAT,
+          process.pid,
+          moment().format(DATE_FORMAT),
+          ctx.method,
+          ctx.path,
+          ctx.req.httpVersion,
+          ctx.status,
+          ctx.length ? ctx.length.toString() : "-",
+          error,
+          JSON.stringify(msgMetadatas)
+        )
+      );
+      // Returning data to the requested client
+      ctx.status = error.status || 500;
+      ctx.body = error.message;
+    }
   };
 };

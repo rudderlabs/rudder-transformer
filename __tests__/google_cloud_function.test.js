6aa1b8ca9b32d07d763d0baed544ed6810ef671e
RohitKhatta <rohitkhatta710@gmail.com>
2022-08-18 17:56:35 +0530

    bugfix(integrations): google cloud function adding batch disabled testcases

--- __tests__/google_cloud_function.test.js
+++ __tests__/google_cloud_function.test.js
@@ -7,21 +7,54 @@ const version = "v0";
 
 const transformer = require(`../${version}/destinations/${integration}/transform`);
 
-// Router Test Data
-const inputRouterDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_router_input.json`)
+// Router Test Data Batch Enabled
+const inputRouterDataFileBatchEnabled = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_router_input_BatchEnable.json`)
 );
-const outputRouterDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_router_output.json`)
+const outputRouterDataFileBatchEnabled = fs.readFileSync(
+  path.resolve(
+    __dirname,
+    `./data/${integration}_router_output_BatchEnable.json`
+  )
 );
-const inputRouterData = JSON.parse(inputRouterDataFile);
-const expectedRouterData = JSON.parse(outputRouterDataFile);
+
+// Router Test Data Batch Disable
+const inputRouterDataFileBatchDisable = fs.readFileSync(
+  path.resolve(
+    __dirname,
+    `./data/${integration}_router_input_BatchDisable.json`
+  )
+);
+const outputRouterDataFileBatchDisable = fs.readFileSync(
+  path.resolve(
+    __dirname,
+    `./data/${integration}_router_output_BatchDisable.json`
+  )
+);
+const inputRouterData1 = JSON.parse(inputRouterDataFileBatchEnabled);
+const expectedRouterData1 = JSON.parse(outputRouterDataFileBatchEnabled);
+const inputRouterData2 = JSON.parse(inputRouterDataFileBatchDisable);
+const expectedRouterData2 = JSON.parse(outputRouterDataFileBatchDisable);
 
 describe(`${name} Tests`, () => {
   describe("Router Tests", () => {
     it("Payload", async () => {
-      const routerOutput = await transformer.processRouterDest(inputRouterData);
-      expect(routerOutput).toEqual(expectedRouterData);
+      const routerOutput = await transformer.processRouterDest(
+        inputRouterData1
+      );
+      expect(routerOutput).toEqual(expectedRouterData1);
+    });
+  });
+});
+
+describe(`${name} Tests`, () => {
+  describe("Router Tests", () => {
+    it("Payload", async () => {
+      const routerOutput = await transformer.processRouterDest(
+        inputRouterData2
+      );
+      console.log(JSON.stringify(routerOutput));
+      expect(routerOutput).toEqual(expectedRouterData2);
     });
   });
 });

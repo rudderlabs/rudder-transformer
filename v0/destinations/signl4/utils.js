eb5c74977f953e1479b793c8d7ab4d5b08caef77
ujjwal-ab <ujjwalabhishek98@gmail.com>
2022-09-20 15:17:57 +0530

    feature(SIGNL4): Add Validation for eventToTitleMapping

--- v0/destinations/signl4/utils.js
+++ v0/destinations/signl4/utils.js
@@ -31,11 +31,13 @@ const populatePayload = (message, Config) => {
   payload = { ...payload, ...properties };
 
   // Overriding event name with Customizable Title if avaiblable
+  if(eventToTitleMapping){
   eventToTitleMapping.forEach(mapping => {
     if (mapping.from === event) {
       payload.Title = mapping.to;
     }
   });
+}
 
   // Populating the payload with the fields(properties) taken from UI
   Object.keys(propertyMappingObj).forEach(element => {

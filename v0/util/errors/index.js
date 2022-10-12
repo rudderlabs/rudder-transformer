4be5a44f8f9cca1b95bb084b8e32f8a9ca66d9ee
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2022-10-12 23:04:07 +0530

    Enhancement(error-class): restructuring error classes

---
+++ v0/util/errors/index.js
@@ -0,0 +1,8 @@
+const { TransformationError, ApiError } = require("./error-classes");
+const { RudderBaseError } = require("./base");
+
+module.exports = {
+  TransformationError,
+  ApiError,
+  RudderBaseError
+};

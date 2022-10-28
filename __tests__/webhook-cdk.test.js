3fef40e42f14701c338e9a35dc81a595da26e3e1
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2022-10-28 11:12:10 +0530

    merging latest master

--- __tests__/webhook-cdk.test.js
+++ __tests__/webhook-cdk.test.js
@@ -1,13 +1,10 @@
 const fs = require("fs");
 const path = require("path");
 const { TRANSFORMER_METRIC } = require("../v0/util/constant");
-const {getWorkflowEngine } = require("../cdk/v2/handler");
+const { processCdkV2Workflow } = require("../cdk/v2/handler");
 
 const integration = "webhook";
 const name = "Webhook";
-
-const procWorkflowEnginePromise = getWorkflowEngine(integration, TRANSFORMER_METRIC.ERROR_AT.PROC);
-const rtWorkflowEnginePromise = getWorkflowEngine(integration, TRANSFORMER_METRIC.ERROR_AT.RT);
 
 const inputDataFile = fs.readFileSync(
   path.resolve(__dirname, `./data/${integration}_input.json`)
@@ -18,39 +15,50 @@ const outputDataFile = fs.readFileSync(
 const inputData = JSON.parse(inputDataFile);
 const expectedData = JSON.parse(outputDataFile);
 
-// Router Test Data
-const inputRouterDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_router_input.json`)
-);
-const outputRouterDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_router_output.json`)
-);
-const inputRouterData = JSON.parse(inputRouterDataFile);
-const expectedRouterData = JSON.parse(outputRouterDataFile);
-
 describe(`${name} Tests`, () => {
   describe("Processor Tests", () => {
     inputData.forEach((input, index) => {
       it(`${name} - payload: ${index}`, async () => {
         const expected = expectedData[index];
         try {
-          const procWorkflowEngine = await procWorkflowEnginePromise;
-          const result = await procWorkflowEngine.execute(input);
-          expect(result.output).toEqual(expected);
+          const output = await processCdkV2Workflow(
+            integration,
+            input,
+            TRANSFORMER_METRIC.ERROR_AT.PROC
+          );
+          expect(output).toEqual(expected);
         } catch (error) {
-          expect(error.message).toEqual(expected.error);
+          expect(error.message).toEqual(expected.message);
         }
       });
     });
   });
 
+  const inputRouterDataFile = fs.readFileSync(
+    path.resolve(__dirname, `./data/${integration}_router_input.json`)
+  );
+  const outputRouterDataFile = fs.readFileSync(
+    path.resolve(__dirname, `./data/${integration}_router_output.json`)
+  );
+  const inputRouterData = JSON.parse(inputRouterDataFile);
+  const expectedRouterData = JSON.parse(outputRouterDataFile);
+
   describe("Router Tests", () => {
-    it("Payload", async () => {
-      const rtWorkflowEngine = await rtWorkflowEnginePromise;
-      const result = await rtWorkflowEngine.execute(inputRouterData);
-      expect(JSON.parse(JSON.stringify(result.output))).toEqual(
-        expectedRouterData
-      );
+    inputRouterData.forEach((input, index) => {
+      it(`${name} - payload: ${index}`, async () => {
+        const expected = expectedRouterData[index];
+        try {
+          const output = await processCdkV2Workflow(
+            integration,
+            input,
+            TRANSFORMER_METRIC.ERROR_AT.RT
+          );
+          expect(output).toEqual(expected);
+        } catch (error) {
+          // console.log(error);
+          expect(error.message).toEqual(expected.message);
+        }
+      });
     });
   });
 });

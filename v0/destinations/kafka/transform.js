82b1f6796b172cc98ace78ce798caf32ad71b8d4
Utsab Chowdhury <utsab.c97@gmail.com>
2022-11-02 18:17:27 +0530

    feat(kafka multi topic support): updated for release dependency of unblocking messages without topic

--- v0/destinations/kafka/transform.js
+++ v0/destinations/kafka/transform.js
@@ -1,10 +1,10 @@
 /* eslint-disable no-restricted-syntax */
 const groupBy = require("lodash/groupBy");
 const cloneDeep = require("lodash/cloneDeep");
-const { getDynamicMeta } = require("../../../adapters/utils/networkUtils");
+// const { getDynamicMeta } = require("../../../adapters/utils/networkUtils");
 const { getIntegrationsObj } = require("../../util");
-const { TRANSFORMER_METRIC } = require("../../util/constant");
-const ErrorBuilder = require("../../util/error");
+// const { TRANSFORMER_METRIC } = require("../../util/constant");
+// const ErrorBuilder = require("../../util/error");
 
 function batch(destEvents) {
   const respList = [];
@@ -35,19 +35,20 @@ function process(event) {
   const integrationsObj = getIntegrationsObj(message, "kafka");
   const { schemaId } = integrationsObj || {};
   const topic = integrationsObj?.topic || destination.Config?.topic;
-  if (!topic) {
-    throw new ErrorBuilder()
-      .setStatus(400)
-      .setMessage("Topic is required for Kafka destination")
-      .isTransformResponseFailure(true)
-      .setStatTags({
-        destType: "KAFKA",
-        stage: TRANSFORMER_METRIC.TRANSFORMER_STAGE.RESPONSE_TRANSFORM,
-        scope: TRANSFORMER_METRIC.MEASUREMENT_TYPE.API.SCOPE,
-        meta: getDynamicMeta(400)
-      })
-      .build();
-  }
+  // TODO: Remove commented lines after server release
+  // if (!topic) {
+  //   throw new ErrorBuilder()
+  //     .setStatus(400)
+  //     .setMessage("Topic is required for Kafka destination")
+  //     .isTransformResponseFailure(true)
+  //     .setStatTags({
+  //       destType: "KAFKA",
+  //       stage: TRANSFORMER_METRIC.TRANSFORMER_STAGE.RESPONSE_TRANSFORM,
+  //       scope: TRANSFORMER_METRIC.MEASUREMENT_TYPE.API.SCOPE,
+  //       meta: getDynamicMeta(400)
+  //     })
+  //     .build();
+  // }
   const userId = message.userId || message.anonymousId;
   if (schemaId) {
     return {

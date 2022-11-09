2714fe0887532df815ca80ff791e8042abb73246
Sai Sankeerth <sanpj2292@github.com>
2022-11-09 15:15:09 +0530

    feat: add unit-tests to verify the delete users request preparation

--- __tests__/deleteUsers.test.js
+++ __tests__/deleteUsers.test.js
@@ -14,22 +14,78 @@ const deleteUserDestinations = [
 ];
 const { handleDeletionOfUsers } = require("../versionedRouter");
 const { default: axios } = require("axios");
+const version = "v0";
 
+const buildPrepareDeleteRequestTestCases = () => {
+  const testCases = deleteUserDestinations
+  .reduce((acc, currentVal) => {
+    try {
+      const delUsers = require(`../${version}/destinations/${currentVal}/deleteUsers`);
+      const inputJson = require(`./data/${currentVal}_deleteUsers_req_input.json`);
+      const outputJson = require(`./data/${currentVal}_deleteUsers_req_output.json`);
+      acc.push({
+        inputJson,
+        outputJson,
+        destination: currentVal,
+        method: delUsers?.prepareDeleteRequest
+      });
+    } catch (error) {
+      // Do nothing
+    }
+    return acc;
+  }, [])
+  .map(v => {
+    const { inputJson, outputJson, method, destination } = v;
+    const cases = inputJson.map((reqInput, ind) => {
+      
+      return [
+        destination,
+        {
+          input: reqInput,
+          prepareDeleteRequest: method,
+          expected: outputJson[ind],
+          index: ind
+        }
+      ];
+    });
+    return cases;
+  }).flat();
+  return testCases;
+}
+const reqBuildTestCases = buildPrepareDeleteRequestTestCases();
 
+describe("DeleteUsers request build tests", () => {
+  describe.each(reqBuildTestCases)(
+    ".prepareDeleteRequest(%s)",
+    (_destination, { input, prepareDeleteRequest, expected, index }) => {
+      test(`Payload - ${index}`, () => {
+        expect(
+          prepareDeleteRequest?.(
+            input.userAttributes,
+            input.config,
+            input.destInfo
+          )
+        ).toEqual(expected);
+      });
+    }
+  );
+});
 
 // delete user tests
 deleteUserDestinations.forEach(destination => {
   const inputData = require(`./data/${destination}_deleteUsers_proxy_input.json`);
   const expectedData = require(`./data/${destination}_deleteUsers_proxy_output.json`);
-  
+
   describe(`${name} Tests: ${destination}`, () => {
     beforeAll(() => {
       let axiosResponses;
       try {
         axiosResponses = require(`./data/${destination}_deleteUsers_response.json`);
       } catch (error) {
-       // Do nothing
-       logger.error(`Error while reading ${destination}_deleteUsers_response.json: ${error}`)
+        // Do nothing
+        logger.error(
+          `Error while reading ${destination}_deleteUsers_response.json: ${error}`
+        );
       }
       if (Array.isArray(axiosResponses)) {
         formAxiosMock(axiosResponses);
@@ -38,7 +94,7 @@ deleteUserDestinations.forEach(destination => {
         jest.mock("axios");
         axios.mockImplementation(mockedAxiosClient);
       }
-    })
+    });
     inputData.forEach((input, index) => {
       it(`Payload - ${index}`, async () => {
         try {

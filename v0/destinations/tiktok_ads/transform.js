9e97ca27adc4a541b1aa1cba9dedf0b1b64f28f1
Arnab <arnab.compsc@gmail.com>
2022-11-17 10:36:20 +0530

    Added partner_name for TikTok Ads destination

--- v0/destinations/tiktok_ads/transform.js
+++ v0/destinations/tiktok_ads/transform.js
@@ -99,8 +99,11 @@ const getTrackResponse = (message, Config, event) => {
 
   response.method = defaultPostRequestConfig.requestMethod;
   response.endpoint = TRACK_ENDPOINT;
-  response.body.JSON = removeUndefinedAndNullValues(payload);
-
+  // add partner name
+  response.body.JSON = removeUndefinedAndNullValues({
+    ...payload,
+    partner_name: "RudderStack"
+  });
   return response;
 };
 
@@ -187,6 +190,8 @@ function batchEvents(eventsChunk) {
     chunk.forEach(ev => {
       // Pixel code must be added above "batch": [..]
       delete ev.message.body.JSON.pixel_code;
+      // Partner name must be added above "batch": [..]
+      delete ev.message.body.JSON.partner_name;
       ev.message.body.JSON.type = "track";
       batchResponseList.push(ev.message.body.JSON);
       metadata.push(ev.metadata);
@@ -194,6 +199,7 @@ function batchEvents(eventsChunk) {
 
     batchEventResponse.batchedRequest.body.JSON = {
       pixel_code: pixelCode,
+      partner_name: "RudderStack",
       batch: batchResponseList
     };
 

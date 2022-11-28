a424e9b7f4cef026eec018eb7301f567391b6d14
saikumarrs <saibattinoju@rudderstack.com>
2022-11-28 22:18:37 +0530

    fix: logger utility module

--- logger.js
+++ logger.js
@@ -8,29 +8,29 @@ const levelError = 3; // Logs about errors which dont immediately halt the appli
 
 const logLevel = process.env.LOG_LEVEL
   ? parseInt(process.env.LOG_LEVEL, 10)
-  : levelInfo;
+  : levelError;
 
-const debug = (msg, ...optionalParams) => {
+const debug = (msg = "", ...optionalParams) => {
   if (levelDebug >= logLevel) {
-    console.debug(msg, optionalParams);
+    console.debug(msg, ...optionalParams);
   }
 };
 
-const info = (msg, ...optionalParams) => {
+const info = (msg = "", ...optionalParams) => {
   if (levelInfo >= logLevel) {
-    console.info(msg, optionalParams);
+    console.info(msg, ...optionalParams);
   }
 };
 
-const warn = (msg, ...optionalParams) => {
+const warn = (msg = "", ...optionalParams) => {
   if (levelWarn >= logLevel) {
-    console.warn(msg, optionalParams);
+    console.warn(msg, ...optionalParams);
   }
 };
 
-const error = (msg, ...optionalParams) => {
+const error = (msg = "", ...optionalParams) => {
   if (levelError >= logLevel) {
-    console.error(msg, optionalParams);
+    console.error(msg, ...optionalParams);
   }
 };
 

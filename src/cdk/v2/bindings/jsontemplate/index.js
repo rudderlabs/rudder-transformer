d522cfefe8bde5ee215340e267f5b17a92ec8cba
Dilip Kola <kdilipkola@gmail.com>
2022-12-07 17:46:00 +0530

    feat: add json template utils

--- src/cdk/v2/bindings/jsontemplate/index.js
+++ src/cdk/v2/bindings/jsontemplate/index.js
@@ -1,4 +1,4 @@
-const genericFieldMapping = require("../../../../../v0/util/data/GenericFieldMapping.json");
+const genericFieldMapping = require("../../../../v0/util/data/GenericFieldMapping.json");
 
 exports.getGenericPaths = key => {
   return genericFieldMapping[key].map(path => `.${path}`).join(" ?? ");

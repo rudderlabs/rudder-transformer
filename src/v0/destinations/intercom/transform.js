28ddfe4b3dd7c51d22bc90d2bf9c6b1f1918ab6f
Ujjwal Abhishek <63387036+ujjwal-ab@users.noreply.github.com>
2022-12-15 16:08:24 +0530

    feat: refactor router transformer - 4 (#1690)
    * feat: refactor-processRouterDest
    
    * feat: refactor-processRouterDest

--- src/v0/destinations/intercom/transform.js
+++ src/v0/destinations/intercom/transform.js
@@ -13,15 +13,10 @@ const {
   defaultRequestConfig,
   defaultPostRequestConfig,
   getFieldValueFromMessage,
-  getSuccessRespEvents,
-  getErrorRespEvents,
   addExternalIdToTraits,
-  generateErrorObject
+  simpleProcessRouterDest
 } = require("../../util");
-const {
-  TransformationError,
-  InstrumentationError
-} = require("../../util/errorTypes");
+const { InstrumentationError } = require("../../util/errorTypes");
 
 function getCompanyAttribute(company) {
   const companiesList = [];
@@ -175,43 +170,12 @@ function process(event) {
   return response;
 }
 
-const processRouterDest = async inputs => {
-  if (!Array.isArray(inputs) || inputs.length <= 0) {
-    const respEvents = getErrorRespEvents(null, 400, "Invalid event array");
-    return [respEvents];
-  }
-
-  const respList = await Promise.all(
-    inputs.map(async input => {
-      try {
-        if (input.message.statusCode) {
-          // already transformed event
-          return getSuccessRespEvents(
-            input.message,
-            [input.metadata],
-            input.destination
-          );
-        }
-        // if not transformed
-        return getSuccessRespEvents(
-          await process(input),
-          [input.metadata],
-          input.destination
-        );
-      } catch (error) {
-        const erroObj = generateErrorObject(error);
-        return getErrorRespEvents(
-          [input.metadata],
-          error.response
-            ? error.response.status
-            : error.code
-            ? error.code
-            : 400,
-          error.message || "Error occurred while processing payload.",
-          erroObj.statTags
-        );
-      }
-    })
+const processRouterDest = async (inputs, reqMetadata) => {
+  const respList = await simpleProcessRouterDest(
+    inputs,
+    "INTERCOM",
+    process,
+    reqMetadata
   );
   return respList;
 };

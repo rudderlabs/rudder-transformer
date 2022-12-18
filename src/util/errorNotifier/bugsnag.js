1803d95312c82463234c30ea974c5be553c75fdd
saikumarrs <saibattinoju@rudderstack.com>
2022-12-19 01:43:01 +0530

    Revert "Revert "chore: remove unwanted version file""
    This reverts commit 10075afe5bf66e3ecd306c1440269340268ac219.

--- src/util/errorNotifier/bugsnag.js
+++ src/util/errorNotifier/bugsnag.js
@@ -4,6 +4,7 @@ const {
   CustomError: CDKCustomError,
   DataValidationError
 } = require("rudder-transformer-cdk/build/error/index");
+const { logger } = require("../../logger");
 const pkg = require("../../../package.json");
 const { CustomError } = require("../../v0/util");
 const { ApiError, TransformationError } = require("../../v0/util/errors");
@@ -21,37 +22,45 @@ const errorTypesDenyList = [
   DataValidationError
 ];
 
+let bugsnagClient;
+
 function init() {
-  Bugsnag.start({
-    apiKey,
-    appVersion: pkg.version,
-    metadata: {
-      image: {
-        version: imageVersion
+  if (apiKey) {
+    bugsnagClient = Bugsnag.start({
+      apiKey,
+      appVersion: pkg.version,
+      metadata: {
+        image: {
+          version: imageVersion
+        }
+      },
+      onError(event) {
+        event.severity = "error";
       }
-    },
-    onError(event) {
-      event.severity = "error";
-    }
-  });
+    });
+  } else {
+    logger.error(`Invalid Bugsnag API key: ${apiKey}`);
+  }
 }
 
 function notify(err, context, metadata) {
-  const isDeniedErrType = errorTypesDenyList.some(errType => {
-    return err instanceof errType;
-  });
+  if (bugsnagClient) {
+    const isDeniedErrType = errorTypesDenyList.some(errType => {
+      return err instanceof errType;
+    });
 
-  if (isDeniedErrType) return;
+    if (isDeniedErrType) return;
 
-  // For errors thrown in the code using ErrorBuilder
-  // TODO: This need to be cleaned up once the entire code base
-  // moves into a consistent error reporting format
-  if (err.isExpected === true) return;
+    // For errors thrown in the code using ErrorBuilder
+    // TODO: This need to be cleaned up once the entire code base
+    // moves into a consistent error reporting format
+    if (err.isExpected === true) return;
 
-  Bugsnag.notify(err, event => {
-    event.context = context;
-    event.addMetadata("metadata", metadata);
-  });
+    bugsnagClient.notify(err, event => {
+      event.context = context;
+      event.addMetadata("metadata", metadata);
+    });
+  }
 }
 
 module.exports = {

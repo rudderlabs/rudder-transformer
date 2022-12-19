b57bc27f08ccccc8b3761e5a4a4a0158861a19d2
saikumarrs <saibattinoju@rudderstack.com>
2022-12-19 21:23:12 +0530

    fix: req metadata function in user deletion handler

--- src/versionedRouter.js
+++ src/versionedRouter.js
@@ -1235,18 +1235,10 @@ const handleDeletionOfUsers = async ctx => {
   const getReqMetadata = () => {
     try {
       const reqBody = ctx.request.body;
-      if (Array.isArray(reqBody)) {
-        return {
-          jobInfos: reqBody.map(req => {
-            return {
-              jobId: req.jobId,
-              destType: req.destType,
-              destConfig: req.config
-            };
-          })
-        };
-      }
-      return { destType: reqBody?.destType };
+      return {
+        destType: reqBody[0]?.destType,
+        jobs: reqBody.map(req => req.jobId)
+      };
     } catch (error) {
       // Do nothing
     }

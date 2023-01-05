316fc6ef7a2d2b94222837716db9f12cd5ed0617
Jayc <mjayachand@gmail.com>
2023-01-05 13:17:31 +0530

    integration test

--- src/util/openfaas/index.js
+++ src/util/openfaas/index.js
@@ -11,7 +11,7 @@ const { RetryRequestError } = require("../utils");
 const FAAS_BASE_IMG =
   process.env.FAAS_BASE_IMG || "rudderlabs/openfaas-flask:main";
 const FAAS_MAX_PODS_IN_TEXT = process.env.FAAS_MAX_PODS_IN_TEXT || "100";
-const FAAS_REQUESTS_CPU = process.env.FAAS_REQUESTS_CPU || "0.3";
+const FAAS_REQUESTS_CPU = process.env.FAAS_REQUESTS_CPU || "0.5";
 const FAAS_REQUESTS_MEMORY = process.env.FAAS_REQUESTS_MEMORY || "128Mi";
 const FAAS_LIMITS_CPU = process.env.FAAS_LIMITS_CPU || FAAS_REQUESTS_CPU;
 const FAAS_LIMITS_MEMORY =
--- test/__tests__/user_transformation.integration.test.js
+++ test/__tests__/user_transformation.integration.test.js
@@ -2,8 +2,8 @@ const { when } = require("jest-when");
 jest.mock("node-fetch");
 const fetch = require("node-fetch", () => jest.fn());
 
-const { userTransformHandler } = require("../../src/routerUtils");
 const {
+  userTransformHandler,
   setupUserTransformHandler
 } = require("../../src/util/customTransformer");
 const {
@@ -11,7 +11,8 @@ const {
 } = require("../../src/util/customTransformer-faas");
 const {
   deleteFunction,
-  getFunctionList
+  getFunctionList,
+  getFunction
 } = require("../../src/util/openfaas/faasApi");
 const { invalidateFnCache } = require("../../src/util/openfaas/index");
 const { RetryRequestError } = require("../../src/util/utils");
@@ -23,29 +24,32 @@ jest.mock("axios", () => ({
 
 const workspaceId = "workspaceId";
 const versionId = "versionId";
-const TR_REV_CODE = {
-  codeVersion: "1",
-  language: "pythonfaas",
-  testName: "pytest",
-  code: "def transformEvent(event, metadata):\n    return event\n",
-  workspaceId,
-  versionId
+const contructTrRevCode = vid => {
+  return {
+    codeVersion: "1",
+    language: "pythonfaas",
+    testName: "pytest",
+    code: "def transformEvent(event, metadata):\n    return event\n",
+    workspaceId,
+    versionId: vid
+  };
 };
 
 describe("Function Creation Tests", () => {
   afterAll(async done => {
     (await getFunctionList()).forEach(fn => {
-      deleteFunction(fn.name);
+      deleteFunction(fn.name).catch(() => {});
     });
     done();
   });
 
+  const trRevCode = contructTrRevCode(versionId);
   const funcName = generateFunctionName({ workspaceId, versionId }, false);
 
   const expectedData = { success: true, publishedVersion: funcName };
 
   it("Setting up function with testWithPublish as true - creates faas function", async () => {
-    const outputData = await setupUserTransformHandler(TR_REV_CODE, [], true);
+    const outputData = await setupUserTransformHandler(trRevCode, [], true);
 
     expect(outputData).toEqual(expectedData);
 
@@ -57,7 +61,7 @@ describe("Function Creation Tests", () => {
 
   it("Setting up already existing function with testWithPublish as true - return from cache", async () => {
     const fnCreatedAt = (await getFunctionList())[0].createdAt;
-    const outputData = await setupUserTransformHandler(TR_REV_CODE, [], true);
+    const outputData = await setupUserTransformHandler(trRevCode, [], true);
 
     expect(outputData).toEqual(expectedData);
 
@@ -72,24 +76,56 @@ describe("Function Creation Tests", () => {
   it("Setting up already existing function with cache clearing - return retry request error", async () => {
     invalidateFnCache();
     await expect(async () => {
-      await setupUserTransformHandler(TR_REV_CODE, [], true);
+      await setupUserTransformHandler(trRevCode, [], true);
     }).rejects.toThrow(RetryRequestError);
   });
 });
 
 describe("Function invocation & creation tests", () => {
+  afterAll(async done => {
+    (await getFunctionList()).forEach(fn => {
+      deleteFunction(fn.name).catch(() => {});
+    });
+    done();
+  });
+
   it("Function creation & invocation in test mode - Unmodified events returned", async () => {
     const inputEvents = require(`./data/user_transformation_input.json`);
     const outputEvents = require(`./data/user_transformation_pycode_test_output.json`);
 
-    const response = await userTransformHandler()(
+    const trRevCode = contructTrRevCode(versionId);
+
+    const response = await userTransformHandler(
       inputEvents,
       versionId,
       [],
-      TR_REV_CODE,
+      trRevCode,
       true
     );
 
     expect(response).toEqual(outputEvents);
+  });
+
+  it("Function invocation & creation - Creates if not exists", async () => {
+    const inputEvents = require(`./data/user_transformation_input.json`);
+
+    const respBody = contructTrRevCode("1234");
+    const funcName = generateFunctionName(respBody, false);
+
+    const transformerUrl = `https://api.rudderlabs.com/transformation/getByVersionId?versionId=${respBody.versionId}`;
+    when(fetch)
+      .calledWith(transformerUrl)
+      .mockResolvedValue({
+        status: 200,
+        json: jest.fn().mockResolvedValue(respBody)
+      });
+
+    await expect(async () => {
+      await userTransformHandler(inputEvents, respBody.versionId, []);
+    }).rejects.toThrow(RetryRequestError);
+
+    // If function is not found, it will be created
+    const deployedFn = await getFunction(funcName);
+    expect(deployedFn.name).toEqual(funcName);
   });
 });

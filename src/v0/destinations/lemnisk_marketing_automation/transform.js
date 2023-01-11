07750a45726e9b882d0ea65565afab6fa1191a8e
praveendsviz <7056589+praveendsviz@users.noreply.github.com>
2023-01-11 16:31:44 +0530

    feat(destination): onboard lemnisk integration (#1728)
    * Added all transformation logic and test cases
    
    * Modified transforme.js and some tests for Lemnisk integration
    
    Co-authored-by: Praveen DS <--unset>
    Co-authored-by: Anant Jain <62471433+anantjain45823@users.noreply.github.com>

---
+++ src/v0/destinations/lemnisk_marketing_automation/transform.js
@@ -0,0 +1,183 @@
+
+const { EventType } = require("../../../constants");
+const {
+  CONFIG_CATEGORIES,
+  MAPPING_CONFIG,
+  DIAPI_CONFIG_CATEGORIES,
+  DIAPI_MAPPING_CONFIG
+} = require("./config");
+const {
+  constructPayload,
+  defaultPostRequestConfig,
+  removeUndefinedAndNullValues,
+  defaultRequestConfig,
+  getSuccessRespEvents,
+  getErrorRespEvents,
+} = require("../../util");
+const {
+  ConfigurationError,
+  TransformationError,
+  InstrumentationError
+} = require("../../util/errorTypes");
+function responseBuilder(message, category, destination) {
+  let platform, payload, response;
+  const {writeKey, pl, diapi, apiKey, passkey, wk, srcId } = destination.Config;
+  platform = fetchPlatform(destination);
+  response = defaultRequestConfig();
+  response.method = defaultPostRequestConfig.requestMethod;
+  switch(platform){
+    case 'pl':
+      payload= constructPayload(message, MAPPING_CONFIG[category.name]);
+      payload.writeKey = writeKey;
+      payload.context.userAgent = {    
+        ua: payload.context.userAgent
+      }
+      response.endpoint = pl;
+      response.headers = {
+        "Content-Type": "application/json"
+      };
+      response.userId = message.anonymousId || message.userId;
+      break;
+    case 'diapi':
+      payload= constructPayload(message, DIAPI_MAPPING_CONFIG[category.name]);
+      if(wk !== ''){
+        payload.WriteKey = wk;
+      }
+      if(srcId !== ''){
+        payload.srcid = srcId;
+      }
+      response.endpoint = diapi;
+      response.headers = {
+        "Content-Type": "application/json",
+        "x-api-passkey": passkey,
+        "x-api-key": apiKey
+      };
+      response.userId = message.anonymousId || message.userId;
+      break;
+    default:
+      throw new ConfigurationError("Platform type not supported", 400);
+  }
+  if (payload) {
+    switch (category.type) {
+      case "identify":
+        payload.type = "identify";
+        break;
+      case "page":
+        payload.type = "page";
+        break;
+      case "track":
+        payload.type = "track";
+        break;
+      default:
+        throw new InstrumentationError(
+          `Event type ${category.type} is not supported`
+        );
+    }
+    response.body.JSON = removeUndefinedAndNullValues(payload);
+  } else {
+    // fail-safety for developer error
+    throw new TransformationError("Payload could not be constructed");
+  }
+  return response;
+}
+
+const processEvent = (message, destination) => {
+  if (!message.type) {
+    throw new InstrumentationError("Event type is required");
+  }
+
+  const messageType = message.type.toLowerCase();
+  let category, response, platform;
+  platform = fetchPlatform(destination);
+  if(platform === 'pl'){
+    switch (messageType) {
+      case EventType.PAGE:
+        category = CONFIG_CATEGORIES.PAGE;
+        response = responseBuilder(message, category, destination);
+        break;
+      case EventType.IDENTIFY:
+        category = CONFIG_CATEGORIES.IDENTIFY;
+        response = responseBuilder(message, category, destination);
+        break;
+      case EventType.TRACK:
+        category = CONFIG_CATEGORIES.TRACK;
+        response = responseBuilder(message, category, destination);
+        break;
+      default:
+        throw new InstrumentationError(
+          `Event type ${messageType} is not supported`
+        );
+    }
+  }
+  if(platform === 'diapi'){
+    switch (messageType) {
+      case EventType.TRACK:
+        category = DIAPI_CONFIG_CATEGORIES.TRACK;
+        response = responseBuilder(message, category, destination);
+        break;
+      default:
+        throw new InstrumentationError(
+          `Event type ${messageType} is not supported`
+        );
+    }
+  }
+  return response;
+};
+
+const process = event => {
+  return processEvent(event.message, event.destination);
+};
+
+const processRouterDest = async inputs => {
+  if (!Array.isArray(inputs) || inputs.length <= 0) {
+    const respEvents = getErrorRespEvents(null, 400, "Invalid event array");
+    return [respEvents];
+  }
+
+  const respList = await Promise.all(
+    inputs.map(async input => {
+      try {
+        if (input.message.statusCode) {
+          // already transformed event
+          return getSuccessRespEvents(
+            input.message,
+            [input.metadata],
+            input.destination
+          );
+        }
+        // if not transformed
+        return getSuccessRespEvents(
+          await process(input),
+          [input.metadata],
+          input.destination
+        );
+      } catch (error) {
+        return getErrorRespEvents(
+          [input.metadata],
+          error.response
+            ? error.response.status
+            : error.code
+            ? error.code
+            : 400,
+          error.message || "Error occurred while processing payload."
+        );
+      }
+    })
+  );
+  return respList;
+};
+
+function fetchPlatform(destination){
+  let platform = '';
+  const {writeKey, pl, diapi, apiKey, passkey } = destination.Config;
+  if(diapi!=='' && apiKey!=='' && passkey!=='' && pl === '' && writeKey ===''){
+    platform = 'diapi'
+  }else if(pl!== '' && writeKey !== '' && diapi === '' && apiKey === '' && passkey === ''){
+    platform = 'pl';
+  }else{
+    throw new TransformationError("Payload contains invalid configuration");
+  }
+  return platform
+}
+
+module.exports = { process, processRouterDest };

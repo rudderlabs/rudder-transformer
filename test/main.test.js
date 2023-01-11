eb148cbe408cf545a5141d8db882c24fe8a2c7b4
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-01-11 16:04:40 +0530

    refactor test files

--- test/main.test.js
+++ test/main.test.js
@@ -4,9 +4,7 @@ const path = require("path");
 
 let dirIndex;
 
-const supertest = require("supertest");
-
-const app = require("./app");
+const { executeTests } = require("./utilities/util");
 
 process.argv.forEach((arg, index) => {
   if (arg.includes("--root")) {
@@ -19,54 +17,6 @@ if (!dirIndex) {
 const rootDir =
   dirIndex === -1 ? "test_dirs" : process.argv[dirIndex].split("=")[1];
 const tests = path.join(__dirname, rootDir);
-
-const version = "v0";
-
-const destRoutePath = `/${version}/destinations/`;
-const sourceRoutePath = `/${version}/sources/`;
-
-// execution
-const executeTests = (dataPoint, index) => {
-  switch (dataPoint.testType) {
-    case "proc_tf":
-      it(`${index}. ${dataPoint.destinationName} destination (proc_tf) tests - ${dataPoint.description}`, async () => {
-        let response = await supertest(app.callback())
-          .post(`${destRoutePath}${dataPoint.destinationName}`)
-          .set("Accept", "application/json")
-          .send(dataPoint.input);
-        expect(response.statusCode).toBe(200);
-        response = JSON.parse(response.text);
-        expect(response[0]).toEqual(dataPoint.output);
-      });
-      break;
-    case "router_tf":
-      it(`${index}. ${dataPoint.destinationName} destination (router_tf) tests -`, async () => {
-        let response = await supertest(app.callback())
-          .post(`/routerTransform`)
-          .send(dataPoint.input);
-        expect(response.statusCode).toBe(200);
-        response = JSON.parse(response.text);
-        expect(response.output).toEqual(dataPoint.output);
-      });
-      break;
-    case "source_tf":
-      it(`${index}. ${dataPoint.sourceName} Source Tests: payload: ${index}`, async () => {
-        let response = await supertest(app.callback())
-          .post(`${sourceRoutePath}${dataPoint.sourceName}`)
-          .send(dataPoint.input);
-        console.log(response);
-        expect(response.statusCode).toBe(200);
-        response = JSON.parse(response.text);
-        console.log(`source: ${response}`);
-        expect(response[0].error ? response[0] : response[0].output).toEqual(
-          dataPoint.output
-        );
-      });
-      break;
-    default:
-      break;
-  }
-};
 
 // Execute tests
 const testFile = inputDataFile => {
@@ -95,7 +45,7 @@ function getFilePaths(dir, filePaths = []) {
   return filePaths;
 }
 
-// Iterate over all the files in test_dirs
+// Iterate over all the test files(".test.json") in root dir test_dirs(default)
 const testDirectory = tests;
 const filePaths = getFilePaths(testDirectory);
 
@@ -104,53 +54,3 @@ filePaths.forEach(file => {
     testFile(fs.readFileSync(file));
   }
 });
-
-// const executeTests = (dataPoint, index) => {
-//   const transformer = getExecutionPath(dataPoint, dataPoint.testType);
-//   switch (dataPoint.testType) {
-//     case "proc_tf":
-//       it(`${index}. ${dataPoint.destinationName} destination (proc_tf) tests - ${dataPoint.description}`, async () => {
-//         const response = await axiosClient.post(
-//           `${BASE_URL}/${version}/${dataPoint.destinationName}`,
-//           dataPoint.input
-//         );
-//         const output = response.data[0].error
-//           ? response.data[0]
-//           : response.data[0].output;
-//         expect(response.status).toEqual(200);
-//         expect(output).toEqual(dataPoint.output);
-//       });
-//       break;
-//     case "router_tf":
-//       it(`${index}. ${dataPoint.destinationName} destination (router_tf) tests -`, async () => {
-//         const output = await getDestHandler(
-//           version,
-//           dataPoint.destinationName
-//         ).processRouterDest(dataPoint.input);
-//         expect(output).toEqual(dataPoint.output);
-//       });
-//       break;
-//     case "source_tf":
-//       it(`${index}. ${dataPoint.sourceName} Source Tests: payload: ${index}`, () => {
-//         try {
-//           const output = transformer.process(dataPoint.input);
-//           expect(output).toEqual(dataPoint.output);
-//         } catch (error) {
-//           expect(error.message).toEqual(dataPoint.output);
-//         }
-//       });
-//       break;
-//     default:
-//       break;
-//   }
-// };
-
-// // Execute tests
-// const testFile = inputDataFile => {
-//   const testData = JSON.parse(inputDataFile);
-//   describe("Test", () => {
-//     testData.forEach((dataPoint, index) => {
-//       executeTests(dataPoint, index, axiosClient);
-//     });
-//   });
-// };
--- test/utilities/util.js
+++ test/utilities/util.js
@@ -1,4 +1,3 @@
-import { getDestHandler } from "../../src/versionedRouter";
 const supertest = require("supertest");
 
 const app = require("../app");
@@ -8,24 +7,25 @@ const version = "v0";
 const destRoutePath = `/${version}/destinations/`;
 const sourceRoutePath = `/${version}/sources/`;
 
+// execution
 const executeTests = (dataPoint, index) => {
   switch (dataPoint.testType) {
     case "proc_tf":
       it(`${index}. ${dataPoint.destinationName} destination (proc_tf) tests - ${dataPoint.description}`, async () => {
         let response = await supertest(app.callback())
           .post(`${destRoutePath}${dataPoint.destinationName}`)
+          .set("Accept", "application/json")
           .send(dataPoint.input);
         expect(response.statusCode).toBe(200);
         response = JSON.parse(response.text);
-        expect(response[0].error ? response[0] : response[0].output).toEqual(
-          dataPoint.output
-        );
+        expect(response[0]).toEqual(dataPoint.output);
       });
       break;
     case "router_tf":
       it(`${index}. ${dataPoint.destinationName} destination (router_tf) tests -`, async () => {
         let response = await supertest(app.callback())
           .post(`/routerTransform`)
+          .set("Accept", "application/json")
           .send(dataPoint.input);
         expect(response.statusCode).toBe(200);
         response = JSON.parse(response.text);
@@ -37,12 +37,11 @@ const executeTests = (dataPoint, index) => {
         let response = await supertest(app.callback())
           .post(`${sourceRoutePath}${dataPoint.sourceName}`)
           .send(dataPoint.input);
+        console.log(response);
         expect(response.statusCode).toBe(200);
         response = JSON.parse(response.text);
         console.log(`source: ${response}`);
-        expect(response[0].error ? response[0] : response[0].output).toEqual(
-          dataPoint.output
-        );
+        expect(response[0]).toEqual(dataPoint.output);
       });
       break;
     default:

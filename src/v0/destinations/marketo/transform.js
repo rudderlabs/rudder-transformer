5364b771bd5da3dace8c2207b0d290dc68c949a6
Sai Sankeerth <sanpj2292@github.com>
2023-01-12 11:34:45 +0530

    fix: making use of optional chaining for safety

--- src/v0/destinations/marketo/transform.js
+++ src/v0/destinations/marketo/transform.js
@@ -518,7 +518,7 @@ const processRouterDest = async (inputs, reqMetadata) => {
       errObj.message,
       errObj.statTags
     );
-    return [{ ...respEvents, destination: inputs[0].destination }];
+    return [{ ...respEvents, destination: inputs?.[0]?.destination }];
   }
 
   // Checking previous status Code. Initially setting to false.
--- src/v0/util/index.js
+++ src/v0/util/index.js
@@ -1640,7 +1640,7 @@ const handleRtTfSingleEventError = (input, error, reqMetadata) => {
     ...getEventReqMetadata(input)
   });
 
-  return { ...resp, destination: input.destination };
+  return { ...resp, destination: input?.destination };
 };
 
 /**

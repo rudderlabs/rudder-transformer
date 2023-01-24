d36dc346b85612458f441226b3baadb9427e88fc
Anant <anant@rudderstack.com>
2023-01-24 19:43:00 +0530

    feat: added batching

--- src/v0/destinations/criteo_audience/config.js
+++ src/v0/destinations/criteo_audience/config.js
@@ -2,7 +2,9 @@ const BASE_ENDPOINT = "https://api.criteo.com/2022-10/";
 
 const operation = ["add", "remove"];
 
+const MAX_IDENTIFIERS = 50000;
 module.exports = {
   BASE_ENDPOINT,
-  operation
+  operation,
+  MAX_IDENTIFIERS
 };

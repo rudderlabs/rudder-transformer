695a7828bab631897d0cae1845975448872e0bfd
Sai Sankeerth <sanpj2292@github.com>
2023-01-30 16:53:06 +0530

    fix: add env support for delay introduction

--- src/v0/destinations/default_dest/networkHandler.js
+++ src/v0/destinations/default_dest/networkHandler.js
@@ -4,6 +4,16 @@ const { processAxiosResponse } = require("../../../adapters/utils/networkUtils")
 // eslint-disable-next-line no-promise-executor-return
 const sleep = (s) => new Promise((r) => setTimeout(r, s*1000));
 
+const convertEnvToInt = env => {
+  try {
+    return parseInt(env, 10);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error);
+    return 0;
+  }
+}
+
 /**
  * depricating: handles proxying requests to destinations from server, expects requsts in "defaultRequestConfig"
  * note: needed for test api
@@ -12,7 +22,10 @@ const sleep = (s) => new Promise((r) => setTimeout(r, s*1000));
  */
 // eslint-disable-next-line no-unused-vars
 const destDefProxyRequest = async _request => {
-  await sleep(0.01);
+  const delayFactor = convertEnvToInt(process.env.DELAY_FACTOR)
+  const sleepTime = convertEnvToInt(process.env.DEFAULT_SLEEP_TIME);
+  const defaultSleepTime = sleepTime > 0 ? sleepTime : 0.01;
+  await sleep(defaultSleepTime + (Math.random() * delayFactor));
   return {
     response: {
       status: 200,

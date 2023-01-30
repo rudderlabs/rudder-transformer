8bb9c3e8942be8279d322ac5e55c3e2cc9d8ffa8
saikumarrs <saibattinoju@rudderstack.com>
2023-01-30 19:39:01 +0530

    refactor: format test script and data files

--- test/__tests__/__integration_util_test__/testHelper.js
+++ test/__tests__/__integration_util_test__/testHelper.js
@@ -1,13 +1,12 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
 const getFuncTestData = (funcName) => {
-  const fileData = fs.readFileSync(
-  path.resolve(__dirname, `./data/${funcName}.json`));
+  const fileData = fs.readFileSync(path.resolve(__dirname, `./data/${funcName}.json`));
   const testData = JSON.parse(fileData);
   return testData;
 };
 
 module.exports = {
-  getFuncTestData
-}
+  getFuncTestData,
+};
--- test/__tests__/__integration_util_test__/v0_utilities.test.js
+++ test/__tests__/__integration_util_test__/v0_utilities.test.js
@@ -1,30 +1,26 @@
-
-const utilities = require("../../../src/v0/util");
-const { getFuncTestData } = require("./testHelper");
+const utilities = require('../../../src/v0/util');
+const { getFuncTestData } = require('./testHelper');
 
 const functionsToTest = [
-  "flattenJson",
-  "getDestinationExternalID",
-  "isHybridModeEnabled",
-  "handleSourceKeysOperation",
-  "getValueFromPropertiesOrTraits",
-  "getErrorStatusCode"
-]
+  'flattenJson',
+  'getDestinationExternalID',
+  'isHybridModeEnabled',
+  'handleSourceKeysOperation',
+  'getValueFromPropertiesOrTraits',
+  'getErrorStatusCode',
+];
 
-describe("Utility Functions Tests", () => {
-  describe.each(functionsToTest)("%s Tests", (funcName) => {
+describe('Utility Functions Tests', () => {
+  describe.each(functionsToTest)('%s Tests', (funcName) => {
     const funcTestData = getFuncTestData(funcName);
-    test.each(funcTestData)(
-      "$description",
-      async ({description, input, output}) => {
-        let result;
-        if (Array.isArray(input)) {
-          result = utilities[funcName](...input);
-        } else {
-          result = utilities[funcName](input);
-        }
-        expect(result).toEqual(output);
+    test.each(funcTestData)('$description', async ({ description, input, output }) => {
+      let result;
+      if (Array.isArray(input)) {
+        result = utilities[funcName](...input);
+      } else {
+        result = utilities[funcName](input);
       }
-    );
+      expect(result).toEqual(output);
+    });
   });
 });

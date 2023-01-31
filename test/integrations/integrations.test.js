612a1db16041d31d56e15dae870a0dece6b56485
saikumarrs <saibattinoju@rudderstack.com>
2023-01-31 20:52:44 +0530

    feat: add source transformation test handler

--- test/integrations/integrations.test.js
+++ test/integrations/integrations.test.js
@@ -20,56 +20,72 @@ const rootDir = __dirname;
 const allTestDataFilePaths = getTestDataFilePaths(rootDir);
 const DEFAULT_VERSION = "v0";
 
-const destinationTestHandler = async (tcData) => {
+const testRoute = async (route, tcData) => {
   const inputReq = tcData.input.request;
-  let response;
-  try {
-    let routePath;
-    switch (tcData.feature) {
-      case tags.FEATURES.PROCESSOR:
-        routePath = `/${path.join(tcData.version || DEFAULT_VERSION, "destinations", tcData.name)}`;
-        break;
-      case tags.FEATURES.ROUTER:
-        routePath = `/routerTransform`;
-        break;
-      // TODO: Implement for other feature types
-    }
-
-    routePath = path.join(routePath, tcData.input.pathSuffix);
-
-    let stHandler = supertest(app.callback());
-    switch (inputReq.method) {
-      case "GET":
-        stHandler = stHandler.get(routePath);
-        break;
-      // TODO: Implement other methods here
-      default:
-        stHandler = stHandler.post(routePath);
-        break;
-    }
-
-    response = await stHandler
-    .set(inputReq.headers)
-    .query(inputReq.params)
-    .send(inputReq.body);
-  } catch (error) {
-    console.log(error);
-    expect(true).toEqual(false);
+  let stHandler = supertest(app.callback());
+  const { headers, params, body } = inputReq;
+  switch (inputReq.method) {
+    case "GET":
+      stHandler = stHandler.get(route);
+      break;
+    case "PUT":
+      stHandler = stHandler.put(route);
+      break;
+    case "DELETE":
+      stHandler = stHandler.delete(route);
+      break;
+    default:
+      stHandler = stHandler.post(route);
+      break;
   }
+
+  const response = await stHandler
+  .set(headers)
+  .query(params)
+  .send(body);
 
   const outputResp = tcData.output.response;
   expect(response.status).toEqual(outputResp.status);
-  expect(response.body).toEqual(outputResp.body);
+
+  if (outputResp && outputResp.body) {
+    expect(response.body).toEqual(outputResp.body);
+  }
   
-  if (outputResp.headers !== undefined)
+  if (outputResp.headers !== undefined) {
     expect(response.headers).toEqual(outputResp.headers);
+  }
+}
+
+const destinationTestHandler = async (tcData) => {
+  let route;
+  switch (tcData.feature) {
+    case tags.FEATURES.ROUTER:
+      route = `/routerTransform`;
+      break;
+    case tags.FEATURES.BATCH:
+      route = `/batch`;
+      break;
+    case tags.FEATURES.DATA_DELIVERY:
+      route = `/${path.join(tcData.version || DEFAULT_VERSION, "destinations", tcData.name, "proxy")}`;
+      break;
+    case tags.FEATURES.USER_DELETION:
+      route = 'deleteUsers';
+      break;
+    default:
+      // Processor transformation
+      route = `/${path.join(tcData.version || DEFAULT_VERSION, "destinations", tcData.name)}`;
+      break;
+  }
+  route = path.join(route, tcData.input.pathSuffix);
+  await testRoute(route, tcData);
 };
 
 const sourceTestHandler = async (tcData) => {
-// TODO: Implement this
+  const route = `/${path.join(tcData.version || DEFAULT_VERSION, "sources", tcData.name, tcData.input.pathSuffix)}`;
+  await testRoute(route, tcData);
 };
 
-
+// Trigger the test suites
 describe.each(allTestDataFilePaths)("%s Tests", (testDataPath) => {
   const testData = getTestData(testDataPath);
   test.each(testData)('$name - $module - $feature -> $description', async (tcData) => {
@@ -83,5 +99,5 @@ describe.each(allTestDataFilePaths)("%s Tests", (testDataPath) => {
       default:
         break;
     }
-  })
+  });
 });

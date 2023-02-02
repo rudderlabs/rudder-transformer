fb6403992c76077398a9f8b5ac4cbe9fb28fd073
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2023-02-02 13:29:36 +0530

    feat(integration): rockerbox - allow all properties to be passed over to rockerbox (#1838)
    * feat(integration): rockerbox - allow all properties to be passed over to rockerbox
    
    * feat(integration): rockerbox - fix failing test

--- src/v0/destinations/rockerbox/config.js
+++ src/v0/destinations/rockerbox/config.js
@@ -13,8 +13,26 @@ const CONFIG_CATEGORIES = {
 
 const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
 
+const ROCKERBOX_DEFINED_PROPERTIES = [
+  'userId',
+  'email',
+  'phone',
+  'timestamp',
+  'revenue',
+  'value',
+  'price',
+  'total',
+  'orderId',
+  'order_id',
+  'in_store',
+  'salesforce',
+  'countryCode',
+  'listingId',
+];
+
 module.exports = {
   BASE_ENDPOINT,
   CONFIG_CATEGORIES,
   MAPPING_CONFIG,
+  ROCKERBOX_DEFINED_PROPERTIES,
 };

599594f601462f2562176eef23a079375882c3a4
SoniRani <soni.rani@scheduleonce.com>
2023-02-21 17:15:53 +0530

    ONCEHUB-62684: adding property firstLoginGA4 as false while sending BI tracking events in GA4

--- src/util/oncehub-custom-transformer.js
+++ src/util/oncehub-custom-transformer.js
@@ -1,3 +1,5 @@
+const { EventType } = require('../constants');
+
 const getPIIDestinationList = () => {
     return (process.env.WHITELIST_PII_DESTINATION || "customerio")
       .trim()
@@ -12,11 +14,30 @@ const getPIIDestinationList = () => {
     return event && event.message && event.message.context  && event.message.context.traits;
   };
 
+  const handleFirstLoginGA4Property = (
+    destination,
+    event,
+    eventTraitsPresent,
+    contextTraitsPresent,
+  ) => {
+    if (destination === 'ga4' && event.message.type === EventType.IDENTIFY) {
+      if (eventTraitsPresent && !('firstLoginGA4' in event.message.traits)) {
+        event.message.traits['firstLoginGA4'] = false;
+      }
+
+      if (contextTraitsPresent && !('firstLoginGA4' in event.message.context.traits)) {
+        event.message.context.traits['firstLoginGA4'] = false;
+      }
+    } else if (destination !== 'ga4') {
+      if (contextTraitsPresent) delete event.message.context.traits.firstLoginGA4;
+      if (eventTraitsPresent) delete event.message.traits.firstLoginGA4;
+    }
+  };
+
   const oncehubTransformer = (destination, event) => {
-    if (
-      !getPIIDestinationList().includes(destination) &&
-      doesEventContainsTraits(event)
-    ) {
+  const contextTraitsPresent = doesEventContainContextTraits(event);
+  const eventTraitsPresent = doesEventContainsTraits(event);
+  if (!getPIIDestinationList().includes(destination) && eventTraitsPresent) {
       // eslint-disable-next-line no-param-reassign
       delete event.message.traits.email;
       // eslint-disable-next-line no-param-reassign
@@ -24,8 +45,7 @@ const getPIIDestinationList = () => {
       // eslint-disable-next-line no-param-reassign
       delete event.message.traits.lastName;
 
-      if(doesEventContainContextTraits(event))
-      {
+    if (contextTraitsPresent) {
         // eslint-disable-next-line no-param-reassign
         delete event.message.context.traits.email;
         // eslint-disable-next-line no-param-reassign
@@ -34,6 +54,10 @@ const getPIIDestinationList = () => {
         delete event.message.context.traits.lastName;
       }
     }
+
+  // Adding check for firstLoginGA4 property
+  handleFirstLoginGA4Property(destination, event, eventTraitsPresent, contextTraitsPresent);
+
     // eslint-disable-next-line no-console
     // if(doesEventContainsTraits(event)) console.log("event log=>destination : ", JSON.stringify(destination), " , ==> event traits : ", JSON.stringify(event.message.traits), " , ==> event here : ",JSON.stringify(event));
     return event;

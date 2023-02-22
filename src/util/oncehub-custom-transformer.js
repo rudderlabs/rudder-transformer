42c0c3b3d3484fef1daaa750a4b14d82b899f4c3
chan-singh <chanpreet.singh@oncehub.com>
2023-02-22 14:26:20 +0530

    fix: changed date format for customer io

--- src/util/oncehub-custom-transformer.js
+++ src/util/oncehub-custom-transformer.js
@@ -1,44 +1,57 @@
 const getPIIDestinationList = () => {
-    return (process.env.WHITELIST_PII_DESTINATION || "customerio")
-      .trim()
-      .split(",");
-  };
-  
-  const doesEventContainsTraits = event => {
-    return event && event.message && event.message.traits;
-  };
-  
-  const doesEventContainContextTraits = event => {
-    return event && event.message && event.message.context  && event.message.context.traits;
-  };
+  return (process.env.WHITELIST_PII_DESTINATION || 'customerio').trim().split(',');
+};
 
-  const oncehubTransformer = (destination, event) => {
-    if (
-      !getPIIDestinationList().includes(destination) &&
-      doesEventContainsTraits(event)
-    ) {
-      // eslint-disable-next-line no-param-reassign
-      delete event.message.traits.email;
-      // eslint-disable-next-line no-param-reassign
-      delete event.message.traits.firstName;
-      // eslint-disable-next-line no-param-reassign
-      delete event.message.traits.lastName;
+const doesEventContainsTraits = event => {
+  return event && event.message && event.message.traits;
+};
 
-      if(doesEventContainContextTraits(event))
-      {
-        // eslint-disable-next-line no-param-reassign
-        delete event.message.context.traits.email;
-        // eslint-disable-next-line no-param-reassign
-        delete event.message.context.traits.firstName;
-        // eslint-disable-next-line no-param-reassign
-        delete event.message.context.traits.lastName;
-      }
+const doesEventContainContextTraits = event => {
+  return event && event.message && event.message.context && event.message.context.traits;
+};
+const changeDateFormatForCustomerio = (destination, event) => {
+  if (getPIIDestinationList().includes(destination)) {
+    if (doesEventContainContextTraits(event)) {
+      // eslint-disable-next-line no-param-reassign
+      event.message.context.traits.accountCreatedDate = Math.floor(
+        Date.parse(event.message.context.traits.accountCreatedDate) / 1000,
+      );
     }
-    // eslint-disable-next-line no-console
-    // if(doesEventContainsTraits(event)) console.log("event log=>destination : ", JSON.stringify(destination), " , ==> event traits : ", JSON.stringify(event.message.traits), " , ==> event here : ",JSON.stringify(event));
-    return event;
-  };
-  
-  module.exports = {
-    oncehubTransformer
-  };
+    if (doesEventContainsTraits(event)) {
+      // eslint-disable-next-line no-param-reassign
+      event.message.traits.accountCreatedDate = Math.floor(
+        Date.parse(event.message.traits.accountCreatedDate) / 1000,
+      );
+    }
+  }
+};
+
+const oncehubTransformer = (destination, event) => {
+  changeDateFormatForCustomerio(destination, event);
+
+  if (!getPIIDestinationList().includes(destination) && doesEventContainsTraits(event)) {
+    // eslint-disable-next-line no-param-reassign
+    delete event.message.traits.email;
+    // eslint-disable-next-line no-param-reassign
+    delete event.message.traits.firstName;
+    // eslint-disable-next-line no-param-reassign
+    delete event.message.traits.lastName;
+
+    if (doesEventContainContextTraits(event)) {
+      // eslint-disable-next-line no-param-reassign
+      delete event.message.context.traits.email;
+      // eslint-disable-next-line no-param-reassign
+      delete event.message.context.traits.firstName;
+      // eslint-disable-next-line no-param-reassign
+      delete event.message.context.traits.lastName;
+    }
+  }
+  // eslint-disable-next-line no-console
+
+  // console.log(" , ==> event here : ", JSON.stringify(event));
+  return event;
+};
+
+module.exports = {
+  oncehubTransformer,
+};

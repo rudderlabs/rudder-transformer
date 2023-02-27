cbcd5251ec072338722d4304bc38336ea8a340f1
Jayc <mjayachand@gmail.com>
2023-02-28 00:49:44 +0530

    chore(trackingplan): collecting violation metrics for tracking plan

--- src/util/utils.js
+++ src/util/utils.js
@@ -1,4 +1,7 @@
 /* eslint-disable max-classes-per-file */
+const { violationTypes } = require("./eventValidation");
+const stats = require('./stats');
+
 class RespStatusError extends Error {
   constructor(message, statusCode) {
     super(message);
@@ -21,8 +24,47 @@ const responseStatusHandler = (status, entity, id, url) => {
   }
 };
 
+const sendViolationMetrics = (validationErrors, dropped, metaTags) => {
+  const vTags = {
+    "Unplanned-Event": 0,
+    "Additional-Properties": 0,
+    "Datatype-Mismatch": 0,
+    "Required-Missing": 0,
+    "Unknown-Violation": 0,
+  };
+  
+  validationErrors.forEach(error => {
+    switch (error.type) {
+      case violationTypes.UnplannedEvent:
+        vTags[violationTypes.UnplannedEvent] += 1;
+        break;
+      case violationTypes.AdditionalProperties:
+        vTags[violationTypes.AdditionalProperties] += 1;
+        break;
+      case violationTypes.DatatypeMismatch:
+        vTags[violationTypes.DatatypeMismatch] += 1;
+        break;
+      case violationTypes.RequiredMissing:
+        vTags[violationTypes.RequiredMissing] += 1;
+        break;
+      case violationTypes.UnknownViolation:
+        vTags[violationTypes.UnknownViolation] += 1;
+        break;
+      default:
+        break;
+    }
+  });
+  
+  Object.entries(vTags).forEach(([key, value]) => {
+    if (value > 0) {
+      stats.gauge('hv_metrics', value, { ...metaTags, dropped, violationType: key });
+    }
+  });
+}
+
 module.exports = {
   RespStatusError,
   RetryRequestError,
   responseStatusHandler,
+  sendViolationMetrics,
 };

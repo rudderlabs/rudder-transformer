9e582206652046401262d77fbcf0bc0ec8b65815
saikumarrs <saibattinoju@rudderstack.com>
2023-03-15 18:08:01 +0530

    fix: rules in commitlint.config.js

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']
+    'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']]
   }
 };

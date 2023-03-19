85ce6c03df1cb15c7c55aab557219dc0fd04352d
Utsab Chowdhury <utsab.c97@gmail.com>
2023-03-20 00:18:47 +0530

    feat: update test cases for deduplication logic and added mocking

--- test/__tests__/braze.test.js
+++ test/__tests__/braze.test.js
@@ -40,31 +40,16 @@ const inputRouterData = JSON.parse(inputRouterDataFile);
 const expectedRouterData = JSON.parse(outputRouterDataFile);
 
 describe(`${name} Tests`, () => {
-  describe("Router Tests", () => {
+  describe("Simple Router Tests", () => {
     it("Payload", async () => {
-      const routerOutput = await transformer.processRouterDest(inputRouterData);
-      expect(routerOutput).toEqual(expectedRouterData);
+      const routerOutput = await transformer.processRouterDest(inputRouterData.simpleRouterRequests);
+      expect(routerOutput).toEqual(expectedRouterData.simpleRouterResponse);
     });
   });
-});
-
-const batchInputDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_batch_input.json`)
-);
-const batchOutputDataFile = fs.readFileSync(
-  path.resolve(__dirname, `./data/${integration}_batch_output.json`)
-);
-
-const batchInputData = JSON.parse(batchInputDataFile);
-const batchExpectedData = JSON.parse(batchOutputDataFile);
-
-batchInputData.forEach((input, index) => {
-  test(`${name} Batching ${index}`, () => {
-    const output = transformer.batch(input);
-    expect(Array.isArray(output)).toEqual(true);
-    expect(output.length).toEqual(batchExpectedData[index].length);
-    output.forEach((input, indexInner) => {
-      expect(output[indexInner]).toEqual(batchExpectedData[index][indexInner]);
+  describe("Dedupenabled Router Tests", () => {
+    it("Payload", async () => {
+      const routerOutput = await transformer.processRouterDest(inputRouterData.dedupEnabledRouterRequests);
+      expect(routerOutput).toEqual(expectedRouterData.dedupEnabledRouterRequests);
     });
   });
 });

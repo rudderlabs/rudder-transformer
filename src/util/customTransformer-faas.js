5cfe5177ee01b41b737908e4b7c345f666e69b91
Jayachand <mjayachand@gmail.com>
2023-03-20 19:36:55 +0530

    Revert "chore(transformation): stats for ut (#1958)"
    This reverts commit f0cf49656cd875da240f2bad98b22f5804ec0b94.

--- src/util/customTransformer-faas.js
+++ src/util/customTransformer-faas.js
@@ -127,7 +127,7 @@ async function runOpenFaasUserTransform(events, userTransformation, libraryVersi
   }
 
   const invokeTime = new Date();
-  stats.counter('events_to_process', events.length, tags);
+  stats.gauge('events_to_process', events.length, tags);
   const result = await executeFaasFunction(
     functionName,
     events,

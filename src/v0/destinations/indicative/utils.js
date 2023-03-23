014f487407b3e41896a840ea6dc7db3d3874aa06
Gauravudia <60897972+Gauravudia@users.noreply.github.com>
2023-03-23 12:29:21 +0530

    Update src/v0/destinations/indicative/utils.js
    Co-authored-by: Sudip Paul <67197965+ItsSudip@users.noreply.github.com>

--- src/v0/destinations/indicative/utils.js
+++ src/v0/destinations/indicative/utils.js
@@ -4,10 +4,10 @@ const { removeUndefinedAndNullValues } = require('../../util');
 const getUAInfo = (message) => {
   const ua = uaParser(message.context?.userAgent);
   return removeUndefinedAndNullValues({
-    browser: ua.browser.name,
-    browser_version: ua.browser.version,
-    os: ua.os.name,
-    device: ua.device.model,
+    browser: ua.browser?.name,
+    browser_version: ua.browser?.version,
+    os: ua.os?.name,
+    device: ua.device?.model,
   });
 };
 

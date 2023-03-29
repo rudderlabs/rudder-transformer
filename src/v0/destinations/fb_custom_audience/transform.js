b8662b3dbc6413e2aa61305ffd14355e70b3ad13
Ruchira Moitra <31311577+ruchiramoitra@users.noreply.github.com>
2023-03-29 19:03:07 +0530

    feat: add audience id for retl sources from context

--- src/v0/destinations/fb_custom_audience/transform.js
+++ src/v0/destinations/fb_custom_audience/transform.js
@@ -13,6 +13,7 @@ const {
   isDefinedAndNotNull,
   flattenMap,
   handleRtTfSingleEventError,
+  getDestinationExternalIDInfoForRetl,
 } = require('../../util');
 
 const {
@@ -279,6 +280,10 @@ const processEvent = (message, destination) => {
   const respList = [];
   const toSendEvents = [];
   let wrappedResponse = {};
+
+  console.log('==============Checking fb custom audience Config=======================');
+  console.log(destination.Config);
+
   let { userSchema } = destination.Config;
   const { isHashRequired, audienceId, maxUserCount } = destination.Config;
   if (!message.type) {
@@ -292,13 +297,21 @@ const processEvent = (message, destination) => {
   if (message.type.toLowerCase() !== 'audiencelist') {
     throw new InstrumentationError(` ${message.type} call is not supported `);
   }
-  const operationAudienceId = audienceId;
-
+  let operationAudienceId = audienceId;
+  const mappedToDestination = get(message, MappedToDestinationKey);
+  if (!operationAudienceId && mappedToDestination) {
+    const { objectType } = getDestinationExternalIDInfoForRetl(
+      inputs[0].message,
+      'FB_CUSTOM_AUDIENCE',
+    );
+    console.log(objectType);
+    operationAudienceId = objectType;
+  }
+  console.log(operationAudienceId);
   if (!isDefinedAndNotNullAndNotEmpty(operationAudienceId)) {
     throw new ConfigurationError('Audience ID is a mandatory field');
   }
 
-  const mappedToDestination = get(message, MappedToDestinationKey);
   // If mapped to destination, use the mapped fields instead of destination userschema
   if (mappedToDestination) {
     userSchema = getSchemaForEventMappedToDest(message);

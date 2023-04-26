3907f73fad5617e4b081168c5748932fc56f74e0
chandumlg <54652834+chandumlg@users.noreply.github.com>
2023-04-26 08:30:36 -0400

    chore: request size metric (#2036)

--- src/middleware.js
+++ src/middleware.js
@@ -9,10 +9,28 @@ function durationMiddleware() {
     const labels = {
       method: ctx.method,
       code: ctx.status,
-      // eslint-disable-next-line no-underscore-dangle
-      route: ctx._matchedRoute,
+      route: ctx.request.url,
     };
     stats.timing('http_request_duration', startTime, labels);
+  };
+}
+
+function requestSizeMiddleware() {
+  return async (ctx, next) => {
+    await next();
+
+    const labels = {
+      method: ctx.method,
+      code: ctx.status,
+      route: ctx.request.url,
+    };
+
+    const inputLength =
+      ctx.request && ctx.request.body ? Buffer.byteLength(JSON.stringify(ctx.request.body)) : 0;
+    stats.histogram('http_request_size', inputLength, labels);
+    const outputLength =
+      ctx.response && ctx.response.body ? Buffer.byteLength(JSON.stringify(ctx.response.body)) : 0;
+    stats.histogram('http_response_size', outputLength, labels);
   };
 }
 
@@ -20,6 +38,11 @@ function addStatMiddleware(app) {
   app.use(durationMiddleware());
 }
 
+function addRequestSizeMiddleware(app) {
+  app.use(requestSizeMiddleware());
+}
+
 module.exports = {
   addStatMiddleware,
+  addRequestSizeMiddleware,
 };

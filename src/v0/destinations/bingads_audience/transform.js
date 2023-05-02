5c95e35d507eeb17a0f3a2128fb7cf0c601a2dc4
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-05-02 11:11:35 +0530

    feat: onboard bingads_audience destination

---
+++ src/v0/destinations/bingads_audience/transform.js
@@ -0,0 +1,89 @@
+const {
+  BASE_ENDPOINT,
+  ENDPOINTS,
+  BINGADS_SUPPORTED_OPERATION,
+  AUDIENCE_ATTRIBUTE,
+} = require('./config');
+const {
+  defaultRequestConfig,
+  defaultPutRequestConfig,
+  removeUndefinedAndNullValues,
+  simpleProcessRouterDest,
+} = require('../../util');
+
+const { getAccessToken, createPayload } = require('./util');
+const { InstrumentationError } = require('../../util/errorTypes');
+
+/**
+ * This function is used for building the final response to be returned.
+ * @param {*} message
+ * @param {*} destination
+ * @returns
+ */
+const responseBuilder = async (message, destination, accessToken) => {
+  let bingListPayload = {};
+  const { Config } = destination;
+  const { listData } = message.properties;
+  const { audienceId, audienceType } = Config;
+
+  const traitsList = listData[BINGADS_SUPPORTED_OPERATION];
+  if (!traitsList) {
+    throw new InstrumentationError(
+      `The only supported operation for audience updation '${BINGADS_SUPPORTED_OPERATION}' is not present`,
+    );
+  }
+
+  /**
+   * The below written switch case is used to build the response for each of the supported audience type.
+   *  eg. ["email", "deviceId", "ipAddress"].
+   */
+  const audienceAttribute = AUDIENCE_ATTRIBUTE[audienceType];
+  switch (audienceAttribute) {
+    case 'email':
+      // creating the output payload using the audience list and Config
+      bingListPayload = createPayload(traitsList, Config);
+      break;
+    default:
+      throw new InstrumentationError(`Audience Type ${audienceType} is not supported`);
+  }
+
+  const response = defaultRequestConfig();
+  response.endpoint = `${BASE_ENDPOINT}`;
+  response.body.JSON = removeUndefinedAndNullValues(bingListPayload);
+  response.method = defaultPutRequestConfig.requestMethod;
+  response.headers = {
+    'X-Auth-Token': accessToken,
+    'X-Auth-Method': 'OAuth2',
+    'Content-Type': 'application/json',
+  };
+  return response;
+};
+
+const processEvent = async (metadata, message, destination) => {
+  const accessToken = getAccessToken(metadata, 'accessToken');
+  let response;
+  if (!message.type) {
+    throw new InstrumentationError('Event type is required');
+  }
+  if (!message.properties) {
+    throw new InstrumentationError('Message properties is not present. Aborting message');
+  }
+  if (!message.properties.listData) {
+    throw new InstrumentationError('listData is not present inside properties. Aborting message');
+  }
+  if (message.type.toLowerCase() === 'audiencelist') {
+    response = await responseBuilder(message, destination, accessToken);
+  } else {
+    throw new InstrumentationError(`Event type ${message.type} is not supported`, 400);
+  }
+  return response;
+};
+
+const process = async (event) => processEvent(event.metadata, event.message, event.destination);
+
+const processRouterDest = async (inputs, reqMetadata) => {
+  const respList = await simpleProcessRouterDest(inputs, process, reqMetadata);
+  return respList;
+};
+
+module.exports = { process, processRouterDest };
---
+++ src/v0/destinations/bingads_audience/util.js
@@ -0,0 +1,82 @@
+const sha256 = require('sha256');
+const { isDefinedAndNotNullAndNotEmpty } = require('../../util');
+const { AUDIENCE_ATTRIBUTE, BINGADS_SUPPORTED_OPERATION } = require('./config');
+const { InstrumentationError } = require('../../util/errorTypes');
+/**
+ *
+ * @param {*} audienceList  - It is the list of audience to be added in the form of array". eg.
+ * [
+ *   {"email": "abc@email.com"},
+ *   {"email": "abc@email.com"},
+ *   {"email": "abc@email.com"}
+ * ]
+ * @param {*} Config
+ * @returns The function returns a hashed array of Audience List provided by the user like "email", "deviceId", "ipAddress".
+ * eg.[
+ * "251014dafc651f68edac7",
+ * "afbc34416ac6e7fbb9734",
+ * "42cbe7eebb412bbcd5b56",
+ * "379b4653a40878da7a584"
+ * ]
+ */
+const populateIdentifiers = (audienceList, Config) => {
+  const seedList = [];
+  const { audienceType } = Config;
+  const { hashRequired } = Config;
+  const audienceAttribute = AUDIENCE_ATTRIBUTE[audienceType];
+
+  if (isDefinedAndNotNullAndNotEmpty(audienceList)) {
+    // traversing through every userTraits in the add array for the traits to be added.
+    audienceList.forEach((userTraits) => {
+      // storing keys of an object inside the add array.
+      const traits = Object.keys(userTraits);
+      // checking for the audience type the user wants to add is present in the input or not.
+      if (!traits.includes(audienceAttribute)) {
+        // throwing error if the audience type the user wants to add is not present in the input.
+        throw new InstrumentationError(
+          `Required property for ${audienceAttribute} type audience is not available in an object`,
+        );
+      }
+      // here, hashing the data if is not hashed and pushing in the seedList array.
+      if (hashRequired) {
+        seedList.push(sha256(userTraits[audienceAttribute]));
+      } else {
+        seedList.push(userTraits[audienceAttribute]);
+      }
+    });
+  }
+  return seedList;
+};
+
+/**
+ * This function is used to create the output Payload.
+ * @param {*} audienceList - It is the list of audience to be added in the form of array". eg.
+ * [
+ *   {"email": "abc@email.com"},
+ *   {"email": "abc@email.com"},
+ *   {"email": "abc@email.com"}
+ * ]
+ * @param {*} Config
+ * @returns {*} a created payload {dspListPayload) object updated with the required data
+ */
+const createPayload = (audienceList, Config) => {
+  let bingListPayload = {};
+  let seedList = [];
+  const { accountId } = Config;
+
+  // Populating Seed List that conains audience list to be updated
+  seedList = populateIdentifiers(audienceList, Config);
+  // throwing the error if nothing is present in the seedList
+  if (seedList.length === 0) {
+    throw new InstrumentationError(
+      `No attributes are present in the '${BINGADS_SUPPORTED_OPERATION}' property`,
+    );
+  }
+  // Creating dspListPayload
+  bingListPayload = { accountId, seedList };
+  return bingListPayload;
+};
+
+module.exports = {
+  createPayload,
+};

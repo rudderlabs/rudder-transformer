6db1120c9d749e83f465d305650ce09beb5c843e
Utsab Chowdhury <utsab.c97@gmail.com>
2023-05-08 14:51:03 +0530

    feat: add constants for https status

--- src/controllers/util/index.ts
+++ src/controllers/util/index.ts
@@ -1,11 +1,12 @@
 import { Context } from 'koa';
 import { API_VERSION } from '../../routes/utils/constants';
 import { isHttpStatusSuccess } from '../../v0/util';
+import { HttpStatus } from '../../v0/util/constant';
 
 export default class ControllerUtility {
   private static handleForNotfoundStatus(status: number) {
-    if (status === 404) {
-      return 400;
+    if (status === HttpStatus.NOT_FOUND) {
+      return HttpStatus.BAD_REQUEST;
     }
     return status;
   }

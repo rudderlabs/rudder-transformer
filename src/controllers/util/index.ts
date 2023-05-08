4ab2c2320f2044963effa23f82fbb762f716c7c4
Utsab Chowdhury <utsab.c97@gmail.com>
2023-05-08 13:09:58 +0530

    fix: handle 404 response code for proxy route

--- src/controllers/util/index.ts
+++ src/controllers/util/index.ts
@@ -3,6 +3,13 @@ import { API_VERSION } from '../../routes/utils/constants';
 import { isHttpStatusSuccess } from '../../v0/util';
 
 export default class ControllerUtility {
+  private static handleForNotfoundStatus(status: number) {
+    if (status === 404) {
+      return 400;
+    }
+    return status;
+  }
+
   public static postProcess(ctx: Context, status = 200) {
     ctx.set('apiVersion', API_VERSION);
     ctx.status = status;
@@ -10,6 +17,6 @@ export default class ControllerUtility {
 
   public static deliveryPostProcess(ctx: Context, status = 200) {
     ctx.set('apiVersion', API_VERSION);
-    ctx.status = isHttpStatusSuccess(status) ? 200 : status;
+    ctx.status = isHttpStatusSuccess(status) ? 200 : this.handleForNotfoundStatus(status);
   }
 }

4ec9487a6f51c82ae1a5592756a54bbab2562f98
Utsab Chowdhury <utsab.c97@gmail.com>
2023-05-15 00:46:57 +0530

    feat: onboarding swagger ui for transformer
    adding misc routes for basic implementation for validating poc

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,8 @@
 import Koa from 'koa';
+import Router from '@koa/router';
+import { existsSync, readFileSync } from 'fs';
+import dotenv from 'dotenv';
+import { koaSwagger } from 'koa2-swagger-ui';
 import { bulkUploadRoutes } from './bulkUpload';
 import { proxyRoutes } from './delivery';
 import { destinationRoutes } from './destination';
@@ -7,6 +11,11 @@ import { sourceRoutes } from './source';
 import { testEventRoutes } from './testEvents';
 import { trackingPlanRoutes } from './trackingPlan';
 import { userTransformRoutes } from './userTransform';
+import logger from '../logger';
+
+dotenv.config();
+
+const enableSwagger = process.env.ENABLE_SWAGGER !== 'false';
 
 export function applicationRoutes(app: Koa<any, {}>) {
   app.use(bulkUploadRoutes);
@@ -17,4 +26,26 @@ export function applicationRoutes(app: Koa<any, {}>) {
   app.use(testEventRoutes);
   app.use(trackingPlanRoutes);
   app.use(userTransformRoutes);
+}
+
+export function addSwaggerRoutes(app: Koa<any, {}>) {
+  // Ading swagger routes, check swagger docs in http://localhost:5050/docs
+  if (enableSwagger) {
+    const router = new Router();
+    if (existsSync('./dist/src/swagger.json')) {
+      const rawContent = readFileSync('./dist/src/swagger.json', { encoding: 'utf8' });
+      if (rawContent) {
+        const spec = JSON.parse(rawContent);
+        logger.info('Config backend: Swagger route loading');
+        router.get(
+          '/docs',
+          koaSwagger({ routePrefix: false, swaggerOptions: { spec, deepLinking: true } }),
+        );
+        logger.info('Config backend: Swagger route loaded');
+        app.use(router.routes());
+      }
+    } else {
+      logger.error('Swagger file does not exist!');
+    }
+  }
 }

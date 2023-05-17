da57c8dcebbe76f496beb0dc42350b81d7a83b8d
Gauravudia <gauravudia@rudderstack.com>
2023-05-17 12:49:10 +0530

    chore: removed unsued import

--- src/v0/destinations/sendgrid/deleteUsers.js
+++ src/v0/destinations/sendgrid/deleteUsers.js
@@ -5,7 +5,7 @@ const {
   getDynamicErrorType,
 } = require('../../../adapters/utils/networkUtils');
 const { isHttpStatusSuccess } = require('../../util');
-const { NetworkError, ConfigurationError, InstrumentationError } = require('../../util/errorTypes');
+const { NetworkError, ConfigurationError } = require('../../util/errorTypes');
 const tags = require('../../util/tags');
 const { executeCommonValidations } = require('../../util/regulation-api');
 

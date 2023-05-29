ab0c980e5bf42300dc004b311074dd55364986a5
Sai Sankeerth <sanpj2292@github.com>
2023-05-29 12:23:58 +0530

    fix: clean-up status-code logic by defaulting to 500 when status-code not available
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- src/v0/destinations/facebook_pixel/networkHandler.js
+++ src/v0/destinations/facebook_pixel/networkHandler.js
@@ -10,26 +10,17 @@ const tags = require('../../util/tags');
 const ErrorDetailsExtractorBuilder = require('../../../util/error-extractor/builder');
 
 /**
+ * Only under below mentioned scenario(s), add the errorCodes, subCodes etc,. to this map
+ * 
  * The actual API reference doc to which events from Rudderstack are being sent
  * https://developers.facebook.com/docs/marketing-api/reference/ads-pixel/events/v13.0
- *
+ * 
  * The documents referred while formulating the error responses
  * 1. https://developers.facebook.com/docs/graph-api/guides/error-handling/
  *   - This seems like a generic document that contains errors possible for Graph API
  * 2. https://developers.facebook.com/docs/marketing-api/error-reference/
  *   - The doc seems to be more related to Marketing API
- */
-const RETRYABLE_ERROR_CODES = [1, 2, 3, 341, 368, 5000];
-
-/**
- * These error codes were chosen from the below reference:
- * https://developers.facebook.com/docs/marketing-api/error-reference/
- *
- */
-const THROTTLED_ERROR_CODES = [4, 17];
-
-/**
- * Only under below mentioned scenario(s), add the errorCodes, subCodes etc,. to this map
+ * 3. https://developers.facebook.com/docs/marketing-api/error-reference/
 {
   // A scenario where in we have to know the error with code and subcode, we can conclude that
   // this error will have a particular status, statTags
@@ -50,72 +41,116 @@ const THROTTLED_ERROR_CODES = [4, 17];
 const errorDetailsMap = {
   100: {
     // This error talks about event being sent after seven days or so
-    2804003: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
-    2804001: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
-    2804007: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
-    2804016: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
-    2804017: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
-    2804019: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
-    2804048: new ErrorDetailsExtractorBuilder().setStatus(400).setMessageField('error_user_title').build(),
+    2804003: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
+    2804001: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
+    2804007: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
+    2804016: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
+    2804017: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
+    2804019: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
+    2804048: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessageField('error_user_title')
+      .build(),
     // This error-subcode indicates that the business access token expired or is invalid or sufficient permissions are not provided
     // since there is involvement of changes required on dashboard to make event successful
     // for now, we are aborting this error-subCode combination
-    33: new ErrorDetailsExtractorBuilder().setStatus(400)
-      .setMessage('Object with ID \'PIXEL_ID\' does not exist, cannot be loaded due to missing permissions, or does not support this operation')
+    33: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessage(
+        "Object with ID 'PIXEL_ID' does not exist, cannot be loaded due to missing permissions, or does not support this operation",
+      )
       .build(),
-    default: new ErrorDetailsExtractorBuilder().setStatus(400)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
       .setMessage('Invalid Parameter')
       .build(),
   },
   1: {
     // An unknown error occurred.
     // This error may occur if you set level to adset but the correct value should be campaign
-    99: new ErrorDetailsExtractorBuilder().setStatus(500).setMessage('This error may occur if you set level to adset but the correct value should be campaign').build(),
-    default: new ErrorDetailsExtractorBuilder().setStatus(500).setMessage('An unknown error occurred').build(),
+    99: new ErrorDetailsExtractorBuilder()
+      .setStatus(500)
+      .setMessage(
+        'This error may occur if you set level to adset but the correct value should be campaign',
+      )
+      .build(),
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(500)
+      .setMessage('An unknown error occurred')
+      .build(),
   },
   190: {
-    460: new ErrorDetailsExtractorBuilder().setStatus(400)
-      .setMessage('The session has been invalidated because the user changed their password or Facebook has changed the session for security reasons')
+    460: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
+      .setMessage(
+        'The session has been invalidated because the user changed their password or Facebook has changed the session for security reasons',
+      )
       .build(),
-    default: new ErrorDetailsExtractorBuilder().setStatus(400)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
       .setMessage('Invalid OAuth 2.0 access token')
       .build(),
   },
   3: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(400)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(400)
       .setMessage('Capability or permissions issue.')
-      .build(), 
+      .build(),
   },
   2: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(500)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(500)
       .setMessage('Temporary issue due to downtime.')
-      .build(), 
+      .build(),
   },
   341: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(500)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(500)
       .setMessage('Application limit reached: Temporary issue due to downtime or throttling')
-      .build(), 
+      .build(),
   },
   368: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(500)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(500)
       .setMessage('Temporarily blocked for policies violations.')
-      .build(), 
+      .build(),
   },
   5000: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(500)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(500)
       .setMessage('Unknown Error Code')
-      .build(), 
+      .build(),
   },
   4: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(429)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(429)
       .setMessage('API Too Many Calls')
-      .build(), 
+      .build(),
   },
   17: {
-    default: new ErrorDetailsExtractorBuilder().setStatus(429)
+    default: new ErrorDetailsExtractorBuilder()
+      .setStatus(429)
       .setMessage('API User Too Many Calls')
-      .build(), 
-  }
+      .build(),
+  },
 };
 
 const getErrorDetailsFromErrorMap = (error) => {
@@ -129,18 +164,18 @@ const getErrorDetailsFromErrorMap = (error) => {
 
 const getStatus = (error) => {
   const errorDetail = getErrorDetailsFromErrorMap(error);
-  let errorStatus = 400;
+  let errorStatus = 500;
   if (!isEmpty(errorDetail)) {
     errorStatus = errorDetail.status;
   }
   // TODO: Clean-up this logic
-  if (RETRYABLE_ERROR_CODES.includes(error.code)) {
-    errorStatus = 500;
-  }
+  // if (RETRYABLE_ERROR_CODES.includes(error.code)) {
+  //   errorStatus = 500;
+  // }
 
-  if (THROTTLED_ERROR_CODES.includes(error.code)) {
-    errorStatus = 429;
-  }
+  // if (THROTTLED_ERROR_CODES.includes(error.code)) {
+  //   errorStatus = 429;
+  // }
 
   let errorMessage = errorDetail?.messageDetails?.message;
   if (errorDetail?.messageDetails?.field) {
@@ -159,7 +194,7 @@ const errorResponseHandler = (destResponse) => {
   const { error } = response;
   const { status, errorMessage } = getStatus(error);
   throw new NetworkError(
-    `${errorMessage || error.message || "Unknown failure during response transformation"}`,
+    `${errorMessage || error.message || 'Unknown failure during response transformation'}`,
     status,
     {
       [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(status),

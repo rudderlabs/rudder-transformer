ff3ff91f46fb66730809925c6cb78884d8029dc8
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-05-29 07:42:52 +0530

    feat: onboard bingads audience destination (CDK)

---
+++ src/cdk/v2/destinations/bingads_audience/config.js
@@ -0,0 +1,13 @@
+const BASE_ENDPOINT = 'https://testapi.com';
+
+const AUDIENCE_ATTRIBUTE = {
+  EMAIL: 'email',
+};
+
+const BINGADS_SUPPORTED_OPERATION = 'add';
+
+module.exports = {
+  BASE_ENDPOINT,
+  BINGADS_SUPPORTED_OPERATION,
+  AUDIENCE_ATTRIBUTE,
+};

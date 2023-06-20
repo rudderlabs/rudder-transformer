e4ef446210014a36f44d2594f4740d62176b1c5d
AASHISH MALIK <imashishmalik.45@gmail.com>
2023-06-20 16:03:00 +0530

    feat: factorsAI page and group call support

--- src/v0/destinations/factorsai/transform.js
+++ src/v0/destinations/factorsai/transform.js
@@ -41,6 +41,13 @@ function processTrack(message, factorsAIApiKey) {
   return buildResponse(requestJson, factorsAIApiKey);
 }
 
+// process Page Call
+function processPageAndGroup(message, factorsAIApiKey, eventType) {
+  const requestJson = eventType === EventType.PAGE ? constructPayload(message, mappingConfig[ConfigCategories.PAGE.name])
+      : constructPayload(message, mappingConfig[ConfigCategories.GROUP.name]);
+  return buildResponse(requestJson, factorsAIApiKey);
+}
+
 function process(event) {
   const { message, destination } = event;
   const { factorsAIApiKey } = destination.Config;
@@ -56,6 +63,10 @@ function process(event) {
       return processIdentify(message, factorsAIApiKey);
     case EventType.TRACK:
       return processTrack(message, factorsAIApiKey);
+    case EventType.PAGE:
+      return processPageAndGroup(message, factorsAIApiKey, EventType.PAGE);
+    case EventType.GROUP:
+      return processPageAndGroup(message, factorsAIApiKey, EventType.GROUP);
     default:
       throw new InstrumentationError(`Message type ${messageType} is not supported`);
   }

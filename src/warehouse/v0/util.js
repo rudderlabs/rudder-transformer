d009c892473073ad507bf3cba9381edc4f28b3d0
Anant Jain <anantjain45823@gmail.com>
2023-06-21 17:44:43 +0530

    revert warehouse changes

--- src/warehouse/v0/util.js
+++ src/warehouse/v0/util.js
@@ -6,7 +6,7 @@ const toSnakeCase = (str) => {
     return '';
   }
   return String(str)
-    .replace(/^\W+|\W+$/g, '')
+    .replace(/^[^A-Za-z0-9]*|[^A-Za-z0-9]*$/g, '')
     .replace(/([a-z])([A-Z])/g, (m, a, b) => `${a}_${b.toLowerCase()}`)
     .replace(/[^A-Za-z0-9]+|_+/g, '_')
     .toLowerCase();

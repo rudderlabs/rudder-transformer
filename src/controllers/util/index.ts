5c6bc99543a98c0e81d09e20951a460e3c613863
Sai Sankeerth <sanpj2292@github.com>
2023-07-06 08:07:36 +0530

    fix: rETL timestamp order
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- src/controllers/util/index.ts
+++ src/controllers/util/index.ts
@@ -1,10 +1,29 @@
 import { Context } from 'koa';
+import isEmpty from 'lodash/isEmpty';
 import { API_VERSION } from '../../routes/utils/constants';
 import { getCompatibleStatusCode } from '../../adapters/utils/networkUtils';
+import {
+  ProcessorTransformationRequest,
+  RouterTransformationRequestData,
+  RudderMessage,
+} from '../../types';
+import { getValueFromMessage } from '../../v0/util';
+import genericFieldMap from '../../v0/util/data/GenericFieldMapping.json';
+import { EventType } from '../../constants';
 
 export default class ControllerUtility {
+  public static timestampValsMap: Record<string, string[]> = {
+    [EventType.IDENTIFY]: [
+      'context.timestamp',
+      'context.traits.timestamp',
+      'traits.timestamp',
+      ...genericFieldMap['timestamp'],
+    ],
+    [EventType.TRACK]: ['properties.timestamp', ...genericFieldMap['timestamp']],
+  };
+
   private static getCompatibleStatusCode(status: number): number {
-    return getCompatibleStatusCode(status)
+    return getCompatibleStatusCode(status);
   }
 
   public static postProcess(ctx: Context, status = 200) {
@@ -15,5 +34,21 @@ export default class ControllerUtility {
   public static deliveryPostProcess(ctx: Context, status = 200) {
     ctx.set('apiVersion', API_VERSION);
     ctx.status = this.getCompatibleStatusCode(status);
+  }
+
+  public static handleTimestampInEvents(
+    events: Array<ProcessorTransformationRequest | RouterTransformationRequestData>,
+  ): Array<ProcessorTransformationRequest | RouterTransformationRequestData> {
+    return events.map((event) => {
+      const newMsg = { ...event.message } as RudderMessage;
+      if (newMsg.channel === 'sources') {
+        const timestampValsArr = ControllerUtility.timestampValsMap[newMsg.type];
+        if (!Array.isArray(timestampValsArr) || isEmpty(timestampValsArr)) {
+          return event;
+        }
+        newMsg.timestamp = getValueFromMessage(newMsg, timestampValsArr);
+      }
+      return { ...event, message: newMsg };
+    });
   }
 }

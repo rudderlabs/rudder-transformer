9a7d08c76d0c3151b77f7e5012e2d0f3917f2dce
Gauravudia <60897972+Gauravudia@users.noreply.github.com>
2023-07-20 09:40:50 +0530

    feat(mixpanel): batching (#2341)
    * feat(mixpanel): add strict parameter and send unencoded data
    
    * chore: resolve merge conflicts
    
    * fix: post call for create user
    
    * feat(mixpanel): batching
    
    * chore: add unit test cases
    
    * feat: add multiplexing and remove external call for create user
    
    * refactor: api ordering
    
    * refactor: combineBatchRequestsWithSameJobIds utils
    
    * refactor: processAndChunkEvents (#2351)
    
    * fix: processEvent function and router testcase
    
    * refactor: combine batch requests with same job ids (#2378)
    
    refactor: combineBatchRequestsWithSameJobIds
    
    implemented as combineBatchRequestsWithSameJobIds2
    
    * fix: combineBatchRequestsWithSameJobIds2
    
    * fix: combineBatchRequestsWithSameJobIds2
    
    * refactor: combineBatchRequestsWithSameJobIds utils
    
    * refactor: remove unused imports
    
    ---------
    
    Co-authored-by: Dilip Kola <33080863+koladilip@users.noreply.github.com>
    Co-authored-by: Dilip Kola <kdilipkola@gmail.com>

---
+++ src/v0/destinations/mp/util.test.js
@@ -0,0 +1,253 @@
+const {
+  combineBatchRequestsWithSameJobIds,
+  combineBatchRequestsWithSameJobIds2,
+} = require('./util');
+
+const destinationMock = {
+  Config: {
+    token: 'test_api_token',
+    prefixProperties: true,
+    useNativeSDK: false,
+    useOldMapping: true,
+  },
+  DestinationDefinition: {
+    DisplayName: 'Mixpanel',
+    ID: 'test_destination_definition_id',
+    Name: 'MP',
+  },
+  Enabled: true,
+  ID: 'test_id',
+  Name: 'Mixpanel',
+  Transformations: [],
+};
+
+describe('Mixpanel utils test', () => {
+  describe('Unit test cases for combineBatchRequestsWithSameJobIds', () => {
+    it('Combine batch request with same jobIds', async () => {
+      const input = [
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/track/',
+          },
+          metadata: [
+            {
+              jobId: 1,
+            },
+            {
+              jobId: 4,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/import/',
+          },
+          metadata: [
+            {
+              jobId: 3,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/track/',
+          },
+          metadata: [
+            {
+              jobId: 5,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/engage/',
+          },
+          metadata: [
+            {
+              jobId: 1,
+            },
+            {
+              jobId: 3,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/import/',
+          },
+          metadata: [
+            {
+              jobId: 6,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+      ];
+
+      const expectedOutput = [
+        {
+          batchedRequest: [
+            {
+              endpoint: 'https://api.mixpanel.com/track/',
+            },
+            {
+              endpoint: 'https://api.mixpanel.com/engage/',
+            },
+            {
+              endpoint: 'https://api.mixpanel.com/import/',
+            },
+          ],
+          metadata: [
+            {
+              jobId: 1,
+            },
+            {
+              jobId: 4,
+            },
+            {
+              jobId: 3,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/track/',
+          },
+          metadata: [
+            {
+              jobId: 5,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/import/',
+          },
+          metadata: [
+            {
+              jobId: 6,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+      ];
+      expect(combineBatchRequestsWithSameJobIds(input)).toEqual(expectedOutput);
+    });
+
+    it('Each batchRequest contains unique jobIds (no event multiplexing)', async () => {
+      const input = [
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/track/',
+          },
+          metadata: [
+            {
+              jobId: 1,
+            },
+            {
+              jobId: 4,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/engage/',
+          },
+          metadata: [
+            {
+              jobId: 2,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/engage/',
+          },
+          metadata: [
+            {
+              jobId: 5,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+      ];
+
+      const expectedOutput = [
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/track/',
+          },
+
+          metadata: [
+            {
+              jobId: 1,
+            },
+            {
+              jobId: 4,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/engage/',
+          },
+          metadata: [
+            {
+              jobId: 2,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+        {
+          batchedRequest: {
+            endpoint: 'https://api.mixpanel.com/engage/',
+          },
+          metadata: [
+            {
+              jobId: 5,
+            },
+          ],
+          batched: true,
+          statusCode: 200,
+          destination: destinationMock,
+        },
+      ];
+      expect(combineBatchRequestsWithSameJobIds(input)).toEqual(expectedOutput);
+    });
+  });
+});

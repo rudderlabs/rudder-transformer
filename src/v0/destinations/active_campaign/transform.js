65460ff894cb761b15d569c2950022808f099ee0
Anant Jain <62471433+anantjain45823@users.noreply.github.com>
2023-07-21 13:03:28 +0530

    Update src/v0/destinations/active_campaign/transform.js
    Co-authored-by: Sankeerth <sankeerth@rudderstack.com>

--- src/v0/destinations/active_campaign/transform.js
+++ src/v0/destinations/active_campaign/transform.js
@@ -124,7 +124,7 @@ const customTagProcessor = async (message, category, destination, contactId) => 
     if (parseInt(get(res, TOTAL_RECORDS_KEY), 10) > 100) {
       const limit = Math.floor(parseInt(get(res, TOTAL_RECORDS_KEY), 10) / 100);
       for (let i = 0; i < limit; i += 1) {
-        endpoint = `${destination.Config.apiUrl}${category.tagEndPoint}?limit=100&offset=${100 * (i + 1)
+endpoint = `${destination.Config.apiUrl}${category.tagEndPoint}?limit=100&offset=${100 * (i + 1)}`
           }`;
         requestOptions = {
           headers: getHeader(destination),

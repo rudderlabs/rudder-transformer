d84ece5c23219122abe3ecf74972a5c0c7f900b6
Sudip Paul <67197965+ItsSudip@users.noreply.github.com>
2023-07-21 12:01:39 +0530

    feat(new integration): stormly cloud mode destination (#2148) (#2361)
    Co-authored-by: Maurice <teeceeiks@gmail.com>

---
+++ test/__tests__/stormly.test.js
@@ -0,0 +1,47 @@
+const integration = "stormly";
+const name = "STORMLY";
+
+const fs = require("fs");
+const path = require("path");
+
+const version = "v0";
+
+const transformer = require(`../../src/${version}/destinations/${integration}/transform`);
+
+// Processor Test files
+const testDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}.json`)
+);
+const testData = JSON.parse(testDataFile);
+
+// Router Test files
+const inputRouterDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_router_input.json`)
+);
+const outputRouterDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}_router_output.json`)
+);
+const inputRouterData = JSON.parse(inputRouterDataFile);
+const expectedRouterData = JSON.parse(outputRouterDataFile);
+
+describe(`${name} Tests`, () => {
+  describe("Processor", () => {
+    testData.forEach(async (dataPoint, index) => {
+      it(`${index}. ${integration} - ${dataPoint.description}`, async () => {
+        try {
+          const output = await transformer.process(dataPoint.input);
+          expect(output).toEqual(dataPoint.output);
+        } catch (error) {
+          expect(error.message).toEqual(dataPoint.output.error);
+        }
+      });
+    });
+  });
+
+  describe("Router", () => {
+    it("Payload", async () => {
+      const routerOutput = await transformer.processRouterDest(inputRouterData);
+      expect(routerOutput).toEqual(expectedRouterData);
+    });
+  });
+});

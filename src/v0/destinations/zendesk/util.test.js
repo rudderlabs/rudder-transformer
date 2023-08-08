2271eadb8135104ed3cf1fb052388b09b90fd450
Gauravudia <gauravudia@rudderstack.com>
2023-08-08 20:35:39 +0530

    feat(zendesk): add source name setting

---
+++ src/v0/destinations/zendesk/util.test.js
@@ -0,0 +1,17 @@
+const { getSourceName } = require('./util');
+
+describe('getSourceName utility test', () => {
+  it('send source name from config', () => {
+    expect('abc').toEqual(getSourceName({ sourceName: 'abc' }));
+  });
+
+  it('send zendesk as the source name from config', () => {
+    expect(() => getSourceName({ sourceName: 'Zendesk ' })).toThrow(
+      'Invalid source name. The source name `zendesk` is not allowed.',
+    );
+  });
+
+  it('source name is not configured in config', () => {
+    expect('Rudder').toEqual(getSourceName({}));
+  });
+});

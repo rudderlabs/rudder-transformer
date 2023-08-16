bdf2a085feffe52688b6dd44a3eddd461cd26355
Chandra shekar Varkala <chandra@rudderlabs.com>
2023-08-16 11:05:52 -0500

    chore: add timeout for all the axios calls

--- src/util/myAxios.js
+++ src/util/myAxios.js
@@ -1,81 +1,40 @@
-const axios = require('axios');
-const stats = require('./stats');
-
-const fireLatencyStat = (startTime, statTags) => {
-  const destType = statTags.destType ? statTags.destType : '';
-  const feature = statTags.feature ? statTags.feature : '';
-  const endpointPath = statTags.endpointPath ? statTags.endpointPath : '';
-  stats.timing('outgoing_request_latency', startTime, {
-    feature,
-    destType,
-    endpointPath,
-  });
-};
+const {
+  httpSend,
+  httpGET,
+  httpDELETE,
+  httpPOST,
+  httpPUT,
+  httpPATCH,
+} = require('../adapters/network');
 
 const send = async (options, statTags = {}) => {
-  const startTime = new Date();
-  let response;
-  try {
-    response = await axios(options);
-  } finally {
-    fireLatencyStat(startTime, statTags);
-  }
-  return response;
+  const res = await httpSend(options, statTags);
+  return res?.response;
 };
 
 const get = async (url, options, statTags = {}) => {
-  const startTime = new Date();
-  let response;
-  try {
-    response = await axios.get(url, options);
-  } finally {
-    fireLatencyStat(startTime, statTags);
-  }
-  return response;
+  const res = await httpGET(url, options, statTags);
+  return res?.response;
 };
 
 const del = async (url, options, statTags = {}) => {
-  const startTime = new Date();
-  let response;
-  try {
-    response = await axios.delete(url, options);
-  } finally {
-    fireLatencyStat(startTime, statTags);
-  }
-  return response;
+  const res = await httpDELETE(url, options, statTags);
+  return res?.response;
 };
 
 const post = async (url, data, options, statTags = {}) => {
-  const startTime = new Date();
-  let response;
-  try {
-    response = await axios.post(url, data, options);
-  } finally {
-    fireLatencyStat(startTime, statTags);
-  }
-  return response;
+  const res = await httpPOST(url, data, options, statTags);
+  return res?.response;
 };
 
 const put = async (url, data, options, statTags = {}) => {
-  const startTime = new Date();
-  let response;
-  try {
-    response = await axios.put(url, data, options);
-  } finally {
-    fireLatencyStat(startTime, statTags);
-  }
-  return response;
+  const res = await httpPUT(url, data, options, statTags);
+  return res?.response;
 };
 
 const patch = async (url, data, options, statTags = {}) => {
-  const startTime = new Date();
-  let response;
-  try {
-    response = await axios.patch(url, data, options);
-  } finally {
-    fireLatencyStat(startTime, statTags);
-  }
-  return response;
+  const res = await httpPATCH(url, data, options, statTags);
+  return res?.response;
 };
 
 module.exports = {

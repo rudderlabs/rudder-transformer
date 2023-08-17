378854f3b0f86383db016359e324e52ff37c49fe
Gauravudia <60897972+Gauravudia@users.noreply.github.com>
2023-08-17 11:33:02 +0530

    fix(splitio): remove empty objects/ array properties (#2488)
    * fix(splitio): remove empty objects/ array properties
    
    * chore: apply review suggestions

--- src/v0/util/index.js
+++ src/v0/util/index.js
@@ -145,23 +145,27 @@ const removeUndefinedNullEmptyExclBoolInt = (obj) => _.pickBy(obj, isDefinedNotN
  * @returns
  */
 const removeUndefinedNullValuesAndEmptyObjectArray = (obj) => {
-  if (Array.isArray(obj)) {
-    const cleanedArray = obj
-      .map((item) => removeUndefinedNullValuesAndEmptyObjectArray(item))
-      .filter((item) => isDefinedAndNotNull(item));
-    return cleanedArray.length === 0 ? null : cleanedArray;
+  function recursive(obj) {
+    if (Array.isArray(obj)) {
+      const cleanedArray = obj
+        .map((item) => recursive(item))
+        .filter((item) => isDefinedAndNotNull(item));
+      return cleanedArray.length === 0 ? null : cleanedArray;
+    }
+    if (obj && typeof obj === 'object') {
+      const data = {};
+      Object.entries(obj).forEach(([key, value]) => {
+        const cleanedValue = recursive(value);
+        if (isDefinedAndNotNull(cleanedValue)) {
+          data[key] = cleanedValue;
+        }
+      });
+      return Object.keys(data).length === 0 ? null : data;
+    }
+    return obj;
   }
-  if (obj && typeof obj === 'object') {
-    const data = {};
-    Object.entries(obj).forEach(([key, value]) => {
-      const cleanedValue = removeUndefinedNullValuesAndEmptyObjectArray(value);
-      if (isDefinedAndNotNull(cleanedValue)) {
-        data[key] = cleanedValue;
-      }
-    });
-    return Object.keys(data).length === 0 ? null : data;
-  }
-  return obj;
+  const newObj = recursive(obj);
+  return isDefinedAndNotNull(newObj) ? newObj : {};
 };
 
 // Format the destination.Config.dynamicMap arrays to hashMap

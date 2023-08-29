db60fcf1adc95d667b534d10d3365ba3ce2ee137
Gauravudia <gauravudia@rudderstack.com>
2023-08-29 15:29:15 +0530

    docs: add comment

--- src/cdk/v2/destinations/launchdarkly_audience/config.js
+++ src/cdk/v2/destinations/launchdarkly_audience/config.js
@@ -1,9 +1,11 @@
-const AUDIENCE_LIST = 'audiencelist';
+const SUPPORTED_MESSAGE_TYPE = 'audiencelist';
 const ACTION_TYPES = ['add', 'remove'];
+// ref:- https://docs.launchdarkly.com/guides/integrations/build-synced-segments?q=othercapabilities#manifest-details
+// ref:- https://docs.launchdarkly.com/home/segments#targeting-users-in-segments
 const ENDPOINT = 'https://app.launchdarkly.com/api/v2/segment-target/rudderstack';
 
 module.exports = {
-  AUDIENCE_LIST,
+  SUPPORTED_MESSAGE_TYPE,
   ACTION_TYPES,
   ENDPOINT,
 };
--- src/cdk/v2/destinations/launchdarkly_audience/utils.js
+++ src/cdk/v2/destinations/launchdarkly_audience/utils.js
@@ -1,11 +1,25 @@
 const { ACTION_TYPES } = require('./config');
 
+/**
+ * Prepares a list of identifiers based on the provided data and identifier key.
+ * @param {*} listData The data containing lists of members to be added or removed from the audience.
+ * @param {*} identifier The unique identifier key for each member. For example, email.
+ * @returns
+ * {
+      "add": [
+          {
+          "id": "test@gmail.com"
+          }
+      ],
+      "remove": []
+   }
+ */
 const prepareIdentifiersList = (listData, identifier) => {
   const list = {};
   const processList = (actionData) =>
     actionData
-      .filter((item) => item.hasOwnProperty(identifier))
-      .map((item) => ({ id: item[identifier] }));
+      .filter((member) => member.hasOwnProperty(identifier))
+      .map((member) => ({ id: member[identifier] }));
   ACTION_TYPES.forEach((action) => {
     list[action] = listData[action] ? processList(listData[action]) : [];
   });

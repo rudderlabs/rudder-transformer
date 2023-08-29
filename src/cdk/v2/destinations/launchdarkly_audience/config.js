a8adf80da13dcd2ea6d8e2338eea6ca978251af1
Gauravudia <gauravudia@rudderstack.com>
2023-08-29 16:04:38 +0530

    test: add testcases

--- src/cdk/v2/destinations/launchdarkly_audience/config.js
+++ src/cdk/v2/destinations/launchdarkly_audience/config.js
@@ -1,11 +1,11 @@
-const SUPPORTED_MESSAGE_TYPE = 'audiencelist';
+const SUPPORTED_EVENT_TYPE = 'audiencelist';
 const ACTION_TYPES = ['add', 'remove'];
 // ref:- https://docs.launchdarkly.com/guides/integrations/build-synced-segments?q=othercapabilities#manifest-details
 // ref:- https://docs.launchdarkly.com/home/segments#targeting-users-in-segments
 const ENDPOINT = 'https://app.launchdarkly.com/api/v2/segment-target/rudderstack';
 
 module.exports = {
-  SUPPORTED_MESSAGE_TYPE,
+  SUPPORTED_EVENT_TYPE,
   ACTION_TYPES,
   ENDPOINT,
 };

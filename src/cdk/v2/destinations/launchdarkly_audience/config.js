7e493004fdc2a7b2f5732a98ce2ddd8391ab6564
Gauravudia <gauravudia@rudderstack.com>
2023-08-30 16:52:15 +0530

    refactor: update identifier logic

--- src/cdk/v2/destinations/launchdarkly_audience/config.js
+++ src/cdk/v2/destinations/launchdarkly_audience/config.js
@@ -1,5 +1,6 @@
 const SUPPORTED_EVENT_TYPE = 'audiencelist';
 const ACTION_TYPES = ['add', 'remove'];
+const IDENTIFIER_KEY = 'identifier';
 // ref:- https://docs.launchdarkly.com/guides/integrations/build-synced-segments?q=othercapabilities#manifest-details
 // ref:- https://docs.launchdarkly.com/home/segments#targeting-users-in-segments
 const ENDPOINT = 'https://app.launchdarkly.com/api/v2/segment-target/rudderstack';
@@ -7,5 +8,6 @@ const ENDPOINT = 'https://app.launchdarkly.com/api/v2/segment-target/rudderstack
 module.exports = {
   SUPPORTED_EVENT_TYPE,
   ACTION_TYPES,
+  IDENTIFIER_KEY,
   ENDPOINT,
 };
--- src/cdk/v2/destinations/launchdarkly_audience/utils.js
+++ src/cdk/v2/destinations/launchdarkly_audience/utils.js
@@ -1,25 +1,24 @@
-const { ACTION_TYPES } = require('./config');
+const { ACTION_TYPES, IDENTIFIER_KEY } = require('./config');
 
 /**
  * Prepares a list of identifiers based on the provided data and identifier key.
  * @param {*} listData The data containing lists of members to be added or removed from the audience.
- * @param {*} identifier The unique identifier key for each member. For example, email.
  * @returns
  * {
       "add": [
           {
-          "id": "test@gmail.com"
+           "id": "test@gmail.com"
           }
       ],
       "remove": []
    }
  */
-const prepareIdentifiersList = (listData, identifier) => {
+const prepareIdentifiersList = (listData) => {
   const list = {};
   const processList = (actionData) =>
     actionData
-      .filter((member) => member.hasOwnProperty(identifier))
-      .map((member) => ({ id: member[identifier] }));
+      .filter((member) => member.hasOwnProperty(IDENTIFIER_KEY))
+      .map((member) => ({ id: member[IDENTIFIER_KEY] }));
   ACTION_TYPES.forEach((action) => {
     list[action] = listData[action] ? processList(listData[action]) : [];
   });

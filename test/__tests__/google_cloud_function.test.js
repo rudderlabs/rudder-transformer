8af1b784761492eb26e2101f8fad851914c73fc5
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-09-08 10:00:31 +0530

    feat: add processor tests

--- test/__tests__/google_cloud_function.test.js
+++ test/__tests__/google_cloud_function.test.js
@@ -7,6 +7,13 @@ const version = "v0";
 
 const transformer = require(`../../src/${version}/destinations/${integration}/transform`);
 
+// Processor Test Data
+const testDataFile = fs.readFileSync(
+  path.resolve(__dirname, `./data/${integration}.json`)
+);
+const testData = JSON.parse(testDataFile);
+
+
 // Router Test Data
 const inputRouterDataFile = fs.readFileSync(
   path.resolve(__dirname, `./data/${integration}_router_input.json`)
@@ -17,7 +24,22 @@ const outputRouterDataFile = fs.readFileSync(
 const inputRouterData = JSON.parse(inputRouterDataFile);
 const expectedRouterData = JSON.parse(outputRouterDataFile);
 
+
+
 describe(`${name} Tests`, () => {
+  describe("Processor", () => {
+    testData.forEach((dataPoint, index) => {
+      it(`${index}. ${integration} - ${dataPoint.description}`, async () => {
+        try {
+          const output = await transformer.process(dataPoint.input);
+          expect(output).toEqual(dataPoint.output);
+        } catch (error) {
+          expect(error.message).toEqual(dataPoint.output.error);
+        }
+      });
+    });
+  });
+
   describe("Router Tests", () => {
     it("Payload", async () => {
       const routerOutput = await transformer.processRouterDest(inputRouterData);

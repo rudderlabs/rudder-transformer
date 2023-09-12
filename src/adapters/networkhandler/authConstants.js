4997b5b9cf80a6d1c3c34cefc54078df2c2157ec
Sai Sankeerth <sanpj2292@github.com>
2023-09-12 16:22:46 +0530

    chore: remove all references of disable_dest and remove the const disable_dest
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- src/adapters/networkhandler/authConstants.js
+++ src/adapters/networkhandler/authConstants.js
@@ -2,8 +2,6 @@
  * This class is used for handling Auth related errors
  */
 module.exports = {
-  // Deprecated: This constant would be deprecated soon
-  DISABLE_DEST: 'DISABLE_DESTINATION',
   REFRESH_TOKEN: 'REFRESH_TOKEN',
   AUTH_STATUS_INACTIVE: 'AUTH_STATUS_INACTIVE',
 };

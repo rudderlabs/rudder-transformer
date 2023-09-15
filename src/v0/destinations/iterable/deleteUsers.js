f51a8c60b485c19d618d08721048f7485c356b45
mihir-4116 <mihirb@rudderstack.com>
2023-09-15 15:55:15 +0530

    feat(iterable): user deletion support

---
+++ src/v0/destinations/iterable/deleteUsers.js
@@ -0,0 +1,67 @@
+const { httpDELETE } = require('../../../adapters/network');
+const { processAxiosResponse } = require('../../../adapters/utils/networkUtils');
+const { isHttpStatusSuccess } = require('../../util');
+const { getDynamicErrorType } = require('../../../adapters/utils/networkUtils');
+const { NetworkError, ConfigurationError } = require('../../util/errorTypes');
+const { executeCommonValidations } = require('../../util/regulation-api');
+const tags = require('../../util/tags');
+const { JSON_MIME_TYPE } = require('../../util/constant');
+
+// Ref-> https://developers.intercom.com/intercom-api-reference/v1.3/reference/permanently-delete-a-user
+const userDeletionHandler = async (userAttributes, config) => {
+  if (!config) {
+    throw new ConfigurationError('Config for deletion not present');
+  }
+  const { apiKey } = config;
+  if (!apiKey) {
+    throw new ConfigurationError('api key for deletion not present');
+  }
+  const validUserIds = [];
+  userAttributes.forEach((userAttribute) => {
+    // Dropping the user if userId is not present
+    if (userAttribute.userId) {
+      validUserIds.push(userAttribute.userId);
+    }
+  });
+  const failedUserDeletions = [];
+  await Promise.all(
+    validUserIds.map(async (uId) => {
+      const url = `https://api.iterable.com/api/users/byUserId/${uId}`;
+      const requestOptions = {
+        headers: {
+          'Content-Type': JSON_MIME_TYPE,
+          api_key: apiKey,
+        },
+      };
+      const resp = await httpDELETE(url, requestOptions, {
+        destType: 'iterable',
+        feature: 'deleteUsers',
+      });
+      const handledDelResponse = processAxiosResponse(resp);
+      if (!isHttpStatusSuccess(handledDelResponse.status) && handledDelResponse.status !== 404) {
+        failedUserDeletions.push(uId);
+      }
+    }),
+  );
+
+  if (failedUserDeletions.length > 0) {
+    throw new NetworkError(
+      `User deletion request failed for userIds : ${failedUserDeletions}`,
+      400,
+      {
+        [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(400),
+      },
+      failedUserDeletions,
+    );
+  }
+
+  return { statusCode: 200, status: 'successful' };
+};
+const processDeleteUsers = async (event) => {
+  const { userAttributes, config } = event;
+  executeCommonValidations(userAttributes);
+  const resp = await userDeletionHandler(userAttributes, config);
+  return resp;
+};
+
+module.exports = { processDeleteUsers };

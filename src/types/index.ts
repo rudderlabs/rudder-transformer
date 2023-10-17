c8d3882baccc57d7b892c55ff9811c951afb5ec6
srikanth <srikanth.iit@gmail.com>
2023-10-17 18:01:54 -0500

    fix: ut metadata map ts type

--- src/types/index.ts
+++ src/types/index.ts
@@ -52,9 +52,9 @@ type Metadata = {
   transformationId: string;
 };
 
-type MessageIdMetadataMap {
+type MessageIdMetadataMap = {
   [key: string]: Metadata;
-}
+};
 
 type UserTransformationInput = {
   VersionID: string;

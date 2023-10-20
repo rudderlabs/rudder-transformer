44f2772836292a354fdecc43a4acf3d47b8916f4
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-10-20 09:52:00 +0530

    fix: fix merge by values

--- src/cdk/v2/destinations/ortto/config.js
+++ src/cdk/v2/destinations/ortto/config.js
@@ -1,6 +1,14 @@
-const IDENTIFY_ENDPOINT = 'https://api.ap3api.com/v1/person/merge';
+const IDENTIFY_ENDPOINT = {
+  au: 'https://api.au.ap3api.com/v1/person/merge',
+  eu: 'https://api.eu.ap3api.com/v1/person/merge',
+  other: 'https://api.ap3api.com/v1/person/merge',
+};
 // https://help.ortto.com/developer/latest/api-reference/person/merge.html#person-fields
-const TRACK_ENDPOINT = 'https://api.ap3api.com/v1/activities/create;';
+const TRACK_ENDPOINT = {
+  au: 'https://api.au.ap3api.com/v1/activities/create',
+  eu: 'https://api.eu.ap3api.com/v1/activities/create',
+  other: 'https://api.ap3api.com/v1/activities/create',
+};
 // https://help.ortto.com/a-271-create-a-custom-activity-event-create
 
 const maxBatchSize = 1;

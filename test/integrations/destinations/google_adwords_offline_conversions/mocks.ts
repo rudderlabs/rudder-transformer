64bc842e88b463fb947d3168ffee4de7d5b4eba6
Sai Sankeerth <sanpj2292@github.com>
2023-10-21 20:10:54 +0100

    fix: remove unnecessary log
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- test/integrations/destinations/google_adwords_offline_conversions/mocks.ts
+++ test/integrations/destinations/google_adwords_offline_conversions/mocks.ts
@@ -2,7 +2,6 @@ import helper from '../../../../src/v0/destinations/google_adwords_offline_conve
 
 export const timestampMock = (_, timestampValue: string = '2019-10-14 16:45:18+05:30') => {
   jest.spyOn(helper, 'formatTimestamp').mockImplementation((_v) => {
-    console.error(`'Input: ${_v} ---- timestampValue: ${timestampValue}'`);
     return timestampValue;
   });
 };

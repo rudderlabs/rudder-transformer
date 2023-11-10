1b260947ea4e8917fd75cc55d8d4c738e46ed67d
Anant Jain <anantjain45823@gmail.com>
2023-11-10 09:18:58 +0530

    chore: add test case for version v1 for webhook source means no alters in payload

--- src/controllers/util/index.test.ts
+++ src/controllers/util/index.test.ts
@@ -22,7 +22,7 @@ describe('adaptInputToVersion', () => {
 
     expect(result).toEqual(expected);
   });
-  it('should return the input unchanged when the implementation version and request version are the same', () => {
+  it('should return the input unchanged when the implementation version and request version are the same i.e. v0', () => {
     const sourceType = 'pipedream';
     const requestVersion = 'v0';
     const input = [
@@ -43,7 +43,45 @@ describe('adaptInputToVersion', () => {
 
     expect(result).toEqual(expected);
   });
+  it('should return the input unchanged when the implementation version and request version are the same i.e. v1', () => {
+    const sourceType = 'webhook';
+    const requestVersion = 'v1';
+    const input = [
+      {
+        event: { key1: 'val1', key2: 'val2' },
+        source: { id: 'source_id', config: { configField1: 'configVal1' } },
+      },
+      {
+        event: { key1: 'val1', key2: 'val2' },
+        source: { id: 'source_id', config: { configField1: 'configVal1' } },
+      },
+      {
+        event: { key1: 'val1', key2: 'val2' },
+        source: { id: 'source_id', config: { configField1: 'configVal1' } },
+      },
+    ];
+    const expected = {
+      implementationVersion: 'v1',
+      input: [
+        {
+          event: { key1: 'val1', key2: 'val2' },
+          source: { id: 'source_id', config: { configField1: 'configVal1' } },
+        },
+        {
+          event: { key1: 'val1', key2: 'val2' },
+          source: { id: 'source_id', config: { configField1: 'configVal1' } },
+        },
+        {
+          event: { key1: 'val1', key2: 'val2' },
+          source: { id: 'source_id', config: { configField1: 'configVal1' } },
+        },
+      ],
+    };
 
+    const result = ControllerUtility.adaptInputToVersion(sourceType, requestVersion, input);
+
+    expect(result).toEqual(expected);
+  });
   it('should convert input from v0 to v1 when the request version is v0 and the implementation version is v1', () => {
     const sourceType = 'webhook';
     const requestVersion = 'v0';

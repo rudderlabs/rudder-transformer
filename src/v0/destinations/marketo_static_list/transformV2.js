15c9b5ced83bc154f60c75be60d3f3bd2e3330af
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2023-11-23 11:31:41 +0530

    fix: missed error lib

--- src/v0/destinations/marketo_static_list/transformV2.js
+++ src/v0/destinations/marketo_static_list/transformV2.js
@@ -1,4 +1,5 @@
 const lodash = require('lodash');
+const { InstrumentationError, UnauthorizedError } = require('@rudderstack/integrations-lib');
 const {
   defaultPostRequestConfig,
   defaultDeleteRequestConfig,
@@ -11,7 +12,6 @@ const {
 } = require('../../util');
 const { JSON_MIME_TYPE } = require('../../util/constant');
 const { MAX_LEAD_IDS_SIZE } = require('./config');
-const { InstrumentationError, UnauthorizedError } = require('../../util/errorTypes');
 const { getAuthToken } = require('../marketo/transform');
 const { formatConfig } = require('../marketo/config');
 

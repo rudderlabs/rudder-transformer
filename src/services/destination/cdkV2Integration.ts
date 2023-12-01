1db5cd8d0ba97ca2715f03df6c8596f3d28bc70c
Anant Jain <62471433+anantjain45823@users.noreply.github.com>
2023-12-01 17:08:20 +0530

    Update cdkV2Integration.ts

--- src/services/destination/cdkV2Integration.ts
+++ src/services/destination/cdkV2Integration.ts
@@ -64,7 +64,7 @@ export class CDKV2DestinationService implements DestinationService {
             destinationType,
             event,
             tags.FEATURES.PROCESSOR,
-            requestMetadata,
+            requestMetadata
           );
 
           stats.increment('event_transform_success', {
@@ -127,12 +127,7 @@ export class CDKV2DestinationService implements DestinationService {
           metaTo.metadata = destInputArray[0].metadata;
           try {
             const doRouterTransformationResponse: RouterTransformationResponse[] =
-              await processCdkV2Workflow(
-                destinationType,
-                destInputArray,
-                tags.FEATURES.ROUTER,
-                requestMetadata,
-              );
+              await processCdkV2Workflow(destinationType, destInputArray, tags.FEATURES.ROUTER, requestMetadata);
             return DestinationPostTransformationService.handleRouterTransformSuccessEvents(
               doRouterTransformationResponse,
               undefined,

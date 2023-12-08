6a81aadcf6d10a0448cd5307255664b28587fa80
ujjwal-ab <ujjwalabhishek98@gmail.com>
2023-12-08 18:53:02 +0530

    feat: refactor networkHandler

--- src/v0/destinations/reddit/networkHandler.js
+++ src/v0/destinations/reddit/networkHandler.js
@@ -1,4 +1,4 @@
-const { ThrottledError, AbortedError, RetryableError } = require('@rudderstack/integrations-lib');
+const { RetryableError } = require('@rudderstack/integrations-lib');
 const { prepareProxyRequest, proxyRequest } = require('../../../adapters/network');
 const { isHttpStatusSuccess } = require('../../util/index');
 const { REFRESH_TOKEN } = require('../../../adapters/networkhandler/authConstants');
@@ -8,7 +8,7 @@ const {
   processAxiosResponse,
 } = require('../../../adapters/utils/networkUtils');
 
-const redditRespHandler = (destResponse, stageMsg) => {
+const redditRespHandler = (destResponse) => {
   const { status, response } = destResponse;
 
   const errorMessage = response?.invalid_events[0]?.errorMessage;
@@ -23,28 +23,14 @@ const redditRespHandler = (destResponse, stageMsg) => {
       destResponse,
       REFRESH_TOKEN,
     );
-  } else if (status === 429) {
-    throw new ThrottledError(
-      `Request Failed: ${stageMsg} - due to Request Limit exceeded, (Throttled)`,
-      destResponse,
-    );
-  } else if (status === 503 || status === 500) {
-    // see if its 500 internal error or 503 service unavailable
-    throw new RetryableError(`Request Failed: ${stageMsg} (Retryable)`, 500, destResponse);
   }
-  // else throw the error
-  throw new AbortedError(
-    `Request Failed: ${stageMsg} with status "${status}" due to "${errorMessage}`,
-    400,
-    destResponse,
-  );
 };
 const responseHandler = (destinationResponse) => {
   const message = `Request Processed Successfully`;
   const { status } = destinationResponse;
   if (!isHttpStatusSuccess(status)) {
     // if error, successfully return status, message and original destination response
-    redditRespHandler(destinationResponse, 'during reddit response transformation');
+    redditRespHandler(destinationResponse);
   }
   // Mostly any error will not have a status of 2xx
   return {

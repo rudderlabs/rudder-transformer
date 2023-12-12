070099240b119ecfb82b20802ebcf62b8679aae8
AASHISH MALIK <imashishmalik.45@gmail.com>
2023-12-12 13:06:49 +0530

    fix: remove email if not string from attributes

--- src/v0/destinations/braze/transform.js
+++ src/v0/destinations/braze/transform.js
@@ -154,11 +154,16 @@ function getUserAttributesObject(message, mappingJson, destination) {
   Object.keys(mappingJson).forEach((destKey) => {
     let value = get(traits, mappingJson[destKey]);
     if (value || (value === null && reservedKeys.includes(destKey))) {
+      // if email is not string remove it from attributes
+      if (destKey === 'email' && typeof value !== 'string') {
+        return;
+      }
+
       // handle gender special case
       if (destKey === 'gender') {
         value = formatGender(value);
       } else if (destKey === 'email' && isDefinedAndNotNull(value)) {
-        value = value?.toString().toLowerCase();
+        value = value.toString().toLowerCase();
       }
       data[destKey] = value;
     }

99613a098a279f2cce55a394b62c53df78aa1e57
Sudip Paul <67197965+ItsSudip@users.noreply.github.com>
2023-12-20 12:24:47 +0530

    chore: added component test cases for desitnations part3 (#2927)
    * chore: added component test cases for desitnations
    
    * chore: added component test cases for wootric

--- test/__mocks__/axios.js
+++ test/__mocks__/axios.js
@@ -14,13 +14,8 @@ const gainsightRequestHandler = require("./gainsight.mock");
 const mailchimpGetRequestHandler = require("./mailchimp.mock");
 const yahooDspPostRequestHandler = require("./yahoo_dsp.mock");
 const { gainsightPXGetRequestHandler } = require("./gainsight_px.mock");
-const { hsGetRequestHandler, hsPostRequestHandler } = require("./hs.mock");
 const profitwellGetRequestHandler = require("./profitwell.mock");
 const cannyPostRequestHandler = require("./canny.mock");
-const {
-  wootricGetRequestHandler,
-  wootricPostRequestHandler
-} = require("./wootric.mock");
 const { userGetRequestHandler, userPutRequestHandler } = require("./user.mock");
 const { mixpanelPostRequestHandler } = require("./mixpanel.mock");
 const { mondayPostRequestHandler } = require("./monday.mock");
@@ -29,7 +24,6 @@ const { sendinblueGetRequestHandler } = require("./sendinblue.mock");
 const { courierGetRequestHandler } = require("./courier.mock");
 
 const urlDirectoryMap = {
-  "api.hubapi.com": "hs",
   "zendesk.com": "zendesk",
   "salesforce.com": "salesforce",
   "mktorest.com": "marketo",
@@ -94,16 +88,22 @@ function get(url, options) {
   if (url.includes("https://a.klaviyo.com/api/v2/people/search")) {
     return klaviyoGetRequestHandler(getParamEncodedUrl(url, options));
   }
-  if (url.includes("https://api.hubapi.com")) {
-    return hsGetRequestHandler(url, mockData);
-  }
   if (url.includes("https://api.profitwell.com")) {
     return profitwellGetRequestHandler(url, mockData);
   }
-  if (url.includes("https://api.wootric.com")) {
-    return new Promise((resolve, reject) => {
-      resolve(wootricGetRequestHandler(url));
-    });
+  if (
+    url.includes(
+      "https://api.getdrip.com/v2/1809802/subscribers/identified_user@gmail.com"
+    )
+  ) {
+    return Promise.resolve({ status: 200 });
+  }
+  if (
+    url.includes(
+      "https://api.getdrip.com/v2/1809802/subscribers/unidentified_user@gmail.com"
+    )
+  ) {
+    return Promise.reject({ status: 404 });
   }
   if (url.includes("https://commander.user.com")) {
     return new Promise((resolve, reject) => {
@@ -162,14 +162,6 @@ function post(url, payload) {
   if (url.includes("https://canny.io/api/v1/users/retrieve")) {
     return new Promise((resolve, reject) => {
       resolve(cannyPostRequestHandler(url));
-    });
-  }
-  if (url.includes("https://api.hubapi.com")) {
-    return hsPostRequestHandler(payload, mockData);
-  }
-  if (url.includes("https://api.wootric.com")) {
-    return new Promise((resolve, reject) => {
-      resolve(wootricPostRequestHandler(url, payload));
     });
   }
   if (

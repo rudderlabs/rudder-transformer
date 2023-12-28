8e86313c3edd6d168b13160df17b33f892f01b2d
Anant Jain <anantjain45823@gmail.com>
2023-12-28 15:27:33 +0530

    feat: onboard new tiktok version events 2.0

--- src/v0/destinations/tiktok_ads/transformV2.js
+++ src/v0/destinations/tiktok_ads/transformV2.js
@@ -1,8 +1,6 @@
 /* eslint-disable camelcase */
 /* eslint-disable @typescript-eslint/naming-convention */
 const _ = require('lodash');
-const { SHA256 } = require('crypto-js');
-const get = require('get-value');
 const set = require('set-value');
 const { ConfigurationError, InstrumentationError } = require('@rudderstack/integrations-lib');
 const { EventType } = require('../../../constants');
@@ -15,121 +13,62 @@ const {
   getSuccessRespEvents,
   isDefinedAndNotNullAndNotEmpty,
   getDestinationExternalID,
-  getFieldValueFromMessage,
   getHashFromArrayWithDuplicate,
   checkInvalidRtTfEvents,
   handleRtTfSingleEventError,
   batchMultiplexedEvents,
 } = require('../../util');
-const { process: processV2, processRouterDest: processRouterDestV2 } = require('./transformV2');
-const { getContents } = require('./util');
+const { getContents, hashUserField } = require('./util');
 const {
-  trackMapping,
-  TRACK_ENDPOINT,
+  trackMappingV2,
+  trackEndpointV2,
   BATCH_ENDPOINT,
   eventNameMapping,
   MAX_BATCH_SIZE,
   PARTNER_NAME,
+  maxBatchSizeV2,
 } = require('./config');
 const { JSON_MIME_TYPE } = require('../../util/constant');
 
-const USER_EMAIL_KEY_PATH = 'context.user.email';
-const USER_PHONE_NUMBER_KEY_PATH = 'context.user.phone_number';
+/**
+ * Generated track payload for an event using TikTokTrackV2.json config mapping,
+ * hashing user properties and
+ * defining contents from products
+ * @param {*} message
+ * @param {*} Config
+ * @param {*} event
+ * @returns track payload
+ */
+const getTrackResponsePayload = (message, Config, event) => {
+  const payload = constructPayload(message, trackMappingV2);
 
-const checkContentType = (contents, contentType) => {
-  if (Array.isArray(contents)) {
-    contents.forEach((content) => {
-      if (!content.content_type) {
-        // eslint-disable-next-line no-param-reassign
-        content.content_type = contentType || 'product_group';
-      }
-    });
-  }
-  return contents;
-};
-
-const getTrackResponse = (message, Config, event) => {
-  const pixel_code = Config.pixelCode;
-  let payload = constructPayload(message, trackMapping);
-
-  // if contents is not an array
+  // if contents is not an array converting it into array
   if (payload.properties?.contents && !Array.isArray(payload.properties.contents)) {
     payload.properties.contents = [payload.properties.contents];
   }
 
+  // if contents is not present but we have properties.products present which has fields with superset of contents fields
   if (payload.properties && !payload.properties?.contents && message.properties?.products) {
     // retreiving data from products only when contents is not present
-    payload.properties = {
-      ...payload.properties,
-      contents: getContents(message),
-    };
+    payload.properties.contents = getContents(message, false);
   }
 
-  if (payload.properties?.contents) {
-    payload.properties.contents = checkContentType(
-      payload.properties?.contents,
-      message.properties?.contentType,
-    );
-  }
-
+  // getting externalId and hashing it and storing it in
   const externalId = getDestinationExternalID(message, 'tiktokExternalId');
   if (isDefinedAndNotNullAndNotEmpty(externalId)) {
-    set(payload, 'context.user.external_id', externalId);
+    set(payload, 'user.external_id', externalId);
   }
-
-  const traits = getFieldValueFromMessage(message, 'traits');
-
-  // taking user properties like email and phone from traits
-  let email = get(payload, USER_EMAIL_KEY_PATH);
-  if (!isDefinedAndNotNullAndNotEmpty(email) && traits?.email) {
-    set(payload, USER_EMAIL_KEY_PATH, traits.email);
+  if (Config.hashUserProperties && isDefinedAndNotNullAndNotEmpty(payload.user)) {
+    payload.user = hashUserField(payload.user);
   }
-
-  let phone_number = get(payload, USER_PHONE_NUMBER_KEY_PATH);
-  if (!isDefinedAndNotNullAndNotEmpty(phone_number) && traits?.phone) {
-    set(payload, USER_PHONE_NUMBER_KEY_PATH, traits.phone);
-  }
-
-  payload = { pixel_code, event, ...payload };
-
-  /*
-   * Hashing user related detail i.e external_id, email, phone_number
-   */
-
-  if (Config.hashUserProperties) {
-    const external_id = get(payload, 'context.user.external_id');
-    if (isDefinedAndNotNullAndNotEmpty(external_id)) {
-      payload.context.user.external_id = SHA256(external_id.trim()).toString();
-    }
-
-    email = get(payload, USER_EMAIL_KEY_PATH);
-    if (isDefinedAndNotNullAndNotEmpty(email)) {
-      payload.context.user.email = SHA256(email.trim().toLowerCase()).toString();
-    }
-
-    phone_number = get(payload, USER_PHONE_NUMBER_KEY_PATH);
-    if (isDefinedAndNotNullAndNotEmpty(phone_number)) {
-      payload.context.user.phone_number = SHA256(phone_number.trim()).toString();
-    }
-  }
-  const response = defaultRequestConfig();
-  response.headers = {
-    'Access-Token': Config.accessToken,
-    'Content-Type': JSON_MIME_TYPE,
-  };
-
-  response.method = defaultPostRequestConfig.requestMethod;
-  response.endpoint = TRACK_ENDPOINT;
-  // add partner name
-  response.body.JSON = removeUndefinedAndNullValues({
-    ...payload,
-    partner_name: PARTNER_NAME,
-  });
-  return response;
+  payload.event = event;
+  payload.partner_name = PARTNER_NAME;
+  // add partner name and return payload
+  return removeUndefinedAndNullValues(payload);
 };
 
 const trackResponseBuilder = async (message, { Config }) => {
-  const { eventsToStandard, sendCustomEvents } = Config;
+  const { eventsToStandard, sendCustomEvents, accessToken, pixelCode } = Config;
 
   let event = message.event?.toLowerCase().trim();
   if (!event) {
@@ -143,34 +82,43 @@ const trackResponseBuilder = async (message, { Config }) => {
       `Event name (${event}) is not valid, must be mapped to one of standard events`,
     );
   }
-
+  const response = defaultRequestConfig();
+  response.headers = {
+    'Access-Token': accessToken,
+    'Content-Type': JSON_MIME_TYPE,
+  };
+  // tiktok doc for request: https://business-api.tiktok.com/portal/docs?id=1771100865818625
+  response.method = defaultPostRequestConfig.requestMethod;
+  response.endpoint = trackEndpointV2;
   const responseList = [];
   if (standardEventsMap[event]) {
     Object.keys(standardEventsMap).forEach((key) => {
       if (key === event) {
         standardEventsMap[event].forEach((eventName) => {
-          responseList.push(getTrackResponse(message, Config, eventName));
+          responseList.push(getTrackResponsePayload(message, Config, eventName));
         });
       }
     });
-    return responseList;
+  } else {
+    /* 
+    For custom event we do not want to lower case the event or trim it we just want to send those as it is
+    Doc https://ads.tiktok.com/help/article/standard-events-parameters?lang=en
+    */
+    event = eventNameMapping[event] || message.event;
+    // if there exists no event mapping we will build payload with custom event recieved
+    responseList.push(getTrackResponsePayload(message, Config, event));
   }
-  // Doc https://ads.tiktok.com/help/article/standard-events-parameters?lang=en
-  // For custom event we do not want to lower case the event or trim it we just want to send those as it is
-  event = eventNameMapping[event] || message.event;
-  // if there exists no event mapping we will build payload with custom event recieved
-  responseList.push(getTrackResponse(message, Config, event));
-
-  return responseList;
+  // set event source and event_source_id
+  response.body.JSON = { event_source: 'web', event_source_id: pixelCode };
+  response.body.JSON.data = responseList;
+  return response;
 };
 
 const process = async (event) => {
   const { message, destination } = event;
-  if (destination.Config?.version === 'v2') {
-    return processV2(event);
-  }
+
   if (!destination.Config.accessToken) {
-    throw new ConfigurationError('Access Token not found. Aborting ');
+    throw new ConfigurationError('Access Token not found. Aborting');
   }
 
   if (!destination.Config.pixelCode) {
@@ -189,11 +137,10 @@ const process = async (event) => {
   } else {
     throw new InstrumentationError(`Event type ${messageType} is not supported`);
   }
-
   return response;
 };
 
-function batchEvents(eventsChunk) {
+const batchEvents = (eventsChunk) => {
   const { destination, events } = eventsChunk;
   const { accessToken, pixelCode } = destination.Config;
   const { batchedRequest } = defaultBatchRequestConfig();
@@ -203,7 +150,7 @@ function batchEvents(eventsChunk) {
     // extracting destination
     // from the first event in a batch
     const cloneTransformedEvent = _.clone(transformedEvent);
-    delete cloneTransformedEvent.body.JSON.pixel_code;
+    delete cloneTransformedEvent.body.JSON.pixelCode;
     // Partner name must be added above "batch": [..]
     delete cloneTransformedEvent.body.JSON.partner_name;
     cloneTransformedEvent.body.JSON.type = 'track';
@@ -211,7 +158,7 @@ function batchEvents(eventsChunk) {
   });
 
   batchedRequest.body.JSON = {
-    pixel_code: pixelCode,
+    pixelCode: pixelCode,
     partner_name: PARTNER_NAME,
     batch: batchResponseList,
   };
@@ -223,9 +170,9 @@ function batchEvents(eventsChunk) {
   };
 
   return batchedRequest;
-}
+};
 
-function getEventChunks(event, trackResponseList, eventsChunk) {
+const getEventChunks = (event, trackResponseList, eventsChunk) => {
   // only for already transformed payload
   // eslint-disable-next-line no-param-reassign
   event.message = Array.isArray(event.message) ? event.message : [event.message];
@@ -240,14 +187,9 @@ function getEventChunks(event, trackResponseList, eventsChunk) {
       destination: event.destination,
     });
   }
-}
+};
 
 const processRouterDest = async (inputs, reqMetadata) => {
-  const { destination } = inputs[0];
-  const { Config } = destination;
-  if (Config?.version === 'v2') {
-    return processRouterDestV2(inputs, reqMetadata);
-  }
   const errorRespEvents = checkInvalidRtTfEvents(inputs);
   if (errorRespEvents.length > 0) {
     return errorRespEvents;
--- src/v0/destinations/tiktok_ads/util.js
+++ src/v0/destinations/tiktok_ads/util.js
@@ -1,11 +1,20 @@
+const { SHA256 } = require('crypto-js');
 const { removeUndefinedAndNullValues } = require('../../util');
+/**
+ * This returns content type from properties object if nothing is present in payload it return default values 'product
+ * @param {*} properties message.proeprties
+ * @returns
+ */
+const getContentTypeOnly = (properties) =>
+  properties.contentType || properties.content_type || 'product';
 
 /**
  * Prepare contents array from products array
  * @param {*} message
+ * @param {*} getContentType if true contents.$.content_type is mapped otherwise not
  * @returns
  */
-const getContents = (message) => {
+const getContents = (message, getContentType = true) => {
   const contents = [];
   const { properties } = message;
   // eslint-disable-next-line @typescript-eslint/naming-convention
@@ -13,15 +22,16 @@ const getContents = (message) => {
   if (products && Array.isArray(products) && products.length > 0) {
     products.forEach((product) => {
       const singleProduct = {
-        content_type:
-          product.contentType || contentType || product.content_type || content_type || 'product',
+        content_type: getContentType
+          ? product.contentType || contentType || product.contentType || content_type || 'product'
+          : undefined,
         content_id: String(product.product_id),
         content_category: product.category,
         content_name: product.name,
         price: product.price,
         quantity: product.quantity,
         description: product.description,
-        brand: product.brand
+        brand: product.brand,
       };
       contents.push(removeUndefinedAndNullValues(singleProduct));
     });
@@ -29,4 +39,35 @@ const getContents = (message) => {
   return contents;
 };
 
-module.exports = { getContents };
+/*
+ * Hashing user related detail i.e external_id, email, phone_number
+ */
+const hashUserField = (user) => {
+  const updatedUser = { ...user };
+  // hashing external_id
+  const { email, phone, external_id: externalId } = user;
+  if (externalId) {
+    // if there are multiple externalId's in form of array that tiktok supports then hashing every
+    if (Array.isArray(externalId)) {
+      updatedUser.external_id = externalId.map((extId) =>
+        SHA256(extId.toString().trim()).toString(),
+      );
+    } else {
+      updatedUser.external_id = SHA256(externalId.toString().trim()).toString();
+    }
+  }
+  // hashing email
+  if (email && email.length > 0) {
+    updatedUser.email = SHA256(email).toString();
+  }
+  // hashing phone
+  if (phone && phone.length > 0) {
+    if (Array.isArray(phone)) {
+      updatedUser.phone = phone.map((num) => SHA256(num).toString());
+    } else {
+      updatedUser.phone = SHA256(phone).toString();
+    }
+  }
+  return updatedUser;
+};
+module.exports = { getContents, getContentTypeOnly, hashUserField };

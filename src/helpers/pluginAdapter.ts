96db947a21b96080dd5e4ac0fbd2a18b14aa0d3e
Utsab Chowdhury <utsab.c97@gmail.com>
2024-01-07 18:16:03 +0530

    chore: modularise plugin adapter

--- src/helpers/pluginAdapter.ts
+++ src/helpers/pluginAdapter.ts
@@ -21,15 +21,35 @@ import {
 import { generateErrorObject } from '../v0/util';
 import { MappedToDestinationKey } from '../constants';
 
-// error handling
+type TransformedPayloadInfo = {
+  payload: any[];
+  metadata: Metadata[];
+  destination: Destination;
+};
+
+type ErrorInfo = {
+  error: any;
+  metadata: Metadata[];
+  destination: Destination;
+};
+
+type ProcTransformedState = {
+  payload: TransformedOutput;
+  metadata: Metadata;
+  destination: Destination;
+};
+
+type RtTransformedState = {
+  payload: TransformedOutput[];
+  metadata: Metadata[];
+  destination: Destination;
+};
+
+type ErrorState = { metadata: Metadata; response: any; destination: Destination };
 
 export class PluginAdapter {
   private static pluginCache: Map<string, Integration> = new Map();
 
-  /**
-   *
-   * @param metadata Deduplicate metadata based on jobId
-   */
   static deduplicateMetadata(metadata: Metadata[]) {
     const jobIdMap = new Map();
     const deduplicatedMetadata: Metadata[] = [];
@@ -50,17 +70,71 @@ export class PluginAdapter {
     if (this.pluginCache.has(cacheKey)) {
       return this.pluginCache.get(cacheKey) as Integration;
     }
-    // TODO: default integration config need to make it dynamic by making some sort of config call or get it from config file
-    // const integrationConfig: IntegrationConfig = {
-    //   name: integrationName,
-    //   saveResponse: true,
-    //   eventOrdering: true,
-    //   plugins: ['preprocessor', 'multiplexer'],
-    // };
-
     const integration = await IntegrationsFactory.createIntegration(integrationName, workflowType);
     this.pluginCache.set(cacheKey, integration);
     return integration;
+  }
+
+  static handleErrors(errors: ErrorInfo[], destination: Destination): ErrorState[] {
+    const errorList: ErrorState[] = [];
+
+    if (errors.length > 0) {
+      const nestedErrorList = errors.map((e) =>
+        e.metadata.map((metadata) => ({
+          metadata,
+          response: generateErrorObject(e.error),
+          destination,
+        })),
+      );
+      errorList.push(...nestedErrorList.flat());
+    }
+
+    return errorList;
+  }
+
+  // Proc Transform Related Functions
+
+  static handleProcSuccess(
+    responseList: TransformedPayloadInfo[],
+    destination: Destination,
+  ): ProcTransformedState[] {
+    const transformedPayloadList: ProcTransformedState[] = [];
+
+    for (const [_, response] of responseList.entries()) {
+      for (const [index, payload] of response.payload.entries()) {
+        const transformedPayload = payload as TransformedOutput;
+        transformedPayloadList.push({
+          payload: transformedPayload,
+          metadata: response.metadata[index],
+          destination,
+        });
+      }
+    }
+
+    return transformedPayloadList;
+  }
+
+  static async processProcEvents(
+    integrationPlugin: Integration,
+    events: ProcessorTransformationRequest[],
+    destination: Destination,
+  ): Promise<{ transformedPayloadList: ProcTransformedState[]; errorList: ErrorState[] }> {
+    const eventsPayload = events.map((input) => ({
+      event: [{ message: input.message as RudderStackEvent } as RudderStackEventPayload],
+      metadata: [input.metadata],
+    }));
+
+    const output = await integrationPlugin.execute(eventsPayload, destination);
+    const responseList = output.resultContext;
+    const errors = output.errorResults;
+
+    // Handle Errors
+    const errorList = PluginAdapter.handleErrors(errors, destination);
+
+    // Handle Success
+    const transformedPayloadList = PluginAdapter.handleProcSuccess(responseList, destination);
+
+    return { transformedPayloadList, errorList };
   }
 
   public static async transformAtProcessor(
@@ -81,46 +155,17 @@ export class PluginAdapter {
 
     const result = await Promise.all(
       eventsPerDestinationId.map(async (inputs) => {
-        const events = inputs.map((input) => ({
-          event: [{ message: input.message as RudderStackEvent } as RudderStackEventPayload],
-          metadata: [input.metadata],
-        }));
         const { destination } = inputs[0];
-        const output = await integrationPlugin.execute(events, destination);
-        const responseList = output.resultContext;
-        const errors = output.errorResults;
+        const output = await PluginAdapter.processProcEvents(
+          integrationPlugin,
+          inputs,
+          destination,
+        );
 
-        const errorList: { metadata: Metadata; response: any; destination: Destination }[] = [];
-        // handle the error scenario
-        if (errors.length > 0) {
-          const nestedErrorList = errors.map((e) => {
-            const errResponses = e.metadata.map((metadata) => ({
-              metadata,
-              response: generateErrorObject(e.error), // add further tags here
-              destination,
-            }));
-            return errResponses;
-          });
-          errorList.push(...nestedErrorList.flat());
-        }
-
-        // handle the success scenario
-        const transformedPayloadList: {
-          payload: TransformedOutput;
-          metadata: Metadata;
-          destination: Destination;
-        }[] = [];
-        for (const [_, response] of responseList.entries()) {
-          for (const [index, payload] of response.payload.entries()) {
-            const transformedPayload = payload as TransformedOutput;
-            transformedPayloadList.push({
-              payload: transformedPayload,
-              metadata: response.metadata[index],
-              destination,
-            });
-          }
-        }
-        return { transformedPayloadList, errorList };
+        return {
+          transformedPayloadList: output.transformedPayloadList,
+          errorList: output.errorList,
+        };
       }),
     );
 
@@ -133,6 +178,122 @@ export class PluginAdapter {
       result.flatMap((res) => res.errorList);
 
     return { allSuccessList, allErrorList };
+  }
+
+  // Rt Transform Related Functions
+
+  static rankResponsesByUniqueJobIds(responseList: TransformedPayloadInfo[]) {
+    // ranking the responses based on the number of unique jobIds in the metadata array
+    const uniqueJobRank: { uniqueJobIds: number; index: number }[] = [];
+    for (const [index, response] of responseList.entries()) {
+      const uniqueJobIds = Array.from(new Set(response.metadata.map((meta) => meta.jobId)));
+      uniqueJobRank.push({
+        uniqueJobIds: uniqueJobIds.length,
+        index,
+      });
+    }
+    uniqueJobRank.sort((a, b) => b.uniqueJobIds - a.uniqueJobIds);
+    // ranking ends here with uniqueJobRank containing the index of the responseList in the order of the number of unique jobIds in the metadata array
+    // example: [ { uniqueJobIds: 3, index: 0 }, { uniqueJobIds: 2, index: 1 } ]
+
+    return uniqueJobRank;
+  }
+
+  static createFinalResponse(
+    uniqueJobRank: { uniqueJobIds: number; index: number }[],
+    responseList: TransformedPayloadInfo[],
+    destination: Destination,
+  ): RtTransformedState[] {
+    const finalResponse: RtTransformedState[] = [];
+    // creating a map of jobId to position in the metadata array
+    // example: { jobId1: 1, jobId2: 1, jobId3: 0, jobId4: 2}
+    // motivation: prevent metadata duplication in the final response at all levels
+    const jobIdPositionMap: Map<number, number> = new Map();
+
+    for (const rank of uniqueJobRank) {
+      // iteratively checking payloads with the highest number of unique jobIds to lowest
+      const rankedResponse = responseList[rank.index];
+      let isCurrentResponseAddedToFinalPayload = false;
+      // iterate each metadata in the rankedResponse to check if any jobId is already present in the finalResponse
+      for (const meta of rankedResponse.metadata) {
+        // check if the jobId already has a position in final response
+        if (jobIdPositionMap.has(meta.jobId)) {
+          // if yes, then we need append the entire rankedResponse including all the payloads and metadata at same position
+          const position = jobIdPositionMap.get(meta.jobId) as number;
+          const currentOutput = rankedResponse.payload.map(
+            (payload) => payload as TransformedOutput,
+          );
+          finalResponse[position].payload.push(...currentOutput);
+          // push metdata to final response only if it is not already present
+          rankedResponse.metadata.forEach((meta) => {
+            // get all the exisitng jobIds in the metadata array at the position from the finalResponse
+            const jobIdsInResponse = finalResponse[position].metadata.map((fRmeta) => fRmeta.jobId);
+            // check if the jobId is already present in the metadata array
+            if (!jobIdsInResponse.includes(meta.jobId)) {
+              finalResponse[position].metadata.push(meta);
+            }
+          });
+          finalResponse[position].destination = destination;
+          isCurrentResponseAddedToFinalPayload = true;
+          // break the loop as we have already appended the entire rankedResponse to the finalResponse
+          break;
+        }
+      }
+      // if the current rankedResponse is not added to the finalResponse, then we need to add it as a new entry
+      if (!isCurrentResponseAddedToFinalPayload) {
+        finalResponse.push({
+          payload: rankedResponse.payload.map((payload) => payload as TransformedOutput),
+          // only stored deduplicated metadata in the final response
+          metadata: PluginAdapter.deduplicateMetadata(rankedResponse.metadata),
+          destination,
+        });
+        // update the jobIdPositionMap for all the jobIds in the rankedResponse
+        rankedResponse.metadata.forEach((meta) => {
+          jobIdPositionMap.set(meta.jobId, finalResponse.length - 1);
+        });
+      }
+    }
+    return finalResponse;
+  }
+
+  static handleRtSuccess(
+    responseList: TransformedPayloadInfo[],
+    destination: Destination,
+  ): RtTransformedState[] {
+    // Rank responses based on unique jobIds in metadata array
+    const uniqueJobRank = PluginAdapter.rankResponsesByUniqueJobIds(responseList);
+
+    // Create final response with deduplicated metadata
+    const finalResponse = PluginAdapter.createFinalResponse(
+      uniqueJobRank,
+      responseList,
+      destination,
+    );
+
+    return finalResponse;
+  }
+
+  static async processRtEvents(
+    integrationPlugin: Integration,
+    events: RouterTransformationRequestData[],
+    destination: Destination,
+  ): Promise<{ transformedPayloadList: RtTransformedState[]; errorList: ErrorState[] }> {
+    const inputPayload = events.map((input) => ({
+      event: [{ message: input.message as RudderStackEvent } as RudderStackEventPayload],
+      metadata: [input.metadata],
+    }));
+
+    const output = await integrationPlugin.execute(inputPayload, destination);
+    const responseList = output.resultContext;
+    const errors = output.errorResults;
+
+    // Handle Errors
+    const errorList = PluginAdapter.handleErrors(errors, destination);
+
+    // Handle Success
+    const transformedPayloadList = PluginAdapter.handleRtSuccess(responseList, destination);
+
+    return { transformedPayloadList, errorList };
   }
 
   public static async transformAtRouter(
@@ -155,111 +316,20 @@ export class PluginAdapter {
     );
 
     const result = await Promise.all(
-      eventsPerDestinationId.map(async (inputs) => {
-        const input = inputs.map((input) => ({
-          event: [{ message: input.message as RudderStackEvent } as RudderStackEventPayload],
-          metadata: [input.metadata],
-        }));
-
-        const { destination } = inputs[0];
-
-        // calling the plugin and we can expect batched and multiplexed responses
-        // example: [ { payload: [event1, event2, event3], metadata: [metadata1, metadata2, metdata3] }, { payload: [event3, event4], metadata: [metadata3, metadata4] } ]
-
-        const output = await integrationPlugin.execute(input, destination);
-        const responseList = output.resultContext;
-        const errors = output.errorResults;
-
-        // handle error scenario
-        const errorList: { metadata: Metadata; response: any; destination: Destination }[] = [];
-        if (errors.length > 0) {
-          const nestedErrorList = errors.map((e) => {
-            const errResponses = e.metadata.map((metadata) => ({
-              metadata,
-              response: generateErrorObject(e.error), // add further tags here
-              destination,
-            }));
-            return errResponses;
-          });
-          errorList.push(...nestedErrorList.flat());
-        }
-
-        // handle success scenraio
-        // ranking the responses based on the number of unique jobIds in the metadata array
-        const uniqueJobRank: { uniqueJobIds: number; index: number }[] = [];
-        for (const [index, response] of responseList.entries()) {
-          const uniqueJobIds = Array.from(new Set(response.metadata.map((meta) => meta.jobId)));
-          uniqueJobRank.push({
-            uniqueJobIds: uniqueJobIds.length,
-            index,
-          });
-        }
-        uniqueJobRank.sort((a, b) => b.uniqueJobIds - a.uniqueJobIds);
-        // ranking ends here with uniqueJobRank containing the index of the responseList in the order of the number of unique jobIds in the metadata array
-        // example: [ { uniqueJobIds: 3, index: 0 }, { uniqueJobIds: 2, index: 1 } ]
-
-        const finalResponse: {
-          payload: TransformedOutput[];
-          metadata: Metadata[];
-          destination: Destination;
-        }[] = [];
-        // creating a map of jobId to position in the metadata array
-        // example: { jobId1: 1, jobId2: 1, jobId3: 0, jobId4: 2}
-        // motivation: prevent metadata duplication in the final response at all levels
-        const jobIdPositionMap: Map<number, number> = new Map();
-        for (const rank of uniqueJobRank) {
-          // iteratively checking payloads with the highest number of unique jobIds to lowest
-          const rankedResponse = responseList[rank.index];
-          let isCurrentResponseAddedToFinalPayload = false;
-          // iterate each metadata in the rankedResponse to check if any jobId is already present in the finalResponse
-          for (const meta of rankedResponse.metadata) {
-            // check if the jobId already has a position in final response
-            if (jobIdPositionMap.has(meta.jobId)) {
-              // if yes, then we need append the entire rankedResponse including all the payloads and metadata at same position
-              const position = jobIdPositionMap.get(meta.jobId) as number;
-              const currentOutput = rankedResponse.payload.map(
-                (payload) => payload as TransformedOutput,
-              );
-              finalResponse[position].payload.push(...currentOutput);
-              // push metdata to final response only if it is not already present
-              rankedResponse.metadata.forEach((meta) => {
-                // get all the exisitng jobIds in the metadata array at the position from the finalResponse
-                const jobIdsInResponse = finalResponse[position].metadata.map(
-                  (fRmeta) => fRmeta.jobId,
-                );
-                // check if the jobId is already present in the metadata array
-                if (!jobIdsInResponse.includes(meta.jobId)) {
-                  finalResponse[position].metadata.push(meta);
-                }
-              });
-              finalResponse[position].destination = destination;
-              isCurrentResponseAddedToFinalPayload = true;
-              // break the loop as we have already appended the entire rankedResponse to the finalResponse
-              break;
-            }
-          }
-          // if the current rankedResponse is not added to the finalResponse, then we need to add it as a new entry
-          if (!isCurrentResponseAddedToFinalPayload) {
-            finalResponse.push({
-              payload: rankedResponse.payload.map((payload) => payload as TransformedOutput),
-              // only stored deduplicated metadata in the final response
-              metadata: PluginAdapter.deduplicateMetadata(rankedResponse.metadata),
-              destination,
-            });
-            // update the jobIdPositionMap for all the jobIds in the rankedResponse
-            rankedResponse.metadata.forEach((meta) => {
-              jobIdPositionMap.set(meta.jobId, finalResponse.length - 1);
-            });
-          }
-        }
-        return { transformedPayloadList: finalResponse, errorList };
+      eventsPerDestinationId.map(async (events) => {
+        const output = await PluginAdapter.processRtEvents(
+          integrationPlugin,
+          events,
+          events[0].destination,
+        );
+        return {
+          errorList: output.errorList,
+          transformedPayloadList: output.transformedPayloadList,
+        };
       }),
     );
-    const allSuccessList: {
-      payload: TransformedOutput[];
-      metadata: Metadata[];
-      destination: Destination;
-    }[] = result.flatMap((res) => res.transformedPayloadList);
+
+    const allSuccessList = result.flatMap((res) => res.transformedPayloadList);
     const allErrorList: { metadata: Metadata; response: any; destination: Destination }[] =
       result.flatMap((res) => res.errorList);
 

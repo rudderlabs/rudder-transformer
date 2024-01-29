9dd862540cc8e4e56b9bc638cc1da62e5f19c45f
Utsab Chowdhury <utsab.c97@gmail.com>
2024-01-29 17:48:02 +0530

    feat: update proxy tests for cm360 Added new structure for proxy test scnearios for cm360 also added zod validations as part of tests

---
+++ src/types/zodTypes.ts
@@ -0,0 +1,172 @@
+import { z } from 'zod';
+import { isDefinedAndNotNullAndNotEmpty } from '@rudderstack/integrations-lib';
+import { isHttpStatusSuccess } from '../v0/util';
+
+export const ProxyMetadataSchema = z.object({
+  jobId: z.number(),
+  attemptNum: z.number(),
+  userId: z.string(),
+  sourceId: z.string(),
+  destinationId: z.string(),
+  workspaceId: z.string(),
+  secret: z.record(z.unknown()),
+  destInfo: z.object({}).optional(),
+  omitempty: z.record(z.unknown()).optional(),
+  dontBatch: z.boolean(),
+});
+
+export const ProxyV0RequestSchema = z.object({
+  version: z.string(),
+  type: z.string(),
+  method: z.string(),
+  endpoint: z.string(),
+  userId: z.string(),
+  headers: z.record(z.unknown()).optional(),
+  params: z.record(z.unknown()).optional(),
+  body: z
+    .object({
+      JSON: z.record(z.unknown()).optional(),
+      JSON_ARRAY: z.record(z.unknown()).optional(),
+      XML: z.record(z.unknown()).optional(),
+      FORM: z.record(z.unknown()).optional(),
+    })
+    .optional(),
+  files: z.record(z.unknown()).optional(),
+  metadata: ProxyMetadataSchema,
+});
+
+export const ProxyV1RequestSchema = z.object({
+  version: z.string(),
+  type: z.string(),
+  method: z.string(),
+  endpoint: z.string(),
+  userId: z.string(),
+  headers: z.record(z.unknown()).optional(),
+  params: z.record(z.unknown()).optional(),
+  body: z
+    .object({
+      JSON: z.record(z.unknown()).optional(),
+      JSON_ARRAY: z.record(z.unknown()).optional(),
+      XML: z.record(z.unknown()).optional(),
+      FORM: z.record(z.unknown()).optional(),
+    })
+    .optional(),
+  files: z.record(z.unknown()).optional(),
+  metadata: z.array(ProxyMetadataSchema),
+  destinationConfig: z.record(z.unknown()),
+});
+
+export const DeliveryV0ResponseSchema = z
+  .object({
+    status: z.number(),
+    message: z.string(),
+    destinationResponse: z.unknown(),
+    statTags: z.record(z.unknown()).optional(),
+    authErrorCategory: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (!isHttpStatusSuccess(data.status)) {
+        return isDefinedAndNotNullAndNotEmpty(data.statTags);
+      }
+      return true;
+    },
+    {
+      // eslint-disable-next-line sonarjs/no-duplicate-string
+      message: "statTags can't be empty when status is not a 2XX",
+      path: ['statTags'], // Pointing out which field is invalid
+    },
+  );
+
+export const DeliveryV0ResponseSchemaForOauth = z
+  .object({
+    status: z.number(),
+    message: z.string(),
+    destinationResponse: z.unknown(),
+    statTags: z.record(z.unknown()).optional(),
+    authErrorCategory: z.string().optional(),
+  })
+  .refine(
+    (data) => {
+      if (!isHttpStatusSuccess(data.status)) {
+        return isDefinedAndNotNullAndNotEmpty(data.statTags);
+      }
+      return true;
+    },
+    {
+      message: "statTags can't be empty when status is not a 2XX",
+      path: ['statTags'], // Pointing out which field is invalid
+    },
+  )
+  .refine(
+    (data) => {
+      if (!isHttpStatusSuccess(data.status)) {
+        return isDefinedAndNotNullAndNotEmpty(data.authErrorCategory);
+      }
+      return true;
+    },
+    {
+      message: "authErrorCategory can't be empty when status is not a 2XX",
+      path: ['authErrorCategory'], // Pointing out which field is invalid
+    },
+  );
+
+const DeliveryJobStateSchema = z.object({
+  error: z.string(),
+  statusCode: z.number(),
+  metadata: ProxyMetadataSchema,
+});
+
+export const DeliveryV1ResponseSchema = z
+  .object({
+    status: z.number(),
+    message: z.string(),
+    statTags: z.record(z.unknown()).optional(),
+    authErrorCategory: z.string().optional(),
+    response: z.array(DeliveryJobStateSchema),
+  })
+  .refine(
+    (data) => {
+      if (!isHttpStatusSuccess(data.status)) {
+        return isDefinedAndNotNullAndNotEmpty(data.statTags);
+      }
+      return true;
+    },
+    {
+      message: "statTags can't be empty when status is not a 2XX",
+      path: ['statTags'], // Pointing out which field is invalid
+    },
+  );
+
+export const DeliveryV1ResponseSchemaForOauth = z
+  .object({
+    status: z.number(),
+    message: z.string(),
+    statTags: z.record(z.unknown()).optional(),
+    authErrorCategory: z.string().optional(),
+    response: z.array(DeliveryJobStateSchema),
+  })
+  .refine(
+    (data) => {
+      if (!isHttpStatusSuccess(data.status)) {
+        return isDefinedAndNotNullAndNotEmpty(data.statTags);
+      }
+      return true;
+    },
+    {
+      message: "statTags can't be empty when status is not a 2XX",
+      path: ['statTags'], // Pointing out which field is invalid
+    },
+  )
+  .refine(
+    (data) => {
+      if (!isHttpStatusSuccess(data.status)) {
+        return isDefinedAndNotNullAndNotEmpty(data.authErrorCategory);
+      }
+      return true;
+    },
+    {
+      message: "authErrorCategory can't be empty when status is not a 2XX",
+      path: ['authErrorCategory'], // Pointing out which field is invalid
+    },
+  );
---
+++ test/integrations/common/network.ts
@@ -0,0 +1,62 @@
+export const networkCallsData = [
+  {
+    description: 'Mock response depicting SERVICE NOT AVAILABLE error',
+    httpReq: {
+      method: 'post',
+      url: 'https://random_test_url/test_for_service_not_available',
+    },
+    httpRes: {
+      data: {
+        error: {
+          message: 'Service Unavailable',
+          description:
+            'The server is currently unable to handle the request due to temporary overloading or maintenance of the server. Please try again later.',
+        },
+      },
+      status: 503,
+    },
+  },
+  {
+    description: 'Mock response depicting INTERNAL SERVER ERROR error',
+    httpReq: {
+      method: 'post',
+      url: 'https://random_test_url/test_for_internal_server_error',
+    },
+    httpRes: {
+      data: 'Internal Server Error',
+      status: 500,
+    },
+  },
+  {
+    description: 'Mock response depicting GATEWAY TIME OUT error',
+    httpReq: {
+      method: 'post',
+      url: 'https://random_test_url/test_for_gateway_time_out',
+    },
+    httpRes: {
+      data: 'Gateway Timeout',
+      status: 504,
+    },
+  },
+  {
+    description: 'Mock response depicting null response',
+    httpReq: {
+      method: 'post',
+      url: 'https://random_test_url/test_for_null_response',
+    },
+    httpRes: {
+      data: null,
+      status: 500,
+    },
+  },
+  {
+    description: 'Mock response depicting null and no status',
+    httpReq: {
+      method: 'post',
+      url: 'https://random_test_url/test_for_null_and_no_status',
+    },
+    httpRes: {
+      data: null,
+    },
+  },
+];

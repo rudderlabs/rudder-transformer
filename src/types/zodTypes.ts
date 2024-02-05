d2e65f4d936ce9ccbe1308cec0548d1bbde7fea1
Utsab Chowdhury <utsabc@Utsabs-MacBook-Pro.local>
2024-02-05 12:04:03 +0530

    chore: clean up zod type

--- src/types/zodTypes.ts
+++ src/types/zodTypes.ts
@@ -56,6 +56,20 @@ export const ProxyV1RequestSchema = z.object({
   destinationConfig: z.record(z.unknown()),
 });
 
+const validateStatTags = (data: any) => {
+  if (!isHttpStatusSuccess(data.status)) {
+    return isDefinedAndNotNullAndNotEmpty(data.statTags);
+  }
+  return true;
+};
+
+const validateAuthErrorCategory = (data: any) => {
+  if (!isHttpStatusSuccess(data.status)) {
+    return isDefinedAndNotNullAndNotEmpty(data.authErrorCategory);
+  }
+  return true;
+};
+
 export const DeliveryV0ResponseSchema = z
   .object({
     status: z.number(),
@@ -64,19 +78,11 @@ export const DeliveryV0ResponseSchema = z
     statTags: z.record(z.unknown()).optional(),
     authErrorCategory: z.string().optional(),
   })
-  .refine(
-    (data) => {
-      if (!isHttpStatusSuccess(data.status)) {
-        return isDefinedAndNotNullAndNotEmpty(data.statTags);
-      }
-      return true;
-    },
-    {
-      // eslint-disable-next-line sonarjs/no-duplicate-string
-      message: "statTags can't be empty when status is not a 2XX",
-      path: ['statTags'], // Pointing out which field is invalid
-    },
-  );
+  .refine(validateStatTags, {
+    // eslint-disable-next-line sonarjs/no-duplicate-string
+    message: "statTags can't be empty when status is not a 2XX",
+    path: ['statTags'], // Pointing out which field is invalid
+  });
 
 export const DeliveryV0ResponseSchemaForOauth = z
   .object({
@@ -86,30 +92,14 @@ export const DeliveryV0ResponseSchemaForOauth = z
     statTags: z.record(z.unknown()).optional(),
     authErrorCategory: z.string().optional(),
   })
-  .refine(
-    (data) => {
-      if (!isHttpStatusSuccess(data.status)) {
-        return isDefinedAndNotNullAndNotEmpty(data.statTags);
-      }
-      return true;
-    },
-    {
-      message: "statTags can't be empty when status is not a 2XX",
-      path: ['statTags'], // Pointing out which field is invalid
-    },
-  )
-  .refine(
-    (data) => {
-      if (!isHttpStatusSuccess(data.status)) {
-        return isDefinedAndNotNullAndNotEmpty(data.authErrorCategory);
-      }
-      return true;
-    },
-    {
-      message: "authErrorCategory can't be empty when status is not a 2XX",
-      path: ['authErrorCategory'], // Pointing out which field is invalid
-    },
-  );
+  .refine(validateStatTags, {
+    message: "statTags can't be empty when status is not a 2XX",
+    path: ['statTags'], // Pointing out which field is invalid
+  })
+  .refine(validateAuthErrorCategory, {
+    message: "authErrorCategory can't be empty when status is not a 2XX",
+    path: ['authErrorCategory'], // Pointing out which field is invalid
+  });
 
 const DeliveryJobStateSchema = z.object({
   error: z.string(),
@@ -125,18 +115,10 @@ export const DeliveryV1ResponseSchema = z
     authErrorCategory: z.string().optional(),
     response: z.array(DeliveryJobStateSchema),
   })
-  .refine(
-    (data) => {
-      if (!isHttpStatusSuccess(data.status)) {
-        return isDefinedAndNotNullAndNotEmpty(data.statTags);
-      }
-      return true;
-    },
-    {
-      message: "statTags can't be empty when status is not a 2XX",
-      path: ['statTags'], // Pointing out which field is invalid
-    },
-  );
+  .refine(validateStatTags, {
+    message: "statTags can't be empty when status is not a 2XX",
+    path: ['statTags'], // Pointing out which field is invalid
+  });
 
 export const DeliveryV1ResponseSchemaForOauth = z
   .object({
@@ -146,27 +128,11 @@ export const DeliveryV1ResponseSchemaForOauth = z
     authErrorCategory: z.string().optional(),
     response: z.array(DeliveryJobStateSchema),
   })
-  .refine(
-    (data) => {
-      if (!isHttpStatusSuccess(data.status)) {
-        return isDefinedAndNotNullAndNotEmpty(data.statTags);
-      }
-      return true;
-    },
-    {
-      message: "statTags can't be empty when status is not a 2XX",
-      path: ['statTags'], // Pointing out which field is invalid
-    },
-  )
-  .refine(
-    (data) => {
-      if (!isHttpStatusSuccess(data.status)) {
-        return isDefinedAndNotNullAndNotEmpty(data.authErrorCategory);
-      }
-      return true;
-    },
-    {
-      message: "authErrorCategory can't be empty when status is not a 2XX",
-      path: ['authErrorCategory'], // Pointing out which field is invalid
-    },
-  );
+  .refine(validateStatTags, {
+    message: "statTags can't be empty when status is not a 2XX",
+    path: ['statTags'], // Pointing out which field is invalid
+  })
+  .refine(validateAuthErrorCategory, {
+    message: "authErrorCategory can't be empty when status is not a 2XX",
+    path: ['authErrorCategory'], // Pointing out which field is invalid
+  });

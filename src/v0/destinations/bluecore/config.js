87ee0c34f8f3cf908ca5a8926f47f188714ff91e
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2024-02-05 17:59:43 +0530

    feat: initial commit

--- src/v0/destinations/bluecore/config.js
+++ src/v0/destinations/bluecore/config.js
@@ -1,4 +1,5 @@
 const { getMappingConfig } = require('../../util');
+
 const BASE_URL = 'https://api.bluecore.com/api/track/mobile/v1';
 
 const CONFIG_CATEGORIES = {
@@ -12,31 +13,41 @@ const CONFIG_CATEGORIES = {
   },
 };
 
-const EVENT_NAME_MAPPING = {
-  'Product Viewed': 'viewed_product',
-  'Product Added': 'add_to_cart',
-  'Order Completed': 'purchase',
-  'Products Searched': 'search',
-  'Product Added to Wishlist': 'wishlist',
-  'Checkout Step Viewed': 'checkout',
-  'Product Removed': 'remove_from_cart',
-  'Subscribe Interest': 'subscribe_interest',
-  'Unsubscribe Interest': 'unsubscribe_interest',
-  Identify: 'identify',
-};
+// const EVENT_NAME_MAPPING = {
+//   'Product Viewed': 'viewed_product',
+//   'Products Searched': 'search',
+//   'Product Added': 'add_to_cart',
+//   'Product Removed': 'remove_from_cart',
+//   'Product Added to Wishlist': 'wishlist',
+//   'Order Completed': 'purchase',
+//   'Identify': 'identify',
+// };
 
-const BLUECORE_IDENTIFY_EXCLUSION = [
-  'event',
-  'phone',
-  'phoneNumber',
-  'phone_number',
-  'firstName',
-  'firstname',
-  'first_name',
-  'lastName',
-  'lastname',
-  'last_name',
-  'gender',
+const EVENT_NAME_MAPPING = [
+  {
+    src: ['Product Viewed'],
+    dest: 'viewed_product',
+  },
+  {
+    src: ['Products Searched'],
+    dest: 'search',
+  },
+  {
+    src: ['Product Added'],
+    dest: 'add_to_cart',
+  },
+  {
+    src: ['Product Removed'],
+    dest: 'remove_from_cart',
+  },
+  {
+    src: ['Product Added to Wishlist'],
+    dest: 'wishlist',
+  },
+  {
+    src: ['Order Completed'],
+    dest: 'purchase',
+  },
 ];
 
 const MAPPING_CONFIG = getMappingConfig(CONFIG_CATEGORIES, __dirname);
@@ -44,6 +55,5 @@ module.exports = {
   CONFIG_CATEGORIES,
   MAPPING_CONFIG,
   EVENT_NAME_MAPPING,
-  BLUECORE_IDENTIFY_EXCLUSION,
   BASE_URL,
 };
--- src/v0/destinations/bluecore/transform.js
+++ src/v0/destinations/bluecore/transform.js
@@ -1,178 +1,143 @@
+const { getHashFromArrayWithDuplicate, isDefinedAndNotNull, ConfigurationError,  TransformationError,
+  InstrumentationError, } = require('@rudderstack/integrations-lib');
 const { EventType } = require('../../../constants');
 const {
-  constructPayload,
+ constructPayload,
   ErrorMessage,
-  defaultRequestConfig,
   defaultPostRequestConfig,
   getValueFromMessage,
-  isDefinedAndNotNull,
+  // isDefinedAndNotNull,
   simpleProcessRouterDest,
+  validateEventName,
 } = require('../../util');
-
-
-const { JSON_MIME_TYPE } = require('../../util/constant');
-const {
-  TransformationError,
-  InstrumentationError,
-  ConfigurationError,
-} = require('../../util/errorTypes');
 
 const {
   MAPPING_CONFIG,
   CONFIG_CATEGORIES,
   BASE_URL,
   EVENT_NAME_MAPPING,
-  BLUECORE_IDENTIFY_EXCLUSION,
 } = require('./config');
 
-function checkValidEventName(str) {
-  return str.includes('.') || /\d/.test(str) || str.length > 64;
+const verifyTrackPayload = (payload) => {
+switch (payload.event) {
+  case 'search':
+    if (!payload.search_term) {
+      throw new InstrumentationError('[Bluecore] property:: search_query is required for search event');
+    }
+    break;
+  case 'purchase':
+    if (!payload.order_id) {
+      throw new InstrumentationError('[Bluecore] property:: order_id is required for purchase event');
+    }
+    if (!payload.total) {
+      throw new InstrumentationError('[Bluecore] property:: total is required for purchase event');
+    }
+    break;
+    default:
+      break;
+}
+};
+
+const deduceTrackEventName = (trackEventName, Config) => {
+  let eventName;
+  const { eventsMapping } = Config;
+  validateEventName(trackEventName);
+  /*
+  Step 1: If the event is not amongst the above list of ecommerce events, will look for
+          the event mapping in the UI. In case it is similar, will map to that.
+   */
+  if (eventsMapping.length > 0) {
+      const keyMap = getHashFromArrayWithDuplicate(eventsMapping, 'from', 'to', false);
+      eventName = keyMap[trackEventName];
+  }
+  /*
+  Step 2: To find if the particular event is amongst the list of standard
+          Rudderstack ecommerce events, used specifically for Pinterest Conversion API
+          mappings.
+  */
+  if (!eventName) {
+      const eventMapInfo = EVENT_NAME_MAPPING.find((eventMap) => {
+          if (eventMap.src.includes(trackEventName.toLowerCase())) {
+              return eventMap;
+          }
+          return false;
+      });
+      if (!isDefinedAndNotNull(eventMapInfo)) {
+          throw new ConfigurationError(`[Bluecore] Event name ${trackEventName} is not mapped`);
+      } else {
+        return [eventMapInfo.dest];
+      }
+  }
+  // Step 3 : check if one single event is mapped then
+  // if(typeof eventName === 'string') {
+  //     return [eventName];
+  // }
+  return eventName;
+};
+const trackResponseBuilder = async (message, category, { Config }, eventName) => {
+  const event = getValueFromMessage(message, 'event');
+  
+  if (!event) {
+    throw new InstrumentationError('[Bluecore] property:: event is required for track call');
+  }
+
+  const payload = constructPayload(message, MAPPING_CONFIG[category.name]);
+  payload.event = eventName;
+  verifyTrackPayload(payload);
+  payload.token = Config.bluecoreNamespace;
+  if (!payload) {
+    // fail-safety for developer error
+    throw new TransformationError(ErrorMessage.FailedToConstructPayload);
+  }
+  return payload;
+};
+
+// const identifyResponseBuilder = async (message, category, { Config }) => {
+
+// }
+
+const responseBuilderSimple = (response, destination) => {
+  const resp = defaultPostRequestConfig();
+  resp.endpoint = BASE_URL;
+  resp.body.JSON = response;
+  resp.headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${destination.Config.bluecoreNamespace}`,
+  };
+  return resp;
 }
 
-
-const trackResponseBuilder = async (message, category, { Config }) => {
-  let event = getValueFromMessage(message, 'event');
-  if (!event) {
-    throw new InstrumentationError('[BLUECORE] property:: event is required for track call');
-  }
-  if (!Config.token) {
-    throw new ConfigurationError('[BLUECORE] event Api Keys required for Authentication.');
-  }
-  let payload = constructPayload(message, MAPPING_CONFIG[category.name]);
-
-  if (!payload) {
-    // fail-safety for developer error
-    throw new TransformationError(ErrorMessage.FailedToConstructPayload);
-  }
-  event = event.trim();
-
-  if (isDefinedAndNotNull(EVENT_NAME_MAPPING[event])) {
-    payload.event = EVENT_NAME_MAPPING[event];
-  }
-  payload.event = payload.event.replace(/\s+/g, '_');
-  if (checkValidEventName(payload.event)) {
-    throw new InstrumentationError(
-      "[BLUECORE] Event shouldn't contain period(.), numeric value and contains not more than 64 characters",
-      );
-    }
-    
-    // Check the event type and create the product list accordingly
-    if (payload.event.toLowerCase() === 'viewed_product' || payload.event.toLowerCase() === 'add_to_cart'|| payload.event.toLowerCase() === 'remove_from_cart' || payload.event.toLowerCase() === 'purchase'|| payload.event.toLowerCase() === 'wishlist') {
-      let productList = [];
-      if (Array.isArray(message.properties.products)) {
-        // Multiple products
-        productList = message.properties.products.map(product => ({
-          id: product.product_id,
-          name: product.name,
-          price: product.price,
-          sku: product.sku,
-          category: product.category,
-          brand: product.brand,
-          variant: product.variant,
-          quantity: product.quantity,
-          coupon: product.coupon,
-          currency: product.currency,
-          position: product.position,
-          url: product.url,
-          image_url: product.image_url,
-        }));
-      } else if (message.properties) {
-        // Single product
-        productList.push({
-          id: message.properties.product_id,
-          name: message.properties.name,
-          price: message.properties.price,
-          sku: message.properties.sku,
-          category: message.properties.category,
-          brand: message.properties.brand,
-          variant: message.properties.variant,
-          quantity: message.properties.quantity,
-          coupon: message.properties.coupon,
-          currency: message.properties.currency,
-          position: message.properties.position,
-          url: message.properties.url,
-          image_url: message.properties.image_url,
-        });
-      }
-      // Adding the productPayloadList to the payload's properties under the key 'products'
-      if (productList.length > 0) {
-        payload.properties = {
-          ...payload.properties,
-          products: productList,
-        };
-      }
-    }
-    
-  const response = defaultRequestConfig();
-  response.endpoint = `${BASE_URL}`;
-
-  response.method = defaultPostRequestConfig.requestMethod;
-  const basicAuth = Buffer.from(Config.token).toString('base64');
-  response.headers = {
-    Authorization: `Basic ${basicAuth}`,
-    'Content-Type': JSON_MIME_TYPE,
-  };
-  payload.properties.token = Config.token;
-  response.body.JSON = payload;
-  return response;
-};
-
-const identifyResponseBuilder = async (message, category, { Config }) => {
-  if (!Config.token) {
-    throw new ConfigurationError('[BLUECORE] User API Key required for Authentication.');
-  }
-
-  let payload = constructPayload(message, MAPPING_CONFIG[category.name]);
-  payload.event = 'identify';
-  payload.properties.token = Config.token;
-
-  if (!payload) {
-    // fail-safety for developer error
-    throw new TransformationError(ErrorMessage.FailedToConstructPayload);
-  }
-  
-    
-  const response = defaultRequestConfig();
-  response.endpoint = `${BASE_URL}`;
-  response.method = defaultPostRequestConfig.requestMethod;
-  
-  const basicAuth = Buffer.from(Config.token).toString('base64');
-  response.headers = {
-    Authorization: `Basic ${basicAuth}`,
-    'Content-Type': JSON_MIME_TYPE,
-  };
-  response.body.JSON = payload;
-  return response;
-};
-
-
 const process = async (event) => {
+  const deducedEventNameArray = [];
+  const toSendEvents = [];
+  const respList = [];
   const { message, destination } = event;
-  console.log('Incoming Event:', event);
-  console.log('Incoming Destination:', destination);
-
   if (!message.type) {
     throw new InstrumentationError('Message Type is not present. Aborting message.');
   }
 
+  if (!destination.Config.bluecoreNamespace) {
+    throw new ConfigurationError('[BLUECORE] bluecore account namespace required for Authentication.');
+  }
   const messageType = message.type.toLowerCase();
   const category = CONFIG_CATEGORIES[message.type.toUpperCase()];
-  console.log('Category :', category);
-  let response;
   switch (messageType) {
     case EventType.TRACK:
-      response = await trackResponseBuilder(message, category, destination);
+      deducedEventNameArray.push(...deduceTrackEventName(message.event, destination.Config));
+      deducedEventNameArray.forEach((eventName) => {
+        toSendEvents.push(trackResponseBuilder(message, category, destination, eventName));
+      });
       break;
     case EventType.IDENTIFY:
-      response = await identifyResponseBuilder(message, category, destination);
+      // response = await identifyResponseBuilder(message, category, destination);
       break;
     default:
       throw new InstrumentationError(`Message type ${messageType} not supported`);
   }
-  // Log the final response before returning it
-  console.log('Final Response:', response);
-
-  return response;
+  toSendEvents.forEach((sendEvent) => {
+    respList.push(responseBuilderSimple(sendEvent, destination));
+  });
+  return respList;
 };
 
 const processRouterDest = async (inputs, reqMetadata) => {

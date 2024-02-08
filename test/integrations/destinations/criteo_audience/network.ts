eef85905bd46dc4c2b06993145ae43f5d710e311
ItsSudip <sudip.paul1997@gmail.com>
2024-02-08 13:44:20 +0530

    chore: update delivery test cases for criteo audience

--- test/integrations/destinations/criteo_audience/network.ts
+++ test/integrations/destinations/criteo_audience/network.ts
@@ -1,3 +1,23 @@
+const headers = {
+  Authorization: 'Bearer success_access_token',
+  'Content-Type': 'application/json',
+  Accept: 'application/json',
+  'User-Agent': 'RudderLabs',
+};
+const params = { destination: 'criteo_audience' };
+const method = 'PATCH';
+const commonData = {
+  data: {
+    type: 'ContactlistAmendment',
+    attributes: {
+      operation: 'add',
+      identifierType: 'madid',
+      identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
+      internalIdentifiers: false,
+    },
+  },
+};
+
 export const networkCallsData = [
   {
     httpReq: {
@@ -14,39 +34,74 @@ export const networkCallsData = [
           },
         },
       },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
+      params,
+      headers,
+      method,
+    },
+    httpRes: { status: 200 },
+  },
+  {
+    httpReq: {
+      url: 'https://api.criteo.com/2022-10/audiences/34894/contactlist',
+      data: {
+        data: {
+          type: 'ContactlistAmendment',
+          attributes: {
+            operation: 'add',
+            identifierType: 'email',
+            internalIdentifiers: false,
+            identifiers: [
+              'alex@email.com',
+              'amy@email.com',
+              'van@email.com',
+              'alex@email.com',
+              'amy@email.com',
+              'van@email.com',
+            ],
+          },
+        },
       },
-      method: 'PATCH',
+      params,
+      headers,
+      method,
+    },
+    httpRes: { status: 200 },
+  },
+  {
+    httpReq: {
+      url: 'https://api.criteo.com/2022-10/audiences/34893/contactlist',
+      data: {
+        data: {
+          type: 'ContactlistAmendment',
+          attributes: {
+            operation: 'remove',
+            identifierType: 'madid',
+            internalIdentifiers: false,
+            identifiers: [
+              'sample_madid',
+              'sample_madid_1',
+              'sample_madid_2',
+              'sample_madid_10',
+              'sample_madid_13',
+              'sample_madid_11',
+              'sample_madid_12',
+            ],
+          },
+        },
+      },
+      params,
+      headers,
+      method,
     },
     httpRes: { status: 200 },
   },
   {
     httpReq: {
       url: 'https://api.criteo.com/2022-10/audiences/3485/contactlist',
-      data: {
-        data: {
-          type: 'ContactlistAmendment',
-          attributes: {
-            operation: 'add',
-            identifierType: 'madid',
-            identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
-            internalIdentifiers: false,
-          },
-        },
-      },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
-      },
-      method: 'PATCH',
+      data: commonData,
+      params,
+      headers,
+      method,
     },
     httpRes: {
       code: '400',
@@ -67,25 +122,10 @@ export const networkCallsData = [
   {
     httpReq: {
       url: 'https://api.criteo.com/2022-10/audiences/34895/contactlist',
-      data: {
-        data: {
-          type: 'ContactlistAmendment',
-          attributes: {
-            operation: 'add',
-            identifierType: 'madid',
-            identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
-            internalIdentifiers: false,
-          },
-        },
-      },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
-      },
-      method: 'PATCH',
+      data: commonData,
+      params,
+      headers,
+      method,
     },
     httpRes: {
       code: '400',
@@ -106,25 +146,10 @@ export const networkCallsData = [
   {
     httpReq: {
       url: 'https://api.criteo.com/2022-10/audiences/34896/contactlist',
-      data: {
-        data: {
-          type: 'ContactlistAmendment',
-          attributes: {
-            operation: 'add',
-            identifierType: 'madid',
-            identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
-            internalIdentifiers: false,
-          },
-        },
-      },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
-      },
-      method: 'PATCH',
+      data: commonData,
+      params,
+      headers,
+      method,
     },
     httpRes: {
       code: '400',
@@ -143,25 +168,10 @@ export const networkCallsData = [
   {
     httpReq: {
       url: 'https://api.criteo.com/2022-10/audiences/34897/contactlist',
-      data: {
-        data: {
-          type: 'ContactlistAmendment',
-          attributes: {
-            operation: 'add',
-            identifierType: 'madid',
-            identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
-            internalIdentifiers: false,
-          },
-        },
-      },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
-      },
-      method: 'PATCH',
+      data: commonData,
+      params,
+      headers,
+      method,
     },
     httpRes: {
       code: '500',
@@ -180,50 +190,20 @@ export const networkCallsData = [
   {
     httpReq: {
       url: 'https://api.criteo.com/2022-10/audiences/34898/contactlist',
-      data: {
-        data: {
-          type: 'ContactlistAmendment',
-          attributes: {
-            operation: 'add',
-            identifierType: 'madid',
-            identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
-            internalIdentifiers: false,
-          },
-        },
-      },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
-      },
-      method: 'PATCH',
+      data: commonData,
+      params,
+      headers,
+      method,
     },
     httpRes: { code: '429', data: {}, status: 429 },
   },
   {
     httpReq: {
       url: 'https://api.criteo.com/2022-10/audiences/34899/contactlist',
-      data: {
-        data: {
-          type: 'ContactlistAmendment',
-          attributes: {
-            operation: 'add',
-            identifierType: 'madid',
-            identifiers: ['sample_madid', 'sample_madid_1', 'sample_madid_2'],
-            internalIdentifiers: false,
-          },
-        },
-      },
-      params: { destination: 'criteo_audience' },
-      headers: {
-        Authorization: 'Bearer success_access_token',
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-        'User-Agent': 'RudderLabs',
-      },
-      method: 'PATCH',
+      data: commonData,
+      params,
+      headers,
+      method,
     },
     httpRes: { code: '400', data: { message: 'unknown error' }, status: 410 },
   },

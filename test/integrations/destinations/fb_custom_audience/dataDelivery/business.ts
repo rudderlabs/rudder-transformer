c537906912dab5349e02420dfe966ce2d095cfdd
sandeepdigumarty <sandeepdsvs@gmail.com>
2024-02-15 11:15:13 +0530

    chore: fb_custom_audience v1 tests

--- test/integrations/destinations/fb_custom_audience/dataDelivery/business.ts
+++ test/integrations/destinations/fb_custom_audience/dataDelivery/business.ts
@@ -169,4 +169,74 @@ export const testScenariosForV1API = [
       },
     },
   },
+  {
+    id: 'fbca_v1_scenario_3',
+    name: 'fb_custom_audience',
+    description: 'user addition failed due to unavailable audience error',
+    successCriteria: 'Fail with status code 400',
+    scenario: 'Business',
+    feature: 'dataDelivery',
+    module: 'destination',
+    version: 'v1',
+    input: {
+      request: {
+        body: generateProxyV1Payload({
+          method: 'DELETE',
+          endpoint: getEndPoint('aud1'),
+          headers: {
+            'test-dest-response-key': 'audienceUnavailableError',
+          },
+          params: {
+            access_token: 'ABC',
+            payload: {
+              is_raw: true,
+              data_source: {
+                sub_type: 'ANYTHING',
+              },
+              schema: ['DOBY', 'PHONE', 'GEN', 'FI', 'MADID', 'ZIP', 'ST', 'COUNTRY'],
+              data: [['2013', '@09432457768', 'f', 'Ms.', 'ABC', 'ZIP ', '123abc ', 'IN']],
+            },
+          },
+        }),
+        method: 'DELETE',
+      },
+    },
+    output: {
+      response: {
+        status: 400,
+        body: {
+          output: {
+            status: 400,
+            message:
+              'Custom Audience Unavailable: The custom audience you are trying to use has not been shared with your ad account',
+            statTags: {
+              destType: 'FB_CUSTOM_AUDIENCE',
+              destinationId: 'Non-determininable',
+              errorCategory: 'network',
+              errorType: 'aborted',
+              feature: 'dataDelivery',
+              implementation: 'native',
+              module: 'destination',
+              workspaceId: 'Non-determininable',
+            },
+            response: [
+              {
+                error: {
+                  error: {
+                    code: 1487301,
+                    message:
+                      'Custom Audience Unavailable: The custom audience you are trying to use has not been shared with your ad account',
+                    type: 'GraphMethodException',
+                  },
+                  status: 400,
+                },
+                statusCode: 400,
+                metadata: generateMetadata(1),
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
 ];

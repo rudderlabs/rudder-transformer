9683161612c7e3b9c2be95a2728f68ec7dcf69f4
Sankeerth <sankeerth@rudderstack.com>
2024-02-16 18:20:12 +0530

    fix: custify user-regulation logic (#3076)

--- test/integrations/destinations/custify/network.ts
+++ test/integrations/destinations/custify/network.ts
@@ -1,36 +1,85 @@
 export const networkCallsData = [
-    {
-        httpReq: {
-            url: 'https://api.custify.com/company',
-            method: 'POST',
+  {
+    httpReq: {
+      url: 'https://api.custify.com/company',
+      method: 'POST',
+    },
+    httpRes: {
+      data: {
+        company_id: '6',
+        name: 'Pizzeria Presto',
+        signed_up_at: '2019-05-30T12:00:00.000Z',
+        size: 15,
+        website: 'www.pizzeriapresto.com',
+        industry: 'Restaurant',
+        plan: 'Platinum',
+        monthly_revenue: 1234567,
+        churned: false,
+        owners_csm: 'john.doe@mail.com',
+        owners_account: 'john.doe@mail.com',
+        parent_companies: [
+          {
+            id: '5ec50c9829d3c17c7cf455f2',
+          },
+          {
+            id: '5ec50c9829d3c17c7cf457f2',
+          },
+        ],
+        custom_attributes: {
+          restaurants: 5,
+          custom: 'template',
         },
-        httpRes: {
-            data: {
-                "company_id": "6",
-                "name": "Pizzeria Presto",
-                "signed_up_at": "2019-05-30T12:00:00.000Z",
-                "size": 15,
-                "website": "www.pizzeriapresto.com",
-                "industry": "Restaurant",
-                "plan": "Platinum",
-                "monthly_revenue": 1234567,
-                "churned": false,
-                "owners_csm": "john.doe@mail.com",
-                "owners_account": "john.doe@mail.com",
-                "parent_companies": [
-                    {
-                        "id": "5ec50c9829d3c17c7cf455f2"
-                    },
-                    {
-                        "id": "5ec50c9829d3c17c7cf457f2"
-                    }
-                ],
-                "custom_attributes": {
-                    "restaurants": 5,
-                    "custom": "template"
-                }
-            },
-            status: 200
-        },
-    }
+      },
+      status: 200,
+    },
+  },
+
+  {
+    httpReq: {
+      method: 'delete',
+      url: 'https://api.custify.com/people?user_id=rudder1',
+      headers: {
+        Authorization: 'Bearer dummyApiKey',
+      },
+    },
+    httpRes: {
+      data: {
+        msg: 'All users associated with rudder1 were successfully deleted',
+        code: 'Success',
+        params: null,
+      },
+      status: 200,
+    },
+  },
+  {
+    httpReq: {
+      method: 'delete',
+      url: 'https://api.custify.com/people?user_id=rudder2',
+      headers: {
+        Authorization: 'Bearer dummyApiKey',
+      },
+    },
+    httpRes: {
+      data: {
+        error: 'User: rudder2 not found',
+      },
+      status: 404,
+    },
+  },
+
+  {
+    httpReq: {
+      method: 'delete',
+      url: 'https://api.custify.com/people?user_id=rudder3',
+      headers: {
+        Authorization: 'Bearer dummyApiKey',
+      },
+    },
+    httpRes: {
+      data: {
+        error: 'User: rudder3 has a problem',
+      },
+      status: 400,
+    },
+  },
 ];

795a31efd93434b8c6e35ff0b9a1fee3831c6d80
AASHISH MALIK <imashishmalik.45@gmail.com>
2024-02-25 22:30:57 +0530

    chore: update component tests structure for snapchat custom audience

---
+++ test/integrations/destinations/snapchat_custom_audience/dataDelivery/business.ts
@@ -0,0 +1,125 @@
+import { ProxyV1TestData } from '../../../testTypes';
+import {
+  generateProxyV0Payload,
+  generateProxyV1Payload,
+} from '../../../testUtils';
+
+const commonHeaders = {
+  Authorization: 'Bearer abcd123',
+  'Content-Type': 'application/json',
+  'User-Agent': 'RudderLabs',
+};
+
+const commonRequestParameters = {
+  headers: commonHeaders,
+  JSON: {
+    users: [
+      {
+        schema: ['EMAIL_SHA256'],
+        data: [['938758751f5af66652a118e26503af824404bc13acd1cb7642ddff99916f0e1c']],
+      },
+    ],
+  },
+};
+
+export const businessTestScenarios = [
+  {
+    id: 'snapchat_custom_audience_v0_oauth_scenario_1',
+    name: 'snapchat_custom_audience',
+    description: '[Proxy v0 API] :: successfull call',
+    successCriteria: 'Proper response from destination is received',
+    scenario: 'Oauth',
+    feature: 'dataDelivery',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: generateProxyV0Payload({
+          ...commonRequestParameters,
+          endpoint: 'https://adsapi.snapchat.com/v1/segments/123/users',
+          params: {
+            destination: 'snapchat_custom_audience',
+          },
+        }),
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: {
+          output: {
+            status: 200,
+            message: 'Request Processed Successfully',
+            destinationResponse: {
+              response: {
+                request_status: 'SUCCESS',
+                request_id: '12345',
+                users: [
+                  {
+                    sub_request_status: 'SUCCESS',
+                    user: {
+                      number_uploaded_users: 1,
+                    },
+                  },
+                ],
+              },
+              status: 200,
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    id: 'snapchat_custom_audience_v0_oauth_scenario_1',
+    name: 'snapchat_custom_audience',
+    description: '[Proxy v1 API] :: successfull oauth',
+    successCriteria: 'Proper response from destination is received',
+    scenario: 'Oauth',
+    feature: 'dataDelivery',
+    module: 'destination',
+    version: 'v1',
+    input: {
+      request: {
+        body: generateProxyV1Payload({
+          ...commonRequestParameters,
+          endpoint: 'https://adsapi.snapchat.com/v1/segments/123/users',
+          params: {
+            destination: 'snapchat_custom_audience',
+          },
+        }),
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: {
+          output: {
+            status: 200,
+            message: 'Request Processed Successfully',
+            response: [
+              {
+                error: `{\"request_status\":\"SUCCESS\",\"request_id\":\"12345\",\"users\":[{\"sub_request_status\":\"SUCCESS\",\"user\":{\"number_uploaded_users\":1}}]}`,
+                statusCode: 200,
+                metadata: {
+                  jobId: 1,
+                  attemptNum: 1,
+                  userId: 'default-userId',
+                  destinationId: 'default-destinationId',
+                  workspaceId: 'default-workspaceId',
+                  sourceId: 'default-sourceId',
+                  secret: {
+                    accessToken: 'default-accessToken',
+                  },
+                  dontBatch: false,
+                },
+              },
+            ],
+          },
+        },
+      },
+    },
+  },
+];

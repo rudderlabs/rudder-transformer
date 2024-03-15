f51e6b9d2b7324db14ff221ed70efa6484c1f496
Mihir Bhalala <77438541+mihir-4116@users.noreply.github.com>
2024-03-15 13:27:07 +0530

    chore: gaec proxy test refactor (#3177)
    * chore: gaec proxy test refactor
    
    * chore: code review changes
    
    * chore: code review changes
    
    * chore: code review changes

--- src/v0/destinations/google_adwords_enhanced_conversions/networkHandler.js
+++ src/v0/destinations/google_adwords_enhanced_conversions/networkHandler.js
@@ -122,7 +122,7 @@ const responseHandler = (responseParams) => {
     // Ref - https://github.com/googleapis/googleapis/blob/master/google/rpc/code.proto
     if (partialFailureError && partialFailureError.code !== 0) {
       throw new NetworkError(
-        `[Google Ads Offline Conversions]:: partialFailureError - ${JSON.stringify(
+        `[Google Adwords Enhanced Conversions]:: partialFailureError - ${JSON.stringify(
           partialFailureError,
         )}`,
         400,

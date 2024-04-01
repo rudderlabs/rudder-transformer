667095fa8316cd95a066f15b848ad503c6b4af80
Gauravudia <60897972+Gauravudia@users.noreply.github.com>
2024-04-01 14:48:50 +0530

    feat: update movable ink batch size (#3223)
    * feat: update movable ink batch size
    
    * test: add testcase for batching on max request size

--- test/integrations/destinations/movable_ink/router/data.ts
+++ test/integrations/destinations/movable_ink/router/data.ts
@@ -1,6 +1,7 @@
 import { RouterTestData } from '../../../testTypes';
 import { RouterTransformationRequest } from '../../../../../src/types';
 import { generateMetadata } from '../../../testUtils';
+import { defaultMockFns } from '../mocks';
 import {
   destType,
   channel,
@@ -43,6 +44,7 @@ const routerRequest: RouterTransformationRequest = {
         channel,
         anonymousId: 'anonId123',
         userId: 'userId123',
+        event: 'Product Added',
         properties: trackTestProperties['Product Added'],
         integrations: {
           All: true,
@@ -58,19 +60,73 @@ const routerRequest: RouterTransformationRequest = {
         channel,
         anonymousId: 'anonId123',
         userId: 'userId123',
+        event: 'Custom Event',
+        integrations: {
+          All: true,
+        },
+        originalTimestamp: '2024-03-04T15:32:56.409Z',
+      },
+      metadata: generateMetadata(4),
+      destination,
+    },
+    {
+      message: {
+        type: 'track',
+        channel,
+        anonymousId: 'anonId123',
+        userId: 'userId123',
+        event: 'Custom Event',
         properties: trackTestProperties['Custom Event'],
         integrations: {
           All: true,
         },
       },
-      metadata: generateMetadata(4),
+      metadata: generateMetadata(5),
       destination,
     },
   ],
   destType,
 };
 
-export const data: RouterTestData[] = [
+// >5KB payload
+const routerRequest2: RouterTransformationRequest = {
+  input: [
+    {
+      message: {
+        type: 'track',
+        channel,
+        anonymousId: 'anonId123',
+        userId: 'userId123',
+        event: 'Order Completed',
+        properties: trackTestProperties['Order Completed'],
+        integrations: {
+          All: true,
+        },
+        originalTimestamp: '2024-03-04T15:32:56.409Z',
+      },
+      metadata: generateMetadata(1),
+      destination,
+    },
+    {
+      message: {
+        type: 'track',
+        channel,
+        anonymousId: 'anonId123',
+        userId: 'userId123',
+        event: 'Custom Event',
+        integrations: {
+          All: true,
+        },
+        originalTimestamp: '2024-03-04T15:32:56.409Z',
+      },
+      metadata: generateMetadata(2),
+      destination,
+    },
+  ],
+  destType,
+};
+
+export const data = [
   {
     id: 'MovableInk-router-test-1',
     name: destType,
@@ -118,6 +174,7 @@ export const data: RouterTestData[] = [
                         channel,
                         userId: 'userId123',
                         anonymousId: 'anonId123',
+                        event: 'Product Added',
                         properties: trackTestProperties['Product Added'],
                         integrations: {
                           All: true,
@@ -139,6 +196,42 @@ export const data: RouterTestData[] = [
               destination,
             },
             {
+              batchedRequest: {
+                version: '1',
+                type: 'REST',
+                method: 'POST',
+                endpoint: destination.Config.endpoint,
+                headers,
+                params: {},
+                body: {
+                  JSON: {
+                    events: [
+                      {
+                        type: 'track',
+                        channel,
+                        userId: 'userId123',
+                        anonymousId: 'anonId123',
+                        event: 'Custom Event',
+                        integrations: {
+                          All: true,
+                        },
+                        originalTimestamp: '2024-03-04T15:32:56.409Z',
+                        timestamp: 1709566376409,
+                      },
+                    ],
+                  },
+                  JSON_ARRAY: {},
+                  XML: {},
+                  FORM: {},
+                },
+                files: {},
+              },
+              metadata: [generateMetadata(4)],
+              batched: true,
+              statusCode: 200,
+              destination,
+            },
+            {
               metadata: [generateMetadata(2)],
               batched: false,
               statusCode: 400,
@@ -147,7 +240,7 @@ export const data: RouterTestData[] = [
               destination,
             },
             {
-              metadata: [generateMetadata(4)],
+              metadata: [generateMetadata(5)],
               batched: false,
               statusCode: 400,
               error: 'Timestamp is not present. Aborting',
@@ -158,5 +251,105 @@ export const data: RouterTestData[] = [
         },
       },
     },
+    mockFns: defaultMockFns,
+  },
+  {
+    id: 'MovableInk-router-test-2',
+    name: destType,
+    description: 'Basic Router Test to test Max Request Size',
+    scenario: 'Framework',
+    successCriteria:
+      'Some events should be transformed successfully and some should fail for missing fields and status code should be 200',
+    feature: 'router',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: routerRequest2,
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: {
+          output: [
+            {
+              batchedRequest: {
+                version: '1',
+                type: 'REST',
+                method: 'POST',
+                endpoint: destination.Config.endpoint,
+                headers,
+                params: {},
+                body: {
+                  JSON: {
+                    events: [
+                      {
+                        type: 'track',
+                        channel,
+                        userId: 'userId123',
+                        anonymousId: 'anonId123',
+                        event: 'Order Completed',
+                        properties: trackTestProperties['Order Completed'],
+                        integrations: {
+                          All: true,
+                        },
+                        originalTimestamp: '2024-03-04T15:32:56.409Z',
+                        timestamp: 1709566376409,
+                      },
+                    ],
+                  },
+                  JSON_ARRAY: {},
+                  XML: {},
+                  FORM: {},
+                },
+                files: {},
+              },
+              metadata: [generateMetadata(1)],
+              batched: true,
+              statusCode: 200,
+              destination,
+            },
+            {
+              batchedRequest: {
+                version: '1',
+                type: 'REST',
+                method: 'POST',
+                endpoint: destination.Config.endpoint,
+                headers,
+                params: {},
+                body: {
+                  JSON: {
+                    events: [
+                      {
+                        type: 'track',
+                        channel,
+                        userId: 'userId123',
+                        anonymousId: 'anonId123',
+                        event: 'Custom Event',
+                        integrations: {
+                          All: true,
+                        },
+                        originalTimestamp: '2024-03-04T15:32:56.409Z',
+                        timestamp: 1709566376409,
+                      },
+                    ],
+                  },
+                  JSON_ARRAY: {},
+                  XML: {},
+                  FORM: {},
+                },
+                files: {},
+              },
+              metadata: [generateMetadata(2)],
+              batched: true,
+              statusCode: 200,
+              destination,
+            },
+          ],
+        },
+      },
+    },
+    mockFns: defaultMockFns,
   },
 ];

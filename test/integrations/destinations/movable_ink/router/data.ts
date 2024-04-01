86730d90bf4aed4b914090dd26af21267e135386
Gauravudia <gauravudia@rudderstack.com>
2024-04-01 13:10:24 +0530

    test: add testcase for batching on max request size

--- test/integrations/destinations/movable_ink/router/data.ts
+++ test/integrations/destinations/movable_ink/router/data.ts
@@ -88,6 +88,44 @@ const routerRequest: RouterTransformationRequest = {
   destType,
 };
 
+// >5KB payload
+const routerRequest2: RouterTransformationRequest = {
+  input: [
+    {
+      message: {
+        type: 'track',
+        channel,
+        anonymousId: 'anonId123',
+        userId: 'userId123',
+        event: 'Order Completed',
+        properties: trackTestProperties['Order Completed'],
+        integrations: {
+          All: true,
+        },
+        originalTimestamp: '2024-03-04T15:32:56.409Z',
+      },
+      metadata: generateMetadata(1),
+      destination,
+    },
+    {
+      message: {
+        type: 'track',
+        channel,
+        anonymousId: 'anonId123',
+        userId: 'userId123',
+        event: 'Custom Event',
+        integrations: {
+          All: true,
+        },
+        originalTimestamp: '2024-03-04T15:32:56.409Z',
+      },
+      metadata: generateMetadata(2),
+      destination,
+    },
+  ],
+  destType,
+};
+
 export const data = [
   {
     id: 'MovableInk-router-test-1',
@@ -207,6 +245,105 @@ export const data = [
               statusCode: 400,
               error: 'Timestamp is not present. Aborting',
               statTags: RouterInstrumentationErrorStatTags,
+              destination,
+            },
+          ],
+        },
+      },
+    },
+    mockFns: defaultMockFns,
+  },
+  {
+    id: 'MovableInk-router-test-2',
+    name: destType,
+    description: 'Basic Router Test to test Max Request Size',
+    scenario: 'Framework',
+    successCriteria:
+      'Some events should be transformed successfully and some should fail for missing fields and status code should be 200',
+    feature: 'router',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: routerRequest2,
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: {
+          output: [
+            {
+              batchedRequest: {
+                version: '1',
+                type: 'REST',
+                method: 'POST',
+                endpoint: destination.Config.endpoint,
+                headers,
+                params: {},
+                body: {
+                  JSON: {
+                    events: [
+                      {
+                        type: 'track',
+                        channel,
+                        userId: 'userId123',
+                        anonymousId: 'anonId123',
+                        event: 'Order Completed',
+                        properties: trackTestProperties['Order Completed'],
+                        integrations: {
+                          All: true,
+                        },
+                        originalTimestamp: '2024-03-04T15:32:56.409Z',
+                        timestamp: 1709566376409,
+                      },
+                    ],
+                  },
+                  JSON_ARRAY: {},
+                  XML: {},
+                  FORM: {},
+                },
+                files: {},
+              },
+              metadata: [generateMetadata(1)],
+              batched: true,
+              statusCode: 200,
+              destination,
+            },
+            {
+              batchedRequest: {
+                version: '1',
+                type: 'REST',
+                method: 'POST',
+                endpoint: destination.Config.endpoint,
+                headers,
+                params: {},
+                body: {
+                  JSON: {
+                    events: [
+                      {
+                        type: 'track',
+                        channel,
+                        userId: 'userId123',
+                        anonymousId: 'anonId123',
+                        event: 'Custom Event',
+                        integrations: {
+                          All: true,
+                        },
+                        originalTimestamp: '2024-03-04T15:32:56.409Z',
+                        timestamp: 1709566376409,
+                      },
+                    ],
+                  },
+                  JSON_ARRAY: {},
+                  XML: {},
+                  FORM: {},
+                },
+                files: {},
+              },
+              metadata: [generateMetadata(2)],
+              batched: true,
+              statusCode: 200,
               destination,
             },
           ],

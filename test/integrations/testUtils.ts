f2308afb2efa87f90c1ef3fa15e68de059a9aabe
mihir-4116 <mihirb@rudderstack.com>
2024-04-04 11:01:30 +0530

    feat(iterable): component test refactor

--- test/integrations/testUtils.ts
+++ test/integrations/testUtils.ts
@@ -292,6 +292,7 @@ export const generatePageOrScreenPayload: any = (parametersOverride: any, eventT
         'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36',
     }),
     event: parametersOverride.event,
+    name: parametersOverride.name,
     anonymousId: parametersOverride.anonymousId || 'default-anonymousId',
     properties: parametersOverride.properties,
     type: eventType || 'page',

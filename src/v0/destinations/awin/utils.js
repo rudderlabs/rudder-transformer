48cf9f282b803382dd2961b4b834d08a06eaab63
shrouti1507 <60211312+shrouti1507@users.noreply.github.com>
2024-04-08 19:08:58 +0530

    feat: adding product level tracking for awin (#3246)
    * feat: adding product level tracking for awin
    
    * fix: address review comments

--- src/v0/destinations/awin/utils.js
+++ src/v0/destinations/awin/utils.js
@@ -1,3 +1,5 @@
+const lodash = require('lodash');
+
 /**
  * Returns final params
  * @param {*} params
@@ -24,6 +26,55 @@ const getParams = (parameters, advertiserId) => {
   return params;
 };
 
+const areAllValuesDefined = (obj) =>
+  lodash.every(lodash.values(obj), (value) => !lodash.isUndefined(value));
+
+const buildProductPayloadString = (payload) => {
+  // URL-encode each value, and join back with the same key.
+  const encodedPayload = Object.entries(payload).reduce((acc, [key, value]) => {
+    // Encode each value. Assuming that all values are either strings or can be
+    // safely converted to strings.
+    acc[key] = encodeURIComponent(value);
+    return acc;
+  }, {});
+
+  return `AW:P|${encodedPayload.advertiserId}|${encodedPayload.orderReference}|${encodedPayload.productId}|${encodedPayload.productName}|${encodedPayload.productItemPrice}|${encodedPayload.productQuantity}|${encodedPayload.productSku}|${encodedPayload.commissionGroupCode}|${encodedPayload.productCategory}`;
+};
+
+// ref: https://wiki.awin.com/index.php/Advertiser_Tracking_Guide/Product_Level_Tracking#PLT_Via_Conversion_Pixel
+const trackProduct = (properties, advertiserId, commissionParts) => {
+  const transformedProductInfoObj = {};
+  if (
+    properties?.products &&
+    Array.isArray(properties?.products) &&
+    properties.products.length > 0
+  ) {
+    const productsArray = properties.products;
+    let productIndex = 0;
+    productsArray.forEach((product) => {
+      const productPayloadNew = {
+        advertiserId,
+        orderReference: properties.order_id || properties.orderId,
+        productId: product.product_id || product.productId,
+        productName: product.name,
+        productItemPrice: product.price,
+        productQuantity: product.quantity,
+        productSku: product.sku || '',
+        commissionGroupCode: commissionParts || 'DEFAULT',
+        productCategory: product.category || '',
+      };
+      if (areAllValuesDefined(productPayloadNew)) {
+        transformedProductInfoObj[`bd[${productIndex}]`] =
+          buildProductPayloadString(productPayloadNew);
+        productIndex += 1;
+      }
+    });
+  }
+  return transformedProductInfoObj;
+};
+
 module.exports = {
   getParams,
+  trackProduct,
+  buildProductPayloadString,
 };

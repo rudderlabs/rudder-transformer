91b56458a0ae94c5f91714107dd5fd4d34cd1769
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2024-05-08 02:19:54 +0530

    feat: onboard klaviyo bulk upload destination, initial commit

---
+++ src/cdk/v2/destinations/klaviyo_bulk_upload/utils.js
@@ -0,0 +1,43 @@
+function transformSingleMessage(data) {
+  const transformedSingleProfile = {
+    type: 'profile',
+    attributes: {
+      ...data.traits,
+      anonymous_id: data.userId, // Add anonymous ID as user ID
+    },
+  };
+
+  return transformedSingleProfile;
+}
+
+function wrapCombinePayloads(transformedInputs) {
+  return {
+    data: {
+      type: 'profile-bulk-import-job',
+      attributes: {
+        profiles: transformedInputs,
+      },
+    },
+  };
+}
+
+function combinePayloads(inputs) {
+  const transformedInputs = [];
+  const successMetadata = [];
+
+  if (!inputs || inputs.length === 0) {
+    return [];
+  }
+
+  inputs.forEach((input) => {
+    const { message, metadata } = input;
+    const transformedMessage = transformSingleMessage(message);
+
+    successMetadata.push(metadata);
+    transformedInputs.push(transformedMessage);
+  });
+  const finalPayload = wrapCombinePayloads(transformedInputs);
+  return { finalPayload, successMetadata };
+}
+
+export { transformSingleMessage, combinePayloads };

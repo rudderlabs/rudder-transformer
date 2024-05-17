5c6167971d8166d2e7c2bed351e83e8c55ad1d6f
achettyiitr <achetty.iitr@gmail.com>
2024-05-17 11:00:22 +0530

    chore: added health

--- src/grpc/server.js
+++ src/grpc/server.js
@@ -3,6 +3,7 @@ import {logger} from "@rudderstack/workflow-engine";
 const grpc = require('@grpc/grpc-js');
 const {UserTransformService} = require('../services/userTransform');
 const {TransformerServiceService} = require('../proto/transform_grpc_pb');
+const {HealthService} = require('../proto/health_grpc_pb');
 
 
 function transform(call, callback) {
@@ -12,9 +13,14 @@ function transform(call, callback) {
     callback(null, {response: responses});
 }
 
+function health(call, callback) {
+    callback(null, {status: 'SERVING'});
+}
+
 function startGRPCServer() {
     const server = new grpc.Server();
     server.addService(TransformerServiceService, {Transform: transform});
+    server.addService(HealthService, {Check: health});
     server.bindAsync('0.0.0.0:50051', grpc.ServerCredentials.createInsecure(), (err, port) => {
         if (err) {
             logger.error('Failed to bind server:', err);
---
+++ src/proto/health_grpc_pb.js
@@ -0,0 +1,44 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('grpc');
+var src_proto_health_pb = require('../../src/proto/health_pb.js');
+
+function serialize_grpc_health_v1_HealthCheckRequest(arg) {
+  if (!(arg instanceof src_proto_health_pb.HealthCheckRequest)) {
+    throw new Error('Expected argument of type grpc.health.v1.HealthCheckRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_grpc_health_v1_HealthCheckRequest(buffer_arg) {
+  return src_proto_health_pb.HealthCheckRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_grpc_health_v1_HealthCheckResponse(arg) {
+  if (!(arg instanceof src_proto_health_pb.HealthCheckResponse)) {
+    throw new Error('Expected argument of type grpc.health.v1.HealthCheckResponse');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_grpc_health_v1_HealthCheckResponse(buffer_arg) {
+  return src_proto_health_pb.HealthCheckResponse.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+var HealthService = exports.HealthService = {
+  check: {
+    path: '/grpc.health.v1.Health/Check',
+    requestStream: false,
+    responseStream: false,
+    requestType: src_proto_health_pb.HealthCheckRequest,
+    responseType: src_proto_health_pb.HealthCheckResponse,
+    requestSerialize: serialize_grpc_health_v1_HealthCheckRequest,
+    requestDeserialize: deserialize_grpc_health_v1_HealthCheckRequest,
+    responseSerialize: serialize_grpc_health_v1_HealthCheckResponse,
+    responseDeserialize: deserialize_grpc_health_v1_HealthCheckResponse,
+  },
+};
+
+exports.HealthClient = grpc.makeGenericClientConstructor(HealthService);

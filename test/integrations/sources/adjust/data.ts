aea2f4e4bd6a45c1a6c67ee3ca0f891242f97ee6
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2024-05-20 21:11:13 +0530

    fix: onboard adjust source

---
+++ test/integrations/sources/adjust/data.ts
@@ -0,0 +1,77 @@
+import utils from '../../../../src/v0/util';
+
+const defaultMockFns = () => {
+  jest.spyOn(utils, 'generateUUID').mockReturnValue('97fcd7b2-cc24-47d7-b776-057b7b199513');
+};
+
+export const data = [
+  {
+    name: 'adjust',
+    description: 'Simple track call',
+    module: 'source',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            id: 'adjust',
+            query_parameters: {
+              gps_adid: ['38400000-8cf0-11bd-b23e-10b96e40000d'],
+              adid: ['18546f6171f67e29d1cb983322ad1329'],
+              tracker_token: ['abc'],
+              custom: ['custom'],
+              tracker_name: ['dummy'],
+            },
+            updated_at: '2023-02-10T12:16:07.251Z',
+            created_at: '2023-02-10T12:05:04.402Z',
+          },
+        ],
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      pathSuffix: '',
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              batch: [
+                {
+                  context: {
+                    library: {
+                      name: 'unknown',
+                      version: 'unknown',
+                    },
+                    integration: {
+                      name: 'Adjust',
+                    },
+                    device: {
+                      'id ': '18546f6171f67e29d1cb983322ad1329',
+                    },
+                  },
+                  integrations: {
+                    Adjust: false,
+                  },
+                  type: 'track',
+                  properties: {
+                    gps_adid: '38400000-8cf0-11bd-b23e-10b96e40000d',
+                    tracker_token: 'abc',
+                    custom: 'custom',
+                  },
+                  anonymousId: '97fcd7b2-cc24-47d7-b776-057b7b199513',
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+    mockFns: () => {
+      defaultMockFns();
+    },
+  },
+];

bb0b79e103aa5241195c5c4ee7bf64bda86656b1
Aanshi Lahoti <aanshilahoti677@gmail.com>
2024-05-21 16:47:06 +0530

    feat: webhook destination: introduce batching

---
+++ test/integrations/destinations/webhook/commonConfig.ts
@@ -0,0 +1,25 @@
+export const context = {
+  device: {
+    id: 'df16bffa-5c3d-4fbb-9bce-3bab098129a7R',
+    manufacturer: 'Xiaomi',
+    model: 'Redmi 6',
+    name: 'xiaomi',
+  },
+  network: { carrier: 'Banglalink' },
+  os: { name: 'android', version: '8.1.0' },
+  traits: {
+    address: { city: 'Dhaka', country: 'Bangladesh' },
+    anonymousId: 'c82cbdff-e5be-4009-ac78-cdeea09ab4b1',
+  },
+};
+
+export const destination = {
+  Config: {
+    webhookUrl: 'http://6b0e6a60.ngrok.io',
+    headers: [
+      { from: '', to: '' },
+      { from: 'test2', to: 'value2' },
+    ],
+  },
+  DestinationDefinition: { Config: { cdkV2Enabled: true } },
+};

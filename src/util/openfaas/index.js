8a004713f853f2d518a966844897cda12f4de365
Dhawal Sanghvi <sanghvi.dhawal101433@gmail.com>
2024-05-23 16:26:46 +0530

    chore: minor fix

--- src/util/openfaas/index.js
+++ src/util/openfaas/index.js
@@ -304,7 +304,8 @@ const executeFaasFunction = async (
     };
 
     stats.counter('user_transform_function_input_events', events.length, tags);
-    stats.timing('user_transform_function_latency_summary', startTime, tags);
+    stats.timing('user_transform_function_latency', startTime, tags);
+    stats.timingSummary('user_transform_function_latency_summary', startTime, tags);
   }
 };
 

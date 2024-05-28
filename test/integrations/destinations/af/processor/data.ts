9f7fa84474c996a7ac6d398574b4c583d089aab0
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2024-05-28 20:46:27 +0530

    fix: adding test cases for old and new config

--- test/integrations/destinations/af/processor/data.ts
+++ test/integrations/destinations/af/processor/data.ts
@@ -1838,4 +1838,449 @@ export const data = [
       },
     },
   },
+  {
+    name: 'af',
+    description: 'No authentication key is present',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'track',
+              event: 'Order Completed',
+              sentAt: '2020-08-14T05:30:30.118Z',
+              context: {
+                externalId: [{ type: 'appsflyerExternalId', id: 'afUid' }],
+                source: 'test',
+                app: { namespace: 'com.rudderlabs.javascript' },
+                os: { name: 'android' },
+                traits: { anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1' },
+                library: { name: 'rudder-sdk-ruby-sync', version: '1.0.6' },
+              },
+              messageId: '7208bbb6-2c4e-45bb-bf5b-ad426f3593e9',
+              timestamp: '2020-08-14T05:30:30.118Z',
+              properties: {
+                tax: 2,
+                total: 27.5,
+                coupon: 'hasbros',
+                revenue: 48,
+                price: 25,
+                quantity: 2,
+              },
+              anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+              integrations: { AF: { af_uid: 'afUid' } },
+            },
+            destination: {
+              Config: {
+                androidAppId: 'com.rudderlabs.javascript',
+                groupTypeTrait: 'email',
+                groupValueTrait: 'age',
+                trackProductsOnce: false,
+                trackRevenuePerProduct: false,
+                addPropertiesAtRoot: true,
+              },
+            },
+          },
+        ],
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            error: 'No authentication key is present. Aborting.',
+            statTags: {
+              destType: 'AF',
+              errorCategory: 'dataValidation',
+              errorType: 'configuration',
+              feature: 'processor',
+              implementation: 'native',
+              module: 'destination',
+            },
+
+            statusCode: 400,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'af',
+    description: 'with authentication process v1, dev key is mandatory',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'track',
+              event: 'Order Completed',
+              sentAt: '2020-08-14T05:30:30.118Z',
+              context: {
+                externalId: [{ type: 'appsflyerExternalId', id: 'afUid' }],
+                source: 'test',
+                app: { namespace: 'com.rudderlabs.javascript' },
+                os: { name: 'android' },
+                traits: { anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1' },
+                library: { name: 'rudder-sdk-ruby-sync', version: '1.0.6' },
+              },
+              messageId: '7208bbb6-2c4e-45bb-bf5b-ad426f3593e9',
+              timestamp: '2020-08-14T05:30:30.118Z',
+              properties: {
+                tax: 2,
+                total: 27.5,
+                coupon: 'hasbros',
+                revenue: 48,
+                price: 25,
+                quantity: 2,
+              },
+              anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+              integrations: { AF: { af_uid: 'afUid' } },
+            },
+            destination: {
+              Config: {
+                authVersion: 'v1',
+                s2sKey: 'dummy',
+                androidAppId: 'com.rudderlabs.javascript',
+                groupTypeTrait: 'email',
+                groupValueTrait: 'age',
+                trackProductsOnce: false,
+                trackRevenuePerProduct: false,
+                addPropertiesAtRoot: true,
+              },
+            },
+          },
+        ],
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            error: 'dev key is mandatory for v1 authorization. Aborting.',
+            statTags: {
+              destType: 'AF',
+              errorCategory: 'dataValidation',
+              errorType: 'configuration',
+              feature: 'processor',
+              implementation: 'native',
+              module: 'destination',
+            },
+
+            statusCode: 400,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'af',
+    description: 'with authentication process v2, s2s key is mandatory',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'track',
+              event: 'Order Completed',
+              sentAt: '2020-08-14T05:30:30.118Z',
+              context: {
+                externalId: [{ type: 'appsflyerExternalId', id: 'afUid' }],
+                source: 'test',
+                app: { namespace: 'com.rudderlabs.javascript' },
+                os: { name: 'android' },
+                traits: { anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1' },
+                library: { name: 'rudder-sdk-ruby-sync', version: '1.0.6' },
+              },
+              messageId: '7208bbb6-2c4e-45bb-bf5b-ad426f3593e9',
+              timestamp: '2020-08-14T05:30:30.118Z',
+              properties: {
+                tax: 2,
+                total: 27.5,
+                coupon: 'hasbros',
+                revenue: 48,
+                price: 25,
+                quantity: 2,
+              },
+              anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+              integrations: { AF: { af_uid: 'afUid' } },
+            },
+            destination: {
+              Config: {
+                authVersion: 'v2',
+                devKey: 'dummy',
+                androidAppId: 'com.rudderlabs.javascript',
+                groupTypeTrait: 'email',
+                groupValueTrait: 'age',
+                trackProductsOnce: false,
+                trackRevenuePerProduct: false,
+                addPropertiesAtRoot: true,
+              },
+            },
+          },
+        ],
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            error: 's2s key is mandatory for v2 authorization. Aborting.',
+            statTags: {
+              destType: 'AF',
+              errorCategory: 'dataValidation',
+              errorType: 'configuration',
+              feature: 'processor',
+              implementation: 'native',
+              module: 'destination',
+            },
+
+            statusCode: 400,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'af',
+    description: 'New Config and old way of authentication',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'track',
+              event: 'Order Completed',
+              sentAt: '2020-08-14T05:30:30.118Z',
+              context: {
+                externalId: [{ type: 'appsflyerExternalId', id: 'afUid' }],
+                source: 'test',
+                app: { namespace: 'com.rudderlabs.javascript' },
+                os: { name: 'android' },
+                traits: { anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1' },
+                library: { name: 'rudder-sdk-ruby-sync', version: '1.0.6' },
+              },
+              messageId: '7208bbb6-2c4e-45bb-bf5b-ad426f3593e9',
+              timestamp: '2020-08-14T05:30:30.118Z',
+              properties: {
+                tax: 2,
+                total: 27.5,
+                coupon: 'hasbros',
+                revenue: 48,
+                price: 25,
+                quantity: 2,
+                currency: 'ZAR',
+                discount: 2.5,
+                order_id: '50314b8e9bcf000000000000',
+                products: [
+                  {
+                    sku: '45790-32',
+                    url: 'https://www.example.com/product/path',
+                    name: 'Monopoly: 3rd Edition',
+                    price: 19,
+                    category: 'Games',
+                    quantity: 1,
+                    image_url: 'https:///www.example.com/product/path.jpg',
+                    product_id: '507f1f77bcf86cd799439011',
+                  },
+                  {
+                    sku: '46493-32',
+                    name: 'Uno Card Game',
+                    price: 3,
+                    category: 'Games',
+                    quantity: 2,
+                    product_id: '505bd76785ebb509fc183733',
+                  },
+                ],
+                shipping: 3,
+                subtotal: 22.5,
+                affiliation: 'Google Store',
+                checkout_id: 'fksdjfsdjfisjf9sdfjsd9f',
+              },
+              anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+              integrations: { AF: { af_uid: 'afUid' } },
+            },
+            destination: {
+              Config: {
+                authVersion: 'v1',
+                devKey: 'abcde',
+                androidAppId: 'com.rudderlabs.javascript',
+                groupTypeTrait: 'email',
+                groupValueTrait: 'age',
+                trackProductsOnce: false,
+                trackRevenuePerProduct: false,
+                addPropertiesAtRoot: true,
+              },
+            },
+          },
+        ],
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              version: '1',
+              type: 'REST',
+              method: 'POST',
+              endpoint: 'https://api2.appsflyer.com/inappevent/com.rudderlabs.javascript',
+              headers: { 'Content-Type': 'application/json', authentication: 'abcde' },
+              params: {},
+              body: {
+                JSON: {
+                  bundleIdentifier: 'com.rudderlabs.javascript',
+                  eventValue:
+                    '{"tax":2,"total":27.5,"coupon":"hasbros","revenue":48,"price":25,"quantity":2,"currency":"ZAR","discount":2.5,"order_id":"50314b8e9bcf000000000000","products":[{"sku":"45790-32","url":"https://www.example.com/product/path","name":"Monopoly: 3rd Edition","price":19,"category":"Games","quantity":1,"image_url":"https:///www.example.com/product/path.jpg","product_id":"507f1f77bcf86cd799439011"},{"sku":"46493-32","name":"Uno Card Game","price":3,"category":"Games","quantity":2,"product_id":"505bd76785ebb509fc183733"}],"shipping":3,"subtotal":22.5,"affiliation":"Google Store","checkout_id":"fksdjfsdjfisjf9sdfjsd9f","af_revenue":48,"af_price":[19,3],"af_quantity":[1,2],"af_order_id":"50314b8e9bcf000000000000","af_content_id":["507f1f77bcf86cd799439011","505bd76785ebb509fc183733"]}',
+                  eventName: 'Order Completed',
+                  eventCurrency: 'ZAR',
+                  eventTime: '2020-08-14T05:30:30.118Z',
+                  appsflyer_id: 'afUid',
+                },
+                XML: {},
+                JSON_ARRAY: {},
+                FORM: {},
+              },
+              files: {},
+              userId: '',
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'af',
+    description: 'New Config and new way of authentication',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'track',
+              event: 'Order Completed',
+              sentAt: '2020-08-14T05:30:30.118Z',
+              context: {
+                externalId: [{ type: 'appsflyerExternalId', id: 'afUid' }],
+                source: 'test',
+                app: { namespace: 'com.rudderlabs.javascript' },
+                os: { name: 'android' },
+                traits: { anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1' },
+                library: { name: 'rudder-sdk-ruby-sync', version: '1.0.6' },
+              },
+              messageId: '7208bbb6-2c4e-45bb-bf5b-ad426f3593e9',
+              timestamp: '2020-08-14T05:30:30.118Z',
+              properties: {
+                tax: 2,
+                total: 27.5,
+                coupon: 'hasbros',
+                revenue: 48,
+                price: 25,
+                quantity: 2,
+                currency: 'ZAR',
+                discount: 2.5,
+                order_id: '50314b8e9bcf000000000000',
+                products: [
+                  {
+                    sku: '45790-32',
+                    url: 'https://www.example.com/product/path',
+                    name: 'Monopoly: 3rd Edition',
+                    price: 19,
+                    category: 'Games',
+                    quantity: 1,
+                    image_url: 'https:///www.example.com/product/path.jpg',
+                    product_id: '507f1f77bcf86cd799439011',
+                  },
+                  {
+                    sku: '46493-32',
+                    name: 'Uno Card Game',
+                    price: 3,
+                    category: 'Games',
+                    quantity: 2,
+                    product_id: '505bd76785ebb509fc183733',
+                  },
+                ],
+                shipping: 3,
+                subtotal: 22.5,
+                affiliation: 'Google Store',
+                checkout_id: 'fksdjfsdjfisjf9sdfjsd9f',
+              },
+              anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+              integrations: { AF: { af_uid: 'afUid' } },
+            },
+            destination: {
+              Config: {
+                authVersion: 'v2',
+                s2sKey: 'abcde',
+                androidAppId: 'com.rudderlabs.javascript',
+                groupTypeTrait: 'email',
+                groupValueTrait: 'age',
+                trackProductsOnce: false,
+                trackRevenuePerProduct: false,
+                addPropertiesAtRoot: true,
+              },
+            },
+          },
+        ],
+        method: 'POST',
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              version: '1',
+              type: 'REST',
+              method: 'POST',
+              endpoint: 'https://api3.appsflyer.com/inappevent/com.rudderlabs.javascript',
+              headers: { 'Content-Type': 'application/json', authentication: 'abcde' },
+              params: {},
+              body: {
+                JSON: {
+                  bundleIdentifier: 'com.rudderlabs.javascript',
+                  eventValue:
+                    '{"tax":2,"total":27.5,"coupon":"hasbros","revenue":48,"price":25,"quantity":2,"currency":"ZAR","discount":2.5,"order_id":"50314b8e9bcf000000000000","products":[{"sku":"45790-32","url":"https://www.example.com/product/path","name":"Monopoly: 3rd Edition","price":19,"category":"Games","quantity":1,"image_url":"https:///www.example.com/product/path.jpg","product_id":"507f1f77bcf86cd799439011"},{"sku":"46493-32","name":"Uno Card Game","price":3,"category":"Games","quantity":2,"product_id":"505bd76785ebb509fc183733"}],"shipping":3,"subtotal":22.5,"affiliation":"Google Store","checkout_id":"fksdjfsdjfisjf9sdfjsd9f","af_revenue":48,"af_price":[19,3],"af_quantity":[1,2],"af_order_id":"50314b8e9bcf000000000000","af_content_id":["507f1f77bcf86cd799439011","505bd76785ebb509fc183733"]}',
+                  eventName: 'Order Completed',
+                  eventCurrency: 'ZAR',
+                  eventTime: '2020-08-14T05:30:30.118Z',
+                  appsflyer_id: 'afUid',
+                },
+                XML: {},
+                JSON_ARRAY: {},
+                FORM: {},
+              },
+              files: {},
+              userId: '',
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
 ];

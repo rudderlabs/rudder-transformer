0d6499ee84e189bf12fc79530d0e2349d35b5c22
Manish Kumar <manish.kumar@rudderstack.com>
2024-06-17 11:31:52 +0530

    feat: onboard closeCRM source

---
+++ src/v1/sources/closeCRM/transform.js
@@ -0,0 +1,52 @@
+const { removeUndefinedAndNullValues, generateUUID, formatTimeStamp } = require('../../../v0/util');
+const { excludedFieldList } = require('./config');
+const Message = require('../../../v0/sources/message');
+
+function processEvent(inputEvent) {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  const { event, subscription_id } = inputEvent;
+
+  const message = new Message('CloseCRM');
+
+  // Set event type track
+  message.setEventType('track');
+
+  // Set event name
+  const eventName = `${event.object_type} ${event.action}`;
+  message.setEventName(eventName);
+
+  // Set userId
+  if (event.lead_id) {
+    message.setProperty('userId', event.lead_id);
+  } else {
+    message.setProperty('anonymousId', generateUUID());
+  }
+
+  // Set messageId
+  message.setProperty('messageId', event.id);
+
+  // Set Timestamp
+  message.setProperty(
+    'originalTimestamp',
+    formatTimeStamp(event.date_updated, 'yyyy-MM-ddTHH:mm:ss.SSSZ'),
+  );
+
+  // Set properties
+  message.setProperty('properties', event);
+  message.setProperty('properties.subscription_id', subscription_id);
+
+  // Remove excluding fields
+  excludedFieldList.forEach((field) => {
+    delete message.properties[field];
+  });
+
+  return message;
+}
+
+function process(inputEvent) {
+  const { event } = inputEvent;
+  const response = processEvent(event);
+  return removeUndefinedAndNullValues(response);
+}
+
+exports.process = process;

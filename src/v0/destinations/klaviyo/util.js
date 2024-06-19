35dbf6ee78d966ddd82b8e8b20392399881dda59
Sai Sankeerth <sanpj2292@github.com>
2024-06-19 12:53:23 +0530

    chore: metadata propagation in delighted & klaviyo
    Signed-off-by: Sai Sankeerth <sanpj2292@github.com>

--- src/v0/destinations/klaviyo/util.js
+++ src/v0/destinations/klaviyo/util.js
@@ -57,6 +57,7 @@ const getIdFromNewOrExistingProfile = async ({ endpoint, payload, requestOptions
     payload,
     requestOptions,
     {
+      metadata,
       destType: 'klaviyo',
       feature: 'transformation',
       endpointPath,

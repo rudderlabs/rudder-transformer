a281770db70e2722ef7f7bf35591ada0ac583bdb
Anant Jain <anantjain45823@gmail.com>
2024-07-04 12:04:56 +0530

    fix: code changes to maintain order

--- src/v0/destinations/hs/transform.js
+++ src/v0/destinations/hs/transform.js
@@ -67,20 +67,17 @@ const process = async (event) => {
   }
   return processSingleMessage(events[0].message, events[0].destination);
 };
-
-// we are batching by default at routerTransform
-const processRouterDest = async (inputs, reqMetadata) => {
-  const tempNewInputs = batchEventsInOrder(inputs);
+const processBatchRouter = async (inputs, reqMetadata) => {
   let tempInputs = inputs;
-
-  const successRespList = [];
-  const errorRespList = [];
   // using the first destination config for transforming the batch
   const { destination } = tempInputs[0];
   let propertyMap;
   const mappedToDestination = get(tempInputs[0].message, MappedToDestinationKey);
   const { objectType } = getDestinationExternalIDInfoForRetl(tempInputs[0].message, 'HS');
-
+  const successRespList = [];
+  const errorRespList = [];
+  // batch implementation
+  let batchedResponseList = [];
   try {
     if (mappedToDestination && GENERIC_TRUE_VALUES.includes(mappedToDestination?.toString())) {
       // skip splitting the batches to inserts and updates if object it is an association
@@ -100,56 +97,65 @@ const processRouterDest = async (inputs, reqMetadata) => {
     }
   } catch (error) {
     // Any error thrown from the above try block applies to all the events
-    return tempInputs.map((input) => handleRtTfSingleEventError(input, error, reqMetadata));
+    return { batchedResponseList, errorRespList: tempInputs.map((input) => handleRtTfSingleEventError(input, error, reqMetadata)) };
   }
 
-  await Promise.all(
-    tempNewInputs.map(async (input) => {
-      try {
-        if (input.message.statusCode) {
-          // already transformed event
+  await Promise.all(inputs.map(async input => {
+    try {
+      if (input.message.statusCode) {
+        // already transformed event
+        successRespList.push({
+          message: input.message,
+          metadata: input.metadata,
+          destination,
+        });
+      } else {
+        // event is not transformed
+        let receivedResponse = await processSingleMessage(
+          input.message,
+          destination,
+          propertyMap,
+        );
+
+        receivedResponse = Array.isArray(receivedResponse)
+          ? receivedResponse
+          : [receivedResponse];
+
+        // received response can be in array format [{}, {}, {}, ..., {}]
+        // if multiple response is being returned
+        receivedResponse.forEach((element) => {
           successRespList.push({
-            message: input.message,
+            message: element,
             metadata: input.metadata,
             destination,
           });
-        } else {
-          // event is not transformed
-          let receivedResponse = await processSingleMessage(
-            input.message,
-            destination,
-            propertyMap,
-          );
-
-          receivedResponse = Array.isArray(receivedResponse)
-            ? receivedResponse
-            : [receivedResponse];
-
-          // received response can be in array format [{}, {}, {}, ..., {}]
-          // if multiple response is being returned
-          receivedResponse.forEach((element) => {
-            successRespList.push({
-              message: element,
-              metadata: input.metadata,
-              destination,
-            });
-          });
-        }
-      } catch (error) {
-        const errRespEvent = handleRtTfSingleEventError(input, error, reqMetadata);
-        errorRespList.push(errRespEvent);
+        });
       }
-    }),
-  );
+    } catch (error) {
+      const errRespEvent = handleRtTfSingleEventError(input, error, reqMetadata);
+      errorRespList.push(errRespEvent);
+    }
+  }))
 
-  // batch implementation
-  let batchedResponseList = [];
   if (successRespList.length > 0) {
     if (destination.Config.apiVersion === API_VERSION.v3) {
       batchedResponseList = batchEvents(successRespList);
     } else {
       batchedResponseList = legacyBatchEvents(successRespList);
     }
+  }
+  return { batchedResponseList, errorRespList }
+}
+// we are batching by default at routerTransform
+const processRouterDest = async (inputs, reqMetadata) => {
+  const tempNewInputs = batchEventsInOrder(inputs);
+  const batchedResponseList = [];
+  const errorRespList = [];
+  // using promise.all() with map() in async manner is not giving the right order of output
+  for (const inputEvents of tempNewInputs) {
+    const response = await processBatchRouter(inputEvents, reqMetadata)
+    errorRespList.push(...response.errorRespList)
+    batchedResponseList.push(...response.batchedResponseList)
   }
   return [...batchedResponseList, ...errorRespList];
 };

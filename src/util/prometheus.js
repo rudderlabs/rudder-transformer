0bb69ea91c6f0027c0b7775cd934b9da8ef52c2b
Jayc <mjayachand@gmail.com>
2024-07-17 15:40:05 +0530

    core: cache dns resolution for js fetch calls

--- src/util/prometheus.js
+++ src/util/prometheus.js
@@ -892,7 +892,7 @@ class Prometheus {
         name: 'fetch_dns_resolve_time',
         help: 'fetch_dns_resolve_time',
         type: 'histogram',
-        labelNames: ['identifier', 'transformationId', 'workspaceId', 'error'],
+        labelNames: ['identifier', 'transformationId', 'workspaceId', 'error', 'cacheHit'],
       },
       {
         name: 'geo_call_duration',

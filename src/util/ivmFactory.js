f2e699312cb283e4a128ec8dcf94fa6a62beb575
Jayc <mjayachand@gmail.com>
2024-07-23 12:35:51 +0530

    chore: use dns resolution with geolocation

--- src/util/ivmFactory.js
+++ src/util/ivmFactory.js
@@ -236,9 +236,11 @@ async function createIvm(
           throw new Error('ip address is required');
         }
         if (!process.env.GEOLOCATION_URL) throw new Error('geolocation is not available right now');
-        const res = await fetch(`${process.env.GEOLOCATION_URL}/geoip/${args[0]}`, {
-          timeout: GEOLOCATION_TIMEOUT_IN_MS,
-        });
+        const res = await fetchWithDnsWrapper(
+          trTags,
+          `${process.env.GEOLOCATION_URL}/geoip/${args[0]}`,
+          { timeout: GEOLOCATION_TIMEOUT_IN_MS },
+        );
         if (res.status !== 200) {
           throw new Error(`request to fetch geolocation failed with status code: ${res.status}`);
         }

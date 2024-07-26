23581bb9f240511063e51de7eeb72db623388980
Sai Sankeerth <sanpj2292@github.com>
2024-07-26 17:31:49 +0530

    chore: add support for query parameters - chore: add tests generated for cordial source - chore: update headers method in adjust source

--- go/webhook/testcases/testcases.go
+++ go/webhook/testcases/testcases.go
@@ -33,7 +33,7 @@ type Input struct {
 }
 type Request struct {
 	Method   string
-	RawQuery string `json:"query"`
+	RawQuery string `json:"query_parameters"`
 	Headers  map[string]string
 	Body     json.RawMessage
 }

3f2a5a664d32b6f64e40846132e262967b40b1c8
Sankeerth <sankeerth@rudderstack.com>
2024-09-03 14:54:49 +0530

    chore: source integration script filter condition (#3704)
    Co-authored-by: Sai Sankeerth <sanpj2292@github.com>

--- test/integrations/testUtils.ts
+++ test/integrations/testUtils.ts
@@ -24,16 +24,17 @@ const generateAlphanumericId = (size = 36) =>
 export const getTestDataFilePaths = (dirPath: string, opts: OptionValues): string[] => {
   const globPattern = join(dirPath, '**', 'data.ts');
   let testFilePaths = globSync(globPattern);
+  let filteredTestFilePaths: string[] = [];
 
   if (opts.destination || opts.source) {
-    testFilePaths = testFilePaths.filter((testFile) =>
-      testFile.includes(`${opts.destination}/` || opts.source),
+    filteredTestFilePaths = testFilePaths.filter((testFile) =>
+      testFile.includes(`${opts.destination || opts.source}/`),
     );
   }
   if (opts.feature) {
-    testFilePaths = testFilePaths.filter((testFile) => testFile.includes(opts.feature));
+    filteredTestFilePaths = testFilePaths.filter((testFile) => testFile.includes(opts.feature));
   }
-  return testFilePaths;
+  return filteredTestFilePaths;
 };
 
 export const getTestData = (filePath): TestCaseData[] => {

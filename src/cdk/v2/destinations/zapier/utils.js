e87860222d7606b7185972b62fc1d6d115aa5b0b
Aanshi Lahoti <aanshilahoti677@gmail.com>
2024-10-08 17:11:34 +0530

    chore: updated headers

--- src/cdk/v2/destinations/zapier/utils.js
+++ src/cdk/v2/destinations/zapier/utils.js
@@ -1,12 +1,12 @@
 const { defaultRequestConfig } = require('../../../../v0/util');
 
-const buildResponseList = (payload, endpointList, headers) => {
+const buildResponseList = (payload, endpointList) => {
   const responseList = [];
   endpointList.forEach((endpoint) => {
     const response = defaultRequestConfig();
     response.body.JSON = payload;
     response.endpoint = endpoint;
-    response.headers = headers;
+    response.headers = { 'content-type': 'application/json' };
     responseList.push(response);
   });
   return responseList;

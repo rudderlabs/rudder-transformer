1a71bc21e465f7f2ea2a763427b1f5291ecabc86
Aanshi Lahoti <aanshilahoti677@gmail.com>
2024-10-16 23:47:15 +0530

    chore: should filter out falsey reserved attributes from Intercom destination  (#3767)
    * Should filter out reserved attributes from Intercom destination even if they are falsey
    
    * Fix presence check to allow null values
    
    ---------
    
    Co-authored-by: Manish Kumar <144022547+manish339k@users.noreply.github.com>

--- test/integrations/destinations/twitter_ads/processor/data.ts
+++ test/integrations/destinations/twitter_ads/processor/data.ts
@@ -290,7 +290,8 @@ export const data = [
               },
             },
             statusCode: 400,
-            error: '[TWITTER ADS]: one of twclid, phone or email must be present in properties.',
+            error:
+              '[TWITTER ADS]: one of twclid, phone, email, ip_address or user_agent must be present in properties.',
             statTags: {
               errorCategory: 'dataValidation',
               errorType: 'instrumentation',
@@ -868,6 +869,127 @@ export const data = [
                         },
                         {
                           twclid: '543',
+                        },
+                      ],
+                    },
+                  ],
+                },
+                JSON_ARRAY: {},
+                XML: {},
+                FORM: {},
+              },
+              files: {},
+              userId: '',
+            },
+            metadata: {
+              secret: {
+                consumerKey: 'qwe',
+                consumerSecret: 'fdghv',
+                accessToken: 'dummyAccessToken',
+                accessTokenSecret: 'testAccessTokenSecret',
+              },
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'twitter_ads',
+    description: 'Test 6',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'track',
+              event: 'Home Page Viewed',
+              channel: 'web',
+              context: {
+                source: 'test',
+                userAgent: 'chrome',
+                traits: {
+                  anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+                  email: 'abc@gmail.com',
+                  phone: '+1234589947',
+                  ge: 'male',
+                },
+                device: {
+                  advertisingId: 'abc123',
+                },
+                library: {
+                  name: 'rudder-sdk-ruby-sync',
+                  version: '1.0.6',
+                },
+              },
+              messageId: '7208bbb6-2c4e-45bb-bf5b-ad426f3593e9',
+              timestamp: '2020-08-14T05:30:30.118Z',
+              properties: {
+                affiliation: 'Google Store',
+                checkout_id: 'fksdjfsdjfisjf9sdfjsd9f',
+                ip_address: '8.25.197.25',
+              },
+              anonymousId: '50be5c78-6c3f-4b60-be84-97805a316fb1',
+              integrations: {
+                All: true,
+              },
+            },
+            metadata: {
+              secret: {
+                consumerKey: 'qwe',
+                consumerSecret: 'fdghv',
+                accessToken: 'dummyAccessToken',
+                accessTokenSecret: 'testAccessTokenSecret',
+              },
+            },
+            destination: {
+              Config: {
+                pixelId: 'dummyPixelId',
+                rudderAccountId: '2EOknn1JNH7WK1MfNku4fGYKkRK',
+                twitterAdsEventNames: [
+                  {
+                    rudderEventName: 'ABC Searched',
+                    twitterEventId: 'tw-234234324234',
+                  },
+                  {
+                    rudderEventName: 'Home Page Viewed',
+                    twitterEventId: 'tw-odt2o-odt2q',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              version: '1',
+              type: 'REST',
+              method: 'POST',
+              endpoint: 'https://ads-api.twitter.com/12/measurement/conversions/dummyPixelId',
+              headers: {
+                Authorization: authHeaderConstant,
+                'Content-Type': 'application/json',
+              },
+              params: {},
+              body: {
+                JSON: {
+                  conversions: [
+                    {
+                      conversion_time: '2020-08-14T05:30:30.118Z',
+                      event_id: 'tw-odt2o-odt2q',
+                      identifiers: [
+                        {
+                          ip_address: '8.25.197.25',
                         },
                       ],
                     },

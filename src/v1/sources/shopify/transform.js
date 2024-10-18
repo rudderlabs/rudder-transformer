5aa9239c7c4091c50ae04d67c7116ea3d32bf786
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2024-10-19 01:58:52 +0530

    chore: update tests

--- src/v1/sources/shopify/transform.js
+++ src/v1/sources/shopify/transform.js
@@ -6,7 +6,7 @@ const process = async (inputEvent) => {
   const { event } = inputEvent;
   // check on the source Config to identify the event is from the tracker-based (legacy)
   // or the pixel-based (latest) implementation.
-  const { pixelEventLabel: pixelClientEventLabel, id } = event;
+  const { pixelEventLabel: pixelClientEventLabel } = event;
   if (pixelClientEventLabel) {
     // this is a event fired from the web pixel loaded on the browser
     // by the user interactions with the store.
@@ -15,12 +15,6 @@ const process = async (inputEvent) => {
   }
   // this is for common logic for server-side events processing for both pixel and tracker apps.
   const response = await processWebhookEvents(event);
-  // attach everything that is part of webhook event to the context inside a key called shopifyDetails
-  response.context.shopifyDetails = { ...event };
-  // set the messageId to the id property if present the event.
-  if (id) {
-    response.messageId = id;
-  }
   return response;
 };
 

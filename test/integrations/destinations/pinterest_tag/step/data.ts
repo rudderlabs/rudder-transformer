73527364135b6f1f8b0558a95575c3f300eb53a0
ItsSudip <sudip.paul1997@gmail.com>
2024-10-23 11:12:57 +0530

    fix: add missing field for pinterest_tag single product events

--- test/integrations/destinations/pinterest_tag/step/data.ts
+++ test/integrations/destinations/pinterest_tag/step/data.ts
@@ -468,7 +468,9 @@ export const data = [
                     order_id: '50314b8e9bcf000000000000',
                     num_items: 2,
                     content_ids: ['123'],
-                    contents: [{ quantity: 2, item_price: '25' }],
+                    contents: [
+                      { id: '123', item_name: 'undefined', quantity: 2, item_price: '25' },
+                    ],
                   },
                 },
                 JSON_ARRAY: {},
@@ -2420,7 +2422,9 @@ export const data = [
                     order_id: '50314b8e9bcf000000000000',
                     num_items: 0,
                     content_ids: ['1234'],
-                    contents: [{ quantity: 1, item_price: 'undefined' }],
+                    contents: [
+                      { id: '1234', item_name: 'undefined', quantity: 1, item_price: 'undefined' },
+                    ],
                   },
                 },
                 JSON_ARRAY: {},
@@ -2685,7 +2689,7 @@ export const data = [
                   advertiser_id: '123456',
                   app_id: '429047995',
                   custom_data: {
-                    contents: [{ item_price: 'undefined', quantity: 1 }],
+                    contents: [{ item_name: 'undefined', item_price: 'undefined', quantity: 1 }],
                     currency: 'USD',
                     num_items: 0,
                     order_id: '50314b8e9bcf000000000000',
@@ -3605,6 +3609,7 @@ export const data = [
                     num_items: 0,
                     contents: [
                       {
+                        item_name: 'undefined',
                         quantity: 1,
                         item_price: 'undefined',
                       },

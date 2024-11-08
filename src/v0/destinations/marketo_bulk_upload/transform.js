b51672781f8281f514f6d0c4a2a5b012f59d4054
Utsab Chowdhury <utsab.c97@gmail.com>
2024-11-08 17:02:31 +0530

    fix: foramtting

--- src/v0/destinations/marketo_bulk_upload/transform.js
+++ src/v0/destinations/marketo_bulk_upload/transform.js
@@ -36,11 +36,10 @@ function responseBuilderSimple(message, destination) {
     const val = traits[fieldHashmap[key]];
     if (isDefined(val)) {
       let newVal = val;
+      // If value contains comma or newline then we need to escape it
       if (typeof val === 'string') {
         newVal = val.toString().replaceAll(/,/g, '\\,').replaceAll(/\n/g, '\\n');
       }
-      // If value contains comma or newline then we need to escape it
-
       payload[key] = newVal;
     }
   });

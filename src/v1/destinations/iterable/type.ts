9eb31bf7bf7b364b463502a6e839c6d80e49ce43
shrouti1507 <shrouti.gangopadhyay@gmail.com>
2024-12-03 15:48:41 +0530

    fix: removing unnecessary comments

--- src/v1/destinations/iterable/type.ts
+++ src/v1/destinations/iterable/type.ts
@@ -12,15 +12,15 @@ interface FailedUpdates {
 }
 
 export interface CommonResponse {
-  msg?: string; // Optional since it's only in Response 1
-  code?: string; // Optional since it's only in Response 1
-  params?: Record<string, unknown>; // Optional, specific to Response 1
-  successCount?: number; // Shared by Response 2 and 3
-  failCount?: number; // Shared by Response 2 and 3
-  invalidEmails?: string[]; // Shared by Response 2 and 3
-  invalidUserIds?: string[]; // Shared by Response 2 and 3
-  filteredOutFields?: string[]; // Shared by Response 2 and 3
-  createdFields?: string[]; // Shared by Response 2 and 3
-  disallowedEventNames?: string[]; // Specific to Response 3
-  failedUpdates?: FailedUpdates; // Nested object for failed updates
+  msg?: string;
+  code?: string;
+  params?: Record<string, unknown>;
+  successCount?: number;
+  failCount?: number;
+  invalidEmails?: string[];
+  invalidUserIds?: string[];
+  filteredOutFields?: string[];
+  createdFields?: string[];
+  disallowedEventNames?: string[];
+  failedUpdates?: FailedUpdates;
 }

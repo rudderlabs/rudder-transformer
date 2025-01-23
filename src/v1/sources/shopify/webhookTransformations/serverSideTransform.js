b26e207474ff4a3bf92c696694788d7ae4039965
Yashasvi Bajpai <33063622+yashasvibajpai@users.noreply.github.com>
2025-01-23 11:11:46 +0530

    fix: redis await missing

--- src/v1/sources/shopify/webhookTransformations/serverSideTransform.js
+++ src/v1/sources/shopify/webhookTransformations/serverSideTransform.js
@@ -109,7 +109,7 @@ const processEvent = async (inputEvent, metricMetadata) => {
   }
   // attach anonymousId if the event is track event using note_attributes
   if (message.type !== EventType.IDENTIFY) {
-    handleAnonymousId(message, event);
+    await handleAnonymousId(message, event);
   }
   // attach userId, email and other contextual properties
   message = handleCommonProperties(message, event, shopifyTopic);

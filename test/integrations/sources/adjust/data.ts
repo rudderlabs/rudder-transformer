961592da92425fc7725a5f4e6356a69bc7a6e5fa
Vinay Teki <vinayteki95@gmail.com>
2025-02-06 19:32:12 +0530

    chore: adjust v2 test cases

--- test/integrations/sources/adjust/data.ts
+++ test/integrations/sources/adjust/data.ts
@@ -209,6 +209,51 @@ export const data = [
   },
   {
     name: 'adjust',
+    description: 'Simple track call with no query parameters v2 version',
+    module: 'source',
+    version: 'v2',
+    skipGo: 'FIXME',
+    input: {
+      request: {
+        body: [
+          {
+            request: {
+              body: '{"id":"adjust","updated_at":"2023-02-10T12:16:07.251Z","created_at":"2023-02-10T12:05:04.402Z"}',
+            },
+            source: {},
+          },
+        ],
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      pathSuffix: '',
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            error: 'Query_parameters is missing',
+            statTags: {
+              destinationId: 'Non determinable',
+              errorCategory: 'transformation',
+              implementation: 'native',
+              module: 'source',
+              workspaceId: 'Non determinable',
+            },
+            statusCode: 400,
+          },
+        ],
+      },
+    },
+    mockFns: () => {
+      defaultMockFns();
+    },
+  },
+  {
+    name: 'adjust',
     description: 'Simple track call with wrong created at',
     module: 'source',
     version: 'v1',
@@ -230,6 +275,60 @@ export const data = [
               },
               updated_at: '2023-02-10T12:16:07.251Z',
               created_at: 'test',
+            },
+            source: {},
+          },
+        ],
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+      pathSuffix: '',
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            error: 'Failed to parse timestamp: "test"',
+            statTags: {
+              destinationId: 'Non determinable',
+              errorCategory: 'transformation',
+              implementation: 'native',
+              module: 'source',
+              workspaceId: 'Non determinable',
+            },
+            statusCode: 400,
+          },
+        ],
+      },
+    },
+    mockFns: () => {
+      defaultMockFns();
+    },
+  },
+  {
+    name: 'adjust',
+    description: 'Simple track call with wrong created at v2 version',
+    module: 'source',
+    version: 'v2',
+    skipGo: 'FIXME',
+    input: {
+      request: {
+        body: [
+          {
+            request: {
+              body: '{"id":"adjust","updated_at":"2023-02-10T12:16:07.251Z","created_at":"test"}',
+              query_parameters: {
+                gps_adid: ['38400000-8cf0-11bd-b23e-10b96e40000d'],
+                adid: ['18546f6171f67e29d1cb983322ad1329'],
+                tracker_token: ['abc'],
+                custom: ['custom'],
+                tracker_name: ['dummy'],
+                created_at: ['test'],
+                event_name: ['Click'],
+              },
             },
             source: {},
           },

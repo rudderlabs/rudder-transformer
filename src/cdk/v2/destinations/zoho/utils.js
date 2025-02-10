8a8ef5a24424a783b05e885654ec19c8be04da1b
Dilip Kola <kdilipkola@gmail.com>
2025-02-10 19:43:57 +0530

    refactor: zoho utils using cursor

--- src/cdk/v2/destinations/zoho/utils.js
+++ src/cdk/v2/destinations/zoho/utils.js
@@ -9,35 +9,46 @@ const zohoConfig = require('./config');
 const { handleHttpRequest } = require('../../../../adapters/network');
 
 const deduceModuleInfo = (inputs, Config) => {
-  const firstRecord = inputs[0].message;
-  const operationModuleInfo = {};
-  const mappedToDestination = firstRecord?.context?.mappedToDestination;
-  if (mappedToDestination) {
-    const { objectType, identifierType } = getDestinationExternalIDInfoForRetl(firstRecord, 'ZOHO');
-    operationModuleInfo.operationModuleType = objectType;
-    operationModuleInfo.upsertEndPoint = zohoConfig
-      .COMMON_RECORD_ENDPOINT(Config.region)
-      .replace('moduleType', objectType);
-    operationModuleInfo.identifierType = identifierType;
+  if (!Array.isArray(inputs) || inputs.length === 0) {
+    return {};
   }
-  return operationModuleInfo;
+
+  const firstRecord = inputs[0].message;
+  const mappedToDestination = firstRecord?.context?.mappedToDestination;
+
+  if (!mappedToDestination) {
+    return {};
+  }
+
+  const { objectType, identifierType } = getDestinationExternalIDInfoForRetl(firstRecord, 'ZOHO');
+  return {
+    operationModuleType: objectType,
+    upsertEndPoint: zohoConfig
+      .COMMON_RECORD_ENDPOINT(Config.region)
+      .replace('moduleType', objectType),
+    identifierType,
+  };
 };
 
-// eslint-disable-next-line consistent-return
+// Keeping the original function name and return structure
 function validatePresenceOfMandatoryProperties(objectName, object) {
-  if (zohoConfig.MODULE_MANDATORY_FIELD_CONFIG.hasOwnProperty(objectName)) {
-    const requiredFields = zohoConfig.MODULE_MANDATORY_FIELD_CONFIG[objectName];
-    const missingFields =
-      requiredFields.filter(
-        (field) => !object.hasOwnProperty(field) || !isDefinedAndNotNullAndNotEmpty(object[field]),
-      ) || [];
-    return { status: missingFields.length > 0, missingField: missingFields };
+  if (!zohoConfig.MODULE_MANDATORY_FIELD_CONFIG.hasOwnProperty(objectName)) {
+    return undefined; // Maintaining original undefined return for custom objects
   }
-  // No mandatory check performed for custom objects
+
+  const requiredFields = zohoConfig.MODULE_MANDATORY_FIELD_CONFIG[objectName];
+  const missingFields =
+    requiredFields.filter(
+      (field) => !object.hasOwnProperty(field) || !isDefinedAndNotNullAndNotEmpty(object[field]),
+    ) || [];
+
+  return {
+    status: missingFields.length > 0,
+    missingField: missingFields,
+  };
 }
 
 const formatMultiSelectFields = (config, fields) => {
-  // Convert multiSelectFieldLevelDecision array into a hash map for quick lookups
   const multiSelectFields = getHashFromArray(
     config.multiSelectFieldLevelDecision,
     'from',
@@ -45,16 +56,18 @@ const formatMultiSelectFields = (config, fields) => {
     false,
   );
 
-  Object.keys(fields).forEach((eachFieldKey) => {
+  // Creating a shallow copy to avoid mutations
+  const formattedFields = { ...fields };
+
+  Object.keys(formattedFields).forEach((eachFieldKey) => {
     if (multiSelectFields.hasOwnProperty(eachFieldKey)) {
-      // eslint-disable-next-line no-param-reassign
-      fields[eachFieldKey] = [fields[eachFieldKey]];
+      formattedFields[eachFieldKey] = [formattedFields[eachFieldKey]];
     }
   });
-  return fields;
+
+  return formattedFields;
 };
 
-// Utility to handle duplicate check
 const handleDuplicateCheck = (addDefaultDuplicateCheck, identifierType, operationModuleType) => {
   let duplicateCheckFields = [identifierType];
 
@@ -63,10 +76,9 @@ const handleDuplicateCheck = (addDefaultDuplicateCheck, identifierType, operatio
       zohoConfig.MODULE_WISE_DUPLICATE_CHECK_FIELD[operationModuleType];
 
     if (isDefinedAndNotNull(moduleDuplicateCheckField)) {
-      duplicateCheckFields = [...moduleDuplicateCheckField];
-      duplicateCheckFields.unshift(identifierType);
+      duplicateCheckFields = [identifierType, ...moduleDuplicateCheckField];
     } else {
-      duplicateCheckFields.push('Name'); // user chosen duplicate field always carries higher priority
+      duplicateCheckFields.push('Name');
     }
   }
 
@@ -88,45 +100,52 @@ function transformToURLParams(fields, Config) {
   return `${regionBasedEndPoint}/crm/v6/Leads/search?criteria=${criteria}`;
 }
 
-// ref : https://www.zoho.com/crm/developer/docs/api/v6/search-records.html
 const searchRecordId = async (fields, metadata, Config) => {
-  const searchURL = transformToURLParams(fields, Config);
-  const searchResult = await handleHttpRequest(
-    'get',
-    searchURL,
-    {
-      headers: {
-        Authorization: `Zoho-oauthtoken ${metadata.secret.accessToken}`,
+  try {
+    const searchURL = transformToURLParams(fields, Config);
+    const searchResult = await handleHttpRequest(
+      'get',
+      searchURL,
+      {
+        headers: {
+          Authorization: `Zoho-oauthtoken ${metadata.secret.accessToken}`,
+        },
       },
-    },
-    {
-      destType: 'zoho',
-      feature: 'deleteRecords',
-      requestMethod: 'GET',
-      endpointPath: 'crm/v6/Leads/search?criteria=',
-      module: 'router',
-    },
-  );
-  if (!isHttpStatusSuccess(searchResult.processedResponse.status)) {
+      {
+        destType: 'zoho',
+        feature: 'deleteRecords',
+        requestMethod: 'GET',
+        endpointPath: 'crm/v6/Leads/search?criteria=',
+        module: 'router',
+      },
+    );
+
+    if (!isHttpStatusSuccess(searchResult.processedResponse.status)) {
+      return {
+        erroneous: true,
+        message: searchResult.processedResponse.response,
+      };
+    }
+
+    if (searchResult.processedResponse.status === 204) {
+      return {
+        erroneous: true,
+        message: 'No contact is found with record details',
+      };
+    }
+
+    return {
+      erroneous: false,
+      message: searchResult.processedResponse.response.data.map((record) => record.id),
+    };
+  } catch (error) {
     return {
       erroneous: true,
-      message: searchResult.processedResponse.response,
+      message: error.message,
     };
   }
-  if (searchResult.processedResponse.status === 204) {
-    return {
-      erroneous: true,
-      message: 'No contact is found with record details',
-    };
-  }
-  const recordIds = searchResult.processedResponse.response.data.map((record) => record.id);
-  return {
-    erroneous: false,
-    message: recordIds,
-  };
 };
 
-// ref : https://www.zoho.com/crm/developer/docs/api/v6/upsert-records.html#:~:text=The%20trigger%20input%20can%20be%20workflow%2C%20approval%2C%20or%20blueprint.%20If%20the%20trigger%20is%20not%20mentioned%2C%20the%20workflows%2C%20approvals%20and%20blueprints%20related%20to%20the%20API%20will%20get%20executed.%20Enter%20the%20trigger%20value%20as%20%5B%5D%20to%20not%20execute%20the%20workflows.
 const calculateTrigger = (trigger) => {
   if (trigger === 'Default') {
     return null;
@@ -144,6 +163,7 @@ const validateConfigurationIssue = (Config, operationModuleType) => {
     'to',
     false,
   );
+
   if (Object.keys(hashMapMultiselect).length > 0 && Config.module !== operationModuleType) {
     throw new ConfigurationError(
       'Object Chosen in Visual Data Mapper is not consistent with Module type selected in destination configuration. Aborting Events.',

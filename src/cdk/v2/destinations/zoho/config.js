6a1226025927ff7efe6987bcf726f180ee3adfe8
Alexandros Milaios <alexmilaios@gmail.com>
2025-03-21 10:29:45 +0200

    chore: remove unused config

--- src/cdk/v2/destinations/zoho/config.js
+++ src/cdk/v2/destinations/zoho/config.js
@@ -1,15 +1,3 @@
-// https://www.zoho.com/crm/developer/docs/api/v6/access-refresh.html
-const DATA_CENTRE_BASE_ENDPOINTS_MAP = {
-  US: 'https://www.zohoapis.com',
-  AU: 'https://www.zohoapis.com.au',
-  EU: 'https://www.zohoapis.eu',
-  IN: 'https://www.zohoapis.in',
-  CN: 'https://www.zohoapis.com.cn',
-  JP: 'https://www.zohoapis.jp',
-  CA: 'https://www.zohoapiscloud.ca',
-};
-
 module.exports = {
   MAX_BATCH_SIZE: 100,
-  DATA_CENTRE_BASE_ENDPOINTS_MAP,
 };

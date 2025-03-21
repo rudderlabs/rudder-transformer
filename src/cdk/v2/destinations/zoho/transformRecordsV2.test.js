7f83a76c824d57bb3dcb6150cb80163cf34e862d
Alexandros Milaios <alexmilaios@gmail.com>
2025-03-21 09:49:53 +0200

    chore: fix tests

--- src/cdk/v2/destinations/zoho/transformRecordsV2.test.js
+++ src/cdk/v2/destinations/zoho/transformRecordsV2.test.js
@@ -1,13 +1,13 @@
-const { processRecordInputsV2 } = require('./transformRecordV2');
+const { processRecordInputs } = require('./transformRecordV2');
 
-describe('processRecordInputsV2', () => {
+describe('processRecordInputs', () => {
   it('should return an empty array if no inputs are provided', async () => {
-    const result = await processRecordInputsV2([]);
+    const result = await processRecordInputs([]);
     expect(result).toEqual([]);
   });
 
   it('should return an empty array if no destination is provided', async () => {
-    const result = await processRecordInputsV2(
+    const result = await processRecordInputs(
       [
         {
           id: '1',
@@ -21,7 +21,7 @@ describe('processRecordInputsV2', () => {
   });
 
   it('should return an empty array if no destination config is provided', async () => {
-    const result = await processRecordInputsV2([], {
+    const result = await processRecordInputs([], {
       destination: {
         config: {},
       },
@@ -31,7 +31,7 @@ describe('processRecordInputsV2', () => {
 
   it('should return an empty array if no connection is provided', async () => {
     await expect(
-      processRecordInputsV2(
+      processRecordInputs(
         [
           {
             id: '1',
@@ -52,7 +52,7 @@ describe('processRecordInputsV2', () => {
 
   it('should return an empty array if no connection destination config is provided', async () => {
     await expect(
-      processRecordInputsV2(
+      processRecordInputs(
         [
           {
             id: '1',
@@ -74,7 +74,7 @@ describe('processRecordInputsV2', () => {
 
   it('should return an empty array if no object and identifierMappings are provided in destination config', async () => {
     await expect(
-      processRecordInputsV2(
+      processRecordInputs(
         [
           {
             id: '1',

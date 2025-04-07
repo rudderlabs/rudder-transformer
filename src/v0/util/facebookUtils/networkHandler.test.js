7afa34ecfd3284ded994f68c9fefa0505155897a
Mahesh Kutty <bosskutty17@gmail.com>
2025-04-07 17:58:53 +0530

    fix: handle fb_custom_audience oauth error

---
+++ src/v0/util/facebookUtils/networkHandler.test.js
@@ -0,0 +1,262 @@
+const { NetworkError, ConfigurationAuthError } = require('@rudderstack/integrations-lib');
+
+const { errorResponseHandler } = require('./networkHandler');
+
+describe('errorResponseHandler', () => {
+  it('should return successfully when there is no error in the response', () => {
+    const destResponse = {
+      response: {
+        error: null,
+      },
+    };
+
+    expect(() => errorResponseHandler(destResponse)).not.toThrow();
+  });
+
+  it('should throw a ConfigurationAuthError when the error type is AUTH', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 190,
+          error_subcode: 463,
+          error_user_msg: 'Invalid OAuth access token.',
+        },
+      },
+      status: 401,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ConfigurationAuthError);
+      expect(e.message).toBe('Invalid OAuth access token.');
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a NetworkError with appropriate status and message when error details are found', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 100,
+          error_subcode: 2804003,
+          error_user_title: 'Event sent after seven days.',
+        },
+      },
+      status: 400,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe('Event sent after seven days.');
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a NetworkError with default message when error details are not found', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 999,
+          error_subcode: 9999,
+        },
+      },
+      status: 500,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe(
+        JSON.stringify({
+          code: 999,
+          error_subcode: 9999,
+        }),
+      );
+      expect(e.status).toBe(500);
+    }
+  });
+
+  it('should throw a NetworkError with dynamic error type when error details are found', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 4,
+          error_user_msg: 'Rate limit exceeded.',
+        },
+      },
+      status: 429,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe('Rate limit exceeded.');
+      expect(e.status).toBe(429);
+    }
+  });
+
+  it('should throw a ConfigurationAuthError when the error type is AUTH and error details are present', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 190,
+          error_subcode: 460,
+          error_user_msg: 'Invalid OAuth token.',
+        },
+      },
+      status: 401,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ConfigurationAuthError);
+      expect(e.message).toBe('Invalid OAuth token.');
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a ConfigurationAuthError when the error type is AUTH and message is present', () => {
+    const destResponse = {
+      response: {
+        error: {
+          message:
+            'The token has expired on Saturday, 23-Sep-23 23:29:14 PDT. The current time is Monday, 07-Apr-25 03:48:44 PDT.',
+          type: 'OAuthException',
+          code: 190,
+          fbtrace_id: 'AxFa_0JxfQ4sctzqEQisyeJ',
+        },
+      },
+      status: 401,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ConfigurationAuthError);
+      expect(e.message).toBe(
+        'The token has expired on Saturday, 23-Sep-23 23:29:14 PDT. The current time is Monday, 07-Apr-25 03:48:44 PDT.',
+      );
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a ConfigurationAuthError when the error type is AUTH and message is not present', () => {
+    const destResponse = {
+      response: {
+        error: {
+          message: '',
+          type: 'OAuthException',
+          code: 190,
+          fbtrace_id: 'AxFa_0JxfQ4sctzqEQisyeJ',
+        },
+      },
+      status: 401,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(ConfigurationAuthError);
+      expect(e.message).toBe('Unknown auth error during response transformation');
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a NetworkError with error object as stringified', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 100,
+          error_subcode: 9999,
+        },
+      },
+      status: 400,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe('Unknown failure during response transformation');
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a NetworkError with a custom message when error details are fully defined', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 1487301,
+          error_user_msg: 'Custom Audience Unavailable.',
+        },
+      },
+      status: 400,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe(
+        'Custom Audience Unavailable: The custom audience you are trying to use has not been shared with your ad account',
+      );
+      expect(e.status).toBe(400);
+    }
+  });
+
+  it('should throw a NetworkError with a rate limit message when error code indicates rate limiting', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 17,
+          error_user_msg: 'Rate limit exceeded.',
+        },
+      },
+      status: 429,
+    };
+
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe('Rate limit exceeded.');
+      expect(e.status).toBe(429);
+    }
+  });
+
+  it('should throw a NetworkError with a generic message when error code is unhandled', () => {
+    const destResponse = {
+      response: {
+        error: {
+          code: 9999,
+          error_user_msg: 'Unhandled error.',
+        },
+      },
+      status: 500,
+    };
+    expect.assertions(3);
+    try {
+      errorResponseHandler(destResponse);
+    } catch (e) {
+      expect(e).toBeInstanceOf(NetworkError);
+      expect(e.message).toBe('Unhandled error.');
+      expect(e.status).toBe(500);
+    }
+  });
+});

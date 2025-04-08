14ac7075b7bcc395fd927fc52a5f9d389940b36b
ThabetIbrahim <thabet.ig@gmail.com>
2025-04-08 09:04:45 +0200

    feat: onabord userpilot destination
    * Userpilot - Rudderstack transformer
    
    * fix: user id is not required & include `v1` in the path rather than DEFAULT_BASE_URL
    
    * test: remove v1 from apiEndpoint

---
+++ src/cdk/v2/destinations/userpilot/utils.js
@@ -0,0 +1,23 @@
+const { DEFAULT_BASE_URL, MAPPINGS } = require('./config');
+
+const getEndpoints = (config) => {
+  const baseUrl = config.apiEndpoint || DEFAULT_BASE_URL;
+  const normalizedBaseUrl = baseUrl.endsWith('/') ? baseUrl.slice(0, -1) : baseUrl;
+
+  return {
+    IDENTIFY: `${normalizedBaseUrl}/v1/identify`,
+    TRACK: `${normalizedBaseUrl}/v1/track`,
+    GROUP: `${normalizedBaseUrl}/v1/companies/identify`,
+  };
+};
+
+const transformTraits = (traits) =>
+  Object.keys(traits).reduce((acc, key) => {
+    acc[MAPPINGS[key] || key] = traits[key];
+    return acc;
+  }, {});
+
+module.exports = {
+  transformTraits,
+  getEndpoints,
+};

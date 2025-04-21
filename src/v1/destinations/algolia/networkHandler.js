b464a4e32e379e5078a61f22882feab46327bb85
Mahesh Kutty <bosskutty17@gmail.com>
2025-04-21 13:42:46 +0530

    refactor: algolia network handler

--- src/v1/destinations/algolia/networkHandler.js
+++ src/v1/destinations/algolia/networkHandler.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-restricted-syntax */
 const { TransformerProxyError } = require('../../../v0/util/errorTypes');
 const { prepareProxyRequest, proxyRequest } = require('../../../adapters/network');
 const { isHttpStatusSuccess } = require('../../../v0/util/index');
@@ -12,58 +11,40 @@ const tags = require('../../../v0/util/tags');
 const responseHandler = (responseParams) => {
   const { destinationResponse, rudderJobMetadata } = responseParams;
   const message = `[ALGOLIA Response V1 Handler] - Request Processed Successfully`;
-  const responseWithIndividualEvents = [];
   const { response, status } = destinationResponse;
 
   if (isHttpStatusSuccess(status)) {
-    for (const mData of rudderJobMetadata) {
-      const proxyOutputObj = {
-        statusCode: 200,
-        metadata: mData,
-        error: 'success',
-      };
-      responseWithIndividualEvents.push(proxyOutputObj);
-    }
-
     return {
       status,
       message,
       destinationResponse,
-      response: responseWithIndividualEvents,
+      response: rudderJobMetadata.map((metadata) => ({
+        statusCode: 200,
+        metadata,
+        error: 'success',
+      })),
     };
   }
 
   // in case of non 2xx status sending 500 for every event, populate response and update dontBatch to true
-  const errorMessage = response?.error?.message || response?.message || 'unknown error format';
-  for (const metadata of rudderJobMetadata) {
+  const responseWithIndividualEvents = rudderJobMetadata.map((metadata) => {
+    // eslint-disable-next-line no-param-reassign
     metadata.dontBatch = true;
-    responseWithIndividualEvents.push({
+    return {
       statusCode: 500,
-      metadata,
-      error: errorMessage,
-    });
-  }
+      metadata: { ...metadata, dontBatch: true },
+      error: response?.error?.message || response?.message || 'unknown error format',
+    };
+  });
 
-  // At least one event in the batch is invalid.
-  if (status === 422) {
-    // sending back 500 for retry
-    throw new TransformerProxyError(
-      `ALGOLIA: Error transformer proxy v1 during ALGOLIA response transformation`,
-      500,
-      {
-        [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(500),
-      },
-      destinationResponse,
-      '',
-      responseWithIndividualEvents,
-    );
-  }
-
+  // At least one event in the batch is invalid in case of 422
+  // sending back 500 for retry
+  const newStatus = status === 422 ? 500 : status;
   throw new TransformerProxyError(
     `ALGOLIA: Error transformer proxy v1 during ALGOLIA response transformation`,
-    status,
+    newStatus,
     {
-      [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(status),
+      [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(newStatus),
     },
     destinationResponse,
     '',

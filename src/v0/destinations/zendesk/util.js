c21ef549ce7cc5e96546eae7910b26bb1a499b9b
Manish Kumar <144022547+manish339k@users.noreply.github.com>
2025-05-05 14:22:03 +0530

    fix: zendesk indentify flow (#4288)
    * fix: zendesk primary email update issue
    
    * chore: minor changes
    
    * chore: added test for updating secondary email
    
    * chore: minor change
    
    * chore: added util test
    
    * chore: minor change
    
    * chore: minor change
    
    * chore: undo minor change
    
    * chore: fix sonarqube code smell
    
    * fix: addressing comments
    
    * chore: addressing comments

--- src/v0/destinations/zendesk/util.js
+++ src/v0/destinations/zendesk/util.js
@@ -1,4 +1,10 @@
-const { ConfigurationError } = require('@rudderstack/integrations-lib');
+const { ConfigurationError, NetworkError } = require('@rudderstack/integrations-lib');
+const { handleHttpRequest } = require('../../../adapters/network');
+const { isHttpStatusSuccess, getEventType } = require('../../util');
+const tags = require('../../util/tags');
+const { getDynamicErrorType } = require('../../../adapters/utils/networkUtils');
+const { HTTP_STATUS_CODES } = require('../../util/constant');
+const { EventType } = require('../../../constants');
 
 /**
  * Get source name from config or return 'Rudder' as default source name
@@ -13,4 +19,179 @@ const getSourceName = (config) => {
   return sourceName || 'Rudder';
 };
 
-module.exports = { getSourceName };
+const getStatusCode = ({ message }) =>
+  getEventType(message) === EventType.IDENTIFY
+    ? HTTP_STATUS_CODES.SUPPRESS_EVENTS
+    : HTTP_STATUS_CODES.OK;
+
+const getUserIdentities = async (endpoint, headers, metadata) => {
+  const statTags = {
+    metadata,
+    destType: 'zendesk',
+    endpointPath: 'users/userId/identities',
+    feature: 'transformation',
+    requestMethod: 'GET',
+    module: 'router',
+  };
+
+  const { processedResponse: response } = await handleHttpRequest(
+    'GET',
+    endpoint,
+    {
+      headers,
+    },
+    statTags,
+  );
+
+  if (isHttpStatusSuccess(response.status)) {
+    const { identities } = response?.response || {};
+    return identities;
+  }
+
+  throw new NetworkError(
+    '[Zendesk]: unable to get user identities',
+    response.status,
+    {
+      [tags]: getDynamicErrorType(response.status),
+    },
+    response.response,
+  );
+};
+
+const deleteEmailFromUser = async (endpoint, headers, metadata) => {
+  const statTags = {
+    metadata,
+    destType: 'zendesk',
+    endpointPath: '/users',
+    feature: 'transformation',
+    requestMethod: 'DELETE',
+    module: 'router',
+  };
+
+  const { processedResponse: response } = await handleHttpRequest(
+    'DELETE',
+    endpoint,
+    {
+      headers,
+    },
+    statTags,
+  );
+
+  if (!isHttpStatusSuccess(response.status)) {
+    throw new NetworkError(
+      '[Zendesk]: unable to remove email from user',
+      response.status,
+      {
+        [tags]: getDynamicErrorType(response.status),
+      },
+      response.response,
+    );
+  }
+};
+
+const updatePrimaryEmailOfUser = async (endpoint, payload, headers, metadata) => {
+  const statTags = {
+    metadata,
+    destType: 'zendesk',
+    endpointPath: '/users',
+    feature: 'transformation',
+    requestMethod: 'PUT',
+    module: 'router',
+  };
+
+  const { processedResponse: response } = await handleHttpRequest(
+    'PUT',
+    endpoint,
+    payload,
+    {
+      headers,
+    },
+    statTags,
+  );
+
+  if (!isHttpStatusSuccess(response.status)) {
+    throw new NetworkError(
+      '[Zendesk]: unable to update primary email of the user',
+      response.status,
+      {
+        [tags]: getDynamicErrorType(response.status),
+      },
+      response.response,
+    );
+  }
+};
+
+const createOrUpdateUser = async (payload, endpoint, headers, metadata) => {
+  const statTags = {
+    metadata,
+    destType: 'zendesk',
+    endpointPath: '/create_or_update.json',
+    feature: 'transformation',
+    requestMethod: 'POST',
+    module: 'router',
+  };
+
+  const { processedResponse: response } = await handleHttpRequest(
+    'POST',
+    endpoint,
+    payload,
+    {
+      headers,
+    },
+    statTags,
+  );
+
+  if (isHttpStatusSuccess(response.status)) {
+    return response.response?.user?.id;
+  }
+
+  throw new NetworkError(
+    '[Zendesk]: unable to create or update user',
+    response.status,
+    {
+      [tags]: getDynamicErrorType(response.status),
+    },
+    response.response,
+  );
+};
+
+const removeUserFromOrganizationMembership = async (endpoint, headers, metadata) => {
+  const statTags = {
+    metadata,
+    destType: 'zendesk',
+    endpointPath: '/membershipId.json',
+    feature: 'transformation',
+    requestMethod: 'DELETE',
+    module: 'router',
+  };
+
+  const { processedResponse: response } = await handleHttpRequest(
+    'DELETE',
+    endpoint,
+    {
+      headers,
+    },
+    statTags,
+  );
+
+  if (!isHttpStatusSuccess(response.status)) {
+    throw new NetworkError(
+      '[Zendesk]: unable to remove user from organization',
+      response.status,
+      {
+        [tags]: getDynamicErrorType(response.status),
+      },
+      response.response,
+    );
+  }
+};
+
+module.exports = {
+  getSourceName,
+  getStatusCode,
+  getUserIdentities,
+  deleteEmailFromUser,
+  updatePrimaryEmailOfUser,
+  createOrUpdateUser,
+  removeUserFromOrganizationMembership,
+};

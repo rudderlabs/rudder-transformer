c8b277be73b530ff00c2749d927da1a230ab4fab
Mahesh Kutty <bosskutty17@gmail.com>
2025-05-06 17:41:37 +0530

    chore: misc

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,12 +1,12 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']],
   },
 };
 
-// export default {
-//   extends: [config],
+// module.exports = {
+//   extends: ['@commitlint/config-conventional'],
 //   rules: {
 //     'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']],
 //   },

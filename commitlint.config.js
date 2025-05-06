6ff439d013e189cba47473e4b02f86055bea7c2c
Mahesh Kutty <bosskutty17@gmail.com>
2025-05-06 13:39:21 +0530

    chore: ignore dependabot pr for commitlint check

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,4 +3,7 @@ module.exports = {
   rules: {
     'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']],
   },
+  // We need this until https://github.com/dependabot/dependabot-core/issues/2445
+  // is resolved.
+  ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
 };

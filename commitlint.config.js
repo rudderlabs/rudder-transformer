8f9d36b084eb4a9aa9f43e0789c1fcd3077d8f78
Mahesh Kutty <bosskutty17@gmail.com>
2025-05-07 20:12:41 +0530

    chore: increse body line length

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,7 +4,4 @@ module.exports = {
     'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']],
     'body-max-line-length': () => [2, 'always', 500],
   },
-  // We need this until https://github.com/dependabot/dependabot-core/issues/2445
-  // is resolved.
-  ignores: [(msg) => /Signed-off-by: dependabot\[bot]/m.test(msg)],
 };

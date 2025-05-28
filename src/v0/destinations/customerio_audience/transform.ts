af6e5802908e3ad1570ad99c3c880e4ff1fc911c
Utsab Chowdhury <utsab.c97@gmail.com>
2025-05-28 13:40:34 +0530

    Revert "Merge branch 'develop' into main"
    This reverts commit c684b65ee2260490ce6948ecb9152b550df58ff6, reversing
    changes made to 9be968a8751773d9a7d5978742aff10f13e54242.

--- src/v0/destinations/customerio_audience/transform.ts
+++ src/v0/destinations/customerio_audience/transform.ts
@@ -1,10 +1,10 @@
 import {
+  SegmentAction,
   CustomerIOConnection,
   CustomerIODestination,
   CustomerIORouterRequest,
   ProcessedEvent,
 } from './type';
-import { RecordAction } from '../../../types/rudderEvents';
 import { batchResponseBuilder, createEventChunk } from './utils';
 import { handleRtTfSingleEventError } from '../../util';
 
@@ -42,11 +42,11 @@ const processRouterDest = async (inputs: CustomerIORouterRequest[], reqMetadata:
 
   // Split successful events into delete and insert/update lists
   const deleteRespList = successfulEvents
-    .filter((event) => event.eventAction === RecordAction.DELETE)
+    .filter((event) => event.eventAction === SegmentAction.DELETE)
     .map(({ payload, metadata }) => ({ payload, metadata }));
 
   const insertOrUpdateRespList = successfulEvents
-    .filter((event) => event.eventAction !== RecordAction.DELETE)
+    .filter((event) => event.eventAction !== SegmentAction.DELETE)
     .map(({ payload, metadata }) => ({ payload, metadata }));
 
   const batchSuccessfulRespList = batchResponseBuilder(

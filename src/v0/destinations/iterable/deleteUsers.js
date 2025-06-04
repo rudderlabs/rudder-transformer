cef916562cac5c59e062f992f40434f6fc5c6c5c
Dilip Kola <dilipkola@rudderstack.com>
2025-06-04 06:43:26 +0530

    refactor: use forEach in batches for iterable delete users

--- src/v0/destinations/iterable/deleteUsers.js
+++ src/v0/destinations/iterable/deleteUsers.js
@@ -1,4 +1,8 @@
-const { NetworkError, ConfigurationError } = require('@rudderstack/integrations-lib');
+const {
+  NetworkError,
+  ConfigurationError,
+  forEachInBatches,
+} = require('@rudderstack/integrations-lib');
 const { httpDELETE } = require('../../../adapters/network');
 const { processAxiosResponse } = require('../../../adapters/utils/networkUtils');
 const { isHttpStatusSuccess } = require('../../util');
@@ -25,43 +29,48 @@ const userDeletionHandler = async (userAttributes, config) => {
     }
   });
   const failedUserDeletions = [];
-  // eslint-disable-next-line no-restricted-syntax
-  for (const uId of validUserIds) {
-    const endpointCategory = { endpoint: `users/byUserId/${uId}` };
-    const url = constructEndpoint(dataCenter, endpointCategory);
-    const requestOptions = {
-      headers: {
-        'Content-Type': JSON_MIME_TYPE,
-        api_key: apiKey,
-      },
-    };
-    // eslint-disable-next-line no-await-in-loop
-    const resp = await httpDELETE(url, requestOptions, {
-      destType: 'iterable',
-      feature: 'deleteUsers',
-      endpointPath: '/users/byUserId/uId',
-      requestMethod: 'DELETE',
-      module: 'deletion',
-    });
-    const handledDelResponse = processAxiosResponse(resp);
-    if (!isHttpStatusSuccess(handledDelResponse.status) && handledDelResponse.status !== 404) {
-      if (handledDelResponse.status !== 400) {
-        // Generic errors such as invalid api key
-        throw new NetworkError(
-          `User deletion request failed : ${handledDelResponse.response.msg}`,
-          handledDelResponse.status,
-          {
-            [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(handledDelResponse.status),
-            [tags.TAG_NAMES.STATUS]: handledDelResponse.status,
-          },
-          handledDelResponse,
-        );
-      } else {
-        // Specific errors such as user is not found
-        failedUserDeletions.push({ userId: uId, Reason: handledDelResponse.response.msg });
+  await forEachInBatches(
+    validUserIds,
+    async (uId) => {
+      const endpointCategory = { endpoint: `users/byUserId/${uId}` };
+      const url = constructEndpoint(dataCenter, endpointCategory);
+      const requestOptions = {
+        headers: {
+          'Content-Type': JSON_MIME_TYPE,
+          api_key: apiKey,
+        },
+      };
+      const resp = await httpDELETE(url, requestOptions, {
+        destType: 'iterable',
+        feature: 'deleteUsers',
+        endpointPath: '/users/byUserId/uId',
+        requestMethod: 'DELETE',
+        module: 'deletion',
+      });
+      const handledDelResponse = processAxiosResponse(resp);
+      if (!isHttpStatusSuccess(handledDelResponse.status) && handledDelResponse.status !== 404) {
+        if (handledDelResponse.status !== 400) {
+          // Generic errors such as invalid api key
+          throw new NetworkError(
+            `User deletion request failed : ${handledDelResponse.response.msg}`,
+            handledDelResponse.status,
+            {
+              [tags.TAG_NAMES.ERROR_TYPE]: getDynamicErrorType(handledDelResponse.status),
+              [tags.TAG_NAMES.STATUS]: handledDelResponse.status,
+            },
+            handledDelResponse,
+          );
+        } else {
+          // Specific errors such as user is not found
+          failedUserDeletions.push({ userId: uId, Reason: handledDelResponse.response.msg });
+        }
       }
-    }
-  }
+    },
+    {
+      batchSize: 10,
+      sequentialProcessing: false,
+    },
+  );
 
   if (failedUserDeletions.length > 0) {
     throw new NetworkError(

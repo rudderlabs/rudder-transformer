798f6517224c24f314aa0458f33ac683a57d36c6
Sudip Paul <sudip@rudderstack.com>
2025-06-23 16:48:34 +0530

    chore: remove zoho_dev destination

--- test/integrations/destinations/zoho/dataDelivery/data.ts
+++ test/integrations/destinations/zoho/dataDelivery/data.ts
@@ -1,4 +1,3 @@
 import { testScenariosForV1API } from './business';
-import { testScenariosForZOHO_DEV } from './zoho_dev';
 
-export const data = [...testScenariosForV1API, ...testScenariosForZOHO_DEV];
+export const data = [...testScenariosForV1API];

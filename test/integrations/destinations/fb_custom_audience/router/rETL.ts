8ae6bf667a2f47bc6fe716d59ae89eb2bcdf106d
Manish Kumar <144022547+manish339k@users.noreply.github.com>
2025-06-27 18:04:49 +0530

    feat: add value based audience to facebook custom audience destination (#4322)
    * feat: onboarding facebook value based audience
    
    * chore: minor change
    
    * fix: minor issue
    
    * fix: addressing comments
    
    * fix: addressing comment
    
    * chore: addressing comments
    
    * fix: sonar issue

--- test/integrations/destinations/fb_custom_audience/router/rETL.ts
+++ test/integrations/destinations/fb_custom_audience/router/rETL.ts
@@ -44,6 +44,17 @@ const connection: Connection = {
   },
 };
 
+const connectionWithValueBasedAudience: Connection = {
+  ...connection,
+  config: {
+    ...connection.config,
+    destination: {
+      ...(connection.config.destination || {}),
+      isValueBasedAudience: true,
+    },
+  },
+};
+
 const missingAudienceIDConnection: Connection = {
   sourceId: '2MUWghI7u85n91dd1qzGyswpZan',
   destinationId: '1mMy5cqbtfuaKZv1IhVQKnBdVwe',
@@ -158,6 +169,239 @@ export const rETLRecordV2RouterInvalidRequest: RouterTransformationRequest = {
     {
       destination: destinationV2,
       connection: missingAudienceIDConnection,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '2',
+        rudderId: '2',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(1),
+    },
+  ],
+  destType: 'fb_custom_audience',
+};
+
+export const rETLRecordV2RouterInvalidRequestWithLookalikeValue: RouterTransformationRequest = {
+  input: [
+    {
+      destination: destinationV2,
+      connection: connection,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '2',
+        rudderId: '2',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: 100,
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(1),
+    },
+  ],
+  destType: 'fb_custom_audience',
+};
+
+export const rETLRecordV2RouterRequestWithValueBasedAudience: RouterTransformationRequest = {
+  input: [
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '2',
+        rudderId: '2',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: 0,
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(1),
+    },
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '2',
+        rudderId: '2',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: 100.1,
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(2),
+    },
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '3',
+        rudderId: '3',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: 100,
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(3),
+    },
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '4',
+        rudderId: '4',
+        identifiers: {
+          EMAIL: null,
+          FI: null,
+          LOOKALIKE_VALUE: 0.1,
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(4),
+    },
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '5',
+        rudderId: '5',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: '100',
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(5),
+    },
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '6',
+        rudderId: '6',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: [{}],
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(6),
+    },
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
+      message: {
+        action: 'insert',
+        context: {
+          sources: {
+            job_run_id: 'cgiiurt8um7k7n5dq480',
+            task_run_id: 'cgiiurt8um7k7n5dq48g',
+            job_id: '2MUWghI7u85n91dd1qzGyswpZan',
+            version: '895/merge',
+          },
+        },
+        recordId: '7',
+        rudderId: '7',
+        identifiers: {
+          EMAIL: 'subscribed@eewrfrd.com',
+          FI: 'ghui',
+          LOOKALIKE_VALUE: {},
+        },
+        type: 'record',
+      },
+      metadata: generateMetadata(7),
+    },
+  ],
+  destType: 'fb_custom_audience',
+};
+
+export const rETLRecordV2RouterInvalidRequestWithValueBasedAudience: RouterTransformationRequest = {
+  input: [
+    {
+      destination: destinationV2,
+      connection: connectionWithValueBasedAudience,
       message: {
         action: 'insert',
         context: {

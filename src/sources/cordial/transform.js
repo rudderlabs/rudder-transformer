b430edb4a75a7539eec056a0e184f5d0a8fc9f36
Mahesh Kutty <39219085+maheshkutty@users.noreply.github.com>
2025-08-11 17:00:31 +0530

    fix(cordial): optional check for cid (#4553)
    * fix(cordial): optional check for cid
    
    * fix(cordial): optional check for cid
    
    * fix: review comments
    
    * fix: review comments
    
    * fix: review comments

--- src/sources/cordial/transform.js
+++ src/sources/cordial/transform.js
@@ -5,22 +5,22 @@ const { eventsMapping } = require('./config');
 
 const mapping = require('./mapping.json');
 
-const processEvent = (inputPaylaod) => {
+const processEvent = (inputPayload) => {
   const message = new Message(`Cordial`);
-  let eventName = inputPaylaod.event?.a || inputPaylaod.event?.action;
+  let eventName = inputPayload.event?.a || inputPayload.event?.action;
   if (eventName in eventsMapping) {
     eventName = eventsMapping[eventName];
   }
   message.setEventType('track');
   message.setEventName(eventName);
-  message.setPropertiesV2(inputPaylaod, mapping);
+  message.setPropertiesV2(inputPayload, mapping);
 
   const externalId = [];
   // setting up cordial contact_id to externalId
-  if (inputPaylaod.contact.cID) {
+  if (inputPayload?.contact?.cID) {
     externalId.push({
       type: 'cordialContactId',
-      id: inputPaylaod.contact.cID,
+      id: inputPayload.contact.cID,
     });
   }
   message.context.externalId = externalId;
@@ -29,11 +29,11 @@ const processEvent = (inputPaylaod) => {
     message.anonymousId = generateUUID();
   }
   // Due to multiple mappings to the same destination path object some are not showing up due to which we are doing the following
-  message.context.traits = { ...message.context.traits, ...inputPaylaod.contact };
+  message.context.traits = { ...message.context.traits, ...(inputPayload?.contact ?? {}) };
   message.properties = {
     ...message.properties,
-    ...inputPaylaod.event.properties,
-    ...inputPaylaod.event,
+    ...(inputPayload?.event?.properties ?? {}),
+    ...(inputPayload?.event ?? {}),
   };
   delete message.properties.properties;
   delete message.properties.d;

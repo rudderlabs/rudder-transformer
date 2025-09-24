71551e66be153f4cd1ad13a7c9b4f5a7e68e3fe1
Mahesh Kutty <39219085+maheshkutty@users.noreply.github.com>
2025-09-24 17:22:40 +0530

    chore: add dub test (#4633)

---
+++ test/integrations/destinations/dub/processor/data.ts
@@ -0,0 +1,5 @@
+import { leadEventTestData } from './leadEventTestData';
+import { saleEventTestData } from './saleEventTestData';
+import { validationTestData } from './validationTestData';
+
+export const data = [...leadEventTestData, ...saleEventTestData, ...validationTestData];

68be0e71164c0cf02ab7265e9be39248d931e5f2
Sudip Paul <sudip.paul1997@gmail.com>
2025-10-13 09:15:02 +0530

    chore: implement router batching flow

--- src/cdk/v2/destinations/reddit/utils.js
+++ src/cdk/v2/destinations/reddit/utils.js
@@ -1,11 +1,21 @@
+const { isDefinedAndNotNull } = require('@rudderstack/integrations-lib');
 const lodash = require('lodash');
-const { maxBatchSize } = require('./config');
+
+const decideVersion = ({ Config }) => {
+  const configVersion = Config.version;
+  let version = 'v2';
+  if (isDefinedAndNotNull(configVersion) && configVersion === 'v3') {
+    version = 'v3';
+  }
+  return version;
+};
 
 const batchEventChunks = (eventChunks) => {
   const batchedEvents = [];
   if (Array.isArray(eventChunks)) {
     eventChunks.forEach((chunk) => {
       const response = { destination: chunk[0].destination };
+      const version = decideVersion(chunk[0].destination);
       chunk.forEach((event, index) => {
         if (index === 0) {
           const [firstMessage] = event.message;
@@ -13,19 +23,17 @@ const batchEventChunks = (eventChunks) => {
           response.destination = event.destination;
           response.metadata = [event.metadata];
         } else {
-          response.message.body.JSON.events.push(...event.message[0].body.JSON.events);
+          if (version === 'v3') {
+            response.message.body.JSON.data.events.push(...event.message[0].body.JSON.data.events);
+          } else {
+            response.message.body.JSON.events.push(...event.message[0].body.JSON.events);
+          }
           response.metadata.push(event.metadata);
         }
       });
       batchedEvents.push(response);
     });
   }
-  return batchedEvents;
-};
-
-const batchEvents = (successfulEvents) => {
-  const eventChunks = lodash.chunk(successfulEvents, maxBatchSize);
-  const batchedEvents = batchEventChunks(eventChunks);
   return batchedEvents;
 };
 
@@ -121,10 +129,10 @@ const convertToUpperSnakeCase = (type) => {
 };
 
 module.exports = {
-  batchEvents,
   batchEventChunks,
   populateRevenueField,
   calculateDefaultRevenue,
   removeUnsupportedFields,
   convertToUpperSnakeCase,
+  decideVersion,
 };

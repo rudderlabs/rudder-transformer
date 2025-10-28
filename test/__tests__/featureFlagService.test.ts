8a7d43024f524c496977fc03c3e9f5f198a3a2b1
Mahesh Kutty <bosskutty17@gmail.com>
2025-10-28 22:32:48 +0530

    chore: misc

--- test/__tests__/featureFlagService.test.ts
+++ test/__tests__/featureFlagService.test.ts
@@ -57,15 +57,15 @@ describe('FeatureFlagService', () => {
           retryAttempts: 3,
           enableAnalytics: true,
         },
-        expect.arrayContaining([
-          expect.objectContaining({
+        [
+          {
             key: 'enable-pinterest-advertising-tracking-mapping',
-            name: 'Pinterest Advertising',
+            name: 'Pinterest Advertising Tracking Mapping',
             description: 'Enable pinterest tag advertising tracking mapping',
             defaultValue: false,
             type: 'boolean',
-          }),
-        ]),
+          },
+        ],
       );
 
       expect(mockLoggerInfo).toHaveBeenCalledWith(

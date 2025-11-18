5f1c4ad908e5d605adb5fa356f9a601535460a29
Utsab Chowdhury <utsab.c97@gmail.com>
2025-11-18 17:19:41 +0530

    chore: update type checks

--- src/v0/destinations/salesforce/utils.js
+++ src/v0/destinations/salesforce/utils.js
@@ -326,7 +326,10 @@ async function getSalesforceIdForRecordUsingSdk(
     );
   } catch (error) {
     // check if the error message contains 'session expired'
-    if (error.message?.toLowerCase().includes('session expired')) {
+    if (
+      typeof error.message === 'string' &&
+      error.message.toLowerCase().includes('session expired')
+    ) {
       throw new RetryableError(
         `${DESTINATION} Request Failed - due to "INVALID_SESSION_ID", (Retryable) ${error.message}`,
         500,
@@ -412,7 +415,10 @@ async function getSalesforceIdForLeadUsingSdk(salesforceSdk, email, destination)
     );
   } catch (error) {
     // check if the error message contains 'session expired'
-    if (error.message?.toLowerCase().includes('session expired')) {
+    if (
+      typeof error.message === 'string' &&
+      error.message.toLowerCase().includes('session expired')
+    ) {
       throw new RetryableError(
         `${DESTINATION} Request Failed - due to "INVALID_SESSION_ID", (Retryable) ${error.message}`,
         500,

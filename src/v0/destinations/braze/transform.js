6aaf84003912809e5068fd8a243607177c6c170e
Sudip Paul <sudip.paul1997@gmail.com>
2025-11-20 18:48:37 +0530

    feat: send external_ids only when userId is present

--- src/v0/destinations/braze/transform.js
+++ src/v0/destinations/braze/transform.js
@@ -375,7 +375,9 @@ function processGroup(message, destination) {
       );
     }
     subscriptionGroup.subscription_state = message.traits.subscriptionState;
-    subscriptionGroup.external_ids = [message.userId];
+    if (message.userId) {
+      subscriptionGroup.external_ids = [message.userId];
+    }
     const phone = getFieldValueFromMessage(message, 'phone');
     const email = getFieldValueFromMessage(message, 'email');
     if (phone) {

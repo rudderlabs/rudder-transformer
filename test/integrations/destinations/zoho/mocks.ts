53b387ea6b595306a13201c3e05c1431604a53b6
Mahesh Kutty <39219085+maheshkutty@users.noreply.github.com>
2025-12-15 14:30:08 +0530

    feat(zoho): do batch lookup of records for deletion (#4855)
    * feat(zoho): do batch lookup of records for deletion
    
    * fix: review comments
    
    * fix: types
    
    * chore: review comments
    
    * chore: review comments
    
    * chore: improve chucking by considering identifiers
    
    * chore: review comments
    
    * chore: review comments
    
    * chore: check env global level
    
    * chore: use jobId instead eventIndex
    
    * chore: review comments
    
    * chore: update chunkByIdentifierLimit
    
    * chore: intialise env at global level
    
    * chore: add errorType to ProcessedCOQLAPIErrorResponse
    
    * chore: process env in separate function
    
    * chore: parse env when module load

--- test/integrations/destinations/zoho/mocks.ts
+++ test/integrations/destinations/zoho/mocks.ts
@@ -3,3 +3,8 @@ import * as config from '../../../../src/cdk/v2/destinations/zoho/config';
 export const defaultMockFns = () => {
   jest.replaceProperty(config, 'MAX_BATCH_SIZE', 2);
 };
+
+export const deletionBatchMock = () => {
+  jest.replaceProperty(config, 'COQL_BATCH_SIZE', 3);
+  jest.replaceProperty(config, 'MAX_BATCH_SIZE', 3);
+};

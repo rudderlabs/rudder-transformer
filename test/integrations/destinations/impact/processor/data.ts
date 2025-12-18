1bf285c54a50b04a1604131b31def33b870e3a4b
Utsab Chowdhury <utsab.c97@gmail.com>
2025-12-18 12:12:33 +0530

    chore:: update tests

--- test/integrations/destinations/impact/processor/data.ts
+++ test/integrations/destinations/impact/processor/data.ts
@@ -2361,4 +2361,544 @@ export const data = [
       },
     },
   },
+  {
+    name: 'impact',
+    description: 'Identify Call with CustomerEmail as number - should not hash',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'identify',
+              sentAt: '2021-01-03T17:02:53.195Z',
+              userId: 'user123',
+              channel: 'web',
+              context: {
+                os: {
+                  name: 'android',
+                  version: '1.12.3',
+                },
+                app: {
+                  name: 'RudderLabs JavaScript SDK',
+                  build: '1.0.0',
+                  version: '1.1.11',
+                  namespace: 'com.rudderlabs.javascript',
+                },
+                traits: {
+                  email: 12345,
+                  phone: '+917836362334',
+                  userId: 'user123',
+                },
+                locale: 'en-US',
+                device: {
+                  token: 'token',
+                  id: 'id',
+                  type: 'ios',
+                },
+                library: {
+                  name: 'RudderLabs JavaScript SDK',
+                  version: '1.1.11',
+                },
+                userAgent:
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+              },
+              rudderId: '8f8fa6b5-8e24-489c-8e22-61f23f2e364f',
+              messageId: '2116ef8c-efc3-4ca4-851b-02ee60dad6ff',
+              anonymousId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+              originalTimestamp: 'NOW',
+            },
+            destination: {
+              Config: {
+                accountSID: secret1,
+                apiKey: secret2,
+                campaignId: '23224',
+                impactAppId: '2323',
+                eventTypeId: '56446',
+                enableEmailHashing: true,
+                rudderToImpactProperty: [
+                  {
+                    from: 'properties.profit',
+                    to: 'Money1',
+                  },
+                ],
+                productsMapping: [
+                  {
+                    from: 'sku',
+                    to: 'ItemSku',
+                  },
+                ],
+                enableIdentifyEvents: true,
+                enablePageEvents: false,
+                enableScreenEvents: false,
+                actionEventNames: [
+                  {
+                    eventName: 'Product Purchased',
+                  },
+                ],
+                installEventNames: [
+                  {
+                    eventName: 'App Installed',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              body: {
+                XML: {},
+                FORM: {
+                  AppVer: '1.1.11',
+                  AppName: 'RudderLabs JavaScript SDK',
+                  DeviceOs: 'ios',
+                  AndroidId: 'id',
+                  EventDate: 'NOW',
+                  UserAgent:
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+                  AppPackage: '1.0.0',
+                  CampaignId: '23224',
+                  CustomerId: 'user123',
+                  PropertyId: '2323',
+                  DeviceOsVer: '1.12.3',
+                  ImpactAppId: '2323',
+                  DeviceLocale: 'en-US',
+                  CustomerEmail: 12345,
+                  CustomProfileId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+                },
+                JSON: {},
+                JSON_ARRAY: {},
+              },
+              type: 'REST',
+              files: {},
+              method: 'POST',
+              params: {},
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: authHeader1,
+              },
+              version: '1',
+              endpoint: 'https://trkapi.impact.com/PageLoad',
+              userId: '',
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'impact',
+    description: 'Identify Call with CustomerEmail as object - should not hash',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'identify',
+              sentAt: '2021-01-03T17:02:53.195Z',
+              userId: 'user123',
+              channel: 'web',
+              context: {
+                os: {
+                  name: 'android',
+                  version: '1.12.3',
+                },
+                app: {
+                  name: 'RudderLabs JavaScript SDK',
+                  build: '1.0.0',
+                  version: '1.1.11',
+                  namespace: 'com.rudderlabs.javascript',
+                },
+                traits: {
+                  email: { primary: 'user123@email.com', secondary: 'user456@email.com' },
+                  phone: '+917836362334',
+                  userId: 'user123',
+                },
+                locale: 'en-US',
+                device: {
+                  token: 'token',
+                  id: 'id',
+                  type: 'ios',
+                },
+                library: {
+                  name: 'RudderLabs JavaScript SDK',
+                  version: '1.1.11',
+                },
+                userAgent:
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+              },
+              rudderId: '8f8fa6b5-8e24-489c-8e22-61f23f2e364f',
+              messageId: '2116ef8c-efc3-4ca4-851b-02ee60dad6ff',
+              anonymousId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+              originalTimestamp: 'NOW',
+            },
+            destination: {
+              Config: {
+                accountSID: secret1,
+                apiKey: secret2,
+                campaignId: '23224',
+                impactAppId: '2323',
+                eventTypeId: '56446',
+                enableEmailHashing: true,
+                rudderToImpactProperty: [
+                  {
+                    from: 'properties.profit',
+                    to: 'Money1',
+                  },
+                ],
+                productsMapping: [
+                  {
+                    from: 'sku',
+                    to: 'ItemSku',
+                  },
+                ],
+                enableIdentifyEvents: true,
+                enablePageEvents: false,
+                enableScreenEvents: false,
+                actionEventNames: [
+                  {
+                    eventName: 'Product Purchased',
+                  },
+                ],
+                installEventNames: [
+                  {
+                    eventName: 'App Installed',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              body: {
+                XML: {},
+                FORM: {
+                  AppVer: '1.1.11',
+                  AppName: 'RudderLabs JavaScript SDK',
+                  DeviceOs: 'ios',
+                  AndroidId: 'id',
+                  EventDate: 'NOW',
+                  UserAgent:
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+                  AppPackage: '1.0.0',
+                  CampaignId: '23224',
+                  CustomerId: 'user123',
+                  PropertyId: '2323',
+                  DeviceOsVer: '1.12.3',
+                  ImpactAppId: '2323',
+                  DeviceLocale: 'en-US',
+                  CustomerEmail: { primary: 'user123@email.com', secondary: 'user456@email.com' },
+                  CustomProfileId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+                },
+                JSON: {},
+                JSON_ARRAY: {},
+              },
+              type: 'REST',
+              files: {},
+              method: 'POST',
+              params: {},
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: authHeader1,
+              },
+              version: '1',
+              endpoint: 'https://trkapi.impact.com/PageLoad',
+              userId: '',
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'impact',
+    description: 'Identify Call with CustomerEmail as array - should not hash',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'identify',
+              sentAt: '2021-01-03T17:02:53.195Z',
+              userId: 'user123',
+              channel: 'web',
+              context: {
+                os: {
+                  name: 'android',
+                  version: '1.12.3',
+                },
+                app: {
+                  name: 'RudderLabs JavaScript SDK',
+                  build: '1.0.0',
+                  version: '1.1.11',
+                  namespace: 'com.rudderlabs.javascript',
+                },
+                traits: {
+                  email: ['user123@email.com', 'user456@email.com'],
+                  phone: '+917836362334',
+                  userId: 'user123',
+                },
+                locale: 'en-US',
+                device: {
+                  token: 'token',
+                  id: 'id',
+                  type: 'ios',
+                },
+                library: {
+                  name: 'RudderLabs JavaScript SDK',
+                  version: '1.1.11',
+                },
+                userAgent:
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+              },
+              rudderId: '8f8fa6b5-8e24-489c-8e22-61f23f2e364f',
+              messageId: '2116ef8c-efc3-4ca4-851b-02ee60dad6ff',
+              anonymousId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+              originalTimestamp: 'NOW',
+            },
+            destination: {
+              Config: {
+                accountSID: secret1,
+                apiKey: secret2,
+                campaignId: '23224',
+                impactAppId: '2323',
+                eventTypeId: '56446',
+                enableEmailHashing: true,
+                rudderToImpactProperty: [
+                  {
+                    from: 'properties.profit',
+                    to: 'Money1',
+                  },
+                ],
+                productsMapping: [
+                  {
+                    from: 'sku',
+                    to: 'ItemSku',
+                  },
+                ],
+                enableIdentifyEvents: true,
+                enablePageEvents: false,
+                enableScreenEvents: false,
+                actionEventNames: [
+                  {
+                    eventName: 'Product Purchased',
+                  },
+                ],
+                installEventNames: [
+                  {
+                    eventName: 'App Installed',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              body: {
+                XML: {},
+                FORM: {
+                  AppVer: '1.1.11',
+                  AppName: 'RudderLabs JavaScript SDK',
+                  DeviceOs: 'ios',
+                  AndroidId: 'id',
+                  EventDate: 'NOW',
+                  UserAgent:
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+                  AppPackage: '1.0.0',
+                  CampaignId: '23224',
+                  CustomerId: 'user123',
+                  PropertyId: '2323',
+                  DeviceOsVer: '1.12.3',
+                  ImpactAppId: '2323',
+                  DeviceLocale: 'en-US',
+                  CustomerEmail: ['user123@email.com', 'user456@email.com'],
+                  CustomProfileId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+                },
+                JSON: {},
+                JSON_ARRAY: {},
+              },
+              type: 'REST',
+              files: {},
+              method: 'POST',
+              params: {},
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: authHeader1,
+              },
+              version: '1',
+              endpoint: 'https://trkapi.impact.com/PageLoad',
+              userId: '',
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
+  {
+    name: 'impact',
+    description: 'Identify Call with CustomerEmail as boolean - should not hash',
+    feature: 'processor',
+    module: 'destination',
+    version: 'v0',
+    input: {
+      request: {
+        body: [
+          {
+            message: {
+              type: 'identify',
+              sentAt: '2021-01-03T17:02:53.195Z',
+              userId: 'user123',
+              channel: 'web',
+              context: {
+                os: {
+                  name: 'android',
+                  version: '1.12.3',
+                },
+                app: {
+                  name: 'RudderLabs JavaScript SDK',
+                  build: '1.0.0',
+                  version: '1.1.11',
+                  namespace: 'com.rudderlabs.javascript',
+                },
+                traits: {
+                  email: true,
+                  phone: '+917836362334',
+                  userId: 'user123',
+                },
+                locale: 'en-US',
+                device: {
+                  token: 'token',
+                  id: 'id',
+                  type: 'ios',
+                },
+                library: {
+                  name: 'RudderLabs JavaScript SDK',
+                  version: '1.1.11',
+                },
+                userAgent:
+                  'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+              },
+              rudderId: '8f8fa6b5-8e24-489c-8e22-61f23f2e364f',
+              messageId: '2116ef8c-efc3-4ca4-851b-02ee60dad6ff',
+              anonymousId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+              originalTimestamp: 'NOW',
+            },
+            destination: {
+              Config: {
+                accountSID: secret1,
+                apiKey: secret2,
+                campaignId: '23224',
+                impactAppId: '2323',
+                eventTypeId: '56446',
+                enableEmailHashing: true,
+                rudderToImpactProperty: [
+                  {
+                    from: 'properties.profit',
+                    to: 'Money1',
+                  },
+                ],
+                productsMapping: [
+                  {
+                    from: 'sku',
+                    to: 'ItemSku',
+                  },
+                ],
+                enableIdentifyEvents: true,
+                enablePageEvents: false,
+                enableScreenEvents: false,
+                actionEventNames: [
+                  {
+                    eventName: 'Product Purchased',
+                  },
+                ],
+                installEventNames: [
+                  {
+                    eventName: 'App Installed',
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+    },
+    output: {
+      response: {
+        status: 200,
+        body: [
+          {
+            output: {
+              body: {
+                XML: {},
+                FORM: {
+                  AppVer: '1.1.11',
+                  AppName: 'RudderLabs JavaScript SDK',
+                  DeviceOs: 'ios',
+                  AndroidId: 'id',
+                  EventDate: 'NOW',
+                  UserAgent:
+                    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.16; rv:84.0) Gecko/20100101 Firefox/84.0',
+                  AppPackage: '1.0.0',
+                  CampaignId: '23224',
+                  CustomerId: 'user123',
+                  PropertyId: '2323',
+                  DeviceOsVer: '1.12.3',
+                  ImpactAppId: '2323',
+                  DeviceLocale: 'en-US',
+                  CustomerEmail: true,
+                  CustomProfileId: '97c46c81-3140-456d-b2a9-690d70aaca35',
+                },
+                JSON: {},
+                JSON_ARRAY: {},
+              },
+              type: 'REST',
+              files: {},
+              method: 'POST',
+              params: {},
+              headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+                Authorization: authHeader1,
+              },
+              version: '1',
+              endpoint: 'https://trkapi.impact.com/PageLoad',
+              userId: '',
+            },
+            statusCode: 200,
+          },
+        ],
+      },
+    },
+  },
 ];

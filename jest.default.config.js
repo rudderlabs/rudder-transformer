7317135c404c531d186eecaa1e82bb03d56fd143
shekhar-rudder <85345786+shekhar-rudder@users.noreply.github.com>
2026-02-04 12:47:37 +0530

    chore: add user deletion error logs (#4936)

--- jest.default.config.js
+++ jest.default.config.js
@@ -175,9 +175,15 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: {
-  //   '^.+\\.(ts|tsx)$': 'ts-jest',
-  // },
+  transform: {
+    '^.+\\.[tj]sx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+        diagnostics: true,
+      },
+    ],
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
--- src/v0/util/errorTypes/index.js
+++ src/v0/util/errorTypes/index.js
@@ -1,7 +1,9 @@
 const FilteredEventsError = require('./filteredEventsError');
 const TransformerProxyError = require('./transformerProxyError');
+const { DeleteUsersError } = require('./deleteUsersError');
 
 module.exports = {
+  DeleteUsersError,
   FilteredEventsError,
   TransformerProxyError,
 };

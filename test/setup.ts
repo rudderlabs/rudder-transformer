d537aebf6d4a6e023378cba00e6a6b50d9547fd8
Sudip Paul <sudip.paul1997@gmail.com>
2026-02-10 08:52:09 +0530

    fix: update setup.ts to add salesforceSdkMock always

--- test/setup.ts
+++ test/setup.ts
@@ -4,13 +4,5 @@ process.env.DEST_ZOHO_DELETION_BATCHING_SUPPORTED_WORKSPACE_IDS = 'workspaceId2'
 process.env.DEST_BRAZE_MAU_WORKSPACE_IDS_SKIP_LIST = 'workspace-non-mau';
 process.env.DISABLE_TIKTOK_AUDIENCE_CDK_V2 = 'workspace-disable-cdkv2';
 
-// Load Salesforce SDK mock before app loads when running Salesforce component tests
-// so SOQL lookups use the mock and router tests pass (pattern: mock must be in place before first require of integrations-lib)
-const isSalesforceDestinationTest = process.argv.some(
-  (arg) => arg === '--destination=salesforce' || arg.includes('destination=salesforce'),
-);
-if (isSalesforceDestinationTest) {
-  // eslint-disable-next-line global-require
-  const { restoreSalesforceSDKMock } = require('./integrations/mocks/salesforceSDK.mock');
-  restoreSalesforceSDKMock();
-}
+const { restoreSalesforceSDKMock } = require('./integrations/mocks/salesforceSDK.mock');
+restoreSalesforceSDKMock();

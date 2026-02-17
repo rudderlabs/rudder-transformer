4104122ac93142da6942b24c4ef101fc03baa185
Sudip Paul <sudip.paul1997@gmail.com>
2026-02-17 10:20:58 +0530

    chore: make region check optional

--- src/v0/destinations/customerio_audience/config.ts
+++ src/v0/destinations/customerio_audience/config.ts
@@ -8,4 +8,4 @@ const US_BASE_ENDPOINT = 'https://track.customer.io/api/v1/segments';
 const EU_BASE_ENDPOINT = 'https://track-eu.customer.io/api/v1/segments';
 
 export const getBaseEndpoint = (destination: CustomerIODestination): string =>
-  destination.Config.region === 'EU' ? EU_BASE_ENDPOINT : US_BASE_ENDPOINT;
+  destination.Config?.region === 'EU' ? EU_BASE_ENDPOINT : US_BASE_ENDPOINT;

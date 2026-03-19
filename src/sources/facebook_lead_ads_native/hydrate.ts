3bb4e4cb9c796f0ef4a1172ca2aefbb4095b2b5f
Shashank Shekhar <shekhar@rudderstack.com>
2026-03-19 09:27:56 +0530

    fix(facebook-lead-ads): unexpected error during hydration

--- src/sources/facebook_lead_ads_native/hydrate.ts
+++ src/sources/facebook_lead_ads_native/hydrate.ts
@@ -160,7 +160,12 @@ export async function hydrate(input: SourceHydrationRequest): Promise<SourceHydr
 
       // Convert field_data array to traits object
       result.data.field_data?.forEach((field) => {
-        if (field.values.length > 0) {
+        if (!Array.isArray(field.values)) {
+          logger.warn('[facebook_lead_ads_native] field values is not an array', {
+            fieldName: field.name,
+            field: JsonSchemaGenerator.generate(field),
+          });
+        } else if (field.values.length > 0) {
           const [firstValue] = field.values;
           traits[field.name] = firstValue;
         }

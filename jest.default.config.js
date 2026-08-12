f4952b8664da2840295eb2193b2e92eb3977e4c9
Francesco Casula <fracasula@users.noreply.github.com>
2026-08-12 10:50:23 +0200

    chore: cleaning up openfaas (#5445)
    ## What are the changes introduced in this PR?
    
    * First commit removes OpenFaaS
    * Second commit fixes some typos that I came across too while
    refactoring
    
    Fixes
    [PIPE-3222](https://linear.app/rudderstack/issue/PIPE-3222/remove-openfaas-code-from-rudder-transformer)

--- jest.default.config.js
+++ jest.default.config.js
@@ -157,7 +157,7 @@ module.exports = {
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/', 'user_transformation.integration.test.js'],
+  testPathIgnorePatterns: ['/node_modules/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],

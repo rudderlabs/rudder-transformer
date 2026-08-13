ce8b32b00937ec1fc4560e0401f5c2c0889eee69
tech-infra-rudderstack <tech-infra+github-signing@rudderstack.com>
2026-08-13 03:32:05 +0000

    chore(commitlint): allow ci commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test']],
+    'type-enum': () => [2, 'always', ['fix', 'feat', 'chore', 'refactor', 'docs', 'test', 'ci']],
     'body-max-line-length': () => [2, 'always', 500],
   },
 };
